a90a575a3d2bfe3b2d43c85acff63f89f116a627
Sebastian Bergmann <sb@sebastian-bergmann.de>
2006-08-10 09:07:12 +0000

    Separate source files for DualIterator and RecursiveDualIterator. Includes a patch by Kore Nordmann <mail@kore-nordmann.de> for the DualIterator implementation for  comparison in PHPUnit_Framework_Constraint_IsEqual.

--- PHPUnit/Framework/Constraint/IsEqual.php
+++ PHPUnit/Framework/Constraint/IsEqual.php
@@ -49,163 +49,10 @@
 
 require_once 'PHPUnit/Framework.php';
 require_once 'PHPUnit/Util/Array.php';
+require_once 'PHPUnit/Util/DualIterator.php';
 require_once 'PHPUnit/Util/Filter.php';
 
 PHPUnit_Util_Filter::addFileToFilter(__FILE__, 'PHPUNIT');
-
-if (!class_exists('DualIterator', FALSE)) {
-class DualIterator implements Iterator
-{
-    const CURRENT_LHS   = 0x01;
-    const CURRENT_RHS   = 0x02;
-    const CURRENT_ARRAY = 0x03;
-    const CURRENT_0     = 0x00;
-
-    const KEY_LHS   = 0x10;
-    const KEY_RHS   = 0x20;
-    const KEY_ARRAY = 0x30;
-    const KEY_0     = 0x00;
-    
-    const DEFAULT_FLAGS = 0x33;
-    
-    private $lhs;
-    private $rhs;
-    private $flags;
-
-    public function __construct(Iterator $lhs, Iterator $rhs, $flags = 0x33)
-    {
-        $this->lhs   = $lhs;
-        $this->rhs   = $rhs;
-        $this->flags = $flags;
-    }
-
-    public function getLHS()
-    {
-        return $this->lhs;
-    }
-
-    public function getRHS()
-    {
-        return $this->rhs;
-    }
-
-    public function setFlags($flags)
-    {
-        $this->flags = $flags;
-    }
-
-    public function getFlags()
-    {
-        return $this->flags;
-    }
-
-    public function rewind()
-    {
-        $this->lhs->rewind();
-        $this->rhs->rewind();    
-    }
-
-    public function valid()
-    {
-        return $this->lhs->valid() && $this->rhs->valid();    
-    }
-
-    public function current()
-    {
-        switch($this->flags & 0x0F) {
-            default:
-            case self::CURRENT_ARRAY: {
-              return array($this->lhs->current(), $this->rhs->current());
-            }
-
-            case self::CURRENT_LHS: {
-              return $this->lhs->current();
-            }
-
-            case self::CURRENT_RHS: {
-              return $this->rhs->current();
-            }
-
-            case self::CURRENT_0: {
-              return NULL;
-            }
-        }
-    }
-
-    public function key()
-    {
-        switch($this->flags & 0xF0) {
-            default:
-            case self::CURRENT_ARRAY: {
-              return array($this->lhs->key(), $this->rhs->key());
-            }
-
-            case self::CURRENT_LHS: {
-              return $this->lhs->key();
-            }
-
-            case self::CURRENT_RHS: {
-              return $this->rhs->key();
-            }
-
-            case self::CURRENT_0: {
-              return NULL;
-            }
-        }
-    }
-
-    public function next()
-    {
-        $this->lhs->next();
-        $this->rhs->next();
-    }
-
-    public function areIdentical()
-    {
-        return $this->valid()
-             ? $this->lhs->current() === $this->rhs->current()
-            && $this->lhs->key()     === $this->rhs->key()
-             : $this->lhs->valid()   ==  $this->rhs->valid();
-    }
-
-    public function areEqual()
-    {
-        return $this->valid()
-             ? $this->lhs->current() ==  $this->rhs->current()
-            && $this->lhs->key()     ==  $this->rhs->key()
-             : $this->lhs->valid()   ==  $this->rhs->valid();
-    }
-
-    public static function compareIterators(Iterator $lhs, Iterator $rhs, $identical = FALSE)
-    {
-        if ($lhs instanceof RecursiveIterator) {
-            if ($rhs instanceof RecursiveIterator) {
-                $it = new RecursiveDualIterator($lhs, $rhs, self::CURRENT_0 | self::KEY_0);
-            } else {
-                return FALSE;
-            }
-        } else {
-            $it = new DualIterator($lhs, $rhs, self::CURRENT_0 | self::KEY_0);
-        }
-
-        if ($identical) {
-            foreach (new RecursiveIteratorIterator($it) as $n) {
-                if (!$it->areIdentical()) {
-                    return FALSE;
-                }
-            }
-        } else {
-            foreach ($it as $n) {
-                if (!$it->areEqual()) {
-                    return FALSE;
-                }
-            }
-        }
-
-        return $identical ? $it->areIdentical() : $it->areEqual();
-    }
-}
-}
 
 /**
  * Constraint which checks if one value is equal to another.
@@ -246,24 +93,27 @@ class PHPUnit_Framework_Constraint_IsEqual implements PHPUnit_Framework_Constrai
      */
     public function evaluate($other)
     {
-        if (is_array($this->value) && is_array($other)) {
+        if  ((is_array($this->value)  && is_array($other)) ||
+             (is_object($this->value) && is_object($other))) {
             if (count($this->value) != count($other)) {
                 return FALSE;
             }
 
-            return DualIterator::compareIterators(
+            return PHPUnit_Util_DualIterator::compareIterators(
               new RecursiveIteratorIterator(
                 new RecursiveArrayIterator(
-                  PHPUnit_Util_Array::sortRecursively($this->value)
+                  PHPUnit_Util_Array::sortRecursively((array) $this->value)
                 ),
                 RecursiveIteratorIterator::SELF_FIRST
               ),
               new RecursiveIteratorIterator(
                 new RecursiveArrayIterator(
-                  PHPUnit_Util_Array::sortRecursively($other)
+                  PHPUnit_Util_Array::sortRecursively((array) $other)
                 ),
                 RecursiveIteratorIterator::SELF_FIRST
-              )
+              ),
+              FALSE,
+              $this->delta
             );
         }
 
