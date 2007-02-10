d2b081bc47ba51c9e7b977b41e764047423366c3
Sebastian Bergmann <sb@sebastian-bergmann.de>
2007-02-10 11:37:50 +0000

    - Fix #95.

--- 3.0/PHPUnit/TextUI/Command.php
+++ 3.0/PHPUnit/TextUI/Command.php
@@ -198,7 +198,7 @@ class PHPUnit_TextUI_Command
                 break;
 
                 case '--filter': {
-                    if (preg_match('/[a-zA-Z0-9_]/', $option[1])) {
+                    if (preg_match('/^[a-zA-Z0-9_]/', $option[1])) {
                         $arguments['filter'] = '/^' . $option[1] . '$/';
                     } else {
                         $arguments['filter'] = $option[1];
