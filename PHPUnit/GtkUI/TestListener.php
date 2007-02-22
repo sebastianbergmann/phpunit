5fb37f81c3cadfa0eea3c1218393155594b19b83
Tobias Schlitt <tobias@schlitt.info>
2007-02-22 23:32:21 +0000

    - Further migration of TestRunner to abstract Runner class.

--- PHPUnit/GtkUI/TestListener.php
+++ PHPUnit/GtkUI/TestListener.php
@@ -47,7 +47,7 @@
 
 require_once 'PHPUnit/Framework.php';
 require_once 'PHPUnit/GtkUI/Controller/StatusTree.php';
-require_once 'PHPUnit/GtkUI/TestRunner.php';
+require_once 'PHPUnit/GtkUI/Runner.php';
 require_once 'PHPUnit/GtkUI/TestStatusMapper.php';
 require_once 'PHPUnit/Util/Filter.php';
 
