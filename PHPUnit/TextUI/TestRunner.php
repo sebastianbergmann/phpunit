69a804049a7ab3daea39960762d599f0a3c58e9d
Sebastian Bergmann <sb@sebastian-bergmann.de>
2007-08-28 15:09:58 +0000

    Merge [1165].

--- PHPUnit/TextUI/TestRunner.php
+++ PHPUnit/TextUI/TestRunner.php
@@ -367,30 +367,6 @@ class PHPUnit_TextUI_TestRunner extends PHPUnit_Runner_BaseTestRunner
             $this->printer->write("\nGenerating report, this may take a moment.");
             PHPUnit_Util_Report::render($result, $arguments['reportDirectory'], $arguments['reportCharset']);
 
-            $writer = new PHPUnit_Util_Log_CodeCoverage_XML(
-              $arguments['reportDirectory'] . '/coverage.xml'
-            );
-
-            $writer->process($result);
-
-            $writer = new PHPUnit_Util_Log_Metrics(
-              $arguments['reportDirectory'] . '/metrics.xml'
-            );
-
-            $writer->process($result);
-
-            $writer = new PHPUnit_Util_Log_PMD(
-              $arguments['reportDirectory'] . '/pmd.xml'
-            );
-
-            $writer->process($result);
-
-            $writer = new PHPUnit_Util_Log_CPD(
-              $arguments['reportDirectory'] . '/pmd-cpd.xml'
-            );
-
-            $writer->process($result);
-
             $this->printer->write("\n");
         }
 
