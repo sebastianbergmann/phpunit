97c23883f1e7e634d11e100e9b88b9f39651d7a1
Sebastian Bergmann <sb@sebastian-bergmann.de>
2007-11-03 07:28:08 +0000

    - Tweak.

--- PHPUnit/Util/Report/Template/yui_item.js
+++ PHPUnit/Util/Report/Template/yui_item.js
@@ -1,4 +1,4 @@
-        YAHOO.phpunit.container.panel{line} = new YAHOO.widget.Panel("panel{line}", { width:"400px", visible:false, draggable:false, close:true } );
+        YAHOO.phpunit.container.panel{line} = new YAHOO.widget.Panel("panel{line}", { visible:false, draggable:false, close:true } );
         YAHOO.phpunit.container.panel{line}.setHeader("{header}");
         YAHOO.phpunit.container.panel{line}.setBody("<small><ul>{tests}</ul></small>");
         YAHOO.phpunit.container.panel{line}.setFooter("");
