89bab086eedc7bc7bc860262254832fb74c347c2
Sebastian Bergmann <sb@sebastian-bergmann.de>
2007-11-27 09:55:50 +0000

    Merge parts of [1751].

--- PHPUnit/Util/Log/PMD/Rule/Project/CRAP.php
+++ PHPUnit/Util/Log/PMD/Rule/Project/CRAP.php
@@ -39,7 +39,7 @@
  * @author     Sebastian Bergmann <sb@sebastian-bergmann.de>
  * @copyright  2002-2007 Sebastian Bergmann <sb@sebastian-bergmann.de>
  * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
- * @version    SVN: $Id: CodeCoverage.php 1522 2007-10-28 13:42:55Z sb $
+ * @version    SVN: $Id$
  * @link       http://www.phpunit.de/
  * @since      File available since Release 3.2.0
  */
