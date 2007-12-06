9d356557893d0c84b0c5700d9bf56edb1f9bd68e
Sebastian Bergmann <sb@sebastian-bergmann.de>
2007-12-06 07:28:05 +0000

    Merge [1833].

--- PHPUnit/Util/Log/PMD/Rule/Function/ExcessiveMethodLength.php
+++ PHPUnit/Util/Log/PMD/Rule/Function/ExcessiveMethodLength.php
@@ -63,7 +63,7 @@ PHPUnit_Util_Filter::addFileToFilter(__FILE__, 'PHPUNIT');
  */
 class PHPUnit_Util_Log_PMD_Rule_Function_ExcessiveMethodLength extends PHPUnit_Util_Log_PMD_Rule_Function
 {
-    public function __construct($threshold = 100)
+    public function __construct($threshold = 100, $priority = 1)
     {
         parent::__construct($threshold);
     }
