9d1198faa8d1abc572acc84d7bfe2f4b2ea45558
Sebastian Bergmann <sb@sebastian-bergmann.de>
2008-01-18 11:50:21 +0000

    - Merge [2175].

---
+++ PHPUnit/Extensions/SeleniumTestCase/phpunit_coverage.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * PHPUnit
+ *
+ * Copyright (c) 2002-2008, Sebastian Bergmann <sb@sebastian-bergmann.de>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the name of Sebastian Bergmann nor the names of his
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category   Testing
+ * @package    PHPUnit
+ * @author     Sebastian Bergmann <sb@sebastian-bergmann.de>
+ * @copyright  2002-2008 Sebastian Bergmann <sb@sebastian-bergmann.de>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version    SVN: $Id$
+ * @link       http://www.phpunit.de/
+ * @since      File available since Release 3.3.0
+ */
+
+require_once 'PHPUnit/Util/FilterIterator.php';
+
+if (isset($_GET['PHPUNIT_SELENIUM_TEST_ID'])) {
+    $files = new PHPUnit_Util_FilterIterator(
+      new RecursiveIteratorIterator(
+        new RecursiveDirectoryIterator(dirname(__FILE__))
+      ),
+      '.phpunit_' . $_GET['PHPUNIT_SELENIUM_TEST_ID']
+    );
+
+    $coverage = array();
+
+    foreach ($files as $file) {
+        $filename = $file->getPathName();
+
+        $data = unserialize(file_get_contents($filename));
+        unset($filename);
+
+        foreach ($data as $filename => $lines) {
+            if (!isset($coverage[$filename])) {
+                $coverage[$filename] = $lines;
+            } else {
+                foreach ($lines as $line => $flag) {
+                    if (!isset($coverage[$filename][$line]) ||
+                        $flag > $coverage[$filename][$line]) {
+                        $coverage[$filename][$line] = $flag;
+                    }
+                }
+            }
+        }
+    }
+
+    print serialize($coverage);
+}
+?>
