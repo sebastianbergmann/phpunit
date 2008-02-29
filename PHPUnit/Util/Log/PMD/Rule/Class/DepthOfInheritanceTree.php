973e528303da5a28e88339935a6a235b84ef02e4
Sebastian Bergmann <sb@sebastian-bergmann.de>
2008-02-29 12:13:17 +0000

    - Merge [2549].

--- PHPUnit/Util/Log/PMD/Rule/Class/DepthOfInheritanceTree.php
+++ PHPUnit/Util/Log/PMD/Rule/Class/DepthOfInheritanceTree.php
@@ -65,7 +65,7 @@ class PHPUnit_Util_Log_PMD_Rule_Class_DepthOfInheritanceTree extends PHPUnit_Uti
 {
     public function __construct($threshold = 6, $priority = 1)
     {
-        parent::__construct($threshold);
+        parent::__construct($threshold, $priority);
     }
 
     public function apply(PHPUnit_Util_Metrics $metrics)
