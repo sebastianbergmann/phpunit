97841068cd22aa1a14118cd2bdc34082c09af3ca
Michael Lively Jr <mjlivelyjr@gmail.com>
2008-06-30 01:51:02 +0000

    Fix #489

--- PHPUnit/Extensions/Database/DB/MetaData/MySQL.php
+++ PHPUnit/Extensions/Database/DB/MetaData/MySQL.php
@@ -62,8 +62,76 @@ PHPUnit_Util_Filter::addFileToFilter(__FILE__, 'PHPUNIT');
  * @link       http://www.phpunit.de/
  * @since      Class available since Release 3.2.0
  */
-class PHPUnit_Extensions_Database_DB_MetaData_MySQL extends PHPUnit_Extensions_Database_DB_MetaData_InformationSchema
+class PHPUnit_Extensions_Database_DB_MetaData_MySQL extends PHPUnit_Extensions_Database_DB_MetaData
 {
     protected $schemaObjectQuoteChar = '`';
+    
+    protected $columns = array();
+    
+    protected $keys = array();
+    
+    /**
+     * Returns an array containing the names of all the tables in the database.
+     *
+     * @return array
+     */
+    public function getTableNames()
+    {
+        $query = 'SHOW TABLES';
+        $statement = $this->pdo->prepare($query);
+        $statement->execute();
+        
+        $tableNames = array();
+        while (($tableName = $statement->fetchColumn(0))) {
+            $tableNames[] = $tableName;
+        }
+        
+        return $tableNames;
+    }
+    
+    /**
+     * Returns an array containing the names of all the columns in the
+     * $tableName table,
+     *
+     * @param string $tableName
+     * @return array
+     */
+    public function getTableColumns($tableName)
+    {
+        $query = 'SHOW COLUMNS FROM `' . $tableName . '`';
+        $statement = $this->pdo->prepare($query);
+        $statement->execute();
+        
+        $columnNames = array();
+        while (($columnName = $statement->fetchColumn(0))) {
+            $columnNames[] = $columnName;
+        }
+        
+        return $columnNames;
+    }
+    
+    /**
+     * Returns an array containing the names of all the primary key columns in
+     * the $tableName table.
+     *
+     * @param string $tableName
+     * @return array
+     */
+    public function getTablePrimaryKeys($tableName)
+    {
+        $query = 'SHOW INDEX FROM `' . $tableName . '`';
+        $statement = $this->pdo->prepare($query);
+        $statement->execute();
+        $statement->setFetchMode(PDO::FETCH_ASSOC);
+        
+        $columnNames = array();
+        while (($column = $statement->fetch())) {
+            if ($column['Key_name'] == 'PRIMARY') {
+                $columnNames[] = $column['Column_name'];
+            }
+        }
+        
+        return $columnNames;
+    }
 }
 ?>
