69cfb063d560c28df55b7b1ae883eb54788fdb3e
Sebastian Bergmann <sb@sebastian-bergmann.de>
2008-07-12 14:04:49 +0000

    - Merge [3433].

--- PHPUnit/Framework/MockObject/Stub/ReturnArgument.php
+++ PHPUnit/Framework/MockObject/Stub/ReturnArgument.php
@@ -72,8 +72,8 @@ class PHPUnit_Framework_MockObject_Stub_ReturnArgument extends PHPUnit_Framework
 
     public function invoke(PHPUnit_Framework_MockObject_Invocation $invocation)
     {
-        if (isset($invocation->parameters[$this->argumentIndex - 1])) {
-            return $invocation->parameters[$this->argumentIndex - 1];
+        if (isset($invocation->parameters[$this->argumentIndex])) {
+            return $invocation->parameters[$this->argumentIndex];
         } else {
             return NULL;
         }
