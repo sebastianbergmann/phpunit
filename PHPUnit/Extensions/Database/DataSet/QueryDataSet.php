a806124441afa769dd0648c5cebc07f632f6e857
Michael Lively Jr <mjlivelyjr@gmail.com>
2008-08-26 04:24:41 +0000

    Created new query datasets. This will provide for a much more powerful and simple way of setting up database fixtures.

--- PHPUnit/Extensions/Database/DataSet/QueryDataSet.php
+++ PHPUnit/Extensions/Database/DataSet/QueryDataSet.php
@@ -39,7 +39,7 @@
  * @author     Mike Lively <m@digitalsandwich.com>
  * @copyright  2002-2008 Sebastian Bergmann <sb@sebastian-bergmann.de>
  * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
- * @version    SVN: $Id$
+ * @version    SVN: $Id:DataSet.php 1254 2008-09-02 04:36:15Z mlively $
  * @link       http://www.phpunit.de/
  * @since      File available since Release 3.2.0
  */
@@ -47,13 +47,15 @@
 require_once 'PHPUnit/Framework.php';
 require_once 'PHPUnit/Util/Filter.php';
 
+require_once 'PHPUnit/Extensions/Database/DataSet/AbstractDataSet.php';
+require_once 'PHPUnit/Extensions/Database/DataSet/DefaultTableMetaData.php';
+require_once 'PHPUnit/Extensions/Database/DataSet/QueryTable.php';
+require_once 'PHPUnit/Extensions/Database/DataSet/DefaultTableIterator.php';
+
 PHPUnit_Util_Filter::addFileToFilter(__FILE__, 'PHPUNIT');
 
-require_once 'PHPUnit/Extensions/Database/DataSet/AbstractTable.php';
-
 /**
- * Provides the functionality to represent a database result set as a DBUnit
- * table.
+ * Provides access to a database instance as a data set.
  *
  * @category   Testing
  * @package    PHPUnit
@@ -62,31 +64,79 @@ require_once 'PHPUnit/Extensions/Database/DataSet/AbstractTable.php';
  * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
  * @version    Release: @package_version@
  * @link       http://www.phpunit.de/
- * @deprecated The PHPUnit_Extension_Database_DataSet_QueryTable should be used instead
- * @see        PHPUnit_Extension_Database_DataSet_QueryTable
- * @see        PHPUnit_Extension_Database_DataSet_QueryDataSet
  * @since      Class available since Release 3.2.0
  */
-class PHPUnit_Extensions_Database_DB_ResultSetTable extends PHPUnit_Extensions_Database_DataSet_AbstractTable
+class PHPUnit_Extensions_Database_DataSet_QueryDataSet extends PHPUnit_Extensions_Database_DataSet_AbstractDataSet
 {
 
     /**
-     * Creates a new result set table.
+     * An array of ITable objects.
+     *
+     * @var array
+     */
+    protected $tables = array();
+
+    /**
+     * The database connection this dataset is using.
+     *
+     * @var PHPUnit_Extensions_Database_DB_DefaultDatabaseConnection
+     */
+    protected $databaseConnection;
+
+    /**
+     * Creates a new dataset using the given database connection.
+     *
+     * @param PHPUnit_Extensions_Database_DB_DefaultDatabaseConnection $databaseConnection
+     */
+    public function __construct(PHPUnit_Extensions_Database_DB_DefaultDatabaseConnection $databaseConnection)
+    {
+        $this->databaseConnection = $databaseConnection;
+    }
+    
+    public function addTable($tableName, $query = NULL)
+    {
+        if ($query === NULL) {
+            $query = 'SELECT * FROM ' . $tableName;
+        }
+        
+        $this->tables[$tableName] = new PHPUnit_Extensions_Database_DataSet_QueryTable($tableName, $query, $this->databaseConnection);
+    }
+
+    /**
+     * Creates an iterator over the tables in the data set. If $reverse is
+     * true a reverse iterator will be returned.
+     *
+     * @param bool $reverse
+     * @return PHPUnit_Extensions_Database_DB_TableIterator
+     */
+    protected function createIterator($reverse = FALSE)
+    {
+        return new PHPUnit_Extensions_Database_DataSet_DefaultTableIterator($this->tables, $reverse);
+    }
+
+    /**
+     * Returns a table object for the given table.
      *
      * @param string $tableName
-     * @param PDOStatement $pdoStatement
+     * @return PHPUnit_Extensions_Database_DB_Table
      */
-    public function __construct($tableName, PDOStatement $pdoStatement)
+    public function getTable($tableName)
     {
-        $this->data = $pdoStatement->fetchAll(PDO::FETCH_ASSOC);
-
-        if (count($this->data)) {
-            $columns = array_keys($this->data[0]);
-        } else {
-            $columns = array();
+        if (!isset($this->tables[$tableName])) {
+            throw new InvalidArgumentException("$tableName is not a table in the current database.");
         }
+        
+        return $this->tables[$tableName];
+    }
 
-        $this->setTableMetaData(new PHPUnit_Extensions_Database_DataSet_DefaultTableMetaData($tableName, $columns));
+    /**
+     * Returns a list of table names for the database
+     *
+     * @return Array
+     */
+    public function getTableNames()
+    {
+        return array_keys($this->tables);
     }
 }
 ?>
