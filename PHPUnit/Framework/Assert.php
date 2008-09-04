0019556ebd2dd76d1f53bd5810de3e3feeea9122
Sebastian Bergmann <sb@sebastian-bergmann.de>
2008-09-04 14:11:48 +0000

    - Cleanup assertTag() examples.

--- PHPUnit/Framework/Assert.php
+++ PHPUnit/Framework/Assert.php
@@ -1332,65 +1332,86 @@ abstract class PHPUnit_Framework_Assert
      *    - `only`         : another hash consisting of the keys to use to match on the children, and only matching children will be counted
      *
      * <code>
-     * // assert there is an element with an id="my_id"
+     * // Matcher that asserts that there is an element with an id="my_id".
      * $matcher = array('id' => 'my_id');
-     * $this->assertTag($matcher, $html);
      *
-     * // assert that there is a "span" tag
+     * // Matcher that asserts that there is a "span" tag.
      * $matcher = array('tag' => 'span');
-     * $this->assertTag($matcher, $html);
      *
-     * // assert that there is a "span" tag with the content "Hello World"
+     * // Matcher that asserts that there is a "span" tag with the content
+     * // "Hello World".
      * $matcher = array('tag' => 'span', 'content' => 'Hello World');
-     * $this->assertTag($matcher, $html);
      *
-     * // assert that there is a "span" tag with content matching the regexp pattern
+     * // Matcher that asserts that there is a "span" tag with content matching the
+     * // regular expression pattern.
      * $matcher = array('tag' => 'span', 'content' => '/Try P(HP|ython)/');
+     *
+     * // Matcher that asserts that there is a "span" with an "list" class attribute.
+     * $matcher = array(
+     *   'tag'        => 'span',
+     *   'attributes' => array('class' => 'list')
+     * );
+     *
+     * // Matcher that asserts that there is a "span" inside of a "div".
+     * $matcher = array(
+     *   'tag'    => 'span',
+     *   'parent' => array('tag' => 'div')
+     * );
+     *
+     * // Matcher that asserts that there is a "span" somewhere inside a "table".
+     * $matcher = array(
+     *   'tag'      => 'span',
+     *   'ascestor' => array('tag' => 'table')
+     * );
+     *
+     * // Matcher that asserts that there is a "span" with at least one "em" child.
+     * $matcher = array(
+     *   'tag'   => 'span',
+     *   'child' => array('tag' => 'em')
+     * );
+     *
+     * // Matcher that asserts that there is a "span" containing a (possibly nested)
+     * // "strong" tag.
+     * $matcher = array(
+     *   'tag'        => 'span',
+     *   'descendant' => array('tag' => 'strong')
+     * );
+     *
+     * // Matcher that asserts that there is a "span" containing 5-10 "em" tags as
+     * // immediate children.
+     * $matcher = array(
+     *   'tag'      => 'span',
+     *   'children' => array(
+     *     'less_than'    => 11,
+     *     'greater_than' => 4,
+     *     'only'         => array('tag' => 'em')
+     *   )
+     * );
+     *
+     * // Matcher that asserts that there is a "div", with an "ul" ancestor and a "li"
+     * // parent (with class="enum"), and containing a "span" descendant that contains
+     * // an element with id="my_test" and the text "Hello World".
+     * $matcher = array(
+     *   'tag'        => 'div',
+     *   'ancestor'   => array('tag' => 'ul'),
+     *   'parent'     => array(
+     *     'tag'        => 'li',
+     *     'attributes' => array('class' => 'enum')
+     *   ),
+     *   'descendant' => array(
+     *     'tag'   => 'span',
+     *     'child' => array(
+     *       'id'      => 'my_test',
+     *       'content' => 'Hello World'
+     *     )
+     *   )
+     * );
+     *
+     * // Use assertTag() to apply a $matcher to a piece of $html.
      * $this->assertTag($matcher, $html);
      *
-     * // assert that there is a "span" with an "list" class attribute
-     * $matcher = array('tag' => 'span',
-     *                  'attributes' => array('class' => 'list')));
-     * $this->assertTag($matcher, $html);
-     *
-     * // assert that there is a "span" inside of a "div"
-     * $matcher = array('tag' => 'span',
-     *                  'parent' => array('tag' => 'div')));
-     * $this->assertTag($matcher, $html);
-     *
-     * // assert that there is a "span" somewhere inside a "table"
-     * $matcher = array('tag' => 'span',
-     *                  'ascestor' => array('tag' => 'table')));
-     * $this->assertTag($matcher, $html);
-     *
-     * // assert that there is a "span" with at least one "em" child
-     * $matcher = array('tag' => 'span',
-     *                  'child' => array('tag' => 'em')));
-     * $this->assertTag($matcher, $html);
-     *
-     * // assert that there is a "span" containing a (possibly nested) "strong" tag.
-     * $matcher = array('tag' => 'span',
-     *                  'descendant' => array('tag' => 'strong')));
-     * $this->assertTag($matcher, $html);
-     *
-     * // assert that there is a "span" containing 5-10 "em" tags as immediate children
-     * $matcher = array('tag' => 'span',
-     *                  'children'  => array('less_than'    => 11,
-     *                                       'greater_than' => 4,
-     *                                       'only'         => array('tag' => 'em')));
-     * $this->assertTag($matcher, $html);
-     *
-     * // complex: assert that there is a "div", with an "ul" ancestor and a "li" parent
-     * // (with class="enum"), and containing a "span" descendant that contains element
-     * // with id="my_test" and the text "Hello World".
-     * $matcher = array('tag' => 'div',
-     *                  'ancestor' => array('tag' => 'ul'),
-     *                  'parent'   => array('tag'        => 'li,
-     *                                      'attributes' => array('class' => 'enum')),
-     *                  'descendant' => array('tag'   => 'span',
-     *                                        'child' => array('id'      => 'my_test',
-     *                                                        'content' => 'Hello World')));
-     * $this->assertTag($matcher, $html);
+     * // Use assertTag() to apply a $matcher to a piece of $xml.
+     * $this->assertTag($matcher, $xml, '', FALSE);
      * </code>
      *
      * The second argument ($actual) is a string containing either HTML or
