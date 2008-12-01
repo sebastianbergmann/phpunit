1e8be0d145b40097aeced92ce98c08c3ed09fa63
Sebastian Bergmann <sb@sebastian-bergmann.de>
2008-12-01 21:00:14 +0000

    Merge [4169].

--- PHPUnit/Framework/TestCase.php
+++ PHPUnit/Framework/TestCase.php
@@ -390,6 +390,9 @@ abstract class PHPUnit_Framework_TestCase extends PHPUnit_Framework_Assert imple
             $this->backupGlobals();
         }
 
+        // Clean up stat cache.
+        clearstatcache();
+
         // Set up the fixture.
         $this->setUp();
 
