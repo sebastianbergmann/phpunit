06910dae58306dea1eac68bd952a83d5c6788229
Sebastian Bergmann <sb@sebastian-bergmann.de>
2008-12-17 04:48:28 +0000

    Merge [4288] and [4289].

--- PHPUnit/Framework/ComparisonFailure.php
+++ PHPUnit/Framework/ComparisonFailure.php
@@ -36,7 +36,6 @@
  *
  * @category   Testing
  * @package    PHPUnit
- * @author     Jan Borsodi <jb@ez.no>
  * @author     Sebastian Bergmann <sb@sebastian-bergmann.de>
  * @copyright  2002-2008 Sebastian Bergmann <sb@sebastian-bergmann.de>
  * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
@@ -58,7 +57,6 @@ if (!class_exists('PHPUnit_Framework_ComparisonFailure', FALSE)) {
  *
  * @category   Testing
  * @package    PHPUnit
- * @author     Jan Borsodi <jb@ez.no>
  * @author     Sebastian Bergmann <sb@sebastian-bergmann.de>
  * @copyright  2002-2008 Sebastian Bergmann <sb@sebastian-bergmann.de>
  * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
@@ -142,20 +140,24 @@ abstract class PHPUnit_Framework_ComparisonFailure extends PHPUnit_Framework_Ass
             return new PHPUnit_Framework_ComparisonFailure_Type($expected, $actual, TRUE, $message);
         }
 
-        elseif (is_string($expected)) {
-            return new PHPUnit_Framework_ComparisonFailure_String($expected, $actual, TRUE, $message);
-        }
-
-        elseif (is_null($expected) || is_scalar($expected)) {
-            return new PHPUnit_Framework_ComparisonFailure_Scalar($expected, $actual, TRUE, $message);
-        }
-
-        elseif (is_array($expected)) {
+        else if (is_array($expected) && is_array($actual)) {
             return new PHPUnit_Framework_ComparisonFailure_Array($expected, $actual, TRUE, $message);
         }
 
-        elseif (is_object($expected)) {
+        else if (is_numeric($expected) && is_numeric($actual)) {
+            return new PHPUnit_Framework_ComparisonFailure_Numeric($expected, $actual, TRUE, $message);
+        }
+
+        else if (is_object($expected) && is_object($actual)) {
             return new PHPUnit_Framework_ComparisonFailure_Object($expected, $actual, TRUE, $message);
+        }
+
+        else if (is_string($expected) && !is_object($actual)) {
+            return new PHPUnit_Framework_ComparisonFailure_String($expected, $actual, TRUE, $message);
+        }
+
+        else if (is_null($expected) || is_scalar($expected)) {
+            return new PHPUnit_Framework_ComparisonFailure_Scalar($expected, $actual, TRUE, $message);
         }
     }
 
@@ -173,20 +175,24 @@ abstract class PHPUnit_Framework_ComparisonFailure extends PHPUnit_Framework_Ass
      */
     public static function diffEqual($expected, $actual, $message = '')
     {
-        if (is_string($expected) && !is_object($actual)) {
-            return new PHPUnit_Framework_ComparisonFailure_String($expected, $actual, FALSE, $message);
-        }
-
-        elseif (is_null($expected) || is_scalar($expected)) {
-            return new PHPUnit_Framework_ComparisonFailure_Scalar($expected, $actual, FALSE, $message);
-        }
-
-        elseif (is_array($expected)) {
+        if (is_array($expected) && is_array($actual)) {
             return new PHPUnit_Framework_ComparisonFailure_Array($expected, $actual, FALSE, $message);
         }
 
-        elseif (is_object($expected)) {
+        else if (is_numeric($expected) && is_numeric($actual)) {
+            return new PHPUnit_Framework_ComparisonFailure_Numeric($expected, $actual, FALSE, $message);
+        }
+
+        else if (is_object($expected) && is_object($actual)) {
             return new PHPUnit_Framework_ComparisonFailure_Object($expected, $actual, FALSE, $message);
+        }
+
+        else if (is_string($expected) && !is_object($actual)) {
+            return new PHPUnit_Framework_ComparisonFailure_String($expected, $actual, FALSE, $message);
+        }
+
+        else if (is_null($expected) || is_scalar($expected)) {
+            return new PHPUnit_Framework_ComparisonFailure_Scalar($expected, $actual, FALSE, $message);
         }
     }
 }
@@ -194,6 +200,7 @@ abstract class PHPUnit_Framework_ComparisonFailure extends PHPUnit_Framework_Ass
 }
 
 require_once 'PHPUnit/Framework/ComparisonFailure/Array.php';
+require_once 'PHPUnit/Framework/ComparisonFailure/Numeric.php';
 require_once 'PHPUnit/Framework/ComparisonFailure/Object.php';
 require_once 'PHPUnit/Framework/ComparisonFailure/Scalar.php';
 require_once 'PHPUnit/Framework/ComparisonFailure/String.php';
