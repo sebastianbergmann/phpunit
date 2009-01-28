a255f4e81dabbbbc8dbad973e9753aa079d1cf04
Sebastian Bergmann <sb@sebastian-bergmann.de>
2009-01-28 07:36:45 +0000

    Fix #688.

--- phpunit.php
+++ phpunit.php
@@ -47,5 +47,7 @@ PHPUnit_Util_Filter::addFileToFilter(__FILE__, 'PHPUNIT');
 
 require 'PHPUnit/TextUI/Command.php';
 
+define('PHPUnit_MAIN_METHOD', 'PHPUnit_TextUI_Command::main');
+
 PHPUnit_TextUI_Command::main();
 ?>
