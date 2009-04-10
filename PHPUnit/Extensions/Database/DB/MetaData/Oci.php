138367c22caa7f54c250ffc3008642eeb6da04bd
Sebastian Bergmann <sb@sebastian-bergmann.de>
2009-04-10 09:56:20 +0000

    Merge [4781] and [4782].

--- PHPUnit/Extensions/Database/DB/MetaData/Oci.php
+++ PHPUnit/Extensions/Database/DB/MetaData/Oci.php
@@ -64,6 +64,11 @@ PHPUnit_Util_Filter::addFileToFilter(__FILE__, 'PHPUNIT');
  */
 class PHPUnit_Extensions_Database_DB_MetaData_Oci extends PHPUnit_Extensions_Database_DB_MetaData
 {
+    /**
+     * No character used to quote schema objects.
+     */
+    protected $schemaObjectQuoteChar = '';
+
     protected $columns = array();
     protected $keys = array();
 
@@ -153,7 +158,7 @@ class PHPUnit_Extensions_Database_DB_MetaData_Oci extends PHPUnit_Extensions_Dat
         }
 
         $keyQuery = "SELECT b.column_name
-                       FROM all_constraints a, all_cons_columns b
+                       FROM user_constraints a, user_cons_columns b
                       WHERE a.constraint_type='P'
                         AND a.constraint_name=b.constraint_name
                         $conOwnerQuery
