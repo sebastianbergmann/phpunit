a43f76319108aa8d64099ae83ed6973114816989
Sebastian Bergmann <sb@sebastian-bergmann.de>
2009-07-30 15:22:24 +0000

    Deprecate --repeat.

--- PHPUnit/TextUI/Command.php
+++ PHPUnit/TextUI/Command.php
@@ -483,6 +483,11 @@ class PHPUnit_TextUI_Command
                 break;
 
                 case '--repeat': {
+                    $this->showMessage(
+                      'The --repeat functionality is deprecated and will be removed in the future.',
+                      FALSE
+                    );
+
                     $this->arguments['repeat'] = (int)$option[1];
                 }
                 break;
@@ -912,7 +917,6 @@ Usage: phpunit [switches] UnitTest [UnitTest.php]
   --list-groups            List available test groups.
 
   --loader <loader>        TestSuiteLoader implementation to use.
-  --repeat <times>         Runs the test(s) repeatedly.
 
   --story                  Report test execution progress in Story/BDD format.
   --tap                    Report test execution progress in TAP format.
