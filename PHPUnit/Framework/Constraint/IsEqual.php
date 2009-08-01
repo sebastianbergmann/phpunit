4cd5d8a263500aa34a68f9ed57fa9cf597c912b2
Sebastian Bergmann <sb@sebastian-bergmann.de>
2009-08-01 13:36:29 +0000

    - Require PHP 5.2.10.

--- PHPUnit/Framework/Constraint/IsEqual.php
+++ PHPUnit/Framework/Constraint/IsEqual.php
@@ -259,11 +259,7 @@ class PHPUnit_Framework_Constraint_IsEqual extends PHPUnit_Framework_Constraint
                 unset($_b);
             }
 
-            if (version_compare(PHP_VERSION, '5.2.0RC1', '>=')) {
-                return ($a->C14N() == $b->C14N());
-            } else {
-                return ($a->saveXML() == $b->saveXML());
-            }
+            return ($a->C14N() == $b->C14N());
         }
 
         if (is_object($a) && is_object($b) &&
