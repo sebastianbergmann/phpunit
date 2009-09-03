b1deb072cb97ec79a845e3112a0a05ba5a03ff22
Sebastian Bergmann <sb@sebastian-bergmann.de>
2009-09-03 16:10:20 +0000

    Fix #847.

--- PHPUnit/Util/Test.php
+++ PHPUnit/Util/Test.php
@@ -425,7 +425,7 @@ class PHPUnit_Util_Test
     {
         $annotations = array();
 
-        if (preg_match_all('(@(?P<name>[A-Za-z_-]+)\s+(?P<value>.*?)\s*$)m', $docblock, $matches)) {
+        if (preg_match_all('/@(?P<name>[A-Za-z_-]+)(?:[ \t]+(?P<value>.*?))?[ \t]*$/m', $docblock, $matches)) {
             $numMatches = count($matches[0]);
 
             for ($i = 0; $i < $numMatches; ++$i) {
