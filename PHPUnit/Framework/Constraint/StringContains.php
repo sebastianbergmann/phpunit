718d41c463c150b848e700db375849e75e98854b
Sebastian Bergmann <sb@sebastian-bergmann.de>
2009-11-14 20:36:17 +0000

    - Merge [5328].

--- PHPUnit/Framework/Constraint/StringContains.php
+++ PHPUnit/Framework/Constraint/StringContains.php
@@ -46,7 +46,6 @@
 
 require_once 'PHPUnit/Framework.php';
 require_once 'PHPUnit/Util/Filter.php';
-require_once 'PHPUnit/Util/Type.php';
 
 PHPUnit_Util_Filter::addFileToFilter(__FILE__, 'PHPUNIT');
 
