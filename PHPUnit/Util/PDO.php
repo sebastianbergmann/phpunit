e5ad6f752ab834fbe9dc751ca65bf373ddbbfcaf
Sebastian Bergmann <sb@sebastian-bergmann.de>
2009-12-10 21:53:30 +0000

    Leftover.

--- PHPUnit/Util/PDO.php
+++ PHPUnit/Util/PDO.php
@@ -44,9 +44,7 @@
  * @since      File available since Release 3.1.4
  */
 
-require_once 'PHPUnit/Util/Filter.php';
-
-PHPUnit_Util_Filter::addFileToFilter(__FILE__, 'PHPUNIT');
+PHP_CodeCoverage_Filter::getInstance()->addFileToBlacklist(__FILE__, 'PHPUNIT');
 
 /**
  * PDO helpers.
