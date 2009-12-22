e5924f2a39cb119f5f519a4d098c614d48a3f82a
Sebastian Bergmann <sb@sebastian-bergmann.de>
2009-12-22 17:38:49 +0000

    Merge [5471].

--- PHPUnit/Util/Getopt.php
+++ PHPUnit/Util/Getopt.php
@@ -82,8 +82,13 @@ class PHPUnit_Util_Getopt
         }
 
         reset($args);
+        array_map('trim', $args);
 
         while (list($i, $arg) = each($args)) {
+            if ($arg == '') {
+                continue;
+            }
+
             if ($arg == '--') {
                 $non_opts = array_merge($non_opts, array_slice($args, $i + 1));
                 break;
