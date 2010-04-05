ef6aef19780b287008e22a68d48b5a2c49f0b426
matslindh <mats@lindh.no>
2010-04-06 00:27:50 +0200

    Adding fallback to the PEAR environment variable (PHP_PEAR_PHP_BIN) if the file PHPBIN points to doesn't exist.

--- phpunit.bat
+++ phpunit.bat
@@ -35,4 +35,9 @@ REM POSSIBILITY OF SUCH DAMAGE.
 REM
 
 if "%PHPBIN%" == "" set PHPBIN="@php_bin@"
+if not exist "%PHPBIN%" if "%PHP_PEAR_PHP_BIN%" neq "" goto USE_PEAR_PATH
+GOTO RUN
+:USE_PEAR_PATH
+set PHPBIN="%PHP_PEAR_PHP_BIN%"
+:RUN
 %PHPBIN% "@bin_dir@\phpunit" %*
