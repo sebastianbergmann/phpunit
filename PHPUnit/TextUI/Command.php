5e0cc7dd3cf900d3ecf400644711a79f1189a44f
Sebastian Bergmann <sb@sebastian-bergmann.de>
2010-06-15 07:57:17 +0200

    Cleanup.

--- PHPUnit/TextUI/Command.php
+++ PHPUnit/TextUI/Command.php
@@ -566,17 +566,32 @@ class PHPUnit_TextUI_Command
             $this->arguments['loader'] = $this->handleLoader($this->arguments['loader']);
         }
 
-        if (isset($this->arguments['configuration']) && is_dir($this->arguments['configuration'])) {
-            if (file_exists($this->arguments['configuration'].'/phpunit.xml')) {
-                $this->arguments['configuration'] = realpath($this->arguments['configuration'].'/phpunit.xml');
-            } else if (file_exists($this->arguments['configuration'].'/phpunit.xml.dist')) {
-                $this->arguments['configuration'] = realpath($this->arguments['configuration'].'/phpunit.xml.dist');
+        if (isset($this->arguments['configuration']) &&
+            is_dir($this->arguments['configuration'])) {
+            $configurationFile = $this->arguments['configuration'] .
+                                 '/phpunit.xml';
+
+            if (file_exists($configurationFile)) {
+                $this->arguments['configuration'] = realpath(
+                  $configurationFile
+                );
             }
-        } elseif (!isset($this->arguments['configuration']) && $this->arguments['useDefaultConfiguration']) {
+
+            else if (file_exists($configurationFile . '.dist')) {
+                $this->arguments['configuration'] = realpath(
+                  $configurationFile . '.dist'
+                );
+            }
+        }
+
+        else if (!isset($this->arguments['configuration']) &&
+                 $this->arguments['useDefaultConfiguration']) {
             if (file_exists('phpunit.xml')) {
                 $this->arguments['configuration'] = realpath('phpunit.xml');
             } else if (file_exists('phpunit.xml.dist')) {
-                $this->arguments['configuration'] = realpath('phpunit.xml.dist');
+                $this->arguments['configuration'] = realpath(
+                  'phpunit.xml.dist'
+                );
             }
         }
 
