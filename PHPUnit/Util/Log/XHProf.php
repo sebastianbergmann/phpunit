d347ade422d20f1bdbc8a16e8805fff28dd99cc2
Sebastian Bergmann <sb@sebastian-bergmann.de>
2010-06-19 17:51:54 +0200

    Cleanup.

---
+++ PHPUnit/Util/Log/XHProf.php
@@ -0,0 +1,252 @@
+<?php
+/**
+ * PHPUnit
+ *
+ * Copyright (c) 2002-2010, Sebastian Bergmann <sb@sebastian-bergmann.de>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the name of Sebastian Bergmann nor the names of his
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package    PHPUnit
+ * @subpackage Util_Log
+ * @author     Benjamin Eberlei <kontakt@beberlei.de>
+ * @author     Sebastian Bergmann <sb@sebastian-bergmann.de>
+ * @copyright  2002-2010 Sebastian Bergmann <sb@sebastian-bergmann.de>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @link       http://www.phpunit.de/
+ * @since      File available since Release 3.5.0
+ */
+
+/**
+ * A TestListener that integrates with XHProf.
+ *
+ * Here is an example XML configuration for activating this listener:
+ *
+ * <code>
+ * <listeners>
+ *  <listener class="MyListener" file="/optional/path/to/MyListener.php">
+ *   <arguments>
+ *    <array>
+ *     <element key="xhprofLibFile">
+ *      <string>/var/www/xhprof_lib/utils/xhprof_lib.php</string>
+ *     </element>
+ *     <element key="xhprofRunsFile">
+ *      <string>/var/www/xhprof_lib/utils/xhprof_runs.php</string>
+ *     </element>
+ *     <element key="xhprofWeb">
+ *      <string>http://localhost/xhprof_html/index.php&lt;/string>
+ *     </element>
+ *     <element key="appNamespace">
+ *      <string>Doctrine2</string>
+ *     </element>
+ *     <element key="xhprofFlags">
+ *      <string>XHPROF_FLAGS_CPU,XHPROF_FLAGS_MEMORY</string>
+ *     </element>
+ *    </array>
+ *   </arguments>
+ *  </listener>
+ * </listeners>
+ * </code>
+ *
+ * @package    PHPUnit
+ * @subpackage Util_Log
+ * @author     Benjamin Eberlei <kontakt@beberlei.de>
+ * @author     Sebastian Bergmann <sb@sebastian-bergmann.de>
+ * @copyright  2002-2010 Sebastian Bergmann <sb@sebastian-bergmann.de>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version    Release: @package_version@
+ * @link       http://www.phpunit.de/
+ * @since      Class available since Release 3.5.0
+ */
+class PHPUnit_Util_Log_XHProf implements PHPUnit_Framework_TestListener
+{
+    /**
+     * @var array
+     */
+    protected $runs = array();
+
+    /**
+     * @var array
+     */
+    protected $options = array();
+
+    /**
+     * @var integer
+     */
+    protected $suites = 0;
+
+    /**
+     * Constructor.
+     *
+     * @param array $options
+     */
+    public function __construct(array $options = array())
+    {
+        if (!extension_loaded('xhprof')) {
+            throw new RuntimeException(
+              'The XHProf extension is required for this listener to work.'
+            );
+        }
+
+        if (!isset($options['appNamespace'])) {
+            throw new InvalidArgumentException(
+              'The "appNamespace" option is not set.'
+            );
+        }
+
+        if (!isset($options['xhprofLibFile']) ||
+            !file_exists($options['xhprofLibFile'])) {
+            throw new InvalidArgumentException(
+              'The "xhprofLibFile" option is not set or the configured file does not exist'
+            );
+        }
+
+        if (!isset($options['xhprofRunsFile']) ||
+            !file_exists($options['xhprofRunsFile'])) {
+            throw new InvalidArgumentException(
+              'The "xhprofRunsFile" option is not set or the configured file does not exist'
+            );
+        }
+
+        require_once $options['xhprofLibFile'];
+        require_once $options['xhprofRunsFile'];
+
+        $this->options = $options;
+    }
+
+    /**
+     * An error occurred.
+     *
+     * @param PHPUnit_Framework_Test $test
+     * @param Exception              $e
+     * @param float                  $time
+     */
+    public function addError(PHPUnit_Framework_Test $test, Exception $e, $time)
+    {
+    }
+
+    /**
+     * A failure occurred.
+     *
+     * @param PHPUnit_Framework_Test                 $test
+     * @param PHPUnit_Framework_AssertionFailedError $e
+     * @param float                                  $time
+     */
+    public function addFailure(PHPUnit_Framework_Test $test, PHPUnit_Framework_AssertionFailedError $e, $time)
+    {
+    }
+
+    /**
+     * Incomplete test.
+     *
+     * @param PHPUnit_Framework_Test $test
+     * @param Exception              $e
+     * @param float                  $time
+     */
+    public function addIncompleteTest(PHPUnit_Framework_Test $test, Exception $e, $time)
+    {
+    }
+
+    /**
+     * Skipped test.
+     *
+     * @param PHPUnit_Framework_Test $test
+     * @param Exception              $e
+     * @param float                  $time
+     */
+    public function addSkippedTest(PHPUnit_Framework_Test $test, Exception $e, $time)
+    {
+    }
+
+    /**
+     * A test started.
+     *
+     * @param PHPUnit_Framework_Test $test
+     */
+    public function startTest(PHPUnit_Framework_Test $test)
+    {
+        if (!isset($this->options['xhprofFlags'])) {
+            $flags = XHPROF_FLAGS_CPU + XHPROF_FLAGS_MEMORY;
+        } else {
+            $flags = 0;
+
+            foreach (explode(',', $this->options['xhprofFlags']) as $flag) {
+                $flags += constant($flag);
+            }
+        }
+
+        xhprof_enable($flags);
+    }
+
+    /**
+     * A test ended.
+     *
+     * @param PHPUnit_Framework_Test $test
+     * @param float                  $time
+     */
+    public function endTest(PHPUnit_Framework_Test $test, $time)
+    {
+        $data         = xhprof_disable();
+        $runs         = new XHProfRuns_Default;
+        $run          = $runs->save_run($data, $this->options['appNamespace']);
+        $this->runs[] = $this->options['xhprofWeb'] . '?run=' . $run .
+                        '&source=' . $this->options['appNamespace'];
+    }
+
+    /**
+     * A test suite started.
+     *
+     * @param PHPUnit_Framework_TestSuite $suite
+     */
+    public function startTestSuite(PHPUnit_Framework_TestSuite $suite)
+    {
+        $this->suites++;
+    }
+
+    /**
+     * A test suite ended.
+     *
+     * @param PHPUnit_Framework_TestSuite $suite
+     */
+    public function endTestSuite(PHPUnit_Framework_TestSuite $suite)
+    {
+        $this->suites--;
+
+        if ($this->suites == 0) {
+            print 'XHProf runs: ' . count($this->runs) . "\n";
+
+            foreach ($this->runs as $run) {
+                print ' * ' . $run . "\n";
+            }
+
+            print "\n";
+        }
+    }
+}
