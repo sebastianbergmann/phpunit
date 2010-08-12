32b7c5d813b6401d0e18ffa5eb07b3de0de0b3ad
Matteo Beccati <matteo@beccati.com>
2010-08-12 09:26:18 +0200

    Fixed .bat files to correctly set environment variables without double quotes

--- dbunit.bat
+++ dbunit.bat
@@ -34,10 +34,10 @@ REM ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 REM POSSIBILITY OF SUCH DAMAGE.
 REM
 
-if "%PHPBIN%" == "" set PHPBIN="@php_bin@"
+if "%PHPBIN%" == "" set PHPBIN=@php_bin@
 if not exist "%PHPBIN%" if "%PHP_PEAR_PHP_BIN%" neq "" goto USE_PEAR_PATH
 GOTO RUN
 :USE_PEAR_PATH
-set PHPBIN="%PHP_PEAR_PHP_BIN%"
+set PHPBIN=%PHP_PEAR_PHP_BIN%
 :RUN
-%PHPBIN% "@bin_dir@\dbunit" %*
+"%PHPBIN%" "@bin_dir@\dbunit" %*
