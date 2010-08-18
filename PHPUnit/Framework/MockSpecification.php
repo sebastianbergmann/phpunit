8fc0ff16bdb44b6d57393045fb6b159313384565
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2010-08-18 15:08:01 +0300

    Reached 6/7 of the arguments and added some docblocks

--- PHPUnit/Framework/MockSpecification.php
+++ PHPUnit/Framework/MockSpecification.php
@@ -8,6 +8,7 @@ class PHPUnit_Framework_MockSpecification
     protected $testCase;
     protected $className;
     protected $methods = array();
+    protected $mockClassName = '';
     protected $constructorArgs = array();
     protected $originalConstructor = true;
     protected $originalClone = true;
@@ -23,33 +24,62 @@ class PHPUnit_Framework_MockSpecification
         $this->className = $className;
     }
 
+    /**
+     * @return object
+     */
     public function getMock()
     {
         return $this->testCase->getMock($this->className,
                                         $this->methods,
                                         $this->constructorArgs,
-                                        '',
+                                        $this->mockClassName,
                                         $this->originalConstructor,
                                         $this->originalClone);
     }
 
+    /**
+     * @return PHPUnit_Framework_MockSpecification  provides a fluent interface
+     */
     public function setMethods(array $methods)
     {
         $this->methods = $methods;
+        return $this;
     }
 
+    /**
+     * @return PHPUnit_Framework_MockSpecification  provides a fluent interface
+     */
     public function setConstructorArgs(array $args)
     {
         $this->constructorArgs = $args;
+        return $this;
     }
 
+    /**
+     * @param string
+     * @return PHPUnit_Framework_MockSpecification  provides a fluent interface
+     */
+    public function setMockClassName($name)
+    {
+        $this->mockClassName = $name;
+        return $this;
+    }
+
+    /**
+     * @return PHPUnit_Framework_MockSpecification  provides a fluent interface
+     */
     public function disableOriginalConstructor()
     {
         $this->originalConstructor = false;
+        return $this;
     } 
 
+    /**
+     * @return PHPUnit_Framework_MockSpecification  provides a fluent interface
+     */
     public function disableOriginalClone()
     {
         $this->originalClone = false;
+        return $this;
     } 
 }
