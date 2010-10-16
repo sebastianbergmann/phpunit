622f50b598fa16a8c45f83ed31ff3115373a1fa9
Jeff Welch <whatthejeff@gmail.com>
2010-10-16 14:44:57 -0500

    Added the missing `$message` argument to the assertion that checks for a specific count in `PHPUnit_Framework_Assert::assertSelectEquals()`.  This refs issue_47.

--- PHPUnit/Framework/Assert.php
+++ PHPUnit/Framework/Assert.php
@@ -1837,7 +1837,7 @@ abstract class PHPUnit_Framework_Assert
         // assert specific number of elements
         if (is_numeric($count)) {
             $counted = $tags ? count($tags) : 0;
-            self::assertEquals($count, $counted);
+            self::assertEquals($count, $counted, $message);
         }
 
         // assert any elements exist if true, assert no elements exist if false
