211da24a48a858117e1529a5cd81a1d83670ba8e
Sebastian Bergmann <sb@sebastian-bergmann.de>
2010-12-03 09:48:10 +0100

    Use File_Iterator autoloader.

--- PHPUnit/Runner/BaseTestRunner.php
+++ PHPUnit/Runner/BaseTestRunner.php
@@ -43,8 +43,6 @@
  * @since      File available since Release 2.0.0
  */
 
-require_once 'File/Iterator/Factory.php';
-
 /**
  * Base class for all test runners.
  *
--- PHPUnit/Util/Filter.php
+++ PHPUnit/Util/Filter.php
@@ -43,8 +43,6 @@
  * @since      File available since Release 2.0.0
  */
 
-require_once 'File/Iterator/Factory.php';
-
 /**
  * Utility class for code filtering.
  *
