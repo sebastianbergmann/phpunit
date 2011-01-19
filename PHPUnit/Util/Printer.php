87dbed1a1b62dba77fcce17a0254f5d79b6dfb61
Sebastian Bergmann <sb@sebastian-bergmann.de>
2011-01-19 11:47:14 +0100

    Use correct default value for second argument of mkdir().

--- PHPUnit/Util/Printer.php
+++ PHPUnit/Util/Printer.php
@@ -99,7 +99,7 @@ abstract class PHPUnit_Util_Printer
                     $this->out = fsockopen($out[0], $out[1]);
                 } else {
                     if (!is_dir(dirname($out))) {
-                      mkdir(dirname($out), 0, TRUE);
+                      mkdir(dirname($out), 0777, TRUE);
                     }
 
                     $this->out = fopen($out, 'wt');
