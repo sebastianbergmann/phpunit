eb93e9a642c006a72850777668cb41c6f136e55d
Sebastian Bergmann <sb@sebastian-bergmann.de>
2011-02-14 19:53:21 +0100

    Fix help output.

--- PHPUnit/TextUI/Command.php
+++ PHPUnit/TextUI/Command.php
@@ -837,55 +837,55 @@ class PHPUnit_TextUI_Command
 Usage: phpunit [switches] UnitTest [UnitTest.php]
        phpunit [switches] <directory>
 
-  --log-junit <file>       Log test execution in JUnit XML format to file.
-  --log-tap <file>         Log test execution in TAP format to file.
-  --log-dbus               Log test execution to DBUS.
-  --log-json <file>        Log test execution in JSON format.
+  --log-junit <file>        Log test execution in JUnit XML format to file.
+  --log-tap <file>          Log test execution in TAP format to file.
+  --log-dbus                Log test execution to DBUS.
+  --log-json <file>         Log test execution in JSON format.
 
-  --coverage-html <dir>    Generate code coverage report in HTML format.
-  --coverage-clover <file> Write code coverage data in Clover XML format.
+  --coverage-html <dir>     Generate code coverage report in HTML format.
+  --coverage-clover <file>  Write code coverage data in Clover XML format.
 
-  --testdox-html <file>    Write agile documentation in HTML format to file.
-  --testdox-text <file>    Write agile documentation in Text format to file.
+  --testdox-html <file>     Write agile documentation in HTML format to file.
+  --testdox-text <file>     Write agile documentation in Text format to file.
 
-  --filter <pattern>       Filter which tests to run.
-  --group ...              Only runs tests from the specified group(s).
-  --exclude-group ...      Exclude tests from the specified group(s).
-  --list-groups            List available test groups.
+  --filter <pattern>        Filter which tests to run.
+  --group ...               Only runs tests from the specified group(s).
+  --exclude-group ...       Exclude tests from the specified group(s).
+  --list-groups             List available test groups.
 
-  --loader <loader>        TestSuiteLoader implementation to use.
-  --printer <printer>      TestSuiteListener implementation to use.
-  --repeat <times>         Runs the test(s) repeatedly.
+  --loader <loader>         TestSuiteLoader implementation to use.
+  --printer <printer>       TestSuiteListener implementation to use.
+  --repeat <times>          Runs the test(s) repeatedly.
 
-  --tap                    Report test execution progress in TAP format.
-  --testdox                Report test execution progress in TestDox format.
+  --tap                     Report test execution progress in TAP format.
+  --testdox                 Report test execution progress in TestDox format.
 
-  --colors                 Use colors in output.
-  --stderr                 Write to STDERR instead of STDOUT.
-  --stop-on-error          Stop execution upon first error.
-  --stop-on-failure        Stop execution upon first error or failure.
-  --stop-on-skipped        Stop execution upon first skipped test.
-  --stop-on-incomplete     Stop execution upon first incomplete test.
-  --strict                 Mark a test as incomplete if no assertions are made.
-  -v|--verbose             Output more verbose information.
-  --wait                   Waits for a keystroke after each test.
+  --colors                  Use colors in output.
+  --stderr                  Write to STDERR instead of STDOUT.
+  --stop-on-error           Stop execution upon first error.
+  --stop-on-failure         Stop execution upon first error or failure.
+  --stop-on-skipped         Stop execution upon first skipped test.
+  --stop-on-incomplete      Stop execution upon first incomplete test.
+  --strict                  Mark a test as incomplete if no assertions are made.
+  -v|--verbose              Output more verbose information.
+  --wait                    Waits for a keystroke after each test.
 
-  --skeleton-class         Generate Unit class for UnitTest in UnitTest.php.
-  --skeleton-test          Generate UnitTest class for Unit in Unit.php.
+  --skeleton-class          Generate Unit class for UnitTest in UnitTest.php.
+  --skeleton-test           Generate UnitTest class for Unit in Unit.php.
 
-  --process-isolation      Run each test in a separate PHP process.
-  --no-globals-backup      Do not backup and restore \$GLOBALS for each test.
-  --static-backup          Backup and restore static attributes for each test.
-  --syntax-check           Try to check source files for syntax errors.
+  --process-isolation       Run each test in a separate PHP process.
+  --no-globals-backup       Do not backup and restore \$GLOBALS for each test.
+  --static-backup           Backup and restore static attributes for each test.
+  --syntax-check            Try to check source files for syntax errors.
 
-  --bootstrap <file>       A "bootstrap" PHP file that is run before the tests.
-  -c|--configuration       <file> Read configuration from XML file.
-  --no-configuration       Ignore default configuration file (phpunit.xml).
-  --include-path <path(s)> Prepend PHP's include_path with given path(s).
-  -d key[=value]           Sets a php.ini value.
+  --bootstrap <file>        A "bootstrap" PHP file that is run before the tests.
+  -c|--configuration <file> Read configuration from XML file.
+  --no-configuration        Ignore default configuration file (phpunit.xml).
+  --include-path <path(s)>  Prepend PHP's include_path with given path(s).
+  -d key[=value]            Sets a php.ini value.
 
-  -h|--help                Prints this usage information.
-  --version                Prints the version and exits.
+  -h|--help                 Prints this usage information.
+  --version                 Prints the version and exits.
 
 EOT;
     }
