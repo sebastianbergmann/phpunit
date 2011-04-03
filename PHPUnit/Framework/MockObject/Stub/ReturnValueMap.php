851cd95e75956d18e61cda3ef5e8560c79678ec4
Sebastian Bergmann <sb@sebastian-bergmann.de>
2011-04-03 10:12:05 +0300

    Bump copyright year.

--- PHPUnit/Framework/MockObject/Stub/ReturnValueMap.php
+++ PHPUnit/Framework/MockObject/Stub/ReturnValueMap.php
@@ -2,7 +2,7 @@
 /**
  * PHPUnit
  *
- * Copyright (c) 2010, Sebastian Bergmann <sb@sebastian-bergmann.de>.
+ * Copyright (c) 2010-2011, Sebastian Bergmann <sb@sebastian-bergmann.de>.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
