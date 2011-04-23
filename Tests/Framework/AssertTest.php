d29ae6eb5ea43f50afa2c65431fbd38f1b1fcf7e
Ryan Mitchell <mitchellryanj@gmail.com>
2011-04-22 22:16:58 -0700

    Addressing an issue with using the parent option when multiple elements match the "base" matcher.

--- Tests/Framework/AssertTest.php
+++ Tests/Framework/AssertTest.php
@@ -3557,6 +3557,16 @@ class Framework_AssertTest extends PHPUnit_Framework_TestCase
     }
 
     /**
+    * @covers PHPUnit_Framework_Assert::assertTag
+    */
+    public function testAssertTagMultiplePossibleChildren()
+    {
+	$matcher = array('tag' => 'li',
+			 'parent' => array('tag' => 'ul', 'id' => 'another_ul'));
+	$this->assertTag($matcher, $this->html);
+    }
+
+    /**
      * @covers PHPUnit_Framework_Assert::assertTag
      */
     public function testAssertTagChildTrue()
