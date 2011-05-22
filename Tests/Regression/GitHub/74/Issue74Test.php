1133965e5c1c5e8e0aeb844dc6b8f2b7b744d8e0
Jeff Welch <whatthejeff@gmail.com>
2011-05-22 13:39:17 -0500

    Refs GH-74
     * Regression should test an exception defined within an isolated test.

--- Tests/Regression/GitHub/74/Issue74Test.php
+++ Tests/Regression/GitHub/74/Issue74Test.php
@@ -1,11 +1,9 @@
 <?php
-
-require_once dirname(__FILE__) . '/NewException.php';
-
 class Issue74Test extends PHPUnit_Framework_TestCase
 {
     public function testCreateAndThrowNewExceptionInProcessIsolation()
     {
+        require_once dirname(__FILE__) . '/NewException.php';
         throw new NewException('Testing GH-74');
     }
 }
