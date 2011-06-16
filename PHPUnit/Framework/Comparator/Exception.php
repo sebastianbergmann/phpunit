a31af5345123f7a39b95e9a183dd3fd381f1e890
Sebastian Bergmann <sb@sebastian-bergmann.de>
2011-06-16 09:01:10 -0700

    Cleanup.

--- PHPUnit/Framework/Comparator/Exception.php
+++ PHPUnit/Framework/Comparator/Exception.php
@@ -63,7 +63,6 @@ class PHPUnit_Framework_Comparator_Exception extends PHPUnit_Framework_Comparato
      * @param  mixed $expected The first value to compare
      * @param  mixed $actual The second value to compare
      * @return boolean
-     * @since  Method available since Release 3.6.0
      */
     public function accepts($expected, $actual)
     {
@@ -76,7 +75,6 @@ class PHPUnit_Framework_Comparator_Exception extends PHPUnit_Framework_Comparato
      *
      * @param  object $object
      * @return array
-     * @since  Method available since Release 3.6.0
      */
     protected function toArray($object)
     {
--- PHPUnit/Framework/Comparator/Object.php
+++ PHPUnit/Framework/Comparator/Object.php
@@ -63,7 +63,6 @@ class PHPUnit_Framework_Comparator_Object extends PHPUnit_Framework_Comparator_A
      * @param  mixed $expected The first value to compare
      * @param  mixed $actual The second value to compare
      * @return boolean
-     * @since  Method available since Release 3.6.0
      */
     public function accepts($expected, $actual)
     {
@@ -84,7 +83,6 @@ class PHPUnit_Framework_Comparator_Object extends PHPUnit_Framework_Comparator_A
      * @throws PHPUnit_Framework_ComparisonFailure Thrown when the comparison
      *                           fails. Contains information about the
      *                           specific errors that lead to the failure.
-     * @since  Method available since Release 3.6.0
      */
     public function assertEquals($expected, $actual, $delta = 0, $canonicalize = FALSE, $ignoreCase = FALSE, array &$processed = array())
     {
@@ -140,7 +138,6 @@ class PHPUnit_Framework_Comparator_Object extends PHPUnit_Framework_Comparator_A
      *
      * @param  object $object
      * @return array
-     * @since  Method available since Release 3.6.0
      */
     protected function toArray($object)
     {
