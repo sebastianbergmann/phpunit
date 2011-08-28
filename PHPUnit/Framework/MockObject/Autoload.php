ec35145c5ef62fe502fc93a4f630ab5224826c98
Sebastian Bergmann <sb@sebastian-bergmann.de>
2011-08-28 13:38:34 +0300

    Include autoload script in list of files.

--- PHPUnit/Framework/MockObject/Autoload.php
+++ PHPUnit/Framework/MockObject/Autoload.php
@@ -90,7 +90,7 @@ function phpunit_mockobject_autoload($class = NULL) {
     }
 
     if ($class === NULL) {
-        $result = array();
+        $result = array(__FILE__);
 
         foreach ($classes as $file) {
             $result[] = $path . $file;
