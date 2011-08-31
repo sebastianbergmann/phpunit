6ac554ede32330e3775c231cabe0f7b513770e6c
Oliver Gondža <324706@mail.muni.cz>
2011-08-31 19:21:54 +0200

    Positional argument count check introduced

--- PHPUnit/TextUI/Command.php
+++ PHPUnit/TextUI/Command.php
@@ -528,6 +528,15 @@ class PHPUnit_TextUI_Command
         $this->handleCustomTestSuite();
 
         if (!isset($this->arguments['test'])) {
+            if (count($this->options[1]) > 2) {
+                $this->showMessage(
+                    'More than two positional arguments provided.',
+                    false
+                );
+                $this->showHelp();
+                exit(PHPUnit_TextUI_TestRunner::FAILURE_EXIT);
+            }
+        
             if (isset($this->options[1][0])) {
                 $this->arguments['test'] = $this->options[1][0];
             }
