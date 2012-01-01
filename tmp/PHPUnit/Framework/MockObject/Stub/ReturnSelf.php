0e558212cdd1e372f19e648e8a1e8240b652821a
Sebastian Bergmann <sb@sebastian-bergmann.de>
2012-01-01 11:25:14 +0100

    Bump copyright year.

--- tmp/PHPUnit/Framework/MockObject/Stub/ReturnSelf.php
+++ tmp/PHPUnit/Framework/MockObject/Stub/ReturnSelf.php
@@ -2,7 +2,7 @@
 /**
  * PHPUnit
  *
- * Copyright (c) 2010-2011, Sebastian Bergmann <sb@sebastian-bergmann.de>.
+ * Copyright (c) 2010-2012, Sebastian Bergmann <sb@sebastian-bergmann.de>.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -49,7 +49,7 @@
  * @package    PHPUnit_MockObject
  * @author     Sebastian Bergmann <sb@sebastian-bergmann.de>
  * @author     Kris Wallsmith <kris.wallsmith@gmail.com>
- * @copyright  2010-2011 Sebastian Bergmann <sb@sebastian-bergmann.de>
+ * @copyright  2010-2012 Sebastian Bergmann <sb@sebastian-bergmann.de>
  * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
  * @version    Release: @package_version@
  * @link       http://github.com/sebastianbergmann/phpunit-mock-objects
