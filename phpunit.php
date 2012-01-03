0b649b04186690e8c733c55714418dd6fb798ab5
Sebastian Bergmann <sb@sebastian-bergmann.de>
2012-01-03 10:58:28 +0100

    Revert "Remove superfluous code."
    This reverts commit 638ddb2533de309b5a370c4a34afad26de532f52.

--- phpunit.php
+++ phpunit.php
@@ -35,6 +35,8 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
+define('PHPUnit_MAIN_METHOD', 'PHPUnit_TextUI_Command::main');
+
 if (strpos('@php_bin@', '@php_bin') === 0) {
     require dirname(__FILE__) . DIRECTORY_SEPARATOR . 'PHPUnit' . DIRECTORY_SEPARATOR . 'Autoload.php';
 } else {
