b8f32271f24dd8f8d0122e019ee9c690ce6a5f8d
Alexander Ilyin <alexander@ilyin.eu>
2012-03-22 04:57:34 +0400

    Fixed DocBlock @param name

---
+++ PHPUnit/Framework/TeamCity/TestListener.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: alexanderilyin
+ * Date: 22.03.12
+ * Time: 0:33
+ * To change this template use File | Settings | File Templates.
+ */
+class PHPUnit_Framework_TeamCity_TestListener
+{
+
+}
