9f8584067f5b207442b5a816e0883351b3293687
Alexei Kornienko <alexei.kornienko@gmail.com>
2012-06-18 04:49:51 +0900

    +added filters system

---
+++ PHPUnit/Util/Filters/GroupFilterIterator.php
@@ -0,0 +1,24 @@
+<?php
+
+abstract class PHPUnit_Util_Filters_GroupFilterIterator extends FilterIterator
+{
+
+    /**
+     * @var array
+     */
+    protected $groupTests = array();
+
+    public function __construct($iterator, $groups, PHPUnit_Framework_TestSuite $parentSuite) {
+        parent::__construct($iterator);
+
+        foreach ($parentSuite->getGroupDetails() as $group => $tests) {
+            if (in_array($group, $groups)) {
+                $testHashes = array_map(function($test) {
+                    return spl_object_hash($test);
+                }, $tests);
+                $this->groupTests += $testHashes;
+            }
+        }
+    }
+
+}
