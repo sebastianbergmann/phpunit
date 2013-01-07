3a306bd4874ed15ea3060b4be50b17a33902d438
Sebastian Bergmann <sb@sebastian-bergmann.de>
2013-01-07 11:53:29 +0100

    Bump copyright year

--- Tests/Framework/Constraint/JsonMatches/ErrorMessageProviderTest.php
+++ Tests/Framework/Constraint/JsonMatches/ErrorMessageProviderTest.php
@@ -2,7 +2,7 @@
 /**
  * PHPUnit
  *
- * Copyright (c) 2002-2011, Sebastian Bergmann <sebastian@phpunit.de>.
+ * Copyright (c) 2002-2013, Sebastian Bergmann <sebastian@phpunit.de>.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
