76e6572bf10987cc4232e521a3d977b6f1704307
Sebastian Bergmann <sb@sebastian-bergmann.de>
2013-07-29 15:44:36 +0200

    Fix issue reported as part of #958

--- PHPUnit/Util/GlobalState.php
+++ PHPUnit/Util/GlobalState.php
@@ -295,7 +295,7 @@ class PHPUnit_Util_GlobalState
                 strpos($declaredClasses[$i], 'PHP_CodeCoverage') !== 0 &&
                 strpos($declaredClasses[$i], 'PHP_Invoker') !== 0 &&
                 strpos($declaredClasses[$i], 'PHP_Timer') !== 0 &&
-                strpos($declaredClasses[$i], 'PHP_TokenStream') !== 0 &&
+                strpos($declaredClasses[$i], 'PHP_Token_Stream') !== 0 &&
                 strpos($declaredClasses[$i], 'Symfony') !== 0 &&
                 strpos($declaredClasses[$i], 'Text_Template') !== 0 &&
                 !$declaredClasses[$i] instanceof PHPUnit_Framework_Test) {
