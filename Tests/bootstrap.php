53c2877dc5415788f69ec7ed3f9b7db645e8062f
Sebastian Bergmann <sb@sebastian-bergmann.de>
2013-10-29 10:40:59 +0100

    Do not perform check for date.timezone on HHVM

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -4,7 +4,7 @@ require __DIR__ . '/../PHPUnit/Framework/Assert/Functions.php';
 require __DIR__ . '/_files/CoveredFunction.php';
 require __DIR__ . '/autoload.php';
 
-if (!ini_get('date.timezone')) {
+if (!ini_get('date.timezone') && !function_exists('fb_enable_code_coverage')) {
   echo PHP_EOL . 'Error: PHPUnit\'s test suite requires the "date.timezone" runtime configuration to be set. Please check your php.ini.' . PHP_EOL;
   exit(1);
 }
