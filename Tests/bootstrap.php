3a09542f9a56f90537f4677d3eb0137f2a458839
Spencer Rinehart <anubis@overthemonkey.com>
2013-10-29 13:02:05 -0400

    Fix date.timezone typo.

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -5,7 +5,7 @@ require __DIR__ . '/_files/CoveredFunction.php';
 require __DIR__ . '/autoload.php';
 
 if (!ini_get('date.timezone')) {
-  echo PHP_EOL . 'Error: To properly execute PHPUnits test suite you have to set "date.timzone". Please check your php.ini.' . PHP_EOL;
+  echo PHP_EOL . 'Error: To properly execute PHPUnits test suite you have to set "date.timezone". Please check your php.ini.' . PHP_EOL;
   exit(1);
 }
 
