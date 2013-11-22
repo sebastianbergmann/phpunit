e13b380bf46bd7aeec952ac2ba47730c37e084d4
Jeff Welch <whatthejeff@gmail.com>
2013-11-22 02:55:04 -0500

    Sync c1fe4b7 (it's failing on travis now).

--- Tests/bootstrap-travis.php
+++ Tests/bootstrap-travis.php
@@ -2,3 +2,6 @@
 require __DIR__ . '/../PHPUnit/Framework/Assert/Functions.php';
 require __DIR__ . '/_files/CoveredFunction.php';
 require __DIR__ . '/autoload.php';
+
+ini_set('precision', 14);
+ini_set('serialize_precision', 14);
