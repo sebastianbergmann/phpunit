4d7da577175220d8ab26936657b219ab2d617906
Arne Blankerts <Arne@Blankerts.de>
2013-12-20 17:05:12 +0100

    - Removed DOMDocument Comparator in favor of DOMNode Comparator - Added support for case-sensitivity switch

--- PHPUnit/Framework/ComparatorFactory.php
+++ PHPUnit/Framework/ComparatorFactory.php
@@ -80,7 +80,7 @@ class PHPUnit_Framework_ComparatorFactory
         $this->register(new PHPUnit_Framework_Comparator_Object);
         $this->register(new PHPUnit_Framework_Comparator_Exception);
         $this->register(new PHPUnit_Framework_Comparator_SplObjectStorage);
-        $this->register(new PHPUnit_Framework_Comparator_DOMDocument);
+        $this->register(new PHPUnit_Framework_Comparator_DOMNode);
         $this->register(new PHPUnit_Framework_Comparator_MockObject);
         $this->register(new PHPUnit_Framework_Comparator_DateTime);
     }
