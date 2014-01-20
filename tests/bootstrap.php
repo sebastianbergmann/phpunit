3a5808fbca4ee832c27f13a544fdf8809eec62ec
Sebastian Bergmann <sb@sebastian-bergmann.de>
2014-01-20 17:21:13 +0100

    Cleanup

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../PHPUnit/Framework/Assert/Functions.php';
+require __DIR__ . '/../src/Framework/Assert/Functions.php';
 require __DIR__ . '/_files/CoveredFunction.php';
 require __DIR__ . '/autoload.php';
 
