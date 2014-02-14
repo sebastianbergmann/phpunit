3d7186d08bed083edecb5f85509a1aa995a1d11d
Sebastian Bergmann <sb@sebastian-bergmann.de>
2014-02-14 18:47:24 +0100

    Fix

--- build/phar-manifest.php
+++ build/phar-manifest.php
@@ -33,7 +33,7 @@ function print_git_info($component, $path)
 
     $tag = @exec('git describe --tags 2>&1');
 
-    if (strpos($tag, '-') === false) {
+    if (strpos($tag, '-') === false && strpos($tag, 'No names found') === false) {
         print $tag;
     } else {
         $branch = @exec('git rev-parse --abbrev-ref HEAD');
