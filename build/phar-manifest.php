b79ef3ad39a38ea1b03309067966ffffb77c68a7
Sebastian Bergmann <sb@sebastian-bergmann.de>
2014-02-15 12:04:22 +0100

    Simplify

--- build/phar-manifest.php
+++ build/phar-manifest.php
@@ -1,45 +1,14 @@
 #!/usr/bin/env php
 <?php
-$components = array(
-    'phpunit/dbunit',
-    'phpunit/php-code-coverage',
-    'phpunit/php-file-iterator',
-    'phpunit/php-invoker',
-    'phpunit/php-text-template',
-    'phpunit/php-timer',
-    'phpunit/php-token-stream',
-    'phpunit/phpunit-mock-objects',
-    'phpunit/phpunit-selenium',
-    'sebastian/diff',
-    'sebastian/exporter',
-    'sebastian/version'
-);
+$lock = json_decode(file_get_contents(__DIR__ . '/../composer.lock'));
 
-print_git_info('phpunit/phpunit', __DIR__ . '/../');
+foreach ($lock->packages as $package) {
+    print $package->name . ': ' . $package->version;
 
-foreach ($components as $component) {
-    print_git_info($component, __DIR__ . '/../vendor/' . $component);
-}
-
-function print_git_info($component, $path)
-{
-    if (!is_dir($path)) {
-        return;
-    }
-
-    print $component . ': ';
-
-    chdir($path);
-
-    $tag = @exec('git describe --tags 2>&1');
-
-    if (strpos($tag, '-') === false && strpos($tag, 'No names found') === false) {
-        print $tag;
-    } else {
-        $branch = @exec('git rev-parse --abbrev-ref HEAD');
-        $hash   = @exec('git log -1 --format="%H"');
-        print $branch . '@' . $hash;
+    if (!preg_match('/^[v= ]*(([0-9]+)(\\.([0-9]+)(\\.([0-9]+)(-([0-9]+))?(-?([a-zA-Z-+][a-zA-Z0-9\\.\\-:]*)?)?)?)?)$/', $package->version)) {
+        print '@' . $package->source->reference;
     }
 
     print "\n";
 }
+
