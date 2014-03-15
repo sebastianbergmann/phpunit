3cfc883fe89243bbe576e82f9b23de90f8073c9d
Sebastian Bergmann <sb@sebastian-bergmann.de>
2014-03-15 10:04:53 +0100

    Add missing annotation

--- src/Framework/TestSuite.php
+++ src/Framework/TestSuite.php
@@ -95,6 +95,9 @@ class PHPUnit_Framework_TestSuite implements PHPUnit_Framework_Test, PHPUnit_Fra
      */
     protected $backupStaticAttributes = null;
 
+    /**
+     * @var boolean
+     */
     protected $runTestInSeparateProcess = false;
 
     /**
