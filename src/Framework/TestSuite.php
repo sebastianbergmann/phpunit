sebastianbergmann/phpunit

commit cad11af82d1856ccf8c6bfd7d4d49efd04ccea81

Refactor PHPUnit_Util_Test::getHookMethods() to perform the class_exists() check

src/Framework/TestSuite.php
@@ -630,25 +630,16 @@ class PHPUnit_Framework_TestSuite implements PHPUnit_Framework_Test, PHPUnit_Fra
             return $result;
         }
 
-        // Get hookMethods only if this is a testcase
-        // Otherwise $this->name may look like UnitTest::testMethod (e.g. when using @dataProvider)
-        $hookMethods = array();
-        if ($this->testCase && class_exists($this->name, false)) {
-            $hookMethods = PHPUnit_Util_Test::getHookMethods($this->name);
-        }
+        $hookMethods = PHPUnit_Util_Test::getHookMethods($this->name);
 
         $result->startTestSuite($this);
 
         try {
             $this->setUp();
 
-            // Some extensions use test names that are not classes;
-            // The method_exists() triggers an autoload call that causes issues with die()ing autoloaders.
-            if ($this->testCase && class_exists($this->name, false)) {
-                foreach ($hookMethods['beforeClass'] as $beforeClassMethod) {
-                    if (method_exists($this->name, $beforeClassMethod)) {
-                        call_user_func(array($this->name, $beforeClassMethod));
-                    }
+            foreach ($hookMethods['beforeClass'] as $beforeClassMethod) {
+                if (method_exists($this->name, $beforeClassMethod)) {
+                    call_user_func(array($this->name, $beforeClassMethod));
                 }
             }
         } catch (PHPUnit_Framework_SkippedTestSuiteError $e) {
@@ -684,13 +675,9 @@ class PHPUnit_Framework_TestSuite implements PHPUnit_Framework_Test, PHPUnit_Fra
             $test->run($result);
         }
 
-        // Some extensions use test names that are not classes;
-        // The method_exists() triggers an autoload call that causes issues with die()ing autoloaders.
-        if ($this->testCase && class_exists($this->name, false)) {
-            foreach ($hookMethods['afterClass'] as $afterClassMethod) {
-                if (method_exists($this->name, $afterClassMethod)) {
-                    call_user_func(array($this->name, $afterClassMethod));
-                }
+        foreach ($hookMethods['afterClass'] as $afterClassMethod) {
+            if (method_exists($this->name, $afterClassMethod)) {
+                call_user_func(array($this->name, $afterClassMethod));
             }
         }
 

src/Util/Test.php
@@ -664,13 +664,12 @@ class PHPUnit_Util_Test
      */
     public static function getHookMethods($className)
     {
+        if (!class_exists($className, false)) {
+            return self::emptyHookMethodsArray();
+        }
+
         if (!isset(self::$hookMethods[$className])) {
-            self::$hookMethods[$className] = array(
-                'beforeClass' => array('setUpBeforeClass'),
-                'before' => array('setUp'),
-                'after' => array('tearDown'),
-                'afterClass' => array('tearDownAfterClass')
-            );
+            self::$hookMethods[$className] = self::emptyHookMethodsArray();
 
             try {
                 $class = new ReflectionClass($className);
@@ -701,6 +700,20 @@ class PHPUnit_Util_Test
         }
 
         return self::$hookMethods[$className];
+    }
+
+    /**
+     * @return array
+     * @since  Method available since Release 4.0.9
+     */
+    private static function emptyHookMethodsArray()
+    {
+        return array(
+            'beforeClass' => array('setUpBeforeClass'),
+            'before' => array('setUp'),
+            'after' => array('tearDown'),
+            'afterClass' => array('tearDownAfterClass')
+        );
     }
 
     /**

commit f12e10fddc3ccbddb652a04d9036aeb5a6d54bff

fixed autoloader-issue with not-existing-files, regression in cad11af82d1856ccf8c6bfd7d4d49efd04ccea81

src/Framework/TestSuite.php
@@ -638,7 +638,7 @@ class PHPUnit_Framework_TestSuite implements PHPUnit_Framework_Test, PHPUnit_Fra
             $this->setUp();
 
             foreach ($hookMethods['beforeClass'] as $beforeClassMethod) {
-                if (method_exists($this->name, $beforeClassMethod)) {
+                if ($this->testCase === true && class_exists($this->name, false) && method_exists($this->name, $beforeClassMethod)) {
                     call_user_func(array($this->name, $beforeClassMethod));
                 }
             }
@@ -676,7 +676,7 @@ class PHPUnit_Framework_TestSuite implements PHPUnit_Framework_Test, PHPUnit_Fra
         }
 
         foreach ($hookMethods['afterClass'] as $afterClassMethod) {
-            if (method_exists($this->name, $afterClassMethod)) {
+            if ($this->testCase === true && class_exists($this->name, false) && method_exists($this->name, $afterClassMethod)) {
                 call_user_func(array($this->name, $afterClassMethod));
             }
         }