cf1469504afafdea0d4c6e4201110afc1d5dc21b
Sebastian Bergmann <sb@sebastian-bergmann.de>
2014-04-17 11:46:37 +0200

    Rename _files to _fixture

--- tmp/tests/MockObjectTest.php
+++ tmp/tests/MockObjectTest.php
@@ -730,7 +730,7 @@ class Framework_MockObjectTest extends PHPUnit_Framework_TestCase
      */
     public function testCreateMockFromWsdl()
     {
-        $mock = $this->getMockFromWsdl(__DIR__ . '/_files/GoogleSearch.wsdl', 'WsdlMock');
+        $mock = $this->getMockFromWsdl(__DIR__ . '/_fixture/GoogleSearch.wsdl', 'WsdlMock');
         $this->assertStringStartsWith(
             'Mock_WsdlMock_',
             get_class($mock)
@@ -742,7 +742,7 @@ class Framework_MockObjectTest extends PHPUnit_Framework_TestCase
      */
     public function testCreateNamespacedMockFromWsdl()
     {
-        $mock = $this->getMockFromWsdl(__DIR__ . '/_files/GoogleSearch.wsdl', 'My\\Space\\WsdlMock');
+        $mock = $this->getMockFromWsdl(__DIR__ . '/_fixture/GoogleSearch.wsdl', 'My\\Space\\WsdlMock');
         $this->assertStringStartsWith(
             'Mock_WsdlMock_',
             get_class($mock)
@@ -754,8 +754,8 @@ class Framework_MockObjectTest extends PHPUnit_Framework_TestCase
      */
     public function testCreateTwoMocksOfOneWsdlFile()
     {
-        $mock = $this->getMockFromWsdl(__DIR__ . '/_files/GoogleSearch.wsdl');
-        $mock = $this->getMockFromWsdl(__DIR__ . '/_files/GoogleSearch.wsdl');
+        $mock = $this->getMockFromWsdl(__DIR__ . '/_fixture/GoogleSearch.wsdl');
+        $mock = $this->getMockFromWsdl(__DIR__ . '/_fixture/GoogleSearch.wsdl');
     }
 
     /**
--- tmp/tests/bootstrap.php
+++ tmp/tests/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
 require __DIR__ . '/autoload.php';
-require __DIR__ . '/_files/FunctionCallback.php';
+require __DIR__ . '/_fixture/FunctionCallback.php';
 require __DIR__ . '/../vendor/autoload.php';
 
