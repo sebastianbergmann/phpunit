sebastianbergmann/phpunit

commit 7dadedf6c770feb18c0e17d895e698d57a6fb9bb

Improve comparison of DOMNode objects

src/Framework/Comparator/DOMNode.php
@@ -85,15 +85,15 @@ class PHPUnit_Framework_Comparator_DOMNode extends PHPUnit_Framework_Comparator_
      */
     public function assertEquals($expected, $actual, $delta = 0.0, $canonicalize = false, $ignoreCase = false)
     {
-        $expectedXML = $expected->C14N();
-        $actualXML   = $actual->C14N();
+        $expectedAsString = $this->domToText($expected);
+        $actualAsString   = $this->domToText($actual);
 
         if ($ignoreCase === true) {
-            $expectedXML = strtolower($expectedXML);
-            $actualXML   = strtolower($actualXML);
+            $expectedAsString = strtolower($expectedAsString);
+            $actualAsString   = strtolower($actualAsString);
         }
 
-        if ($expectedXML !== $actualXML) {
+        if ($expectedAsString !== $actualAsString) {
             if ($expected instanceof DOMDocument) {
                 $type = 'documents';
             } else {
@@ -103,8 +103,8 @@ class PHPUnit_Framework_Comparator_DOMNode extends PHPUnit_Framework_Comparator_
             throw new PHPUnit_Framework_ComparisonFailure(
               $expected,
               $actual,
-              $this->domToText($expected),
-              $this->domToText($actual),
+              $expectedAsString,
+              $actualAsString,
               false,
               sprintf('Failed asserting that two DOM %s are equal.', $type)
             );

commit 5934d899b211737949e441d6b45f4282efeb2e97

Fix BC break introduced in 7dadedf6c770feb18c0e17d895e698d57a6fb9bb (see https://github.com/sebastianbergmann/phpunit/pull/1236)

src/Framework/Comparator/DOMNode.php
@@ -85,13 +85,8 @@ class PHPUnit_Framework_Comparator_DOMNode extends PHPUnit_Framework_Comparator_
      */
     public function assertEquals($expected, $actual, $delta = 0.0, $canonicalize = false, $ignoreCase = false)
     {
-        $expectedAsString = $this->domToText($expected);
-        $actualAsString   = $this->domToText($actual);
-
-        if ($ignoreCase === true) {
-            $expectedAsString = strtolower($expectedAsString);
-            $actualAsString   = strtolower($actualAsString);
-        }
+        $expectedAsString = $this->nodeToText($expected, true, $ignoreCase);
+        $actualAsString   = $this->nodeToText($actual, true, $ignoreCase);
 
         if ($expectedAsString !== $actualAsString) {
             if ($expected instanceof DOMDocument) {
@@ -106,7 +101,7 @@ class PHPUnit_Framework_Comparator_DOMNode extends PHPUnit_Framework_Comparator_
               $expectedAsString,
               $actualAsString,
               false,
-              sprintf('Failed asserting that two DOM %s are equal.', $type)
+              sprintf("Failed asserting that two DOM %s are equal.\n", $type)
             );
         }
     }
@@ -116,10 +111,19 @@ class PHPUnit_Framework_Comparator_DOMNode extends PHPUnit_Framework_Comparator_
      * representation of a DOMNode.
      *
      * @param  DOMNode $node
+     * @param  boolean $canonicalize
+     * @param  boolean $ignoreCase
      * @return string
      */
-    protected function domToText(DOMNode $node)
+    private function nodeToText(DOMNode $node, $canonicalize, $ignoreCase)
     {
+        if ($canonicalize) {
+            $document = new DOMDocument;
+            $document->loadXML($node->C14N());
+
+            $node = $document;
+        }
+
         if ($node instanceof DOMDocument) {
             $document = $node;
         } else {
@@ -130,9 +134,15 @@ class PHPUnit_Framework_Comparator_DOMNode extends PHPUnit_Framework_Comparator_
         $document->normalizeDocument();
 
         if ($node instanceof DOMDocument) {
-            return $node->saveXML();
+            $text = $node->saveXML();
+        } else {
+            $text = $document->saveXML($node);
         }
 
-        return $document->saveXML($node);
+        if ($ignoreCase) {
+            $text = strtolower($text);
+        }
+
+        return $text;
     }
 }