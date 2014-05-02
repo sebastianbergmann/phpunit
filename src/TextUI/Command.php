sebastianbergmann/phpunit

commit 18e658bcaf63cfee8248f877249b19c1d99cdf6d

Add --selfupdate alias for --self-update

src/TextUI/Command.php
@@ -255,6 +255,7 @@ class PHPUnit_TextUI_Command
     protected function handleArguments(array $argv)
     {
         if (defined('__PHPUNIT_PHAR__')) {
+            $this->longOptions['selfupdate']  = null;
             $this->longOptions['self-update'] = null;
         }
 
@@ -526,6 +527,7 @@ class PHPUnit_TextUI_Command
                 }
                 break;
 
+                case '--selfupdate': {
                 case '--self-update': {
                     $this->handleSelfUpdate();
                 }

commit 7bd874fee52bdf95178e58ce8bda50586865e37d

Fix syntax error introduced in 18e658bcaf63cfee8248f877249b19c1d99cdf6d

src/TextUI/Command.php
@@ -527,7 +527,7 @@ class PHPUnit_TextUI_Command
                 }
                 break;
 
-                case '--selfupdate': {
+                case '--selfupdate':
                 case '--self-update': {
                     $this->handleSelfUpdate();
                 }