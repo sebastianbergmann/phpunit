299037dc027ef5e35be87813804cc9e18721760f
Jeff Welch <whatthejeff@gmail.com>
2014-05-27 01:33:39 -0400

    Formatting.

--- src/Util/Regex.php
+++ src/Util/Regex.php
@@ -56,7 +56,7 @@
  */
 class PHPUnit_Util_Regex
 {
-    public static function pregMatchSafe($pattern , $subject, $matches = null, $flags = 0, $offset = 0)
+    public static function pregMatchSafe($pattern, $subject, $matches = null, $flags = 0, $offset = 0)
     {
         $handler_terminator = PHPUnit_Util_ErrorHandler::handleErrorOnce(E_WARNING);
         $match = preg_match($pattern, $subject, $matches, $flags, $offset);
