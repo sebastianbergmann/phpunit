47c99e64be0e7467c8f45e55d00b287a80395b01
Jeff Welch <whatthejeff@gmail.com>
2014-05-30 19:51:13 -0400

    Use composer to autoload tests and fixtures.

--- tests/bootstrap-travis.php
+++ tests/bootstrap-travis.php
@@ -1,7 +1,4 @@
 <?php
-require __DIR__ . '/../src/Framework/Assert/Functions.php';
-require __DIR__ . '/_files/CoveredFunction.php';
-require __DIR__ . '/autoload.php';
 
 ini_set('precision', 14);
 ini_set('serialize_precision', 14);
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,5 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../src/Framework/Assert/Functions.php';
-require __DIR__ . '/_files/CoveredFunction.php';
-require __DIR__ . '/autoload.php';
 
 if (!ini_get('date.timezone') && !defined('HHVM_VERSION')) {
   echo PHP_EOL . 'Error: PHPUnit\'s test suite requires the "date.timezone" runtime configuration to be set. Please check your php.ini.' . PHP_EOL;
