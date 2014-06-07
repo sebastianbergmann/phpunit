4308436b5e803f153b660c3b044936a1803929e7
Sebastian Bergmann <sb@sebastian-bergmann.de>
2014-06-07 15:54:52 +0300

    Closes #172

--- src/Framework/MockObject/Generator.php
+++ src/Framework/MockObject/Generator.php
@@ -269,7 +269,7 @@ class PHPUnit_Framework_MockObject_Generator
             }
         } else {
             $class = new ReflectionClass($className);
-            if ($class->isInternal() || version_compare(PHP_VERSION, '5.4.0', '<')) {
+            if ($this->isInternalClass($class) || version_compare(PHP_VERSION, '5.4.0', '<')) {
                 $object = unserialize(
                     sprintf('O:%d:"%s":0:{}', strlen($className), $className)
                 );
@@ -292,6 +292,24 @@ class PHPUnit_Framework_MockObject_Generator
         }
 
         return $object;
+    }
+
+    /**
+     * @param  ReflectionClass $class
+     * @return boolean
+     * @since  Method available since Release 2.0.8
+     */
+    private function isInternalClass(ReflectionClass $class)
+    {
+        while ($class) {
+            if ($class->isInternal()) {
+                return true;
+            }
+
+            $class = $class->getParentClass();
+        }
+
+        return false;
     }
 
     /**
