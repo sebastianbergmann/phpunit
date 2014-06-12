258218fa8302b51cc65cd691af10979f6329e5c3
Remi Collet <fedora@famillecollet.com>
2014-06-12 09:03:05 +0200

    php 5.6 is not yet released, so use 5.6.0-dev here

--- tmp/src/Framework/MockObject/Generator.php
+++ tmp/src/Framework/MockObject/Generator.php
@@ -1105,7 +1105,7 @@ class PHPUnit_Framework_MockObject_Generator
     private function unserializeHackIsSupported()
     {
         if (PHP_VERSION == '5.4.29' || PHP_VERSION == '5.5.13' ||
-            version_compare(PHP_VERSION, '5.6.0', '>=')) {
+            version_compare(PHP_VERSION, '5.6.0-dev', '>=')) {
             return FALSE;
         }
 
