89d74305f8c4a99e6e57551da512b6b3892bd870
Sebastian Bergmann <sb@sebastian-bergmann.de>
2014-06-24 08:29:25 +0200

    Sync with https://github.com/sebastianbergmann/phpunit-mock-objects/commit/84b27568405dca29de30bc2cdf457e25f5bede7e

--- src/Util/Blacklist.php
+++ src/Util/Blacklist.php
@@ -77,7 +77,9 @@ class PHPUnit_Util_Blacklist
         'SebastianBergmann\Comparator\Comparator' => 1,
         'SebastianBergmann\Exporter\Exporter' => 1,
         'SebastianBergmann\Version' => 1,
-        'Composer\Autoload\ClassLoader' => 1
+        'Composer\Autoload\ClassLoader' => 1,
+        'Instantiator\Instantiator' => 1,
+        'LazyMap\AbstractLazyMap' => 1
     );
 
     /**
