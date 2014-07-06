bd59addf933368e035808c3fbb2582fbe9a8b420
Franck Cassedanne <franck@cassedanne.com>
2014-07-06 23:01:46 +0100

    Update Command.php
    Fixed typo in showHelp()

--- PHPUnit/TextUI/Command.php
+++ PHPUnit/TextUI/Command.php
@@ -955,7 +955,7 @@ Usage: phpunit [switches] UnitTest [UnitTest.php]
 
   -h|--help                 Prints this usage information.
   --version                 Prints the version and exits.
-  --check-version           Compares the local and live version of PHPUnit.
+  --version-check           Compares the local and live version of PHPUnit.
 
 EOT;
 
