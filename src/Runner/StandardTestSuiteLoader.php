cd99c33b8f1fb73e547af7c754be9f4debcb9074
Roman Tsiupa <roman.tsiupa@sociomantic.com>
2014-07-09 17:40:29 +0200

    Fix running tests for autoloaded classes

--- src/Runner/StandardTestSuiteLoader.php
+++ src/Runner/StandardTestSuiteLoader.php
@@ -80,8 +80,7 @@ class PHPUnit_Runner_StandardTestSuiteLoader implements PHPUnit_Runner_TestSuite
             $loadedClasses = array_values(
               array_diff(get_declared_classes(), $loadedClasses)
             );
-            
-            $loadedClasses[] = $suiteClassName;
+
         }
 
         if (!class_exists($suiteClassName, false) && !empty($loadedClasses)) {
