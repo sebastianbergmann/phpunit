35addd947405f4411df0a01c5ada019972d0fb15
shabbyrobe <code@shabbyrobe.org>
2014-07-21 12:21:02 +1000

    Fixed multiple backslash global with @runTestsInSeparateProcesses and @preserveGlobalState

--- src/Util/GlobalState.php
+++ src/Util/GlobalState.php
@@ -362,10 +362,9 @@ class PHPUnit_Util_GlobalState
            (is_array($variable) && self::arrayOnlyContainsScalars($variable))) {
             return var_export($variable, true);
         }
-
-        return 'unserialize(\'' .
-                str_replace("'", "\'", serialize($variable)) .
-                '\')';
+        return 'unserialize(' .
+                var_export(serialize($variable), true) .
+                ')';
     }
 
     protected static function arrayOnlyContainsScalars(array $array)
