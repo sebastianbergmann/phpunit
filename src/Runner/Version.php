96bab64d2e051c59cfecc9f395c7a75b3ddfda90
Sebastian Bergmann <sb@sebastian-bergmann.de>
2014-12-04 08:26:43 +0100

    Start PHPUnit 4.6 development

--- src/Runner/Version.php
+++ src/Runner/Version.php
@@ -71,7 +71,7 @@ class PHPUnit_Runner_Version
         }
 
         if (self::$version === null) {
-            $version = new SebastianBergmann\Version('4.5', dirname(dirname(__DIR__)));
+            $version = new SebastianBergmann\Version('4.6', dirname(dirname(__DIR__)));
             self::$version = $version->getVersion();
         }
 
