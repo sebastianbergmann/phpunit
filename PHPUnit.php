55a1c1144cef2a49690ebf48f17401f91f0b980c
Herlander Campos SIlva <herlandercs@hotmail.com>
2015-02-15 22:50:24 +0000

    reload

--- PHPUnit.php
+++ PHPUnit.php
@@ -1,8 +1,8 @@
+#!/usr/bin/env php
 <?php
-/**
- * PHPUnit
+/* PHPUnit
  *
- * Copyright (c) 2002-2007, Sebastian Bergmann <sb@sebastian-bergmann.de>.
+ * Copyright (c) 2001-2013, Sebastian Bergmann <sebastian@phpunit.de>.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -33,98 +33,14 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
- *
- * @category   Testing
- * @package    PHPUnit
- * @author     Sebastian Bergmann <sb@sebastian-bergmann.de>
- * @copyright  2002-2007 Sebastian Bergmann <sb@sebastian-bergmann.de>
- * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
- * @version    SVN: $Id$
- * @link       http://pear.php.net/package/PHPUnit
- * @since      File available since Release 1.0.0
  */
 
-require_once 'PHPUnit/TestCase.php';
-require_once 'PHPUnit/TestResult.php';
-require_once 'PHPUnit/TestSuite.php';
+define('PHPUnit_MAIN_METHOD', 'PHPUnit_TextUI_Command::main');
 
-/**
- * PHPUnit runs a TestSuite and returns a TestResult object.
- *
- * Here is an example:
- *
- * <code>
- * <?php
- * require_once 'PHPUnit.php';
- *
- * class MathTest extends PHPUnit_TestCase {
- *     var $fValue1;
- *     var $fValue2;
- *
- *     function MathTest($name) {
- *       $this->PHPUnit_TestCase($name);
- *     }
- *
- *     function setUp() {
- *       $this->fValue1 = 2;
- *       $this->fValue2 = 3;
- *     }
- *
- *     function testAdd() {
- *       $this->assertTrue($this->fValue1 + $this->fValue2 == 5);
- *     }
- * }
- *
- * $suite = new PHPUnit_TestSuite();
- * $suite->addTest(new MathTest('testAdd'));
- *
- * $result = PHPUnit::run($suite);
- * print $result->toHTML();
- * ?>
- * </code>
- *
- * Alternatively, you can pass a class name to the PHPUnit_TestSuite()
- * constructor and let it automatically add all methods of that class
- * that start with 'test' to the suite:
- *
- * <code>
- * <?php
- * $suite  = new PHPUnit_TestSuite('MathTest');
- * $result = PHPUnit::run($suite);
- * print $result->toHTML();
- * ?>
- * </code>
- *
- * @category   Testing
- * @package    PHPUnit
- * @author     Sebastian Bergmann <sb@sebastian-bergmann.de>
- * @copyright  2002-2007 Sebastian Bergmann <sb@sebastian-bergmann.de>
- * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
- * @version    Release: @package_version@
- * @link       http://pear.php.net/package/PHPUnit
- * @since      Class available since Release 1.0.0
- */
-class PHPUnit {
-    /**
-     * Runs a test(suite).
-     *
-     * @param  mixed
-     * @return PHPUnit_TestResult
-     * @access public
-     */
-    function &run(&$suite) {
-        $result = new PHPUnit_TestResult();
-        $suite->run($result);
-
-        return $result;
-    }
+if (strpos('@php_bin@', '@php_bin') === 0) {
+    require dirname(__FILE__) . DIRECTORY_SEPARATOR . 'PHPUnit' . DIRECTORY_SEPARATOR . 'Autoload.php';
+} else {
+    require '@php_dir@' . DIRECTORY_SEPARATOR . 'PHPUnit' . DIRECTORY_SEPARATOR . 'Autoload.php';
 }
 
-/*
- * Local variables:
- * tab-width: 4
- * c-basic-offset: 4
- * c-hanging-comment-ender-p: nil
- * End:
- */
-?>
+PHPUnit_TextUI_Command::main();
