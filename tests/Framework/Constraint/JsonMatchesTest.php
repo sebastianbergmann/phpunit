de994b125b5e4430c2b9cab33e18a9f5563aaf27
Kalen Petersen <kalenp@gmail.com>
2015-02-26 19:02:40 -0800

    Make JsonMatches constraint stricter
    The JsonMatches constraint considered the following strings equal.
    { "number": 5 }
    { "number": "5" }
    
    These are different objects in JSON, because the values have different
    basic types.  Change from using a simple (==) comparison of objects in
    PHP to a strategy where the JSON is canonicalized and then the
    reformatted strings are compared.

--- tests/Framework/Constraint/JsonMatchesTest.php
+++ tests/Framework/Constraint/JsonMatchesTest.php
@@ -32,10 +32,20 @@ class Framework_Constraint_JsonMatchesTest extends TestCase
     public static function evaluateDataprovider()
     {
         return [
-            'valid JSON'                          => [true, json_encode(['Mascott'                           => 'Tux']), json_encode(['Mascott'                           => 'Tux'])],
-            'error syntax'                        => [false, '{"Mascott"::}', json_encode(['Mascott'         => 'Tux'])],
-            'error UTF-8'                         => [false, json_encode('\xB1\x31'), json_encode(['Mascott' => 'Tux'])],
-            'invalid JSON in class instantiation' => [false, json_encode(['Mascott'                          => 'Tux']), '{"Mascott"::}'],
+            'valid JSON'                              => [true, json_encode(['Mascott'                           => 'Tux']), json_encode(['Mascott'                           => 'Tux'])],
+            'error syntax'                            => [false, '{"Mascott"::}', json_encode(['Mascott'         => 'Tux'])],
+            'error UTF-8'                             => [false, json_encode('\xB1\x31'), json_encode(['Mascott' => 'Tux'])],
+            'invalid JSON in class instantiation'     => [false, json_encode(['Mascott'                          => 'Tux']), '{"Mascott"::}'],
+            'string type not equals number'           => [false, '{"age": "5"}', '{"age": 5}'],
+            'string type not equals boolean'          => [false, '{"age": "true"}', '{"age": true}'],
+            'string type not equals null'             => [false, '{"age": "null"}', '{"age": null}'],
+            'object fields are unordered'             => [true, '{"first":1, "second":"2"}', '{"second":"2", "first":1}'],
+            'child object fields are unordered'       => [true, '{"Mascott": {"name":"Tux", "age":5}}', '{"Mascott": {"age":5, "name":"Tux"}}'],
+            'null field different from missing field' => [false, '{"present": true, "missing": null}', '{"present": true}'],
+            'array elements are ordered'              => [false, '["first", "second"]', '["second", "first"]'],
+            'single boolean valid json'               => [true, 'true', 'true'],
+            'single number valid json'                => [true, '5.3', '5.3'],
+            'single null valid json'                  => [true, 'null', 'null'],
         ];
     }
 }
