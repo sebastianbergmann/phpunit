7b5fe98b28302a8b25693b2298bca74463336975
Sebastian Bergmann <sb@sebastian-bergmann.de>
2015-06-03 07:03:30 +0200

    Prepare release

--- src/Runner/Version.php
+++ src/Runner/Version.php
@@ -36,7 +36,7 @@ class PHPUnit_Runner_Version
         }
 
         if (self::$version === null) {
-            $version = new SebastianBergmann\Version('4.6.9', dirname(dirname(__DIR__)));
+            $version = new SebastianBergmann\Version('4.6.10', dirname(dirname(__DIR__)));
             self::$version = $version->getVersion();
         }
 
