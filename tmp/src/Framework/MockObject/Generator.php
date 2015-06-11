c07b9dd3321b8f7fff0f168d8c484e85f078851e
Sebastian Bergmann <sb@sebastian-bergmann.de>
2015-06-11 17:55:48 +0200

    Closes #233

--- tmp/src/Framework/MockObject/Generator.php
+++ tmp/src/Framework/MockObject/Generator.php
@@ -1053,7 +1053,9 @@ class PHPUnit_Framework_MockObject_Generator
             $typeHint  = '';
 
             if (!$forCall) {
-                if ($parameter->isArray()) {
+                if ($this->hasType($parameter)) {
+                    $typeHint = (string) $parameter->getType() . ' ';
+                } elseif ($parameter->isArray()) {
                     $typeHint = 'array ';
                 } elseif ((defined('HHVM_VERSION') || version_compare(PHP_VERSION, '5.4.0', '>='))
                           && $parameter->isCallable()) {
@@ -1107,6 +1109,16 @@ class PHPUnit_Framework_MockObject_Generator
     private function isVariadic(ReflectionParameter $parameter)
     {
         return method_exists('ReflectionParameter', 'isVariadic') && $parameter->isVariadic();
+    }
+
+    /**
+     * @param  ReflectionParameter $parameter
+     * @return boolean
+     * @since  Method available since Release 2.3.4
+     */
+    private function hasType(ReflectionParameter $parameter)
+    {
+        return method_exists('ReflectionParameter', 'hasType') && $parameter->hasType();
     }
 
     /**
