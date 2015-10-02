ff2b20af9c57c61c9d58a16a34e81127cb8a8795
Sebastian Bergmann <sb@sebastian-bergmann.de>
2015-10-02 07:43:43 +0200

    Fix CS/WS issue

--- src/Util/XML.php
+++ src/Util/XML.php
@@ -100,7 +100,7 @@ class PHPUnit_Util_XML
             @chdir(dirname($filename));
         }
 
-        $document = new DOMDocument;
+        $document                     = new DOMDocument;
         $document->preserveWhiteSpace = false;
 
         $internal  = libxml_use_internal_errors(true);
