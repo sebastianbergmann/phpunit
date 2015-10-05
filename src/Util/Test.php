475a73de2fff9feb9ea09b135009ad2c03d78575
David Stockton <dave@davidstockton.com>
2015-10-04 18:56:14 -0600

    Only use the first word after @use or @covers
    Attempt to fix #1857

--- src/Util/Test.php
+++ src/Util/Test.php
@@ -148,6 +148,8 @@ class PHPUnit_Util_Test
 
             $element = preg_replace('/[\s()]+$/', '', $element);
 
+            $element = explode(' ', $element)[0];
+
             $codeList = array_merge(
                 $codeList,
                 self::resolveElementToReflectionObjects($element)
