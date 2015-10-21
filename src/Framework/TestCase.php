a4513c4864856627b0193bdc0b2e00f0b7a70331
Sebastian Bergmann <sb@sebastian-bergmann.de>
2015-10-21 10:48:49 +0200

    Properly restore the backup of global state when changes to global state are disallowed

--- src/Framework/TestCase.php
+++ src/Framework/TestCase.php
@@ -2058,10 +2058,16 @@ abstract class PHPUnit_Framework_TestCase extends PHPUnit_Framework_Assert imple
         $backupGlobals = $this->backupGlobals === null || $this->backupGlobals === true;
 
         if ($this->disallowChangesToGlobalState) {
-            $this->compareGlobalStateSnapshots(
-                $this->snapshot,
-                $this->createGlobalStateSnapshot($backupGlobals)
-            );
+            try {
+                $this->compareGlobalStateSnapshots(
+                    $this->snapshot,
+                    $this->createGlobalStateSnapshot($backupGlobals)
+                );
+            }
+
+            catch (PHPUnit_Framework_RiskyTestError $rte) {
+                // Intentionally left empty
+            }
         }
 
         $restorer = new Restorer;
@@ -2075,6 +2081,10 @@ abstract class PHPUnit_Framework_TestCase extends PHPUnit_Framework_Assert imple
         }
 
         $this->snapshot = null;
+
+        if (isset($rte)) {
+            throw $rte;
+        }
     }
 
     /**
