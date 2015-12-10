39bb51bdcbd743df8e03a353870f125349394a30
Evgeniy Makhrov <e.makhrov@corp.badoo.com>
2015-12-11 00:07:08 +0300

    Reset previous handler before processing option

--- src/TextUI/Command.php
+++ src/TextUI/Command.php
@@ -644,6 +644,7 @@ class PHPUnit_TextUI_Command
                 default:
                     $optionName = str_replace('--', '', $option[0]);
 
+                    $handler = null;
                     if (isset($this->longOptions[$optionName])) {
                         $handler = $this->longOptions[$optionName];
                     } elseif (isset($this->longOptions[$optionName . '='])) {
