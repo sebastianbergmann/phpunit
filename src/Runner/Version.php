ddf220492ec8346cce03e41a562b8beda094bef4
Sebastian Bergmann <sebastian@thephp.cc>
2016-01-12 10:07:53 +0100

    Fix bug

--- src/Runner/Version.php
+++ src/Runner/Version.php
@@ -44,6 +44,12 @@ class PHPUnit_Runner_Version
      */
     public static function series()
     {
+        if (strpos(self::id(), '-')) {
+            $tmp = explode('-', self::id());
+
+            return $tmp[0];
+        }
+
         return implode('.', array_slice(explode('.', self::id()), 0, 2));
     }
 
