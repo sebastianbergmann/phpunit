72bd9971fe4b771ff6463d6f2df0eb995732e2e8
Sebastian Bergmann <Sebastian Bergmann sb@sebastian-bergmann.de>
2016-02-22 15:54:42 -0500

    Closes #2039

--- tests/Util/TestDox/NamePrettifierTest.php
+++ tests/Util/TestDox/NamePrettifierTest.php
@@ -64,7 +64,7 @@ class Util_TestDox_NamePrettifierTest extends PHPUnit_Framework_TestCase
     {
         $this->assertEquals('This is a test', $this->namePrettifier->prettifyTestMethod('testThisIsATest'));
         $this->assertEquals('This is a test', $this->namePrettifier->prettifyTestMethod('testThisIsATest2'));
-        $this->assertEquals('this is a test', $this->namePrettifier->prettifyTestMethod('this_is_a_test'));
+        $this->assertEquals('This is a test', $this->namePrettifier->prettifyTestMethod('this_is_a_test'));
         $this->assertEquals('Foo for bar is 0', $this->namePrettifier->prettifyTestMethod('testFooForBarIs0'));
         $this->assertEquals('Foo for baz is 1', $this->namePrettifier->prettifyTestMethod('testFooForBazIs1'));
     }
