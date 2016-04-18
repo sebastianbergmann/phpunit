345863118d03c35e6a412b43dcad094f97f9198e
Lars Strojny <lars@strojny.net>
2016-04-18 23:47:18 +0200

    Always unconditionally use skipped syntax

--- src/Util/Log/JUnit.php
+++ src/Util/Log/JUnit.php
@@ -168,14 +168,10 @@ class PHPUnit_Util_Log_JUnit extends PHPUnit_Util_Printer implements PHPUnit_Fra
      */
     public function addIncompleteTest(PHPUnit_Framework_Test $test, Exception $e, $time)
     {
-        if ($this->logIncompleteSkipped && $this->currentTestCase !== null) {
-            $skipped = $this->document->createElement('skipped');
-            $this->currentTestCase->appendChild($skipped);
+        $skipped = $this->document->createElement('skipped');
+        $this->currentTestCase->appendChild($skipped);
 
-            $this->testSuiteSkipped[$this->testSuiteLevel]++;
-        } else {
-            $this->attachCurrentTestCase = false;
-        }
+        $this->testSuiteSkipped[$this->testSuiteLevel]++;
     }
 
     /**
@@ -219,14 +215,10 @@ class PHPUnit_Util_Log_JUnit extends PHPUnit_Util_Printer implements PHPUnit_Fra
      */
     public function addSkippedTest(PHPUnit_Framework_Test $test, Exception $e, $time)
     {
-        if ($this->logIncompleteSkipped && $this->currentTestCase !== null) {
-            $skipped = $this->document->createElement('skipped');
-            $this->currentTestCase->appendChild($skipped);
+        $skipped = $this->document->createElement('skipped');
+        $this->currentTestCase->appendChild($skipped);
 
-            $this->testSuiteSkipped[$this->testSuiteLevel]++;
-        } else {
-            $this->attachCurrentTestCase = false;
-        }
+        $this->testSuiteSkipped[$this->testSuiteLevel]++;
     }
 
     /**
