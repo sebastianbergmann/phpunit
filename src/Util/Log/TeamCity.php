82ff8791430881a2b50af124278a17db84688fec
Philipp Kolesnikov <kolesnikov@farpost.com>
2016-04-27 14:41:54 +1000

    Add type parameter to Teamcity service message

--- src/Util/Log/TeamCity.php
+++ src/Util/Log/TeamCity.php
@@ -121,6 +121,7 @@ class PHPUnit_Util_Log_TeamCity extends PHPUnit_TextUI_ResultPrinter
                 }
 
                 if (!is_null($actualString) && !is_null($expectedString)) {
+                    $parameters['type']     = 'comparisonFailure';
                     $parameters['actual']   = $actualString;
                     $parameters['expected'] = $expectedString;
                 }
