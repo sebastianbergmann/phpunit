5d48b82a329ea1584590c51f20a78eadaae9bad0
Matthieu Napoli <matthieu@mnapoli.fr>
2016-06-04 13:43:14 +0200

    Add tests for TestCase::createMock()

---
+++ tests/_files/Mockable.php
@@ -0,0 +1,21 @@
+<?php
+class Mockable
+{
+    public $constructorCalled = false;
+    public $cloned = false;
+
+    public function __construct()
+    {
+        $this->constructorCalled = false;
+    }
+
+    public function foo()
+    {
+        return true;
+    }
+
+    public function __clone()
+    {
+        $this->cloned = true;
+    }
+}
