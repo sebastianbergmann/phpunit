0a9df0905cda19a27dd547a601f55b579539ef3f
Sebastian Bergmann <sb@sebastian-bergmann.de>
2016-06-17 12:29:27 +0200

    Closes #2221

--- src/Framework/TestCase.php
+++ src/Framework/TestCase.php
@@ -134,7 +134,7 @@ abstract class PHPUnit_Framework_TestCase extends PHPUnit_Framework_Assert imple
     /**
      * The name of the expected Exception.
      *
-     * @var mixed
+     * @var string
      */
     private $expectedException = null;
 
@@ -574,12 +574,16 @@ abstract class PHPUnit_Framework_TestCase extends PHPUnit_Framework_Assert imple
     }
 
     /**
-     * @param mixed $exception
+     * @param string $exception
      *
      * @since Method available since Release 5.2.0
      */
     public function expectException($exception)
     {
+        if (!is_string($exception)) {
+            throw PHPUnit_Util_InvalidArgumentHelper::factory(1, 'string');
+        }
+
         $this->expectedException = $exception;
     }
 
