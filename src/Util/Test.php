884f5fe5f3c4a1ea4598848965ee6ae6c998ee82
Sebastian Bergmann <sb@sebastian-bergmann.de>
2016-06-17 17:16:08 +0200

    Fix CS/WS issues

--- src/Util/Test.php
+++ src/Util/Test.php
@@ -467,18 +467,24 @@ class PHPUnit_Util_Test
             $offset            = strlen($matches[0][0]) + $matches[0][1];
             $annotationContent = substr($docComment, $offset);
             $data              = array();
+
             foreach (explode("\n", $annotationContent) as $candidateRow) {
                 $candidateRow = trim($candidateRow);
+
                 if ($candidateRow[0] !== '[') {
                     break;
                 }
+
                 $dataSet      = json_decode($candidateRow, true);
+
                 if (json_last_error() != JSON_ERROR_NONE) {
                     $error = function_exists('json_last_error_msg') ? json_last_error_msg() : json_last_error();
+
                     throw new PHPUnit_Framework_Exception(
-                        'The dataset for the @testWith annotation cannot be parsed: '.$error
+                        'The dataset for the @testWith annotation cannot be parsed: ' . $error
                     );
                 }
+
                 $data[] = $dataSet;
             }
 
