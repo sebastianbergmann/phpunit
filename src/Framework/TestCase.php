326a479c5407546a99af7c381b8f12905363d1ef
Robotic-Brain <github@roboticbrain.de>
2016-10-17 10:40:32 +0200

    Adding missing startTest and endTest call for Tests which are skipped because of a failed dependency

--- src/Framework/TestCase.php
+++ src/Framework/TestCase.php
@@ -2200,6 +2200,7 @@ abstract class PHPUnit_Framework_TestCase extends PHPUnit_Framework_Assert imple
                 }
 
                 if (!isset($passedKeys[$dependency])) {
+                    $this->result->startTest($this);
                     $this->result->addError(
                         $this,
                         new PHPUnit_Framework_SkippedTestError(
@@ -2210,6 +2211,7 @@ abstract class PHPUnit_Framework_TestCase extends PHPUnit_Framework_Assert imple
                         ),
                         0
                     );
+                    $this->result->endTest($this, 0);
 
                     return false;
                 }
