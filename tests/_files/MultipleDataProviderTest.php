6dfe6ad7eae24ba1a8fe947827baffaf67984522
Herve Seignole <herve.seignole@groupepvcp.com>
2016-10-26 02:14:05 +0200

    Fix multiple data providers mixed (yield / iterator / array).

--- tests/_files/MultipleDataProviderTest.php
+++ tests/_files/MultipleDataProviderTest.php
@@ -10,6 +10,15 @@ class MultipleDataProviderTest extends PHPUnit_Framework_TestCase
     {
     }
 
+    /**
+     * @dataProvider providerD
+     * @dataProvider providerE
+     * @dataProvider providerF
+     */
+    public function testTwo()
+    {
+    }
+
     public static function providerA()
     {
         return [
@@ -35,5 +44,32 @@ class MultipleDataProviderTest extends PHPUnit_Framework_TestCase
             [null, null, 'ok'],
             [null, null, 'ok']
         ];
+    }
+
+    public static function providerD()
+    {
+        yield ['ok', null, null];
+        yield ['ok', null, null];
+        yield ['ok', null, null];
+    }
+
+    public static function providerE()
+    {
+        yield [null, 'ok', null];
+        yield [null, 'ok', null];
+        yield [null, 'ok', null];
+    }
+
+    public static function providerF()
+    {
+        $object = new ArrayObject(
+            [
+                [null, null, 'ok'],
+                [null, null, 'ok'],
+                [null, null, 'ok']
+            ]
+        );
+
+        return $object->getIterator();
     }
 }
