dbb62b805fd2a0cb96a17ebb99cc20bd8360a26e
Sebastian Bergmann <sb@sebastian-bergmann.de>
2016-11-21 10:48:37 +0100

    Closes #2366

--- src/Framework/TestCase.php
+++ src/Framework/TestCase.php
@@ -2607,7 +2607,7 @@ abstract class PHPUnit_Framework_TestCase extends PHPUnit_Framework_Assert imple
         } else {
             foreach ($testArguments as $testArgument) {
                 if ($testArgument instanceof PHPUnit_Framework_MockObject_MockObject) {
-                    $this->registerMockObject($testArgument);
+                    $this->registerMockObject(clone $testArgument);
                 } elseif (is_array($testArgument)) {
                     $this->registerMockObjectsFromTestArguments($testArgument);
                 }
