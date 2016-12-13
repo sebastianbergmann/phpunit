3ad5426acb96617b29fb9bb56301a23bd47a33be
Sebastian Bergmann <sb@sebastian-bergmann.de>
2016-12-13 10:21:03 +0100

    Do not treat `AssertionError` as assertion failure on PHP 5

--- src/Framework/TestResult.php
+++ src/Framework/TestResult.php
@@ -716,27 +716,30 @@ class PHPUnit_Framework_TestResult implements Countable
             } elseif ($e instanceof PHPUnit_Framework_SkippedTestError) {
                 $skipped = true;
             }
-        } catch (AssertionError $e) {
-            $test->addToAssertionCount(1);
-
-            $failure = true;
-            $frame   = $e->getTrace()[0];
-
-            $e = new PHPUnit_Framework_AssertionFailedError(
-                sprintf(
-                    '%s in %s:%s',
-                    $e->getMessage(),
-                    $frame['file'],
-                    $frame['line']
-                )
-            );
         } catch (PHPUnit_Framework_Warning $e) {
             $warning = true;
         } catch (PHPUnit_Framework_Exception $e) {
             $error = true;
         } catch (Throwable $e) {
-            $e     = new PHPUnit_Framework_ExceptionWrapper($e);
-            $error = true;
+            // @see https://github.com/sebastianbergmann/phpunit/issues/2394
+            if (PHP_MAJOR_VERSION === 7 && $e instanceof \AssertionError) {
+                $test->addToAssertionCount(1);
+
+                $failure = true;
+                $frame   = $e->getTrace()[0];
+
+                $e = new PHPUnit_Framework_AssertionFailedError(
+                    sprintf(
+                        '%s in %s:%s',
+                        $e->getMessage(),
+                        $frame['file'],
+                        $frame['line']
+                    )
+                );
+            } else {
+                $e     = new PHPUnit_Framework_ExceptionWrapper($e);
+                $error = true;
+            }
         } catch (Exception $e) {
             $e     = new PHPUnit_Framework_ExceptionWrapper($e);
             $error = true;
