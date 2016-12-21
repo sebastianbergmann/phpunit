9ed6db2110097862f5ee3f3f1db2105991b103c3
Miguel Florido <Miguel.Florido@trivago.com>
2016-12-21 17:01:41 +0100

    #2353 Correcting PHPDocs of the copyright

--- src/Util/GlobalVar/Factory.php
+++ src/Util/GlobalVar/Factory.php
@@ -2,7 +2,7 @@
 /*
  * This file is part of PHPUnit.
  *
- * (c) Miguel Florido <miguel5fv@gmail.com>
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
--- src/Util/GlobalVar/Handler.php
+++ src/Util/GlobalVar/Handler.php
@@ -2,12 +2,11 @@
 /*
  * This file is part of PHPUnit.
  *
- * (c) Miguel Florido <miguel5fv@gmail.com>
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 /**
  * Defines the contract of the global variables handler classes.
  */
