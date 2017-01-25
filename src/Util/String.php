64819a546c1ef1c74bafb54b55c26ab6c9e0d10d
Sebastian Bergmann <sb@sebastian-bergmann.de>
2017-01-25 11:56:32 +0000

    mbstring is listed as a required dependency

--- src/Util/String.php
+++ src/Util/String.php
@@ -24,15 +24,7 @@ class PHPUnit_Util_String
      */
     public static function convertToUtf8($string)
     {
-        if (!self::isUtf8($string)) {
-            if (function_exists('mb_convert_encoding')) {
-                $string = mb_convert_encoding($string, 'UTF-8');
-            } else {
-                $string = utf8_encode($string);
-            }
-        }
-
-        return $string;
+        return mb_convert_encoding($string, 'UTF-8');
     }
 
     /**
