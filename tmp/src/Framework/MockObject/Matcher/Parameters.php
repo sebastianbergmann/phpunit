9a94d44d365ea4d996d93a37163dac332336ff34
Sebastian Bergmann <sb@sebastian-bergmann.de>
2017-01-30 18:25:49 +0100

    Update license header

--- tmp/src/Framework/MockObject/Matcher/Parameters.php
+++ tmp/src/Framework/MockObject/Matcher/Parameters.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * This file is part of the PHPUnit_MockObject package.
+ * This file is part of the phpunit-mock-objects package.
  *
  * (c) Sebastian Bergmann <sebastian@phpunit.de>
  *
--- tmp/tests/GeneratorTest.php
+++ tmp/tests/GeneratorTest.php
@@ -1,4 +1,13 @@
 <?php
+/*
+ * This file is part of the phpunit-mock-objects package.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 class Framework_MockObject_GeneratorTest extends PHPUnit_Framework_TestCase
 {
     /**
