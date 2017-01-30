40ff502a815ad544dc7cd11603163efb3f1c8cfd
Sebastian Bergmann <sb@sebastian-bergmann.de>
2017-01-30 19:25:49 +0200

    Update license header

--- src/Framework/MockObject/Matcher.php
+++ src/Framework/MockObject/Matcher.php
@@ -1,12 +1,13 @@
 <?php
 /*
- * This file is part of the PHPUnit_MockObject package.
+ * This file is part of the phpunit-mock-objects package.
  *
  * (c) Sebastian Bergmann <sebastian@phpunit.de>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
 use PHPUnit\Framework\ExpectationFailedException;
 use PHPUnit\Framework\TestFailure;
 
