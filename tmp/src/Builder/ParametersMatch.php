53d1b3953acb1c01afa4a4fc49b2db96af7f170c
Martin Schröder <mschroeder@github.x-home.org>
2017-02-05 15:16:50 +0100

    Fixed typo in comment

--- tmp/src/Builder/ParametersMatch.php
+++ tmp/src/Builder/ParametersMatch.php
@@ -14,7 +14,7 @@
 interface PHPUnit_Framework_MockObject_Builder_ParametersMatch extends PHPUnit_Framework_MockObject_Builder_Match
 {
     /**
-     * Sets the parameters to match for, each parameter to this funtion will
+     * Sets the parameters to match for, each parameter to this function will
      * be part of match. To perform specific matches or constraints create a
      * new PHPUnit_Framework_Constraint and use it for the parameter.
      * If the parameter value is not a constraint it will use the
