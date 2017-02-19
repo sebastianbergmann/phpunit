4601a46c162eccadffc7d5a6b1f5334df5d99713
Sebastian Bergmann <sb@sebastian-bergmann.de>
2017-02-19 08:25:12 +0100

    Prepare release

--- src/Runner/Version.php
+++ src/Runner/Version.php
@@ -32,7 +32,7 @@ class Version
         }
 
         if (self::$version === null) {
-            $version       = new VersionId('6.0.6', dirname(dirname(__DIR__)));
+            $version       = new VersionId('6.0.7', dirname(dirname(__DIR__)));
             self::$version = $version->getVersion();
         }
 
