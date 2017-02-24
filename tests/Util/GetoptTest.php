2e72e318a819859f5c923d137be3959df5aff766
Carlos C Soto <eclipxe13@gmail.com>
2017-02-24 14:00:43 -0600

    Add more test to cover expected exceptions and behavior

--- tests/Util/GetoptTest.php
+++ tests/Util/GetoptTest.php
@@ -59,4 +59,108 @@ class Util_GetoptTest extends PHPUnit_Framework_TestCase
 
         $this->assertEquals($expected, $actual);
     }
+
+    public function testShortOptionUnrecognizedException()
+    {
+        $args = [
+            'command',
+            'myArgument',
+            '-v',
+        ];
+
+        $this->expectException(PHPUnit\Framework\Exception::class);
+        $this->expectExceptionMessage('unrecognized option -- v');
+
+        Getopt::getopt($args, '');
+    }
+
+    public function testShortOptionRequiresAnArgumentException()
+    {
+        $args = [
+            'command',
+            'myArgument',
+            '-f',
+        ];
+
+        $this->expectException(PHPUnit\Framework\Exception::class);
+        $this->expectExceptionMessage('option requires an argument -- f');
+
+        Getopt::getopt($args, 'f:');
+    }
+
+    public function testShortOptionHandleAnOptionalValue()
+    {
+        $args = [
+            'command',
+            'myArgument',
+            '-f',
+        ];
+        $actual   = Getopt::getopt($args, 'f::');
+        $expected = [
+            [
+                [
+                    'f',
+                    null,
+                ],
+            ],
+            [
+                'myArgument',
+            ],
+        ];
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testLongOptionIsAmbiguousException()
+    {
+        $args = [
+            'command',
+            '--col',
+        ];
+
+        $this->expectException(PHPUnit\Framework\Exception::class);
+        $this->expectExceptionMessage('option --col is ambiguous');
+
+        Getopt::getopt($args, '', ['columns', 'colors']);
+    }
+
+    public function testLongOptionUnrecognizedException()
+    {
+        // the exception 'unrecognized option --option' is not thrown
+        // if the there are not defined extended options
+        $args = [
+            'command',
+            '--foo',
+        ];
+
+        $this->expectException(PHPUnit\Framework\Exception::class);
+        $this->expectExceptionMessage('unrecognized option --foo');
+
+        Getopt::getopt($args, '', ['colors']);
+    }
+
+    public function testLongOptionRequiresAnArgumentException()
+    {
+        $args = [
+            'command',
+            '--foo',
+        ];
+
+        $this->expectException(PHPUnit\Framework\Exception::class);
+        $this->expectExceptionMessage('option --foo requires an argument');
+
+        Getopt::getopt($args, '', ['foo=']);
+    }
+
+    public function testLongOptionDoesNotAllowAnArgumentException()
+    {
+        $args = [
+            'command',
+            '--foo=bar',
+        ];
+
+        $this->expectException(PHPUnit\Framework\Exception::class);
+        $this->expectExceptionMessage("option --foo doesn't allow an argument");
+
+        Getopt::getopt($args, '', ['foo']);
+    }
 }
