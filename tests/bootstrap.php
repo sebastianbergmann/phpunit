695ada4ae1a6b3cd5c9be1c3655cbade7128b92f
Nicole Cordes <typo3@cordes.co>
2017-03-31 18:09:20 +0200

    Use DIRECTORY_SEPARATOR for path comparission
    Using the DIRECTORY_SEPARATOR instead of hard coded forward slashes
    solves a lot of failing tests on Windows systems.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 if (!defined('TEST_FILES_PATH')) {
-    define('TEST_FILES_PATH', __DIR__ . '/_files/');
+    define('TEST_FILES_PATH', __DIR__ . DIRECTORY_SEPARATOR . '_files' . DIRECTORY_SEPARATOR);
 }
 
 ini_set('precision', 14);
