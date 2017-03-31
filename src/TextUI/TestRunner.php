beb46f175bfe7d5109eed246d51deba4ae810b0c
Lee Wilkins <lee.wilkins@kartenmacherei.de>
2017-03-31 16:09:46 +0200

    corrected misspelling

--- src/TextUI/TestRunner.php
+++ src/TextUI/TestRunner.php
@@ -465,7 +465,7 @@ class PHPUnit_TextUI_TestRunner extends PHPUnit_Runner_BaseTestRunner
             }
 
             if (!$this->codeCoverageFilter->hasWhitelist()) {
-                $this->writeMessage('Error', 'Whitelist is improperly configured, no files listed fort testing.');
+                $this->writeMessage('Error', 'Whitelist is improperly configured, no files listed for testing.');
 
                 $codeCoverageReports = 0;
 
