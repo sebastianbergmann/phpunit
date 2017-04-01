e3293596aa36f98c32b8e1128004b85d253e4522
Lee Wilkins <lee.wilkins@kartenmacherei.de>
2017-04-01 15:28:46 +0200

    change the check for exception message from empty() to check for empty string

--- src/Framework/TestCase.php
+++ src/Framework/TestCase.php
@@ -1071,7 +1071,7 @@ abstract class TestCase extends Assert implements Test, SelfDescribing
                 );
 
                 if (\is_string($this->expectedExceptionMessage) &&
-                    !empty($this->expectedExceptionMessage)) {
+                    $this->expectedExceptionMessage == '') {
                     $this->assertThat(
                         $e,
                         new ExceptionMessage(
@@ -1081,7 +1081,7 @@ abstract class TestCase extends Assert implements Test, SelfDescribing
                 }
 
                 if (\is_string($this->expectedExceptionMessageRegExp) &&
-                    !empty($this->expectedExceptionMessageRegExp)) {
+                    $this->expectedExceptionMessageRegExp == '') {
                     $this->assertThat(
                         $e,
                         new ExceptionMessageRegularExpression(
