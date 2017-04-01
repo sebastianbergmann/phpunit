36b62bab4e837c8c0ddfa053fe444050468e0336
Konstantin Koch <korsarNek@googlemail.com>
2017-04-01 18:05:35 +0200

    second fix for #2448

--- src/Util/Fileloader.php
+++ src/Util/Fileloader.php
@@ -30,7 +30,13 @@ class Fileloader
     {
         $includePathFilename = \stream_resolve_include_path($filename);
 
-        if (!$includePathFilename || !\is_readable($includePathFilename)) {
+        //As a fallback, PHP looks in the directory of the file executing the stream_resolve_include_path function.
+        //We don't want to load the Test.php file here, so skip it if it found that.
+        //PHP prioritizes the include_path setting, so if the current directory is in there, it will first look in the
+        //current working directory.
+        $localFile = __DIR__ . DIRECTORY_SEPARATOR . $filename;
+
+        if (!$includePathFilename || !\is_readable($includePathFilename) || $includePathFilename === $localFile) {
             throw new Exception(
                 \sprintf('Cannot open file "%s".' . "\n", $filename)
             );
