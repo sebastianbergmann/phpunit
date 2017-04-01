19dcc6f5e76f13bfe0f5971a8771de8d2b7177db
Sebastian Bergmann <sb@sebastian-bergmann.de>
2017-04-01 15:26:26 +0200

    Revert fix for #2448

--- src/Util/Fileloader.php
+++ src/Util/Fileloader.php
@@ -29,13 +29,8 @@ class Fileloader
     public static function checkAndLoad($filename)
     {
         $includePathFilename = stream_resolve_include_path($filename);
-        //As a fallback, PHP looks in the directory of the file executing the stream_resolve_include_path function.
-        //We don't want to load the Test.php file here, so skip it if it found that.
-        //PHP prioritizes the include_path setting, so if the current directory is in there, it will first look in the
-        //current working directory.
-        $isFile = is_file(__DIR__ . DIRECTORY_SEPARATOR . $filename);
 
-        if (!$includePathFilename || !is_readable($includePathFilename) || $isFile) {
+        if (!$includePathFilename || !is_readable($includePathFilename)) {
             throw new Exception(
                 sprintf('Cannot open file "%s".' . "\n", $filename)
             );
