e9277ff5a5e167382c1dbd39179f490cc4839f46
Jeremy Mikola <jmikola@gmail.com>
2017-04-04 14:21:26 -0400

    Support counting non-Iterator Traversable objects
    Extension classes may implement Traversable without also implementing Iterator or IteratorAggregate.

--- tests/Framework/Constraint/CountTest.php
+++ tests/Framework/Constraint/CountTest.php
@@ -93,4 +93,19 @@ class CountTest extends TestCase
         $countConstraint->evaluate($generator, '', true);
         $this->assertEquals(null, $generator->current());
     }
+
+    public function testCountTraversable()
+    {
+        $countConstraint = new Count(5);
+
+        // DatePeriod is used as an object that is Traversable but does not
+        // implement Iterator or IteratorAggregate. The following ISO 8601
+        // recurring time interval will yield five total DateTime objects.
+        $datePeriod = new \DatePeriod('R4/2017-05-01T00:00:00Z/P1D');
+
+        $this->assertInstanceOf(\Traversable::class, $datePeriod);
+        $this->assertNotInstanceOf(\Iterator::class, $datePeriod);
+        $this->assertNotInstanceOf(\IteratorAggregate::class, $datePeriod);
+        $this->assertTrue($countConstraint->evaluate($datePeriod, '', true));
+    }
 }
