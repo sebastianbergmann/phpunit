7561bbcc058cbe4ae3da73d9727b8ec5426c83e8
Barney Hanlon <barney@shrikeh.net>
2017-10-13 14:35:23 +0200

    Keep `NotHasKey()` tests in `ConstraintTest` for evaluation later

--- tests/Framework/ConstraintTest.php
+++ tests/Framework/ConstraintTest.php
@@ -18,6 +18,58 @@ use PHPUnit\Util\Filter;
 
 class ConstraintTest extends TestCase
 {
+    public function testConstraintArrayNotHasKey()
+    {
+        $constraint = Assert::logicalNot(
+            Assert::arrayHasKey(0)
+        );
+
+        $this->assertFalse($constraint->evaluate([0 => 1], '', true));
+        $this->assertEquals('does not have the key 0', $constraint->toString());
+        $this->assertCount(1, $constraint);
+
+        try {
+            $constraint->evaluate([0 => 1]);
+        } catch (ExpectationFailedException $e) {
+            $this->assertEquals(
+                <<<EOF
+Failed asserting that an array does not have the key 0.
+
+EOF
+                ,
+                TestFailure::exceptionToString($e)
+            );
+
+            return;
+        }
+
+        $this->fail();
+    }
+
+    public function testConstraintArrayNotHasKey2()
+    {
+        $constraint = Assert::logicalNot(
+            Assert::arrayHasKey(0)
+        );
+
+        try {
+            $constraint->evaluate([0], 'custom message');
+        } catch (ExpectationFailedException $e) {
+            $this->assertEquals(
+                <<<EOF
+custom message
+Failed asserting that an array does not have the key 0.
+
+EOF
+                ,
+                TestFailure::exceptionToString($e)
+            );
+
+            return;
+        }
+
+        $this->fail();
+    }
 
     public function testConstraintIsReadable()
     {
