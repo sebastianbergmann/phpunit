29fb96edc65bfcc3511fafe73829720293ea3529
Andreas Möller <am@localheinz.com>
2017-10-14 12:17:54 +0200

    Enhancement: Extract IsEmptyTest

--- tests/Framework/ConstraintTest.php
+++ tests/Framework/ConstraintTest.php
@@ -1460,57 +1460,6 @@ EOF
         $this->fail();
     }
 
-    public function testConstraintIsEmpty()
-    {
-        $constraint = new IsEmpty;
-
-        $this->assertFalse($constraint->evaluate(['foo'], '', true));
-        $this->assertTrue($constraint->evaluate([], '', true));
-        $this->assertFalse($constraint->evaluate(new \ArrayObject(['foo']), '', true));
-        $this->assertTrue($constraint->evaluate(new \ArrayObject([]), '', true));
-        $this->assertEquals('is empty', $constraint->toString());
-        $this->assertCount(1, $constraint);
-
-        try {
-            $constraint->evaluate(['foo']);
-        } catch (ExpectationFailedException $e) {
-            $this->assertEquals(
-                <<<EOF
-Failed asserting that an array is empty.
-
-EOF
-                ,
-                TestFailure::exceptionToString($e)
-            );
-
-            return;
-        }
-
-        $this->fail();
-    }
-
-    public function testConstraintIsEmpty2()
-    {
-        $constraint = new IsEmpty;
-
-        try {
-            $constraint->evaluate(['foo'], 'custom message');
-        } catch (ExpectationFailedException $e) {
-            $this->assertEquals(
-                <<<EOF
-custom message\nFailed asserting that an array is empty.
-
-EOF
-                ,
-                TestFailure::exceptionToString($e)
-            );
-
-            return;
-        }
-
-        $this->fail();
-    }
-
     public function testConstraintCountWithAnArray()
     {
         $constraint = new Count(5);
