38de40fe3afe0fc1f8509fe47f923c71a019ca4e
bscheshirwork <bscheshir.work@gmail.com>
2017-12-07 18:04:57 +0300

    add some tests

---
+++ tests/_files/CustomPrinter2.php
@@ -0,0 +1,21 @@
+<?php
+use PHPUnit\TextUI\ResultPrinter;
+
+class CustomPrinter2 extends ResultPrinter
+{
+    public function __construct(
+        $out = null,
+        $verbose = false,
+        $colors = self::COLOR_DEFAULT,
+        $debug = false,
+        $numberOfColumns = 80,
+        $reverse = false
+    ) {
+        if (!defined('STDOUT')) {
+            define('STDOUT', fopen('php://stdout', 'w'));
+        }
+
+        parent::__construct(STDOUT, $verbose, $colors, $debug, $numberOfColumns, $reverse);
+    }
+
+}
