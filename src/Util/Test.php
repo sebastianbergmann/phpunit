3b0dfb5242567b50353bd13188afa5dceceff398
Sebastian Bergmann <sb@sebastian-bergmann.de>
2017-12-15 15:10:55 +0100

    Add type annotations

--- src/Util/Test.php
+++ src/Util/Test.php
@@ -31,22 +31,72 @@ use Traversable;
  */
 class Test
 {
+    /**
+     * @var int
+     */
     public const UNKNOWN = -1;
-    public const SMALL   = 0;
-    public const MEDIUM  = 1;
-    public const LARGE   = 2;
 
-    /** TODO: This constant should be private (it's public because of TestTest::testGetProvidedDataRegEx) */
-    public const REGEX_DATA_PROVIDER                = '/@dataProvider\s+([a-zA-Z0-9._:-\\\\x7f-\xff]+)/';
-    private const REGEX_TEST_WITH                   = '/@testWith\s+/';
-    private const REGEX_EXPECTED_EXCEPTION          = '(@expectedException\s+([:.\w\\\\x7f-\xff]+)(?:[\t ]+(\S*))?(?:[\t ]+(\S*))?\s*$)m';
-    private const REGEX_REQUIRES_VERSION            = '/@requires\s+(?P<name>PHP(?:Unit)?)\s+(?P<operator>[<>=!]{0,2})\s*(?P<version>[\d\.-]+(dev|(RC|alpha|beta)[\d\.])?)[ \t]*\r?$/m';
+    /**
+     * @var int
+     */
+    public const SMALL = 0;
+
+    /**
+     * @var int
+     */
+    public const MEDIUM = 1;
+
+    /**
+     * @var int
+     */
+    public const LARGE = 2;
+
+    /**
+     * @var string
+     *
+     * @todo This constant should be private (it's public because of TestTest::testGetProvidedDataRegEx)
+     */
+    public const REGEX_DATA_PROVIDER = '/@dataProvider\s+([a-zA-Z0-9._:-\\\\x7f-\xff]+)/';
+
+    /**
+     * @var string
+     */
+    private const REGEX_TEST_WITH = '/@testWith\s+/';
+
+    /**
+     * @var string
+     */
+    private const REGEX_EXPECTED_EXCEPTION = '(@expectedException\s+([:.\w\\\\x7f-\xff]+)(?:[\t ]+(\S*))?(?:[\t ]+(\S*))?\s*$)m';
+
+    /**
+     * @var string
+     */
+    private const REGEX_REQUIRES_VERSION = '/@requires\s+(?P<name>PHP(?:Unit)?)\s+(?P<operator>[<>=!]{0,2})\s*(?P<version>[\d\.-]+(dev|(RC|alpha|beta)[\d\.])?)[ \t]*\r?$/m';
+
+    /**
+     * @var string
+     */
     private const REGEX_REQUIRES_VERSION_CONSTRAINT = '/@requires\s+(?P<name>PHP(?:Unit)?)\s+(?P<constraint>[\d\t -.|~^]+)[ \t]*\r?$/m';
-    private const REGEX_REQUIRES_OS                 = '/@requires\s+(?P<name>OS(?:FAMILY)?)\s+(?P<value>.+?)[ \t]*\r?$/m';
-    private const REGEX_REQUIRES                    = '/@requires\s+(?P<name>function|extension)\s+(?P<value>([^ ]+?))\s*(?P<operator>[<>=!]{0,2})\s*(?P<version>[\d\.-]+[\d\.]?)?[ \t]*\r?$/m';
 
+    /**
+     * @var string
+     */
+    private const REGEX_REQUIRES_OS = '/@requires\s+(?P<name>OS(?:FAMILY)?)\s+(?P<value>.+?)[ \t]*\r?$/m';
+
+    /**
+     * @var string
+     */
+    private const REGEX_REQUIRES = '/@requires\s+(?P<name>function|extension)\s+(?P<value>([^ ]+?))\s*(?P<operator>[<>=!]{0,2})\s*(?P<version>[\d\.-]+[\d\.]?)?[ \t]*\r?$/m';
+
+    /**
+     * @var array
+     */
     private static $annotationCache = [];
-    private static $hookMethods     = [];
+
+    /**
+     * @var array
+     */
+    private static $hookMethods = [];
 
     public static function describe(\PHPUnit\Framework\Test $test): array
     {
