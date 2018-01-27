sebastianbergmann/phpunit

commit 3a9494be34135e5d42964ec7f5f17605e736a2c7

Fix CS/WS issues

src/Util/Configuration.php
@@ -162,6 +162,8 @@ class Configuration
      * Loads a PHPUnit configuration file.
      *
      * @param string $filename
+     *
+     * @throws Exception
      */
     protected function __construct($filename)
     {
@@ -179,13 +181,15 @@ class Configuration
      *
      * @param string $filename
      *
+     * @throws Exception
+     *
      * @return Configuration
      */
     public static function getInstance($filename): self
     {
-        $realpath = \realpath($filename);
+        $realPath = \realpath($filename);
 
-        if ($realpath === false) {
+        if ($realPath === false) {
             throw new Exception(
                 \sprintf(
                     'Could not read "%s".',
@@ -194,15 +198,16 @@ class Configuration
             );
         }
 
-        if (!isset(self::$instances[$realpath])) {
-            self::$instances[$realpath] = new self($realpath);
+        /** @var string $realPath */
+        if (!isset(self::$instances[$realPath])) {
+            self::$instances[$realPath] = new self($realPath);
         }
 
-        return self::$instances[$realpath];
+        return self::$instances[$realPath];
     }
 
     /**
-     * Returns the realpath to the configuration file.
+     * Returns the real path to the configuration file.
      *
      * @return string
      */
@@ -309,6 +314,7 @@ class Configuration
         $result = [];
 
         foreach ($this->xpath->query('listeners/listener') as $listener) {
+            /** @var DOMElement $listener */
             $class     = (string) $listener->getAttribute('class');
             $file      = '';
             $arguments = [];
@@ -321,15 +327,23 @@ class Configuration
             }
 
             foreach ($listener->childNodes as $node) {
-                if ($node instanceof DOMElement && $node->tagName == 'arguments') {
-                    foreach ($node->childNodes as $argument) {
-                        if ($argument instanceof DOMElement) {
-                            if ($argument->tagName == 'file' || $argument->tagName == 'directory') {
-                                $arguments[] = $this->toAbsolutePath((string) $argument->textContent);
-                            } else {
-                                $arguments[] = Xml::xmlToVariable($argument);
-                            }
-                        }
+                if (!$node instanceof DOMElement) {
+                    continue;
+                }
+
+                if (!$node->tagName === 'arguments') {
+                    continue;
+                }
+
+                foreach ($node->childNodes as $argument) {
+                    if (!$argument instanceof DOMElement) {
+                        continue;
+                    }
+
+                    if ($argument->tagName === 'file' || $argument->tagName === 'directory') {
+                        $arguments[] = $this->toAbsolutePath((string) $argument->textContent);
+                    } else {
+                        $arguments[] = Xml::xmlToVariable($argument);
                     }
                 }
             }
@@ -354,6 +368,7 @@ class Configuration
         $result = [];
 
         foreach ($this->xpath->query('logging/log') as $log) {
+            /** @var DOMElement $log */
             $type   = (string) $log->getAttribute('type');
             $target = (string) $log->getAttribute('target');
 
@@ -363,7 +378,7 @@ class Configuration
 
             $target = $this->toAbsolutePath($target);
 
-            if ($type == 'coverage-html') {
+            if ($type === 'coverage-html') {
                 if ($log->hasAttribute('lowUpperBound')) {
                     $result['lowUpperBound'] = $this->getInteger(
                         (string) $log->getAttribute('lowUpperBound'),
@@ -377,14 +392,14 @@ class Configuration
                         90
                     );
                 }
-            } elseif ($type == 'coverage-crap4j') {
+            } elseif ($type === 'coverage-crap4j') {
                 if ($log->hasAttribute('threshold')) {
                     $result['crap4jThreshold'] = $this->getInteger(
                         (string) $log->getAttribute('threshold'),
                         30
                     );
                 }
-            } elseif ($type == 'coverage-text') {
+            } elseif ($type === 'coverage-text') {
                 if ($log->hasAttribute('showUncoveredFiles')) {
                     $result['coverageTextShowUncoveredFiles'] = $this->getBoolean(
                         (string) $log->getAttribute('showUncoveredFiles'),
@@ -435,6 +450,7 @@ class Configuration
         }
 
         foreach ($this->xpath->query('php/ini') as $ini) {
+            /** @var DOMElement $ini */
             $name  = (string) $ini->getAttribute('name');
             $value = (string) $ini->getAttribute('value');
 
@@ -442,6 +458,7 @@ class Configuration
         }
 
         foreach ($this->xpath->query('php/const') as $const) {
+            /** @var DOMElement $const */
             $name  = (string) $const->getAttribute('name');
             $value = (string) $const->getAttribute('value');
 
@@ -450,6 +467,7 @@ class Configuration
 
         foreach (['var', 'env', 'post', 'get', 'cookie', 'server', 'files', 'request'] as $array) {
             foreach ($this->xpath->query('php/' . $array) as $var) {
+                /** @var DOMElement $var */
                 $name     = (string) $var->getAttribute('name');
                 $value    = (string) $var->getAttribute('value');
                 $verbatim = false;
@@ -510,7 +528,9 @@ class Configuration
         }
 
         foreach (['var', 'post', 'get', 'cookie', 'server', 'files', 'request'] as $array) {
-            // See https://github.com/sebastianbergmann/phpunit/issues/277
+            /*
+             * @see https://github.com/sebastianbergmann/phpunit/issues/277
+             */
             switch ($array) {
                 case 'var':
                     $target = &$GLOBALS;
@@ -537,7 +557,7 @@ class Configuration
             $value = $data['value'];
             $force = $data['force'] ?? false;
 
-            if ($force || false === \getenv($name)) {
+            if ($force || \getenv($name) === false) {
                 \putenv("{$name}={$value}");
             }
 
@@ -545,7 +565,7 @@ class Configuration
                 $_ENV[$name] = $value;
             }
 
-            if (true === $force) {
+            if ($force === true) {
                 $_ENV[$name] = $value;
             }
         }
@@ -571,7 +591,7 @@ class Configuration
         if ($root->hasAttribute('columns')) {
             $columns = (string) $root->getAttribute('columns');
 
-            if ($columns == 'max') {
+            if ($columns === 'max') {
                 $result['columns'] = 'max';
             } else {
                 $result['columns'] = $this->getInteger($columns, 80);
@@ -589,7 +609,7 @@ class Configuration
         }
 
         /*
-         * Issue #657
+         * @see https://github.com/sebastianbergmann/phpunit/issues/657
          */
         if ($root->hasAttribute('stderr')) {
             $result['stderr'] = $this->getBoolean(
@@ -867,21 +887,22 @@ class Configuration
      *
      * @param null|string $testSuiteFilter
      *
+     * @throws Exception
+     *
      * @return TestSuite
      */
     public function getTestSuiteConfiguration($testSuiteFilter = null): TestSuite
     {
         $testSuiteNodes = $this->xpath->query('testsuites/testsuite');
 
-        if ($testSuiteNodes->length == 0) {
+        if ($testSuiteNodes->length === 0) {
             $testSuiteNodes = $this->xpath->query('testsuite');
         }
 
-        if ($testSuiteNodes->length == 1) {
+        if ($testSuiteNodes->length === 1) {
             return $this->getTestSuite($testSuiteNodes->item(0), $testSuiteFilter);
         }
 
-        //if ($testSuiteNodes->length > 1) { there cannot be a negative number of Nodes
         $suite = new TestSuite;
 
         foreach ($testSuiteNodes as $testSuiteNode) {
@@ -901,8 +922,9 @@ class Configuration
     public function getTestSuiteNames(): array
     {
         $names = [];
-        $nodes = $this->xpath->query('*/testsuite');
-        foreach ($nodes as $node) {
+
+        foreach ($this->xpath->query('*/testsuite') as $node) {
+            /* @var DOMElement $node */
             $names[] = $node->getAttribute('name');
         }
 
@@ -912,6 +934,8 @@ class Configuration
     /**
      * @param DOMElement  $testSuiteNode
      * @param null|string $testSuiteFilter
+     *
+     * @throws \PHPUnit\Framework\Exception
      *
      * @return TestSuite
      */
@@ -938,6 +962,7 @@ class Configuration
         $testSuiteFilter    = $testSuiteFilter ? \explode(self::TEST_SUITE_FILTER_SEPARATOR, $testSuiteFilter) : [];
 
         foreach ($testSuiteNode->getElementsByTagName('directory') as $directoryNode) {
+            /** @var DOMElement $directoryNode */
             if (!empty($testSuiteFilter) && !\in_array($directoryNode->parentNode->getAttribute('name'), $testSuiteFilter)) {
                 continue;
             }
@@ -948,16 +973,17 @@ class Configuration
                 continue;
             }
 
+            $phpVersion         = PHP_VERSION;
+            $phpVersionOperator = '>=';
+            $prefix             = '';
+            $suffix             = 'Test.php';
+
             if ($directoryNode->hasAttribute('phpVersion')) {
                 $phpVersion = (string) $directoryNode->getAttribute('phpVersion');
-            } else {
-                $phpVersion = PHP_VERSION;
             }
 
             if ($directoryNode->hasAttribute('phpVersionOperator')) {
                 $phpVersionOperator = (string) $directoryNode->getAttribute('phpVersionOperator');
-            } else {
-                $phpVersionOperator = '>=';
             }
 
             if (!\version_compare(PHP_VERSION, $phpVersion, $phpVersionOperator)) {
@@ -966,14 +992,10 @@ class Configuration
 
             if ($directoryNode->hasAttribute('prefix')) {
                 $prefix = (string) $directoryNode->getAttribute('prefix');
-            } else {
-                $prefix = '';
             }
 
             if ($directoryNode->hasAttribute('suffix')) {
                 $suffix = (string) $directoryNode->getAttribute('suffix');
-            } else {
-                $suffix = 'Test.php';
             }
 
             $files = $fileIteratorFacade->getFilesAsArray(
@@ -982,10 +1004,12 @@ class Configuration
                 $prefix,
                 $exclude
             );
+
             $suite->addTestFiles($files);
         }
 
         foreach ($testSuiteNode->getElementsByTagName('file') as $fileNode) {
+            /** @var DOMElement $fileNode */
             if (!empty($testSuiteFilter) && !\in_array($fileNode->parentNode->getAttribute('name'), $testSuiteFilter)) {
                 continue;
             }
@@ -996,7 +1020,6 @@ class Configuration
                 continue;
             }
 
-            // Get the absolute path to the file
             $file = $fileIteratorFacade->getFilesAsArray(
                 $this->toAbsolutePath($file)
             );
@@ -1005,18 +1028,16 @@ class Configuration
                 continue;
             }
 
-            $file = $file[0];
+            $file               = $file[0];
+            $phpVersion         = PHP_VERSION;
+            $phpVersionOperator = '>=';
 
             if ($fileNode->hasAttribute('phpVersion')) {
                 $phpVersion = (string) $fileNode->getAttribute('phpVersion');
-            } else {
-                $phpVersion = PHP_VERSION;
             }
 
             if ($fileNode->hasAttribute('phpVersionOperator')) {
                 $phpVersionOperator = (string) $fileNode->getAttribute('phpVersionOperator');
-            } else {
-                $phpVersionOperator = '>=';
             }
 
             if (!\version_compare(PHP_VERSION, $phpVersion, $phpVersionOperator)) {
@@ -1041,11 +1062,11 @@ class Configuration
      */
     protected function getBoolean($value, $default)
     {
-        if (\strtolower($value) == 'false') {
+        if (\strtolower($value) === 'false') {
             return false;
         }
 
-        if (\strtolower($value) == 'true') {
+        if (\strtolower($value) === 'true') {
             return true;
         }
 
@@ -1076,29 +1097,28 @@ class Configuration
     {
         $directories = [];
 
-        foreach ($this->xpath->query($query) as $directory) {
-            $directoryPath = (string) $directory->textContent;
+        foreach ($this->xpath->query($query) as $directoryNode) {
+            /** @var DOMElement $directoryNode */
+            $directoryPath = (string) $directoryNode->textContent;
 
             if (!$directoryPath) {
                 continue;
             }
 
-            if ($directory->hasAttribute('prefix')) {
-                $prefix = (string) $directory->getAttribute('prefix');
-            } else {
-                $prefix = '';
+            $prefix = '';
+            $suffix = '.php';
+            $group  = 'DEFAULT';
+
+            if ($directoryNode->hasAttribute('prefix')) {
+                $prefix = (string) $directoryNode->getAttribute('prefix');
             }
 
-            if ($directory->hasAttribute('suffix')) {
-                $suffix = (string) $directory->getAttribute('suffix');
-            } else {
-                $suffix = '.php';
+            if ($directoryNode->hasAttribute('suffix')) {
+                $suffix = (string) $directoryNode->getAttribute('suffix');
             }
 
-            if ($directory->hasAttribute('group')) {
-                $group = (string) $directory->getAttribute('group');
-            } else {
-                $group = 'DEFAULT';
+            if ($directoryNode->hasAttribute('group')) {
+                $group = (string) $directoryNode->getAttribute('group');
             }
 
             $directories[] = [
@@ -1159,7 +1179,6 @@ class Configuration
             return $path;
         }
 
-        // Stream
         if (\strpos($path, '://') !== false) {
             return $path;
         }

src/Framework/TestSuite.php
@@ -14,7 +14,7 @@ use IteratorAggregate;
 use PHPUnit\Runner\BaseTestRunner;
 use PHPUnit\Runner\Filter\Factory;
 use PHPUnit\Runner\PhptTestCase;
-use PHPUnit\Util\Fileloader;
+use PHPUnit\Util\FileLoader;
 use PHPUnit\Util\InvalidArgumentHelper;
 use ReflectionClass;
 use ReflectionMethod;
@@ -337,7 +337,7 @@ class TestSuite implements Test, SelfDescribing, IteratorAggregate
 
         // The given file may contain further stub classes in addition to the
         // test class itself. Figure out the actual test class.
-        $filename   = Fileloader::checkAndLoad($filename);
+        $filename   = FileLoader::checkAndLoad($filename);
         $newClasses = \array_diff(\get_declared_classes(), $this->declaredClasses);
 
         // The diff is empty in case a parent class (with test methods) is added

src/Runner/StandardTestSuiteLoader.php
@@ -10,7 +10,7 @@
 namespace PHPUnit\Runner;
 
 use PHPUnit\Framework\TestCase;
-use PHPUnit\Util\Fileloader;
+use PHPUnit\Util\FileLoader;
 use PHPUnit\Util\Filesystem;
 use ReflectionClass;
 
@@ -41,7 +41,7 @@ class StandardTestSuiteLoader implements TestSuiteLoader
         if (!\class_exists($suiteClassName, false)) {
             $loadedClasses = \get_declared_classes();
 
-            $filename = Fileloader::checkAndLoad($suiteClassFile);
+            $filename = FileLoader::checkAndLoad($suiteClassFile);
 
             $loadedClasses = \array_values(
                 \array_diff(\get_declared_classes(), $loadedClasses)

src/TextUI/Command.php
@@ -25,7 +25,7 @@ use PHPUnit\Runner\TestSuiteLoader;
 use PHPUnit\Runner\Version;
 use PHPUnit\Util\Configuration;
 use PHPUnit\Util\ConfigurationGenerator;
-use PHPUnit\Util\Fileloader;
+use PHPUnit\Util\FileLoader;
 use PHPUnit\Util\Filesystem;
 use PHPUnit\Util\Getopt;
 use PHPUnit\Util\Log\TeamCity;
@@ -990,7 +990,7 @@ class Command
     protected function handleBootstrap($filename): void
     {
         try {
-            Fileloader::checkAndLoad($filename);
+            FileLoader::checkAndLoad($filename);
         } catch (Exception $e) {
             $this->exitWithErrorMessage($e->getMessage());
         }

src/Util/ErrorHandler.php
@@ -32,46 +32,36 @@ class ErrorHandler
         return self::$errorStack;
     }
 
-    /**
-     * @param int    $errno
-     * @param string $errstr
-     * @param string $errfile
-     * @param int    $errline
-     *
-     * @throws Error
-     *
-     * @return false
-     */
-    public static function handleError($errno, $errstr, $errfile, $errline): bool
+    public static function handleError(int $errorNumber, string $errorString, string $errorFile, int $errorLine): bool
     {
-        if (!($errno & \error_reporting())) {
+        if (!($errorNumber & \error_reporting())) {
             return false;
         }
 
-        self::$errorStack[] = [$errno, $errstr, $errfile, $errline];
+        self::$errorStack[] = [$errorNumber, $errorString, $errorFile, $errorLine];
 
         $trace = \debug_backtrace();
         \array_shift($trace);
 
         foreach ($trace as $frame) {
-            if ($frame['function'] == '__toString') {
+            if ($frame['function'] === '__toString') {
                 return false;
             }
         }
 
-        if ($errno == E_NOTICE || $errno == E_USER_NOTICE || $errno == E_STRICT) {
+        if ($errorNumber === E_NOTICE || $errorNumber === E_USER_NOTICE || $errorNumber === E_STRICT) {
             if (Notice::$enabled !== true) {
                 return false;
             }
 
             $exception = Notice::class;
-        } elseif ($errno == E_WARNING || $errno == E_USER_WARNING) {
+        } elseif ($errorNumber === E_WARNING || $errorNumber === E_USER_WARNING) {
             if (Warning::$enabled !== true) {
                 return false;
             }
 
             $exception = Warning::class;
-        } elseif ($errno == E_DEPRECATED || $errno == E_USER_DEPRECATED) {
+        } elseif ($errorNumber === E_DEPRECATED || $errorNumber === E_USER_DEPRECATED) {
             if (Deprecated::$enabled !== true) {
                 return false;
             }
@@ -81,7 +71,7 @@ class ErrorHandler
             $exception = Error::class;
         }
 
-        throw new $exception($errstr, $errno, $errfile, $errline);
+        throw new $exception($errorString, $errorNumber, $errorFile, $errorLine);
     }
 
     /**
@@ -98,20 +88,23 @@ class ErrorHandler
     {
         $terminator = function () {
             static $expired = false;
+
             if (!$expired) {
                 $expired = true;
-                // cleans temporary error handler
+
                 return \restore_error_handler();
             }
         };
 
-        \set_error_handler(function ($errno, $errstr) use ($severity) {
-            if ($errno === $severity) {
-                return;
-            }
+        \set_error_handler(
+            function ($errorNumber, $errorString) use ($severity) {
+                if ($errorNumber === $severity) {
+                    return;
+                }
 
-            return false;
-        });
+                return false;
+            }
+        );
 
         return $terminator;
     }

src/Util/FileLoader.php
@@ -14,11 +14,15 @@ use PHPUnit\Framework\Exception;
 /**
  * Utility methods to load PHP sourcefiles.
  */
-class Fileloader
+class FileLoader
 {
     /**
-     * Checks if a PHP sourcefile is readable.
-     * The sourcefile is loaded through the load() method.
+     * Checks if a PHP sourcecode file is readable. The sourcecode file is loaded through the load() method.
+     *
+     * As a fallback, PHP looks in the directory of the file executing the stream_resolve_include_path function.
+     * We do not want to load the Test.php file here, so skip it if it found that.
+     * PHP prioritizes the include_path setting, so if the current directory is in there, it will first look in the
+     * current working directory.
      *
      * @param string $filename
      *
@@ -29,14 +33,11 @@ class Fileloader
     public static function checkAndLoad($filename): string
     {
         $includePathFilename = \stream_resolve_include_path($filename);
+        $localFile           = __DIR__ . DIRECTORY_SEPARATOR . $filename;
 
-        // As a fallback, PHP looks in the directory of the file executing the stream_resolve_include_path function.
-        // We don't want to load the Test.php file here, so skip it if it found that.
-        // PHP prioritizes the include_path setting, so if the current directory is in there, it will first look in the
-        // current working directory.
-        $localFile = __DIR__ . DIRECTORY_SEPARATOR . $filename;
-
-        // @see https://github.com/sebastianbergmann/phpunit/pull/2751
+        /**
+         * @see https://github.com/sebastianbergmann/phpunit/pull/2751
+         */
         $isReadable = @\fopen($includePathFilename, 'r') !== false;
 
         if (!$includePathFilename || !$isReadable || $includePathFilename === $localFile) {
@@ -64,13 +65,10 @@ class Fileloader
         include_once $filename;
 
         $newVariables     = \get_defined_vars();
-        $newVariableNames = \array_diff(
-            \array_keys($newVariables),
-            $oldVariableNames
-        );
+        $newVariableNames = \array_diff(\array_keys($newVariables), $oldVariableNames);
 
         foreach ($newVariableNames as $variableName) {
-            if ($variableName != 'oldVariableNames') {
+            if ($variableName !== 'oldVariableNames') {
                 $GLOBALS[$variableName] = $newVariables[$variableName];
             }
         }

src/Util/Filter.php
@@ -96,8 +96,8 @@ class Filter
     private static function frameExists(array $trace, $file, $line): bool
     {
         foreach ($trace as $frame) {
-            if (isset($frame['file']) && $frame['file'] == $file &&
-                isset($frame['line']) && $frame['line'] == $line) {
+            if (isset($frame['file']) && $frame['file'] === $file &&
+                isset($frame['line']) && $frame['line'] === $line) {
                 return true;
             }
         }

src/Util/Getopt.php
@@ -16,7 +16,16 @@ use PHPUnit\Framework\Exception;
  */
 class Getopt
 {
-    public static function getopt(array $args, $short_options, $long_options = null): array
+    /**
+     * @param array      $args
+     * @param string     $short_options
+     * @param null|array $long_options
+     *
+     * @throws Exception
+     *
+     * @return array
+     */
+    public static function getopt(array $args, string $short_options, array $long_options = null): array
     {
         if (empty($args)) {
             return [[], []];
@@ -29,7 +38,7 @@ class Getopt
             \sort($long_options);
         }
 
-        if (isset($args[0][0]) && $args[0][0] != '-') {
+        if (isset($args[0][0]) && $args[0][0] !== '-') {
             \array_shift($args);
         }
 
@@ -37,25 +46,27 @@ class Getopt
 
         $args = \array_map('trim', $args);
 
+        /* @noinspection ComparisonOperandsOrderInspection */
         while (false !== $arg = \current($args)) {
             $i = \key($args);
             \next($args);
-            if ($arg == '') {
+
+            if ($arg === '') {
                 continue;
             }
 
-            if ($arg == '--') {
+            if ($arg === '--') {
                 $non_opts = \array_merge($non_opts, \array_slice($args, $i + 1));
 
                 break;
             }
 
-            if ($arg[0] != '-' || (\strlen($arg) > 1 && $arg[1] == '-' && !$long_options)) {
+            if ($arg[0] !== '-' || (\strlen($arg) > 1 && $arg[1] === '-' && !$long_options)) {
                 $non_opts[] = $args[$i];
 
                 continue;
             }
-            if (\strlen($arg) > 1 && $arg[1] == '-') {
+            if (\strlen($arg) > 1 && $arg[1] === '-') {
                 self::parseLongOption(
                     \substr($arg, 2),
                     $long_options,
@@ -75,7 +86,15 @@ class Getopt
         return [$opts, $non_opts];
     }
 
-    protected static function parseShortOption($arg, $short_options, &$opts, &$args): void
+    /**
+     * @param string $arg
+     * @param string $short_options
+     * @param array  $opts
+     * @param array  $args
+     *
+     * @throws Exception
+     */
+    protected static function parseShortOption(string $arg, string $short_options, array &$opts, array &$args): void
     {
         $argLen = \strlen($arg);
 
@@ -83,24 +102,27 @@ class Getopt
             $opt     = $arg[$i];
             $opt_arg = null;
 
-            if (($spec = \strstr($short_options, $opt)) === false || $arg[$i] == ':') {
+            if ($arg[$i] === ':' || ($spec = \strstr($short_options, $opt)) === false) {
                 throw new Exception(
                     "unrecognized option -- $opt"
                 );
             }
 
-            if (\strlen($spec) > 1 && $spec[1] == ':') {
+            if (\strlen($spec) > 1 && $spec[1] === ':') {
                 if ($i + 1 < $argLen) {
                     $opts[] = [$opt, \substr($arg, $i + 1)];
 
                     break;
                 }
-                if (!(\strlen($spec) > 2 && $spec[2] == ':')) {
+
+                if (!(\strlen($spec) > 2 && $spec[2] === ':')) {
+                    /* @noinspection ComparisonOperandsOrderInspection */
                     if (false === $opt_arg = \current($args)) {
                         throw new Exception(
                             "option requires an argument -- $opt"
                         );
                     }
+
                     \next($args);
                 }
             }
@@ -109,7 +131,15 @@ class Getopt
         }
     }
 
-    protected static function parseLongOption($arg, $long_options, &$opts, &$args): void
+    /**
+     * @param string $arg
+     * @param array  $long_options
+     * @param array  $opts
+     * @param array  $args
+     *
+     * @throws Exception
+     */
+    protected static function parseLongOption(string $arg, array $long_options, array &$opts, array &$args): void
     {
         $count   = \count($long_options);
         $list    = \explode('=', $arg);
@@ -126,27 +156,30 @@ class Getopt
             $long_opt  = $long_options[$i];
             $opt_start = \substr($long_opt, 0, $opt_len);
 
-            if ($opt_start != $opt) {
+            if ($opt_start !== $opt) {
                 continue;
             }
 
             $opt_rest = \substr($long_opt, $opt_len);
 
-            if ($opt_rest != '' && $opt[0] != '=' && $i + 1 < $count &&
-                $opt == \substr($long_options[$i + 1], 0, $opt_len)) {
+            if ($opt_rest !== '' && $i + 1 < $count && $opt[0] !== '=' &&
+                \strpos($long_options[$i + 1], $opt) === 0) {
                 throw new Exception(
                     "option --$opt is ambiguous"
                 );
             }
 
-            if (\substr($long_opt, -1) == '=') {
-                if (\substr($long_opt, -2) != '==') {
+            if (\substr($long_opt, -1) === '=') {
+                if (\substr($long_opt, -2) !== '==') {
+                    /* @noinspection StrlenInEmptyStringCheckContextInspection */
                     if (!\strlen($opt_arg)) {
+                        /* @noinspection ComparisonOperandsOrderInspection */
                         if (false === $opt_arg = \current($args)) {
                             throw new Exception(
                                 "option --$opt requires an argument"
                             );
                         }
+
                         \next($args);
                     }
                 }

commit e04322642bcbd3063e65dc0aff93b24066bff055

Fix comparison changed in the CS commit
It was broken in 3a9494be34135e5d42964ec7f5f17605e736a2c7

src/Util/Configuration.php
@@ -346,7 +346,7 @@ final class Configuration
                     continue;
                 }
 
-                if (!$node->tagName === 'arguments') {
+                if ($node->tagName !== 'arguments') {
                     continue;
                 }
 