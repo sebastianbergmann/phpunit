d3bd9c3516d7f3125c285b07a3cd3a6d01afa337
Mateusz Sip <mateusz.sip@gmail.com>
2018-02-10 15:02:06 +0100

    assertResourceNotOfType, IsResourceNotOfType constraint

--- src/Framework/Assert.php
+++ src/Framework/Assert.php
@@ -36,6 +36,7 @@ use PHPUnit\Framework\Constraint\IsJson;
 use PHPUnit\Framework\Constraint\IsNan;
 use PHPUnit\Framework\Constraint\IsNull;
 use PHPUnit\Framework\Constraint\IsReadable;
+use PHPUnit\Framework\Constraint\IsResourceNotOfType;
 use PHPUnit\Framework\Constraint\IsResourceOfType;
 use PHPUnit\Framework\Constraint\IsTrue;
 use PHPUnit\Framework\Constraint\IsType;
@@ -2012,6 +2013,20 @@ abstract class Assert
     public static function assertResourceOfType(string $expectedType, $actual, string $message = '')
     {
         static::assertThat($actual, new IsResourceOfType($expectedType), $message);
+    }
+
+    /**
+     * Asserts that given value is resource not of specified type
+     *
+     * @param string $expectedType
+     * @param resource $actual
+     * @param string $message
+     *
+     * @throws \Exception
+     */
+    public static function assertResourceNotOfType(string $expectedType, $actual, string $message = '')
+    {
+        static::assertThat($actual, new IsResourceNotOfType($expectedType), $message);
     }
 
     /**
