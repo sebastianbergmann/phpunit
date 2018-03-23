61e49af8afe7bc52bb2e185ea0c8fdf48167a32c
Qrzy <qrzy88@gmail.com>
2018-03-23 16:12:27 +0100

    Fix printer construct
    Check if directory exists BEFORE trying to create it

--- src/Util/Printer.php
+++ src/Util/Printer.php
@@ -53,7 +53,7 @@ class Printer
 
                     $this->out = \fsockopen($out[0], $out[1]);
                 } else {
-                    if (\strpos($out, 'php://') === false && !@\mkdir(\dirname($out), 0777, true) && !\is_dir(\dirname($out))) {
+                    if (\strpos($out, 'php://') === false && !\is_dir(\dirname($out)) && !@\mkdir(\dirname($out), 0777, true)) {
                         throw new \RuntimeException(\sprintf('Directory "%s" was not created', \dirname($out)));
                     }
 
