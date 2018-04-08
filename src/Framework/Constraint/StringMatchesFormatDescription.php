a0afc726c05f821c956efecf3c601db97f226a34
Sebastian Bergmann <sb@sebastian-bergmann.de>
2018-04-08 12:07:23 +0200

    Fix CS/WS issues

--- src/Framework/Constraint/StringMatchesFormatDescription.php
+++ src/Framework/Constraint/StringMatchesFormatDescription.php
@@ -37,7 +37,7 @@ class StringMatchesFormatDescription extends RegularExpression
      * Evaluates the constraint for parameter $other. Returns true if the
      * constraint is met, false otherwise.
      *
-     * @param mixed $other Value or object to evaluate.
+     * @param mixed $other value or object to evaluate
      *
      * @return bool
      */
