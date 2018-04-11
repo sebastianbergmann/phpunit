f7c63f5823721c21859d1f49ffbd97cf9f271bc3
Ewout Pieter den Ouden <epdouden@gmail.com>
2018-04-11 11:26:28 +0200

    Added short summary of the depedency resolution algorithm.

--- src/Framework/TestSuiteIterator.php
+++ src/Framework/TestSuiteIterator.php
@@ -124,6 +124,24 @@ class TestSuiteIterator implements RecursiveIterator
         return $this->tests[$this->position] instanceof TestSuite;
     }
 
+    /**
+     * Reorder Tests within a TestCase in such a way as to resolve as many dependencies as possible.
+     * The algorithm will leave the tests in original running order when it can.
+     * For more details see the documentation for the @depends annotation
+     *
+     * The final running order will be:
+     * 1. tests without dependencies
+     * 2. tests with resolved dependencies
+     * 3. tests for which the dependencies could not be resolved
+     *
+     * Short description of algorithm:
+     * 1. Compile two lists of Tests in original order: with and without dependencies.
+     * 2. Independent tests can be run first.
+     * 3a. Pick the next Test from the list of dependants.
+     *  b. When all dependencies run before this Test, move it to the reordered list
+     *  c. Start again from the top of the list of dependants.
+     * 4. When we reach the end add any leftover tests to the end. These will be marked 'skipped'.
+     */
     private function reorderTestsByDependencies()
     {
         if (empty($this->tests)) {
