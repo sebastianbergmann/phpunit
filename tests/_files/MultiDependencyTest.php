ab0c2020ae2d495c5eaa98b702ae6871a73b2b18
Ewout Pieter den Ouden <epdouden@gmail.com>
2018-04-11 22:39:55 +0200

    Added new tests for running tests in reverse order

--- tests/_files/MultiDependencyTest.php
+++ tests/_files/MultiDependencyTest.php
@@ -26,4 +26,12 @@ class MultiDependencyTest extends TestCase
         $this->assertEquals('foo', $a);
         $this->assertEquals('bar', $b);
     }
+
+    /**
+     * @depends MultiDependencyTest::testThree
+     */
+    public function testFour()
+    {
+        $this->assertTrue(true);
+    }
 }
