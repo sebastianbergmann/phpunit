31cb85728ea7ce215acd3917dd30407ff2e9b8e2
Remon van de Kamp <rpkamp@gmail.com>
2018-04-28 17:49:24 +0200

    Catch and display errors during bootstrapping when using testdox printer

---
+++ tests/Regression/GitHub/3107/Issue3107Test.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * This file is part of PHPUnit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Issue3107;
+
+use PHPUnit\Framework\TestCase;
+
+class Issue3107Test extends TestCase
+{
+    public static function setUpBeforeClass(): void
+    {
+        does_not_exist();
+    }
+
+    public function testOne(): void
+    {
+        $this->assertTrue(true);
+    }
+}
