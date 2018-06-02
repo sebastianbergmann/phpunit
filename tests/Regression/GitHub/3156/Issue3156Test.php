9853431d006c96890772f8812ee486fb1b4e0298
Ewout Pieter den Ouden <epdouden@gmail.com>
2018-06-02 19:16:00 +0200

    CS/WS fixes

--- tests/Regression/GitHub/3156/Issue3156Test.php
+++ tests/Regression/GitHub/3156/Issue3156Test.php
@@ -1,35 +1,42 @@
 <?php
-declare(strict_types = 1);
+declare(strict_types=1);
+/*
+ * This file is part of PHPUnit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Test;
+
 use PHPUnit\Framework\TestCase;
-use \stdClass;
+use stdClass;
 
 class TestTest extends TestCase
 {
-
     public function testConstants(): stdClass
     {
-        $this->assertStringEndsWith('/', "/");
+        $this->assertStringEndsWith('/', '/');
+
         return new stdClass();
     }
 
     public function dataSelectOperatorsProvider(): array
     {
         return [
-            ["1"],
-            ["2"]
+            ['1'],
+            ['2']
         ];
     }
 
     /**
      * @depends testConstants
      * @dataProvider dataSelectOperatorsProvider
-     *
-     * @return void
      */
     public function testDependsRequire(string $val, stdClass $obj): void
     {
-        $this->assertStringEndsWith('/', "/");
+        $this->assertStringEndsWith('/', '/');
     }
 }
