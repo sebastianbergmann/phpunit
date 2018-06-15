6f841ea157254015c75629453065a0422bdd8a13
Ewout Pieter den Ouden <ewoutdenouden@gmail.com>
2018-06-15 19:53:07 +0200

    Fix #3093 by improving test name handling in dependency resolver
    The dependency resolver doesn't need to check dependencies between data rows.
    Format names as TestClassName::testMethodName internally for better matching.

---
+++ tests/Regression/GitHub/3093/Issue3093Test.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * This file is part of PHPUnit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+class Issue3093Test extends \PHPUnit\Framework\TestCase
+{
+    public function someDataProvider(): array
+    {
+        return [['some values']];
+    }
+
+    public function testFirstWithoutDependencies(): void
+    {
+        self::assertTrue(true);
+    }
+
+    /**
+     * @depends testFirstWithoutDependencies
+     * @dataProvider someDataProvider
+     */
+    public function testSecondThatDependsOnFirstAndDataprovider($value)
+    {
+        self::assertTrue(true);
+    }
+}
