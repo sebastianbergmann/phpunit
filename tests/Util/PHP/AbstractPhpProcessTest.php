34b954a2326d60cdf59566aaef51b7c670fa59d1
Kevin Abel <kevin.abel.0@gmail.com>
2018-07-08 16:47:43 -0500

    Clean up the php process command builder
    * Gets rid of the need for eval-stdin.php in phpdbg with the s option
    * Only include the args separator when a file is not given

--- tests/Util/PHP/AbstractPhpProcessTest.php
+++ tests/Util/PHP/AbstractPhpProcessTest.php
@@ -83,8 +83,7 @@ class AbstractPhpProcessTest extends TestCase
 
     public function testShouldHaveFileToCreateCommand(): void
     {
-        $argumentEscapingCharacter = \DIRECTORY_SEPARATOR === '\\' ? '"' : '\'';
-        $expectedCommandFormat     = \sprintf('%%s -%%c %1$sfile.php%1$s', $argumentEscapingCharacter);
+        $expectedCommandFormat     = '%s %cfile.php%c';
         $actualCommand             = $this->phpProcess->getCommand([], 'file.php');
 
         $this->assertStringMatchesFormat($expectedCommandFormat, $actualCommand);
