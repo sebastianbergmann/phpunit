b13558205fce86ef6a2608b36d8a905a1aa06915
Siad Ardroumli <siad.ardroumli@gmail.com>
2018-07-26 19:40:45 +0200

    Fixed TypeHinting for JUnit::setWriteDocument

--- src/Util/Log/JUnit.php
+++ src/Util/Log/JUnit.php
@@ -363,12 +363,12 @@ class JUnit extends Printer implements TestListener
      *
      * This is a "hack" needed for the integration of
      * PHPUnit with Phing.
+     * 
+     * @param bool $flag
      */
-    public function setWriteDocument($flag): ?string
+    public function setWriteDocument(bool $flag): void
     {
-        if (\is_bool($flag)) {
-            $this->writeDocument = $flag;
-        }
+        $this->writeDocument = $flag;
     }
 
     /**
