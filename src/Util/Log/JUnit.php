b3ae39001fb14035d3e9dc47665699432f3c5255
Siad Ardroumli <siad.ardroumli@googlemail.com>
2018-07-26 20:14:21 +0200

    Removed whitespace for cs-fixer

--- src/Util/Log/JUnit.php
+++ src/Util/Log/JUnit.php
@@ -363,7 +363,7 @@ class JUnit extends Printer implements TestListener
      *
      * This is a "hack" needed for the integration of
      * PHPUnit with Phing.
-     * 
+     *
      * @param bool $flag Enables or disables the writing of the document.
      *                   default: `true`
      */
