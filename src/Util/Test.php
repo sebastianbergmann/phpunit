sebastianbergmann/phpunit

commit 1dd9b941a516863aa02163e9eda52e6352215df7

Initial work on #3195

src/Util/Test.php
@@ -782,6 +782,15 @@ final class Test
             $element = \explode(' ', $element);
             $element = $element[0];
 
+            if ($mode === 'covers' && \interface_exists($element)) {
+                throw new InvalidCoversTargetException(
+                    \sprintf(
+                        'Trying to @cover interface "%s".',
+                        $className
+                    )
+                );
+            }
+
             $codeList = \array_merge(
                 $codeList,
                 self::resolveElementToReflectionObjects($element)

commit 39f224e0ea85c1a6abf79b8bdebf9618052638ac

Fix issue introduced in 1dd9b941a516863aa02163e9eda52e6352215df7

src/Util/Test.php
@@ -786,7 +786,7 @@ final class Test
                 throw new InvalidCoversTargetException(
                     \sprintf(
                         'Trying to @cover interface "%s".',
-                        $className
+                        $element
                     )
                 );
             }