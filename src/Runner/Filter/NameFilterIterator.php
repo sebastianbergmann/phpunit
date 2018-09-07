7f4a153f23a707adb974b8fc6d554ce2879b7021
Sebastian Bergmann <sb@sebastian-bergmann.de>
2018-09-07 19:05:28 +0200

    PHPUnit\Util\Test::describe() only accepts one argument

--- src/Runner/Filter/NameFilterIterator.php
+++ src/Runner/Filter/NameFilterIterator.php
@@ -50,7 +50,7 @@ class NameFilterIterator extends RecursiveFilterIterator
             return true;
         }
 
-        $tmp = \PHPUnit\Util\Test::describe($test, false);
+        $tmp = \PHPUnit\Util\Test::describe($test);
 
         if ($test instanceof WarningTestCase) {
             $name = $test->getMessage();
