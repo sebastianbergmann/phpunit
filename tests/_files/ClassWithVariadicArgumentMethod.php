6814dd41be4de16c024402b59e73661f6b63eb36
Michel Hartmann <michel.hartmann@flyeralarm.com>
2018-09-08 11:58:16 +0200

    Added tests for methods with variadic arguments

---
+++ tests/_files/ClassWithVariadicArgumentMethod.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * This file is part of PHPUnit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * A class with a method that takes a variadic argument.
+ */
+class ClassWithVariadicArgumentMethod
+{
+    public function foo(...$args)
+    {
+        return $args;
+    }
+}
