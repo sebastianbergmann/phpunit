cf34d3f02a533086c4aa530e5e949f88c6e7da93
Tomas Votruba <tomas.vot@gmail.com>
2018-09-21 00:19:45 +0200

    upgrade tests to new exception syntax

--- tests/unit/Framework/TestSuiteTest.php
+++ tests/unit/Framework/TestSuiteTest.php
@@ -201,12 +201,10 @@ class TestSuiteTest extends TestCase
         $this->assertCount(2, $result);
     }
 
-    /**
-     * @expectedException PHPUnit\Framework\Exception
-     * @expectedExceptionMessage No valid test provided.
-     */
     public function testCreateTestForConstructorlessTestClass(): void
     {
+        $this->expectException('PHPUnit\Framework\Exception');
+        $this->expectExceptionMessage('No valid test provided.');
         $reflection = $this->getMockBuilder(\ReflectionClass::class)
             ->setConstructorArgs([$this])
             ->getMock();
