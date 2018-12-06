1a46f392faa326eae9e529a65ac8b4b625c05eb1
Ewout Pieter den Ouden <ewoutdenouden@gmail.com>
2018-12-07 00:50:32 +0100

    Make basic ANSI colors available in NamePrettifier. Colorize data sets.

---
+++ src/Util/TestDox/Color.php
@@ -0,0 +1,52 @@
+<?php
+/*
+ * This file is part of PHPUnit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace PHPUnit\Util\TestDox;
+
+class Color
+{
+    /**
+     * @var array
+     */
+    private static $ansiCodes = [
+        'reset'             => '0',
+        'bold'              => '1',
+        'dim'               => '2',
+        'underlined'        => '4',
+        'fg-black'          => '30',
+        'fg-red'            => '31',
+        'fg-green'          => '32',
+        'fg-yellow'         => '33',
+        'fg-blue'           => '34',
+        'fg-magenta'        => '35',
+        'fg-cyan'           => '36',
+        'fg-white'          => '37',
+        'bg-black'          => '40',
+        'bg-red'            => '41',
+        'bg-green'          => '42',
+        'bg-yellow'         => '43',
+        'bg-blue'           => '44',
+        'bg-magenta'        => '45',
+        'bg-cyan'           => '46',
+        'bg-white'          => '47',
+    ];
+
+    public static function colorize(string $color, string $buffer): string
+    {
+        $codes   = \array_map('\trim', \explode(',', $color));
+
+        foreach ($codes as $code) {
+            $styles[] = self::$ansiCodes[$code] ?? '';
+        }
+
+        $style = \sprintf("\x1b[%sm", \implode(';', $styles));
+
+        return $style . $buffer . "\x1b[0m";
+    }
+}
