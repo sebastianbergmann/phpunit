e53378d02cbd15cc12d63af33de7cbdd3ada773f
Niko Granö <niko@lamia.fi>
2018-12-12 16:47:31 +0200

    Implemented tests for PR 3349

---
+++ tests/_files/PR3349.php
@@ -0,0 +1,209 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\PR3349;
+use PHPUnit\Framework\TestCase;
+
+final class Tests extends TestCase
+{
+
+    /**
+     * @see \Test\PR3349\Tests::testSameClassDependencyCorrectOrder
+     * @group PR3349_Group1
+     */
+    public function testSameClassDependencyCorrectOrderDependency(): int
+    {
+        $this->addToAssertionCount(1);
+        return 100;
+    }
+
+    /**
+     * @depends \Test\PR3349\Tests::testSameClassDependencyCorrectOrderDependency
+     * @group PR3349_Group1
+     */
+    public function testSameClassDependencyCorrectOrder(int $int): void
+    {
+        $this->assertEquals(100, $int);
+    }
+
+    /**
+     * @depends \Test\PR3349\Tests::testSameClassDependencyReversedOrderDependency
+     * @group PR3349_Group2
+     */
+    public function testSameClassDependencyReversedOrder(int $int): void
+    {
+        $this->addToAssertionCount(1);
+        $this->assertEquals(100, $int);
+    }
+
+    /**
+     * @see \Test\PR3349\Tests::testSameClassDependencyReversedOrder
+     * @group PR3349_Group2
+     */
+    public function testSameClassDependencyReversedOrderDependency(): int
+    {
+        $this->addToAssertionCount(1);
+        return 100;
+    }
+
+    /**
+     * @depends \Test\PR3349\Test_Dependencies::testExternalClassDependencySuccessReturn100
+     * @group PR3349_Group3
+     */
+    public function testExternalClassDependencyCorrectOrder(int $int): void
+    {
+        $this->assertEquals(100, $int);
+    }
+
+    /**
+     * @depends \Test\PR3349\Test_FailingSetUp::testExternalClassDependencySuccessReturn100WithFailingSetUp
+     * @group PR3349_Group4
+     */
+    public function testExternalClassDependencyFailingSetUp(int $int): void
+    {
+        $this->assertEquals(100, $int);
+    }
+
+    /**
+     * @depends \Test\PR3349\Test_ChainOne::testExternalClassDependencyChainFunctionOne
+     * @group PR3349_Group5
+     */
+    public function testExternalClassChainedMultipleDependencies(int $int): void
+    {
+        $this->assertEquals(100, $int);
+    }
+
+    /**
+     * @depends \Test\PR3349\Test_Dependencies::testExternalClassDependencyFailure
+     * @group PR3349_Group6
+     */
+    public function testExternalClassDependencyFailed(int $int): void
+    {
+        $this->addToAssertionCount(1);
+    }
+
+    /**
+     * @depends \Test\PR3349\Test_Dependencies::testExternalClassDependencySkipped
+     * @group PR3349_Group7
+     */
+    public function testExternalClassDependencySkipped(int $int): void
+    {
+        $this->addToAssertionCount(1);
+    }
+}
+
+final class Test_Dependencies extends TestCase
+{
+    /**
+     * @see \Test\PR3349\Tests::testExternalClassDependencyCorrectOrder
+     * @group PR3349_Group3
+     */
+    public function testExternalClassDependencySuccessReturn100(): int
+    {
+        $this->addToAssertionCount(1);
+        return 100;
+    }
+
+    /**
+     * @see \Test\PR3349\Tests::testExternalClassDependencyFailed
+     * @group PR3349_Group6
+     */
+    public function testExternalClassDependencyFailure(): int
+    {
+        $this->fail('Something Went Wrong With Purpose.');
+        return 100;
+    }
+
+    /**
+     * @see \Test\PR3349\Tests::testExternalClassDependencySkipped
+     * @group PR3349_Group7
+     */
+    public function testExternalClassDependencySkipped(): int
+    {
+        $this->fail('Test Skipped With Purpose.');
+        return 100;
+    }
+}
+
+final class Test_FailingSetUp extends TestCase
+{
+    /**
+     * @throws \Exception
+     * @group PR3349_Group4
+     * @see \Test\PR3349\Tests::testExternalClassDependencyFailingSetUp
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+        throw new \Exception('This is failing with purpose.');
+    }
+
+    /**
+     * @see \Test\PR3349\Tests::testExternalClassDependencyFailingSetUp
+     * @group PR3349_Group4
+     */
+    public function testExternalClassDependencySuccessReturn100WithFailingSetUp(): int
+    {
+        $this->addToAssertionCount(1);
+        return 100;
+    }
+}
+
+final class Test_ChainOne extends TestCase{
+
+    /**
+     * @see \Test\PR3349\Tests::testExternalClassChainedMultipleDependencies
+     * @group PR3349_Group5
+     * @depends \Test\PR3349\Test_ChainTwo::testExternalClassDependencyChainFunctionTwo
+     */
+    public function testExternalClassDependencyChainFunctionOne(int $int): int
+    {
+        $int += 40;
+        $this->assertEquals(100, $int);
+        return $int;
+    }
+}
+
+final class Test_ChainTwo extends TestCase{
+
+    /**
+     * @see \Test\PR3349\Tests::testExternalClassChainedMultipleDependencies
+     * @group PR3349_Group5
+     * @depends \Test\PR3349\Test_ChainThree::testExternalClassDependencyChainFunctionThree
+     */
+    public function testExternalClassDependencyChainFunctionTwo(int $int): int
+    {
+        $int += 30;
+        $this->assertEquals(60, $int);
+        return $int;
+    }
+}
+
+final class Test_ChainThree extends TestCase{
+
+    /**
+     * @see \Test\PR3349\Tests::testExternalClassChainedMultipleDependencies
+     * @group PR3349_Group5
+     * @depends \Test\PR3349\Test_ChainFour::testExternalClassDependencyChainFunctionFour
+     */
+    public function testExternalClassDependencyChainFunctionThree(int $int): int
+    {
+        $int += 20;
+        $this->assertEquals(30, $int);
+        return $int;
+    }
+}
+
+final class Test_ChainFour extends TestCase{
+
+    /**
+     * @see \Test\PR3349\Tests::testExternalClassChainedMultipleDependencies
+     * @group PR3349_Group5
+     */
+    public function testExternalClassDependencyChainFunctionFour(): int
+    {
+        $this->addToAssertionCount(1);
+        return 10;
+    }
+}
---
+++ tests/end-to-end/depedency-not-run-yet-pr-3349.phpt
@@ -0,0 +1,56 @@
+--TEST--
+phpunit --reverse-order --ignore-dependencies ../_files/PR3349.php
+--FILE--
+<?php
+$_SERVER['argv'][1] = '--no-configuration';
+$_SERVER['argv'][2] = '--debug';
+$_SERVER['argv'][3] = '--verbose';
+$_SERVER['argv'][4] = 'StackTest';
+$_SERVER['argv'][5] = __DIR__ . '/../_files/PR3349.php';
+
+require __DIR__ . '/../bootstrap.php';
+PHPUnit\TextUI\Command::main();
+?>
+--EXPECTF--
+PHPUnit %s by Sebastian Bergmann and contributors.
+
+Runtime:       %s
+
+Test 'Test\PR3349\Tests::testSameClassDependencyCorrectOrderDependency' started
+Test 'Test\PR3349\Tests::testSameClassDependencyCorrectOrderDependency' ended
+Test 'Test\PR3349\Tests::testSameClassDependencyCorrectOrder' started
+Test 'Test\PR3349\Tests::testSameClassDependencyCorrectOrder' ended
+Test 'Test\PR3349\Tests::testSameClassDependencyReversedOrder' started
+Test 'Test\PR3349\Tests::testSameClassDependencyReversedOrder' ended
+Test 'Test\PR3349\Tests::testSameClassDependencyReversedOrderDependency' started
+Test 'Test\PR3349\Tests::testSameClassDependencyReversedOrderDependency' ended
+Test 'Test\PR3349\Tests::testExternalClassDependencyCorrectOrder' started
+Test 'Test\PR3349\Tests::testExternalClassDependencyCorrectOrder' ended
+Test 'Test\PR3349\Tests::testExternalClassDependencyFailingSetUp' started
+Test 'Test\PR3349\Tests::testExternalClassDependencyFailingSetUp' ended
+Test 'Test\PR3349\Tests::testExternalClassChainedMultipleDependencies' started
+Test 'Test\PR3349\Tests::testExternalClassChainedMultipleDependencies' ended
+Test 'Test\PR3349\Tests::testExternalClassDependencyFailed' started
+Test 'Test\PR3349\Tests::testExternalClassDependencyFailed' ended
+Test 'Test\PR3349\Tests::testExternalClassDependencySkipped' started
+Test 'Test\PR3349\Tests::testExternalClassDependencySkipped' ended
+
+
+Time: %s, Memory: %s
+
+There were 4 skipped tests:
+
+1) Test\PR3349\Tests::testSameClassDependencyReversedOrder
+Reordering same class dependency function is not implemented. Please reorder "testSameClassDependencyReversedOrderDependency" before "testSameClassDependencyReversedOrder".
+
+2) Test\PR3349\Tests::testExternalClassDependencyFailingSetUp
+This test depends on "Test\PR3349\Test_FailingSetUp::testExternalClassDependencySuccessReturn100WithFailingSetUp" to pass.
+
+3) Test\PR3349\Tests::testExternalClassDependencyFailed
+This test depends on "Test\PR3349\Test_Dependencies::testExternalClassDependencyFailure" to pass.
+
+4) Test\PR3349\Tests::testExternalClassDependencySkipped
+This test depends on "Test\PR3349\Test_Dependencies::testExternalClassDependencySkipped" to pass.
+
+OK, but incomplete, skipped, or risky tests!
+Tests: 9, Assertions: 5, Skipped: 4.
