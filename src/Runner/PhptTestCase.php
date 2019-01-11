67553331f9562e968c159798cf8f158db0055a31
Ewout Pieter den Ouden <ewoutdenouden@gmail.com>
2019-01-11 15:25:48 +0100

    Files start on line 1

--- src/Runner/PhptTestCase.php
+++ src/Runner/PhptTestCase.php
@@ -666,7 +666,7 @@ class PhptTestCase implements Test, SelfDescribing
         if (empty($needle)) {
             return [
                 'file'     => \realpath($this->filename),
-                'line'     => 0,
+                'line'     => 1,
             ];
         }
 
@@ -722,7 +722,7 @@ class PhptTestCase implements Test, SelfDescribing
         // No section specified, show user start of code
         return [
             'file'     => \realpath($this->filename),
-            'line'     => 0,
+            'line'     => 1,
         ];
     }
 }
