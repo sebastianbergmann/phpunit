e338dc90fe8d23355ec8ddf20804d0c0bb7892af
Ewout Pieter den Ouden <ewoutdenouden@gmail.com>
2019-01-11 16:18:10 +0100

    Exploratory testing: give Windows CI something to chew on

--- src/Runner/PhptTestCase.php
+++ src/Runner/PhptTestCase.php
@@ -666,7 +666,7 @@ class PhptTestCase implements Test, SelfDescribing
         if (empty($needle)) {
             return [
                 'file'     => \realpath($this->filename),
-                'line'     => 1,
+                'line'     => 0,
             ];
         }
 
@@ -722,7 +722,7 @@ class PhptTestCase implements Test, SelfDescribing
         // No section specified, show user start of code
         return [
             'file'     => \realpath($this->filename),
-            'line'     => 1,
+            'line'     => 0,
         ];
     }
 }
