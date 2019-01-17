77e1eefb798bcfc5d770aabba9668413e3f9140a
Sebastian Bergmann <sb@sebastian-bergmann.de>
2019-01-17 17:35:35 +0100

    Bump

--- .phan/config.php
+++ .phan/config.php
@@ -3,5 +3,5 @@ return [
     'directory_list'                  => [ 'src/', 'vendor/' ],
     'exclude_analysis_directory_list' => [ 'vendor/', 'src/Framework/Assert/' ],
 
-    'target_php_version'              => '7.1',
+    'target_php_version'              => '7.2',
 ];
