09b7c66ffe35b142ff2eb9d4ab92faeaab7ac7c2
Sebastian Bergmann <sb@sebastian-bergmann.de>
2019-01-18 07:46:56 +0100

    Fix

--- tests/end-to-end/regression/GitHub/2591/bootstrapNoBootstrap.php
+++ tests/end-to-end/regression/GitHub/2591/bootstrapNoBootstrap.php
@@ -7,8 +7,8 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-\ini_set('display_errors', 0);
-\ini_set('log_errors', 1);
+\ini_set('display_errors', '0');
+\ini_set('log_errors', '1');
 
 $globalString                          = 'Hello';
 
