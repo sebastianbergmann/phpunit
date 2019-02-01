ea373d818246646bc1483d179ea12bbe293b4776
Guilliam Xavier <guilliamxavier@users.noreply.github.com>
2019-02-01 11:40:53 +0100

    Add numeric string failing test for StringEndsWith

--- tests/unit/Framework/Constraint/StringEndsWithTest.php
+++ tests/unit/Framework/Constraint/StringEndsWithTest.php
@@ -17,13 +17,29 @@ class StringEndsWithTest extends ConstraintTestCase
     public function testConstraintStringEndsWithCorrectValueAndReturnResult(): void
     {
         $constraint = new StringEndsWith('suffix');
+
         $this->assertTrue($constraint->evaluate('foosuffix', '', true));
     }
 
     public function testConstraintStringEndsWithNotCorrectValueAndReturnResult(): void
     {
         $constraint = new StringEndsWith('suffix');
+
         $this->assertFalse($constraint->evaluate('suffixerror', '', true));
+    }
+
+    public function testConstraintStringEndsWithCorrectNumericValueAndReturnResult(): void
+    {
+        $constraint = new StringEndsWith('0E1');
+
+        $this->assertTrue($constraint->evaluate('zzz0E1', '', true));
+    }
+
+    public function testConstraintStringEndsWithNotCorrectNumericValueAndReturnResult(): void
+    {
+        $constraint = new StringEndsWith('0E1');
+
+        $this->assertFalse($constraint->evaluate('zzz0E2', '', true));
     }
 
     public function testConstraintStringEndsWithToStringMethod(): void
