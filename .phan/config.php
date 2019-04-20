608d795a7deb6d41603042cb75de91561f099715
Sebastian Bergmann <sb@sebastian-bergmann.de>
2019-04-20 17:24:24 +0200

    Delete Phan configuration

--- .phan/config.php
+++
@@ -1,7 +0,0 @@
-<?php declare(strict_types=1);
-return [
-    'directory_list'                  => [ 'src/', 'vendor/' ],
-    'exclude_analysis_directory_list' => [ 'vendor/', 'src/Framework/Assert/' ],
-
-    'target_php_version'              => '7.1',
-];
