4872eefacceca4cf6dd57c12eac76a134bc488e3
t.buettner <thomas.buettner@flyeralarm.com>
2019-04-26 15:53:46 +0200

    Coverage for ArrayHasKey 100%

--- tests/unit/Framework/Constraint/ArrayHasKeyTest.php
+++ tests/unit/Framework/Constraint/ArrayHasKeyTest.php
@@ -64,4 +64,22 @@ EOF
 
         $this->fail();
     }
+
+    public function testConstraintArrayHasKey0(): void
+    {
+        $contraint = new ArrayHasKey(0);
+
+        try {
+            $contraint->evaluate(0, '');
+        } catch (ExpectationFailedException  $e) {
+            $this->assertEquals(
+                <<<EOF
+Failed asserting that an array has the key 0.
+
+EOF
+                ,
+                TestFailure::exceptionToString($e)
+            );
+        }
+    }
 }
