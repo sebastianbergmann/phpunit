c455c1770f280e92d0bc6cc011538be4c94e8032
t.buettner <thomas.buettner@flyeralarm.com>
2019-04-27 11:48:18 +0200

    Coverage for LogicalOr Complete

--- tests/unit/Framework/Constraint/LogicalAndTest.php
+++ tests/unit/Framework/Constraint/LogicalAndTest.php
@@ -18,25 +18,6 @@ use PHPUnit\Framework\TestFailure;
  */
 final class LogicalAndTest extends ConstraintTestCase
 {
-    public function testFromConstraintsReturnsConstraint(): void
-    {
-        $count = 5;
-
-        $constraints = \array_map(function () {
-            static $count = 0;
-
-            $constraint = $this->getMockBuilder(Constraint::class)->getMock();
-
-            ++$count;
-
-            return $constraint;
-        }, \array_fill(0, $count, null));
-
-        $constraint = LogicalAnd::fromConstraints(...$constraints);
-
-        $this->assertInstanceOf(LogicalAnd::class, $constraint);
-    }
-
     public function testSetConstraintsRejectsInvalidConstraint(): void
     {
         $constraints = [
@@ -89,9 +70,7 @@ final class LogicalAndTest extends ConstraintTestCase
             return \NamedConstraint::fromName($name);
         }, $names);
 
-        $constraint = new LogicalAnd;
-
-        $constraint->setConstraints($constraints);
+        $constraint = LogicalAnd::fromConstraints(...$constraints);
 
         $expected = \implode(' and ', $names);
 
