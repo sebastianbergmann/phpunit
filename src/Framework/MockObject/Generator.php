278379abe717aca5f93f58c6695efdb0b92dd0ab
Sergei Morozov <morozov@tut.by>
2019-05-16 09:39:57 -0700

    Do not ignore constructor arguments based on type being mocked

--- src/Framework/MockObject/Generator.php
+++ src/Framework/MockObject/Generator.php
@@ -593,9 +593,7 @@ class Generator
     {
         $this->evalClass($code, $className);
 
-        if ($callOriginalConstructor &&
-            \is_string($type) &&
-            !\interface_exists($type, $callAutoload)) {
+        if ($callOriginalConstructor) {
             if (\count($arguments) === 0) {
                 $object = new $className;
             } else {
