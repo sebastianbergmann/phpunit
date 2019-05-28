9742ff641a2603f6bab93b6f21ff30295b38026f
Noah Gary <noah.gary@healthcareassetnetwork.com>
2019-05-28 17:41:35 -0400

    This now prints unique logfile names if the user sets <log timestamps="true"> in the phpunit.xml file.

--- src/Util/Configuration.php
+++ src/Util/Configuration.php
@@ -272,7 +272,18 @@ final class Configuration
             \assert($log instanceof DOMElement);
 
             $type   = (string) $log->getAttribute('type');
-            $target = (string) $log->getAttribute('target');
+            if ($log->getAttribute('timestamps') == true)
+                //User wants timestamps prefixing the logfile name
+            {
+                $targetArray = explode("/", $log->getAttribute('target'));
+                $end = sizeof($targetArray)-1;
+                //prefix unix timestamp
+                //add unique identifier to allow for concurrent phpunit tests to be run without collisions on
+                $targetArray[$end] = time() . "-" . uniqid() . "-" . $targetArray[$end];
+                $target = (string)implode("/",$targetArray);
+            } else {
+                $target = (string)$log->getAttribute('target');
+            }
 
             if (!$target) {
                 continue;
