a967a09dac7cae5455a07db8cede0ce6bccb7bf9
Marco Pivetta <ocramius@gmail.com>
2019-06-05 23:13:49 +0200

    Corrected type-check errors detected by Psalm run over the whole codebase

--- src/Framework/TestCase.php
+++ src/Framework/TestCase.php
@@ -1432,7 +1432,7 @@ abstract class TestCase extends Assert implements SelfDescribing, Test
      * @throws Exception
      *
      * @psalm-template RealInstanceType of object
-     * @psalm-param class-string<RealInstanceType>|string[] $originalClassName
+     * @psalm-param class-string<RealInstanceType> $originalClassName
      * @psalm-return MockObject&RealInstanceType
      */
     protected function createTestProxy(string $originalClassName, array $constructorArguments = []): MockObject
@@ -1510,7 +1510,7 @@ abstract class TestCase extends Assert implements SelfDescribing, Test
      * @throws Exception
      *
      * @psalm-template RealInstanceType of object
-     * @psalm-param class-string<RealInstanceType>|string[] $originalClassName
+     * @psalm-param class-string<RealInstanceType> $originalClassName
      * @psalm-return MockObject&RealInstanceType
      */
     protected function getMockForAbstractClass($originalClassName, array $arguments = [], $mockClassName = '', $callOriginalConstructor = true, $callOriginalClone = true, $callAutoload = true, $mockedMethods = [], $cloneArguments = false): MockObject
