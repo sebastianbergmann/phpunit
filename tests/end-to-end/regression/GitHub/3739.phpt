688e4bd7a24b2ea19ed0913cde372ec435204447
Sebastian Bergmann <sb@sebastian-bergmann.de>
2019-06-27 13:48:42 +0200

    Expand test case

--- tests/end-to-end/regression/GitHub/3739.phpt
+++ tests/end-to-end/regression/GitHub/3739.phpt
@@ -12,8 +12,17 @@ PHPUnit\TextUI\Command::main();
 --EXPECTF--
 PHPUnit %s by Sebastian Bergmann and contributors.
 
-.                                                                   1 / 1 (100%)
+.E                                                                  2 / 2 (100%)
 
 Time: %s, Memory: %s
 
-OK (1 test, 1 assertion)
+There was 1 error:
+
+1) Issue3739\Issue3739Test::testWithoutErrorSuppression
+unlink(%s/end-to-end/regression/GitHub/3739/DOES_NOT_EXIST): No such file or directory
+
+%s/Issue3739Test.php:15
+%s/Issue3739Test.php:28
+
+ERRORS!
+Tests: 2, Assertions: 1, Errors: 1.
--- tests/end-to-end/regression/GitHub/3739/Issue3739Test.php
+++ tests/end-to-end/regression/GitHub/3739/Issue3739Test.php
@@ -5,16 +5,26 @@ use PHPUnit\Framework\TestCase;
 
 class Issue3739
 {
-    public function unlinkFileThatDoesNotExist(): bool
+    public function unlinkFileThatDoesNotExistWithErrorSuppression(): bool
     {
         return @unlink(__DIR__ . '/DOES_NOT_EXIST');
+    }
+
+    public function unlinkFileThatDoesNotExistWithoutErrorSuppression(): bool
+    {
+        return unlink(__DIR__ . '/DOES_NOT_EXIST');
     }
 }
 
 final class Issue3739Test extends TestCase
 {
-    public function testOne(): void
+    public function testWithErrorSuppression(): void
     {
-        $this->assertFalse((new Issue3739())->unlinkFileThatDoesNotExist());
+        $this->assertFalse((new Issue3739())->unlinkFileThatDoesNotExistWithErrorSuppression());
+    }
+
+    public function testWithoutErrorSuppression(): void
+    {
+        $this->assertFalse((new Issue3739())->unlinkFileThatDoesNotExistWithoutErrorSuppression());
     }
 }
