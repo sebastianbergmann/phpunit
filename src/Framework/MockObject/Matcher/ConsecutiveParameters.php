72098d80f0cfc06c7e0652d331602685ce5b4b51
ShiraNai7 <shiranai7@hotmail.com>
2019-08-28 12:34:04 +0200

    Reject non-iterable parameter groups

--- src/Framework/MockObject/Matcher/ConsecutiveParameters.php
+++ src/Framework/MockObject/Matcher/ConsecutiveParameters.php
@@ -12,6 +12,7 @@ namespace PHPUnit\Framework\MockObject\Matcher;
 use PHPUnit\Framework\Constraint\Constraint;
 use PHPUnit\Framework\Constraint\IsEqual;
 use PHPUnit\Framework\ExpectationFailedException;
+use PHPUnit\Framework\InvalidParameterGroupException;
 use PHPUnit\Framework\MockObject\Invocation as BaseInvocation;
 
 /**
@@ -42,6 +43,16 @@ class ConsecutiveParameters extends StatelessInvocation
     public function __construct(array $parameterGroups)
     {
         foreach ($parameterGroups as $index => $parameters) {
+            if (!\is_iterable($parameters)) {
+                throw new InvalidParameterGroupException(
+                    \sprintf(
+                        'Parameter group #%d must be an array or Traversable, got %s',
+                        $index,
+                        \gettype($parameters)
+                    )
+                );
+            }
+
             foreach ($parameters as $parameter) {
                 if (!$parameter instanceof Constraint) {
                     $parameter = new IsEqual($parameter);
--- tests/unit/Framework/MockObject/Matcher/ConsecutiveParametersTest.php
+++ tests/unit/Framework/MockObject/Matcher/ConsecutiveParametersTest.php
@@ -9,6 +9,7 @@
  */
 
 use PHPUnit\Framework\ExpectationFailedException;
+use PHPUnit\Framework\InvalidParameterGroupException;
 use PHPUnit\Framework\TestCase;
 
 class ConsecutiveParametersTest extends TestCase
@@ -64,5 +65,22 @@ class ConsecutiveParametersTest extends TestCase
         $this->expectException(ExpectationFailedException::class);
 
         $mock->foo('invalid');
+    }
+
+    public function testIntegrationFailsWithNonIterableParameterGroup(): void
+    {
+        $mock = $this->getMockBuilder(stdClass::class)
+            ->setMethods(['foo'])
+            ->getMock();
+
+        $this->expectException(InvalidParameterGroupException::class);
+        $this->expectExceptionMessage('Parameter group #1 must be an array or Traversable, got object');
+
+        $mock->expects($this->any())
+            ->method('foo')
+            ->withConsecutive(
+                ['bar'],
+                $this->identicalTo([21, 42]) // this is not an array
+            );
     }
 }
