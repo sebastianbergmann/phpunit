7d5db729ade90c8e1cf06b1cdfee9549e0997351
Marco Pivetta <ocramius@gmail.com>
2019-09-06 11:58:40 +0200

    #3120 pseudo-docs for `:self` for when we can upgrade to PHP 7.4
    `InvocationStubber` methods cannot have return type declarations until
    then.

--- src/Framework/MockObject/Builder/InvocationStubber.php
+++ src/Framework/MockObject/Builder/InvocationStubber.php
@@ -17,46 +17,46 @@ interface InvocationStubber
     public function will(Stub $stub): Identity;
 
     /** @return self */
-    public function willReturn($value, ...$nextValues);
+    public function willReturn($value, ...$nextValues) /*: self */;
 
     /**
      * @param mixed $reference
      *
      * @return self
      */
-    public function willReturnReference(&$reference);
+    public function willReturnReference(&$reference) /*: self */;
 
     /**
      * @param array<int, array<int, mixed>> $valueMap
      *
      * @return self
      */
-    public function willReturnMap(array $valueMap);
+    public function willReturnMap(array $valueMap) /*: self */;
 
     /**
      * @param int $argumentIndex
      *
      * @return self
      */
-    public function willReturnArgument($argumentIndex);
+    public function willReturnArgument($argumentIndex) /*: self */;
 
     /**
      * @param callable $callback
      *
      * @return self
      */
-    public function willReturnCallback($callback);
+    public function willReturnCallback($callback) /*: self */;
 
     /** @return self */
-    public function willReturnSelf();
+    public function willReturnSelf() /*: self */;
 
     /**
      * @param mixed $values
      *
      * @return self
      */
-    public function willReturnOnConsecutiveCalls(...$values);
+    public function willReturnOnConsecutiveCalls(...$values) /*: self */;
 
     /** @return self */
-    public function willThrowException(\Throwable $exception);
+    public function willThrowException(\Throwable $exception) /*: self */;
 }
