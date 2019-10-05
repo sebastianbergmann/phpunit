8ac4d54b6c1ceb2d4acb9a6d6e5773d2e2672a20
Andreas Möller <am@localheinz.com>
2019-10-05 20:12:39 +0200

    Enhancement: Add test for UnsupportedEvent
    Co-authored-by: Andreas Möller <am@localheinz.com>
    Co-authored-by: Arne Blankerts <Arne@Blankerts.de>

---
+++ tests/unit/Event/UnsupportedEventTest.php
@@ -0,0 +1,52 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of PHPUnit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace PHPUnit\Event;
+
+use function sprintf;
+use Exception;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \PHPUnit\Event\UnsupportedEvent
+ */
+final class UnsupportedEventTest extends TestCase
+{
+    public function testTypeReturnsUnsupportedEvent(): void
+    {
+        $type = new class('foo') implements Type {
+            private string $name;
+
+            public function __construct(string $name)
+            {
+                $this->name = $name;
+            }
+
+            public function is(Type $other): bool
+            {
+                return $other->asString() === $this->asString();
+            }
+
+            public function asString(): string
+            {
+                return $this->name;
+            }
+        };
+
+        $exception = UnsupportedEvent::type($type);
+
+        $message = sprintf(
+            'Type "%s" not supported',
+            $type->asString()
+        );
+
+        $this->assertInstanceOf(Exception::class, $exception);
+        $this->assertSame($message, $exception->getMessage());
+    }
+}
