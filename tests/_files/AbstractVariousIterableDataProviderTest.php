48d3752fcdf7581313afcb23f08aea5e30ce15e8
Wout Gevaert <woutgevaert@gmail.com>
2019-10-06 23:03:19 +0200

    Make test to test abstract dataproviders and dataproviders in abstract classes

---
+++ tests/_files/AbstractVariousIterableDataProviderTest.php
@@ -0,0 +1,56 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of PHPUnit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+abstract class AbstractVariousIterableDataProviderTest
+{
+    abstract public function asArrayProvider();
+
+    abstract public function asIteratorProvider();
+
+    abstract public function asTraversableProvider();
+
+    public function asArrayProviderInParent()
+    {
+        return [
+            ['J'],
+            ['K'],
+            ['L'],
+        ];
+    }
+
+    public function asIteratorProviderInParent()
+    {
+        yield ['M'];
+
+        yield ['N'];
+
+        yield ['O'];
+    }
+
+    public function asTraversableProviderInParent()
+    {
+        return new WrapperIteratorAggregate([
+            ['P'],
+            ['Q'],
+            ['R'],
+        ]);
+    }
+
+    /**
+     * @dataProvider asArrayProvider
+     * @dataProvider asIteratorProvider
+     * @dataProvider asTraversableProvider
+     * @dataProvider asArrayProviderInParent
+     * @dataProvider asIteratorProviderInParent
+     * @dataProvider asTraversableProviderInParent
+     */
+    public function testAbstract(): void
+    {
+    }
+}
