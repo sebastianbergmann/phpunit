a635c59c67b70438f163862e8acfa3c53679c06f
Florian Engelhardt <florian.engelhardt@oxid-esales.com>
2019-10-10 11:37:07 +0200

    fix regression #3889

---
+++ tests/end-to-end/regression/GitHub/3889/Issue3889Test.test.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of PHPUnit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+use PHPUnit\Framework\TestCase;
+
+final class Issue3889Test extends TestCase
+{
+    public function testOne(): void
+    {
+        $this->assertTrue(true);
+    }
+}
