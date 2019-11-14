11e235b95ead437584f8eaecc91fcb32b2bf7864
Arne Blankerts <Arne@Blankerts.de>
2019-11-14 13:30:58 +0100

    Drop 'get' prefix
    Co-authored-by: Andreas Möller <am@localheinz.com>
    Co-authored-by: Arne Blankerts <Arne@Blankerts.de>

--- src/Event/Emitter.php
+++ src/Event/Emitter.php
@@ -34,200 +34,200 @@ final class Emitter
 
     public function applicationConfigured(): void
     {
-        $this->dispatcher->dispatch(new Application\Configured($this->getTelemetryInfo()));
+        $this->dispatcher->dispatch(new Application\Configured($this->telemetryInfo()));
     }
 
     public function applicationStarted(): void
     {
-        $this->dispatcher->dispatch(new Application\Started($this->getTelemetryInfo()));
+        $this->dispatcher->dispatch(new Application\Started($this->telemetryInfo()));
     }
 
     public function assertionMade(): void
     {
-        $this->dispatcher->dispatch(new Assertion\Made($this->getTelemetryInfo()));
+        $this->dispatcher->dispatch(new Assertion\Made($this->telemetryInfo()));
     }
 
     public function bootstrapFinished(): void
     {
-        $this->dispatcher->dispatch(new Bootstrap\Finished($this->getTelemetryInfo()));
+        $this->dispatcher->dispatch(new Bootstrap\Finished($this->telemetryInfo()));
     }
 
     public function comparatorRegistered(): void
     {
-        $this->dispatcher->dispatch(new Comparator\Registered($this->getTelemetryInfo()));
+        $this->dispatcher->dispatch(new Comparator\Registered($this->telemetryInfo()));
     }
 
     public function extensionLoaded(): void
     {
-        $this->dispatcher->dispatch(new Extension\Loaded($this->getTelemetryInfo()));
+        $this->dispatcher->dispatch(new Extension\Loaded($this->telemetryInfo()));
     }
 
     public function globalStateCaptured(): void
     {
-        $this->dispatcher->dispatch(new GlobalState\Captured($this->getTelemetryInfo()));
+        $this->dispatcher->dispatch(new GlobalState\Captured($this->telemetryInfo()));
     }
 
     public function globalStateModified(): void
     {
-        $this->dispatcher->dispatch(new GlobalState\Modified($this->getTelemetryInfo()));
+        $this->dispatcher->dispatch(new GlobalState\Modified($this->telemetryInfo()));
     }
 
     public function globalStateRestored(): void
     {
-        $this->dispatcher->dispatch(new GlobalState\Restored($this->getTelemetryInfo()));
+        $this->dispatcher->dispatch(new GlobalState\Restored($this->telemetryInfo()));
     }
 
     public function testRunConfigured(): void
     {
-        $this->dispatcher->dispatch(new Test\RunConfigured($this->getTelemetryInfo()));
+        $this->dispatcher->dispatch(new Test\RunConfigured($this->telemetryInfo()));
     }
 
     public function testRunErrored(): void
     {
-        $this->dispatcher->dispatch(new Test\RunErrored($this->getTelemetryInfo()));
+        $this->dispatcher->dispatch(new Test\RunErrored($this->telemetryInfo()));
     }
 
     public function testRunFailed(): void
     {
-        $this->dispatcher->dispatch(new Test\RunFailed($this->getTelemetryInfo()));
+        $this->dispatcher->dispatch(new Test\RunFailed($this->telemetryInfo()));
     }
 
     public function testRunFinished(): void
     {
-        $this->dispatcher->dispatch(new Test\RunFinished($this->getTelemetryInfo()));
+        $this->dispatcher->dispatch(new Test\RunFinished($this->telemetryInfo()));
     }
 
     public function testRunPassed(): void
     {
-        $this->dispatcher->dispatch(new Test\RunPassed($this->getTelemetryInfo()));
+        $this->dispatcher->dispatch(new Test\RunPassed($this->telemetryInfo()));
     }
 
     public function testRunRisky(): void
     {
-        $this->dispatcher->dispatch(new Test\RunRisky($this->getTelemetryInfo()));
+        $this->dispatcher->dispatch(new Test\RunRisky($this->telemetryInfo()));
     }
 
     public function testRunSkippedByDataProvider(): void
     {
-        $this->dispatcher->dispatch(new Test\RunSkippedByDataProvider($this->getTelemetryInfo()));
+        $this->dispatcher->dispatch(new Test\RunSkippedByDataProvider($this->telemetryInfo()));
     }
 
     public function testRunSkippedIncomplete(): void
     {
-        $this->dispatcher->dispatch(new Test\RunSkippedIncomplete($this->getTelemetryInfo()));
+        $this->dispatcher->dispatch(new Test\RunSkippedIncomplete($this->telemetryInfo()));
     }
 
     public function testRunSkippedWithFailedRequirements(): void
     {
-        $this->dispatcher->dispatch(new Test\RunSkippedWithFailedRequirements($this->getTelemetryInfo()));
+        $this->dispatcher->dispatch(new Test\RunSkippedWithFailedRequirements($this->telemetryInfo()));
     }
 
     public function testRunSkippedWithWarning(): void
     {
-        $this->dispatcher->dispatch(new Test\RunSkippedWithWarning($this->getTelemetryInfo()));
+        $this->dispatcher->dispatch(new Test\RunSkippedWithWarning($this->telemetryInfo()));
     }
 
     public function testRunStarted(): void
     {
-        $this->dispatcher->dispatch(new Test\RunStarted($this->getTelemetryInfo()));
+        $this->dispatcher->dispatch(new Test\RunStarted($this->telemetryInfo()));
     }
 
     public function testSetUpFinished(): void
     {
-        $this->dispatcher->dispatch(new Test\SetUpFinished($this->getTelemetryInfo()));
+        $this->dispatcher->dispatch(new Test\SetUpFinished($this->telemetryInfo()));
     }
 
     public function testTearDownFinished(): void
     {
-        $this->dispatcher->dispatch(new Test\TearDownFinished($this->getTelemetryInfo()));
+        $this->dispatcher->dispatch(new Test\TearDownFinished($this->telemetryInfo()));
     }
 
     public function testCaseAfterClassFinished(): void
     {
-        $this->dispatcher->dispatch(new TestCase\AfterClassFinished($this->getTelemetryInfo()));
+        $this->dispatcher->dispatch(new TestCase\AfterClassFinished($this->telemetryInfo()));
     }
 
     public function testCaseBeforeClassFinished(): void
     {
-        $this->dispatcher->dispatch(new TestCase\BeforeClassFinished($this->getTelemetryInfo()));
+        $this->dispatcher->dispatch(new TestCase\BeforeClassFinished($this->telemetryInfo()));
     }
 
     public function testCaseSetUpBeforeClassFinished(): void
     {
-        $this->dispatcher->dispatch(new TestCase\SetUpBeforeClassFinished($this->getTelemetryInfo()));
+        $this->dispatcher->dispatch(new TestCase\SetUpBeforeClassFinished($this->telemetryInfo()));
     }
 
     public function testCaseSetUpFinished(): void
     {
-        $this->dispatcher->dispatch(new TestCase\SetUpFinished($this->getTelemetryInfo()));
+        $this->dispatcher->dispatch(new TestCase\SetUpFinished($this->telemetryInfo()));
     }
 
     public function testCaseTearDownAfterClassFinished(): void
     {
-        $this->dispatcher->dispatch(new TestCase\TearDownAfterClassFinished($this->getTelemetryInfo()));
+        $this->dispatcher->dispatch(new TestCase\TearDownAfterClassFinished($this->telemetryInfo()));
     }
 
     public function testDoubleMockCreated(): void
     {
-        $this->dispatcher->dispatch(new TestDouble\MockCreated($this->getTelemetryInfo()));
+        $this->dispatcher->dispatch(new TestDouble\MockCreated($this->telemetryInfo()));
     }
 
     public function testDoubleMockForTraitCreated(): void
     {
-        $this->dispatcher->dispatch(new TestDouble\MockForTraitCreated($this->getTelemetryInfo()));
+        $this->dispatcher->dispatch(new TestDouble\MockForTraitCreated($this->telemetryInfo()));
     }
 
     public function testDoublePartialMockCreated(): void
     {
-        $this->dispatcher->dispatch(new TestDouble\PartialMockCreated($this->getTelemetryInfo()));
+        $this->dispatcher->dispatch(new TestDouble\PartialMockCreated($this->telemetryInfo()));
     }
 
     public function testDoubleProphecyCreated(): void
     {
-        $this->dispatcher->dispatch(new TestDouble\ProphecyCreated($this->getTelemetryInfo()));
+        $this->dispatcher->dispatch(new TestDouble\ProphecyCreated($this->telemetryInfo()));
     }
 
     public function testDoubleTestProxyCreated(): void
     {
-        $this->dispatcher->dispatch(new TestDouble\TestProxyCreated($this->getTelemetryInfo()));
+        $this->dispatcher->dispatch(new TestDouble\TestProxyCreated($this->telemetryInfo()));
     }
 
     public function testSuiteAfterClassFinished(): void
     {
-        $this->dispatcher->dispatch(new TestSuite\AfterClassFinished($this->getTelemetryInfo()));
+        $this->dispatcher->dispatch(new TestSuite\AfterClassFinished($this->telemetryInfo()));
     }
 
     public function testSuiteBeforeClassFinished(): void
     {
-        $this->dispatcher->dispatch(new TestSuite\BeforeClassFinished($this->getTelemetryInfo()));
+        $this->dispatcher->dispatch(new TestSuite\BeforeClassFinished($this->telemetryInfo()));
     }
 
     public function testSuiteConfigured(): void
     {
-        $this->dispatcher->dispatch(new TestSuite\Configured($this->getTelemetryInfo()));
+        $this->dispatcher->dispatch(new TestSuite\Configured($this->telemetryInfo()));
     }
 
     public function testSuiteLoaded(): void
     {
-        $this->dispatcher->dispatch(new TestSuite\Loaded($this->getTelemetryInfo()));
+        $this->dispatcher->dispatch(new TestSuite\Loaded($this->telemetryInfo()));
     }
 
     public function testSuiteRunFinished(): void
     {
-        $this->dispatcher->dispatch(new TestSuite\RunFinished($this->getTelemetryInfo()));
+        $this->dispatcher->dispatch(new TestSuite\RunFinished($this->telemetryInfo()));
     }
 
     public function testSuiteRunStarted(): void
     {
-        $this->dispatcher->dispatch(new TestSuite\RunStarted($this->getTelemetryInfo()));
+        $this->dispatcher->dispatch(new TestSuite\RunStarted($this->telemetryInfo()));
     }
 
     public function testSuiteSorted(): void
     {
-        $this->dispatcher->dispatch(new TestSuite\Sorted($this->getTelemetryInfo()));
+        $this->dispatcher->dispatch(new TestSuite\Sorted($this->telemetryInfo()));
     }
 
-    private function getTelemetryInfo(): Info
+    private function telemetryInfo(): Info
     {
         $current = $this->system->snapshot();
 
