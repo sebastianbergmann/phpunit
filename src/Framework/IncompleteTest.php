a72a88e588a4c491fbd1346b7e118af34aef7220
Sebastian Bergmann <sb@sebastian-bergmann.de>
2019-11-26 06:45:28 +0100

    Leftover from da34f4f8f0071b5d1eb6255dc5ebe347f36d1f80

--- src/Framework/IncompleteTest.php
+++ src/Framework/IncompleteTest.php
@@ -12,6 +12,6 @@ namespace PHPUnit\Framework;
 /**
  * @internal This class is not covered by the backward compatibility promise for PHPUnit
  */
-interface IncompleteTest
+interface IncompleteTest extends \Throwable
 {
 }
