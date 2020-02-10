a1542a4da63fd02fe9452836b32850a7e8e12634
Florian Engelhardt <flo@dotbox.org>
2020-02-10 20:40:39 +0100

    Add deprecation notice to usage output

--- src/TextUI/Help.php
+++ src/TextUI/Help.php
@@ -91,7 +91,7 @@ final class Help
             ['arg'    => '--debug', 'desc' => 'Display debugging information'],
             ['spacer' => ''],
 
-            ['arg'    => '--loader <loader>', 'desc' => 'TestSuiteLoader implementation to use'],
+            ['arg'    => '--loader <loader>', 'desc' => 'TestSuiteLoader implementation to use. Deprecated, see https://github.com/sebastianbergmann/phpunit/issues/4039'],
             ['arg'    => '--repeat <times>', 'desc' => 'Runs the test(s) repeatedly'],
             ['arg'    => '--teamcity', 'desc' => 'Report test execution progress in TeamCity format'],
             ['arg'    => '--testdox', 'desc' => 'Report test execution progress in TestDox format'],
