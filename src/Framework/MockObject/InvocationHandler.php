sebastianbergmann/phpunit

commit 4f0b297aec516b73c69817daf6691778ea8ec113

Consistently use fully-qualified names of built-in classes and interfaces

src/Framework/MockObject/InvocationHandler.php
@@ -9,7 +9,6 @@
  */
 namespace PHPUnit\Framework\MockObject;
 
-use Exception;
 use PHPUnit\Framework\ExpectationFailedException;
 use PHPUnit\Framework\MockObject\Builder\InvocationMocker;
 use PHPUnit\Framework\MockObject\Rule\InvocationOrder;

build/scripts/generate-global-assert-wrappers.php
@@ -20,11 +20,6 @@ $buffer = '<?php declare(strict_types=1);
  */
 namespace PHPUnit\Framework;
 
-use ArrayAccess;
-use Countable;
-use DOMDocument;
-use DOMElement;
-use Throwable;
 use PHPUnit\Framework\Constraint\Constraint;
 use PHPUnit\Framework\ExpectationFailedException;
 use PHPUnit\Framework\MockObject\Rule\AnyInvokedCount as AnyInvokedCountMatcher;

src/Framework/Assert.php
@@ -9,10 +9,6 @@
  */
 namespace PHPUnit\Framework;
 
-use ArrayAccess;
-use Countable;
-use DOMDocument;
-use DOMElement;
 use PHPUnit\Framework\Constraint\ArrayHasKey;
 use PHPUnit\Framework\Constraint\Callback;
 use PHPUnit\Framework\Constraint\ClassHasAttribute;
@@ -58,7 +54,6 @@ use PHPUnit\Framework\Constraint\TraversableContainsIdentical;
 use PHPUnit\Framework\Constraint\TraversableContainsOnly;
 use PHPUnit\Util\Type;
 use PHPUnit\Util\Xml;
-use Traversable;
 
 /**
  * A set of assertion methods.
@@ -73,8 +68,8 @@ abstract class Assert
     /**
      * Asserts that an array has a specified key.
      *
-     * @param int|string        $key
-     * @param array|ArrayAccess $array
+     * @param int|string         $key
+     * @param array|\ArrayAccess $array
      *
      * @throws ExpectationFailedException
      * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
@@ -89,7 +84,7 @@ abstract class Assert
             );
         }
 
-        if (!(\is_array($array) || $array instanceof ArrayAccess)) {
+        if (!(\is_array($array) || $array instanceof \ArrayAccess)) {
             throw InvalidArgumentException::create(
                 2,
                 'array or ArrayAccess'
@@ -104,8 +99,8 @@ abstract class Assert
     /**
      * Asserts that an array does not have a specified key.
      *
-     * @param int|string        $key
-     * @param array|ArrayAccess $array
+     * @param int|string         $key
+     * @param array|\ArrayAccess $array
      *
      * @throws ExpectationFailedException
      * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
@@ -120,7 +115,7 @@ abstract class Assert
             );
         }
 
-        if (!(\is_array($array) || $array instanceof ArrayAccess)) {
+        if (!(\is_array($array) || $array instanceof \ArrayAccess)) {
             throw InvalidArgumentException::create(
                 2,
                 'array or ArrayAccess'
@@ -245,7 +240,7 @@ abstract class Assert
     /**
      * Asserts the number of elements of an array, Countable or Traversable.
      *
-     * @param Countable|iterable $haystack
+     * @param \Countable|iterable $haystack
      *
      * @throws ExpectationFailedException
      * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
@@ -253,7 +248,7 @@ abstract class Assert
      */
     public static function assertCount(int $expectedCount, $haystack, string $message = ''): void
     {
-        if (!$haystack instanceof Countable && !\is_iterable($haystack)) {
+        if (!$haystack instanceof \Countable && !\is_iterable($haystack)) {
             throw InvalidArgumentException::create(2, 'countable or iterable');
         }
 
@@ -267,7 +262,7 @@ abstract class Assert
     /**
      * Asserts the number of elements of an array, Countable or Traversable.
      *
-     * @param Countable|iterable $haystack
+     * @param \Countable|iterable $haystack
      *
      * @throws ExpectationFailedException
      * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
@@ -275,7 +270,7 @@ abstract class Assert
      */
     public static function assertNotCount(int $expectedCount, $haystack, string $message = ''): void
     {
-        if (!$haystack instanceof Countable && !\is_iterable($haystack)) {
+        if (!$haystack instanceof \Countable && !\is_iterable($haystack)) {
             throw InvalidArgumentException::create(2, 'countable or iterable');
         }
 
@@ -1815,8 +1810,8 @@ abstract class Assert
      * Assert that the size of two arrays (or `Countable` or `Traversable` objects)
      * is the same.
      *
-     * @param Countable|iterable $expected
-     * @param Countable|iterable $actual
+     * @param \Countable|iterable $expected
+     * @param \Countable|iterable $actual
      *
      * @throws ExpectationFailedException
      * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
@@ -1824,11 +1819,11 @@ abstract class Assert
      */
     public static function assertSameSize($expected, $actual, string $message = ''): void
     {
-        if (!$expected instanceof Countable && !\is_iterable($expected)) {
+        if (!$expected instanceof \Countable && !\is_iterable($expected)) {
             throw InvalidArgumentException::create(1, 'countable or iterable');
         }
 
-        if (!$actual instanceof Countable && !\is_iterable($actual)) {
+        if (!$actual instanceof \Countable && !\is_iterable($actual)) {
             throw InvalidArgumentException::create(2, 'countable or iterable');
         }
 
@@ -1843,8 +1838,8 @@ abstract class Assert
      * Assert that the size of two arrays (or `Countable` or `Traversable` objects)
      * is not the same.
      *
-     * @param Countable|iterable $expected
-     * @param Countable|iterable $actual
+     * @param \Countable|iterable $expected
+     * @param \Countable|iterable $actual
      *
      * @throws ExpectationFailedException
      * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
@@ -1852,11 +1847,11 @@ abstract class Assert
      */
     public static function assertNotSameSize($expected, $actual, string $message = ''): void
     {
-        if (!$expected instanceof Countable && !\is_iterable($expected)) {
+        if (!$expected instanceof \Countable && !\is_iterable($expected)) {
             throw InvalidArgumentException::create(1, 'countable or iterable');
         }
 
-        if (!$actual instanceof Countable && !\is_iterable($actual)) {
+        if (!$actual instanceof \Countable && !\is_iterable($actual)) {
             throw InvalidArgumentException::create(2, 'countable or iterable');
         }
 
@@ -2073,7 +2068,7 @@ abstract class Assert
     /**
      * Asserts that two XML documents are equal.
      *
-     * @param DOMDocument|string $actualXml
+     * @param \DOMDocument|string $actualXml
      *
      * @throws ExpectationFailedException
      * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
@@ -2090,7 +2085,7 @@ abstract class Assert
     /**
      * Asserts that two XML documents are not equal.
      *
-     * @param DOMDocument|string $actualXml
+     * @param \DOMDocument|string $actualXml
      *
      * @throws ExpectationFailedException
      * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
@@ -2107,8 +2102,8 @@ abstract class Assert
     /**
      * Asserts that two XML documents are equal.
      *
-     * @param DOMDocument|string $expectedXml
-     * @param DOMDocument|string $actualXml
+     * @param \DOMDocument|string $expectedXml
+     * @param \DOMDocument|string $actualXml
      *
      * @throws ExpectationFailedException
      * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
@@ -2125,8 +2120,8 @@ abstract class Assert
     /**
      * Asserts that two XML documents are not equal.
      *
-     * @param DOMDocument|string $expectedXml
-     * @param DOMDocument|string $actualXml
+     * @param \DOMDocument|string $expectedXml
+     * @param \DOMDocument|string $actualXml
      *
      * @throws ExpectationFailedException
      * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
@@ -2151,7 +2146,7 @@ abstract class Assert
      *
      * @deprecated https://github.com/sebastianbergmann/phpunit/issues/4091
      */
-    public static function assertEqualXMLStructure(DOMElement $expectedElement, DOMElement $actualElement, bool $checkAttributes = false, string $message = ''): void
+    public static function assertEqualXMLStructure(\DOMElement $expectedElement, \DOMElement $actualElement, bool $checkAttributes = false, string $message = ''): void
     {
         self::createWarning('assertEqualXMLStructure() is deprecated and will be removed in PHPUnit 10.');
 

src/Framework/Assert/Functions.php
@@ -9,10 +9,6 @@
  */
 namespace PHPUnit\Framework;
 
-use ArrayAccess;
-use Countable;
-use DOMDocument;
-use DOMElement;
 use PHPUnit\Framework\Constraint\ArrayHasKey;
 use PHPUnit\Framework\Constraint\Callback;
 use PHPUnit\Framework\Constraint\ClassHasAttribute;
@@ -67,13 +63,12 @@ use PHPUnit\Framework\MockObject\Stub\ReturnCallback as ReturnCallbackStub;
 use PHPUnit\Framework\MockObject\Stub\ReturnSelf as ReturnSelfStub;
 use PHPUnit\Framework\MockObject\Stub\ReturnStub;
 use PHPUnit\Framework\MockObject\Stub\ReturnValueMap as ReturnValueMapStub;
-use Throwable;
 
 /**
  * Asserts that an array has a specified key.
  *
- * @param int|string        $key
- * @param array|ArrayAccess $array
+ * @param int|string         $key
+ * @param array|\ArrayAccess $array
  *
  * @throws ExpectationFailedException
  * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
@@ -89,8 +84,8 @@ function assertArrayHasKey($key, $array, string $message = ''): void
 /**
  * Asserts that an array does not have a specified key.
  *
- * @param int|string        $key
- * @param array|ArrayAccess $array
+ * @param int|string         $key
+ * @param array|\ArrayAccess $array
  *
  * @throws ExpectationFailedException
  * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
@@ -183,7 +178,7 @@ function assertNotContainsOnly(string $type, iterable $haystack, ?bool $isNative
 /**
  * Asserts the number of elements of an array, Countable or Traversable.
  *
- * @param Countable|iterable $haystack
+ * @param \Countable|iterable $haystack
  *
  * @throws ExpectationFailedException
  * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
@@ -199,7 +194,7 @@ function assertCount(int $expectedCount, $haystack, string $message = ''): void
 /**
  * Asserts the number of elements of an array, Countable or Traversable.
  *
- * @param Countable|iterable $haystack
+ * @param \Countable|iterable $haystack
  *
  * @throws ExpectationFailedException
  * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
@@ -1581,8 +1576,8 @@ function assertNotRegExp(string $pattern, string $string, string $message = ''):
  * Assert that the size of two arrays (or `Countable` or `Traversable` objects)
  * is the same.
  *
- * @param Countable|iterable $expected
- * @param Countable|iterable $actual
+ * @param \Countable|iterable $expected
+ * @param \Countable|iterable $actual
  *
  * @throws ExpectationFailedException
  * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
@@ -1599,8 +1594,8 @@ function assertSameSize($expected, $actual, string $message = ''): void
  * Assert that the size of two arrays (or `Countable` or `Traversable` objects)
  * is not the same.
  *
- * @param Countable|iterable $expected
- * @param Countable|iterable $actual
+ * @param \Countable|iterable $expected
+ * @param \Countable|iterable $actual
  *
  * @throws ExpectationFailedException
  * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
@@ -1795,7 +1790,7 @@ function assertXmlFileNotEqualsXmlFile(string $expectedFile, string $actualFile,
 /**
  * Asserts that two XML documents are equal.
  *
- * @param DOMDocument|string $actualXml
+ * @param \DOMDocument|string $actualXml
  *
  * @throws ExpectationFailedException
  * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
@@ -1811,7 +1806,7 @@ function assertXmlStringEqualsXmlFile(string $expectedFile, $actualXml, string $
 /**
  * Asserts that two XML documents are not equal.
  *
- * @param DOMDocument|string $actualXml
+ * @param \DOMDocument|string $actualXml
  *
  * @throws ExpectationFailedException
  * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
@@ -1827,8 +1822,8 @@ function assertXmlStringNotEqualsXmlFile(string $expectedFile, $actualXml, strin
 /**
  * Asserts that two XML documents are equal.
  *
- * @param DOMDocument|string $expectedXml
- * @param DOMDocument|string $actualXml
+ * @param \DOMDocument|string $expectedXml
+ * @param \DOMDocument|string $actualXml
  *
  * @throws ExpectationFailedException
  * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
@@ -1844,8 +1839,8 @@ function assertXmlStringEqualsXmlString($expectedXml, $actualXml, string $messag
 /**
  * Asserts that two XML documents are not equal.
  *
- * @param DOMDocument|string $expectedXml
- * @param DOMDocument|string $actualXml
+ * @param \DOMDocument|string $expectedXml
+ * @param \DOMDocument|string $actualXml
  *
  * @throws ExpectationFailedException
  * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
@@ -1870,7 +1865,7 @@ function assertXmlStringNotEqualsXmlString($expectedXml, $actualXml, string $mes
  * @deprecated https://github.com/sebastianbergmann/phpunit/issues/4091
  * @see Assert::assertEqualXMLStructure
  */
-function assertEqualXMLStructure(DOMElement $expectedElement, DOMElement $actualElement, bool $checkAttributes = false, string $message = ''): void
+function assertEqualXMLStructure(\DOMElement $expectedElement, \DOMElement $actualElement, bool $checkAttributes = false, string $message = ''): void
 {
     Assert::assertEqualXMLStructure(...\func_get_args());
 }

src/Framework/Constraint/Constraint.php
@@ -9,7 +9,6 @@
  */
 namespace PHPUnit\Framework\Constraint;
 
-use Countable;
 use PHPUnit\Framework\ExpectationFailedException;
 use PHPUnit\Framework\SelfDescribing;
 use SebastianBergmann\Comparator\ComparisonFailure;
@@ -18,7 +17,7 @@ use SebastianBergmann\Exporter\Exporter;
 /**
  * Abstract base class for constraints which can be applied to any value.
  */
-abstract class Constraint implements Countable, SelfDescribing
+abstract class Constraint implements \Countable, SelfDescribing
 {
     /**
      * @var ?Exporter

src/Framework/Constraint/Exception.php
@@ -10,7 +10,6 @@
 namespace PHPUnit\Framework\Constraint;
 
 use PHPUnit\Util\Filter;
-use Throwable;
 
 final class Exception extends Constraint
 {
@@ -59,7 +58,7 @@ final class Exception extends Constraint
         if ($other !== null) {
             $message = '';
 
-            if ($other instanceof Throwable) {
+            if ($other instanceof \Throwable) {
                 $message = '. Message was: "' . $other->getMessage() . '" at'
                     . "\n" . Filter::getFilteredStacktrace($other);
             }

src/Framework/Constraint/IsEmpty.php
@@ -9,8 +9,6 @@
  */
 namespace PHPUnit\Framework\Constraint;
 
-use Countable;
-
 /**
  * Constraint that checks whether a variable is empty().
  */
@@ -36,7 +34,7 @@ final class IsEmpty extends Constraint
             return true;
         }
 
-        if ($other instanceof Countable) {
+        if ($other instanceof \Countable) {
             return \count($other) === 0;
         }
 

src/Framework/Constraint/ObjectHasAttribute.php
@@ -9,8 +9,6 @@
  */
 namespace PHPUnit\Framework\Constraint;
 
-use ReflectionObject;
-
 /**
  * Constraint that asserts that the object it is evaluated for has a given
  * attribute.
@@ -27,6 +25,6 @@ final class ObjectHasAttribute extends ClassHasAttribute
      */
     protected function matches($other): bool
     {
-        return (new ReflectionObject($other))->hasProperty($this->attributeName());
+        return (new \ReflectionObject($other))->hasProperty($this->attributeName());
     }
 }

commit 0784cf51066ca18cf8c14930e111432357783eac

Fix regression introduced in 4f0b297aec516b73c69817daf6691778ea8ec113

src/Framework/MockObject/InvocationHandler.php
@@ -126,7 +126,7 @@ final class InvocationHandler
                         $hasReturnValue = true;
                     }
                 }
-            } catch (Exception $e) {
+            } catch (\Exception $e) {
                 $exception = $e;
             }
         }