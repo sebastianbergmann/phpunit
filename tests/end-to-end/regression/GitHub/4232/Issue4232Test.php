57fa6489dfe80bbbfc98a8f9846a60462b8aa357
Florian Engelhardt <florian.engelhardt@oxid-esales.com>
2020-05-20 11:42:16 +0200

    Fix coding style issues

--- tests/end-to-end/regression/GitHub/4232/Issue4232Test.php
+++ tests/end-to-end/regression/GitHub/4232/Issue4232Test.php
@@ -7,10 +7,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 namespace Foo\Bar;
-
-use PHPUnit\Framework\TestCase;
 
 require __DIR__ . '/ParentIssue4232Test.php';
 
