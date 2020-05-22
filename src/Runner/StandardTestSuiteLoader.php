15bace64caab305b476b3f37f2adb5d49eb0677e
Sebastian Bergmann <sb@sebastian-bergmann.de>
2020-05-22 10:40:20 +0200

    Fix CS/WS issues

--- src/Runner/StandardTestSuiteLoader.php
+++ src/Runner/StandardTestSuiteLoader.php
@@ -45,10 +45,8 @@ final class StandardTestSuiteLoader implements TestSuiteLoader
             $offset = 0 - \strlen($suiteClassName);
 
             foreach ($loadedClasses as $loadedClass) {
-                if (
-                    \substr($loadedClass, $offset) === $suiteClassName &&
-                    \basename(\str_replace('\\', '/', $loadedClass)) == $suiteClassName
-                ) {
+                if (\substr($loadedClass, $offset) === $suiteClassName &&
+                    \basename(\str_replace('\\', '/', $loadedClass)) === $suiteClassName) {
                     $suiteClassName = $loadedClass;
 
                     break;
