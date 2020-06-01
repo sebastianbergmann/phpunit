75e430f8c0896c82d8ccbe5e902bb792fcdd2179
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2020-06-01 19:12:23 +0200

    fix namespace in LogicalXorTest.php

--- tests/unit/Framework/Constraint/LogicalXorTest.php
+++ tests/unit/Framework/Constraint/LogicalXorTest.php
@@ -7,10 +7,8 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-namespace Framework\Constraint;
+namespace PHPUnit\Framework\Constraint;
 
-use PHPUnit\Framework\Constraint\Constraint;
-use PHPUnit\Framework\Constraint\LogicalXor;
 use PHPUnit\Framework\TestCase;
 
 /**
