e81147fd8278eff99f62ee6fc9eef0ec311fccca
Sebastian Bergmann <sb@sebastian-bergmann.de>
2020-06-17 08:06:34 +0200

    Closes #4291

--- src/Framework/MockObject/InvocationHandler.php
+++ src/Framework/MockObject/InvocationHandler.php
@@ -112,11 +112,30 @@ final class InvocationHandler
     public function invoke(Invocation $invocation)
     {
         $exception      = null;
+        $hasReturnValue = false;
         $returnValue    = null;
-        $match          = $this->findMatcher($invocation);
 
-        if ($match !== null) {
-            return $match->invoked($invocation);
+        foreach ($this->matchers as $match) {
+            try {
+                if ($match->matches($invocation)) {
+                    $value = $match->invoked($invocation);
+
+                    if (!$hasReturnValue) {
+                        $returnValue    = $value;
+                        $hasReturnValue = true;
+                    }
+                }
+            } catch (\Exception $e) {
+                $exception = $e;
+            }
+        }
+
+        if ($exception !== null) {
+            throw $exception;
+        }
+
+        if ($hasReturnValue) {
+            return $returnValue;
         }
 
         if (!$this->returnValueGeneration) {
@@ -163,32 +182,6 @@ final class InvocationHandler
         if ($this->deferredError) {
             throw $this->deferredError;
         }
-    }
-
-    /**
-     * @throws RuntimeException
-     */
-    private function findMatcher(Invocation $invocation): ?Matcher
-    {
-        $result = [];
-
-        foreach ($this->matchers as $matcher) {
-            if ($matcher->matches($invocation)) {
-                $result[] = $matcher;
-            }
-        }
-
-        if (\count($result) > 1) {
-            throw new RuntimeException(
-                \sprintf(
-                    'More than one invocation handler has been configured for %s::%s()',
-                    $invocation->getClassName(),
-                    $invocation->getMethodName()
-                )
-            );
-        }
-
-        return \current($result) ?: null;
     }
 
     private function addMatcher(Matcher $matcher): void
