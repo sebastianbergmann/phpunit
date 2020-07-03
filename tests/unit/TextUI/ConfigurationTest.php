14869ece900ea2b009fa55a7f93e423d3c9b2b0f
Ewout Pieter den Ouden <ewoutdenouden@gmail.com>
2020-07-03 20:30:04 +0200

    Improve test coverage of Configuration

--- tests/unit/TextUI/ConfigurationTest.php
+++ tests/unit/TextUI/ConfigurationTest.php
@@ -12,12 +12,189 @@ use PHPUnit\TextUI\CliArguments\Configuration;
 
 class ConfigurationTest extends TestCase
 {
-    public function testCallingGetterOnUndefinedPropertyThrowsException(): void
+    public static function configurationPropertyProvider(): Generator
+    {
+        yield ['argument'];
+
+        yield ['atLeastVersion'];
+
+        yield ['backupGlobals'];
+
+        yield ['backupStaticAttributes'];
+
+        yield ['beStrictAboutChangesToGlobalState'];
+
+        yield ['beStrictAboutResourceUsageDuringSmallTests'];
+
+        yield ['bootstrap'];
+
+        yield ['cacheResult'];
+
+        yield ['cacheResultFile'];
+
+        yield ['checkVersion'];
+
+        yield ['colors'];
+
+        yield ['columns'];
+
+        yield ['configuration'];
+
+        yield ['coverageFilter'];
+
+        yield ['coverageClover'];
+
+        yield ['coverageCrap4J'];
+
+        yield ['coverageHtml'];
+
+        yield ['coveragePhp'];
+
+        yield ['coverageText'];
+
+        yield ['coverageTextShowUncoveredFiles'];
+
+        yield ['coverageTextShowOnlySummary'];
+
+        yield ['coverageXml'];
+
+        yield ['debug'];
+
+        yield ['defaultTimeLimit'];
+
+        yield ['disableCodeCoverageIgnore'];
+
+        yield ['disallowTestOutput'];
+
+        yield ['disallowTodoAnnotatedTests'];
+
+        yield ['enableOutputBuffer'];
+
+        yield ['enforceTimeLimit'];
+
+        yield ['excludeGroups'];
+
+        yield ['executionOrder'];
+
+        yield ['executionOrderDefects'];
+
+        yield ['failOnEmptyTestSuite'];
+
+        yield ['failOnIncomplete'];
+
+        yield ['failOnRisky'];
+
+        yield ['failOnSkipped'];
+
+        yield ['failOnWarning'];
+
+        yield ['filter'];
+
+        yield ['generateConfiguration'];
+
+        yield ['groups'];
+
+        yield ['help'];
+
+        yield ['includePath'];
+
+        yield ['iniSettings'];
+
+        yield ['junitLogfile'];
+
+        yield ['listGroups'];
+
+        yield ['listSuites'];
+
+        yield ['listTests'];
+
+        yield ['listTestsXml'];
+
+        yield ['loader'];
+
+        yield ['noCoverage'];
+
+        yield ['noExtensions'];
+
+        yield ['extensions'];
+
+        yield ['unavailableExtensions'];
+
+        yield ['noInteraction'];
+
+        yield ['noLogging'];
+
+        yield ['printer'];
+
+        yield ['processIsolation'];
+
+        yield ['randomOrderSeed'];
+
+        yield ['repeat'];
+
+        yield ['reportUselessTests'];
+
+        yield ['resolveDependencies'];
+
+        yield ['reverseList'];
+
+        yield ['stderr'];
+
+        yield ['strictCoverage'];
+
+        yield ['stopOnDefect'];
+
+        yield ['stopOnError'];
+
+        yield ['stopOnFailure'];
+
+        yield ['stopOnIncomplete'];
+
+        yield ['stopOnRisky'];
+
+        yield ['stopOnSkipped'];
+
+        yield ['stopOnWarning'];
+
+        yield ['teamcityLogfile'];
+
+        yield ['testdoxExcludeGroups'];
+
+        yield ['testdoxGroups'];
+
+        yield ['testdoxHtmlFile'];
+
+        yield ['testdoxTextFile'];
+
+        yield ['testdoxXmlFile'];
+
+        yield ['testSuffixes'];
+
+        yield ['testSuite'];
+
+        yield ['unrecognizedOrderBy'];
+
+        yield ['useDefaultConfiguration'];
+
+        yield ['verbose'];
+
+        yield ['version'];
+
+        yield ['xdebugFilterFile'];
+    }
+
+    /**
+     * @testdox Getting uninitialized property $propertyName throws exception
+     * @dataProvider configurationPropertyProvider
+     */
+    public function testCallingGetterOnUndefinedPropertyThrowsException(string $propertyName): void
     {
         $config = $this->callConfigurationConstructor();
 
+        $this->assertTrue(method_exists($config, $propertyName), "Unknown property {$propertyName}");
+
         $this->expectException("PHPUnit\TextUI\CliArguments\Exception");
-        $this->assertTrue($config->enableOutputBuffer());
+        $this->assertTrue($config->{$propertyName}());
     }
 
     private function callConfigurationConstructor(): Configuration
