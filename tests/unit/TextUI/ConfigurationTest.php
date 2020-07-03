33a699eb2581c0d682fb071531bf1a885a3d8957
Ewout Pieter den Ouden <ewoutdenouden@gmail.com>
2020-07-03 13:57:52 +0200

    Add some coverage for new Configuration value object

---
+++ tests/unit/TextUI/ConfigurationTest.php
@@ -0,0 +1,113 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of PHPUnit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+use PHPUnit\Framework\TestCase;
+use PHPUnit\TextUI\CliArguments\Configuration;
+
+class ConfigurationTest extends TestCase
+{
+    public function testCallingGetterOnUndefinedPropertyThrowsException(): void
+    {
+        $config = $this->callConfigurationConstructor();
+
+        $this->expectException("PHPUnit\TextUI\CliArguments\Exception");
+        $this->assertTrue($config->enableOutputBuffer());
+    }
+
+    private function callConfigurationConstructor(): Configuration
+    {
+        return new Configuration(
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            [],
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+        );
+    }
+}
