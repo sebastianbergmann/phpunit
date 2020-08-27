a404c34fc61e564614a05da7569e9a70a327fb79
Toshiyuki Goto <ngyuki.jp@gmail.com>
2020-08-27 10:47:12 +0900

    Fix: Regenerate global assert wrapper

--- src/Framework/Assert/Functions.php
+++ src/Framework/Assert/Functions.php
@@ -11,6 +11,9 @@ namespace PHPUnit\Framework;
 
 use function func_get_args;
 use ArrayAccess;
+use Countable;
+use DOMDocument;
+use DOMElement;
 use PHPUnit\Framework\Constraint\ArrayHasKey;
 use PHPUnit\Framework\Constraint\Callback;
 use PHPUnit\Framework\Constraint\ClassHasAttribute;
