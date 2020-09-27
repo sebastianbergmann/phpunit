155e5b0549093e2c494cc303c2910daeac0b046c
Sebastian Bergmann <sb@sebastian-bergmann.de>
2020-09-27 17:14:46 +0200

    Motivated by #4471, do not even try to add non-public methods as test methods

--- src/Framework/TestSuite.php
+++ src/Framework/TestSuite.php
@@ -192,6 +192,10 @@ class TestSuite implements \IteratorAggregate, SelfDescribing, Test
                 continue;
             }
 
+            if (!TestUtil::isTestMethod($method)) {
+                continue;
+            }
+
             $this->addTestMethod($theClass, $method);
         }
 
@@ -744,25 +748,7 @@ class TestSuite implements \IteratorAggregate, SelfDescribing, Test
      */
     protected function addTestMethod(\ReflectionClass $class, \ReflectionMethod $method): void
     {
-        if (!TestUtil::isTestMethod($method)) {
-            return;
-        }
-
         $methodName = $method->getName();
-
-        if (!$method->isPublic()) {
-            $this->addTest(
-                new WarningTestCase(
-                    \sprintf(
-                        'Test method "%s" in test class "%s" is not public.',
-                        $methodName,
-                        $class->getName()
-                    )
-                )
-            );
-
-            return;
-        }
 
         $test = (new TestBuilder)->build($class, $methodName);
 
