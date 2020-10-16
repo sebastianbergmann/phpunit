2cf7145b6fdf338e1601ea22877d345d1376e5dd
Filippo Tessarotto <zoeslam@gmail.com>
2020-10-16 09:49:32 +0200

    Fix hasRandomOrderSeer -> hasRandomOrderSeed typo

--- src/TextUI/CliArguments/Configuration.php
+++ src/TextUI/CliArguments/Configuration.php
@@ -1641,7 +1641,7 @@ final class Configuration
         return $this->processIsolation;
     }
 
-    public function hasRandomOrderSeer(): bool
+    public function hasRandomOrderSeed(): bool
     {
         return $this->randomOrderSeed !== null;
     }
