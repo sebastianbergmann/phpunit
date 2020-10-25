068e79c5a7ecce3266c79f21933338bd449221ed
Sebastian Bergmann <sb@sebastian-bergmann.de>
2020-10-25 10:58:38 +0100

    Initial work on Error and ErrorTestCase

---
+++ src/Framework/ErrorTestCase.php
@@ -0,0 +1,66 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of PHPUnit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace PHPUnit\Framework;
+
+/**
+ * @internal This class is not covered by the backward compatibility promise for PHPUnit
+ */
+final class ErrorTestCase extends TestCase
+{
+    /**
+     * @var bool
+     */
+    protected $backupGlobals = false;
+
+    /**
+     * @var bool
+     */
+    protected $backupStaticAttributes = false;
+
+    /**
+     * @var bool
+     */
+    protected $runTestInSeparateProcess = false;
+
+    /**
+     * @var string
+     */
+    private $message;
+
+    public function __construct(string $message = '')
+    {
+        $this->message = $message;
+
+        parent::__construct('Error');
+    }
+
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    /**
+     * Returns a string representation of the test case.
+     */
+    public function toString(): string
+    {
+        return 'Error';
+    }
+
+    /**
+     * @throws Exception
+     *
+     * @psalm-return never-return
+     */
+    protected function runTest(): void
+    {
+        throw new Error($this->message);
+    }
+}
