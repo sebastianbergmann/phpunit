dd2ddc9abba79cb8b9bb4d1c28a6dbcdc205012d
Florian Engelhardt <flo@dotbox.org>
2020-10-28 11:01:18 +0100

    allow for case insensitive class names #4498

---
+++ tests/end-to-end/regression/GitHub/4498/Issue4498Test.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of PHPUnit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace PHPUnit\TestFixture;
+
+use PHPUnit\Framework\TestCase;
+
+class issue4498test extends TestCase
+{
+    public function testFoo(): void
+    {
+        $this->assertTrue(true);
+    }
+}
