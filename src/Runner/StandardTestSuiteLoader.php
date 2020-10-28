356d444775067a9d2e6e82d9c692597014f4e1cf
Florian Engelhardt <flo@dotbox.org>
2020-10-28 11:01:18 +0100

    allow for case insensitive class names #4498

--- src/Runner/StandardTestSuiteLoader.php
+++ src/Runner/StandardTestSuiteLoader.php
@@ -15,7 +15,7 @@ use function basename;
 use function class_exists;
 use function get_declared_classes;
 use function sprintf;
-use function str_replace;
+use function stripos;
 use function strlen;
 use function substr;
 use PHPUnit\Framework\TestCase;
@@ -52,11 +52,11 @@ final class StandardTestSuiteLoader implements TestSuiteLoader
         }
 
         if (!class_exists($suiteClassName, false)) {
+            // this block will handle namespaced classes
             $offset = 0 - strlen($suiteClassName);
 
             foreach ($loadedClasses as $loadedClass) {
-                if (substr($loadedClass, $offset) === $suiteClassName &&
-                    basename(str_replace('\\', '/', $loadedClass)) === $suiteClassName) {
+                if (stripos(substr($loadedClass, $offset - 1), '\\' . $suiteClassName) === 0) {
                     $suiteClassName = $loadedClass;
 
                     break;
