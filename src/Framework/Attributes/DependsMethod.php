7ab9f5e3be251b8f6383586e6e9dc43df30db274
Sebastian Bergmann <sb@sebastian-bergmann.de>
2020-11-07 08:34:23 +0100

    More work on attribute objects for #4502

---
+++ src/Framework/Attributes/DependsMethod.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of PHPUnit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace PHPUnit\Framework\Attributes;
+
+use Attribute;
+
+#[Attribute(Attribute::IS_REPEATABLE)]
+final class DependsMethod
+{
+    /**
+     * @var string
+     */
+    private $className;
+
+    /**
+     * @var string
+     */
+    private $methodName;
+
+    public function __construct(string $className, string $methodName)
+    {
+        $this->className  = $className;
+        $this->methodName = $methodName;
+    }
+
+    public function className(): string
+    {
+        return $this->className;
+    }
+
+    public function methodName(): string
+    {
+        return $this->methodName;
+    }
+}
