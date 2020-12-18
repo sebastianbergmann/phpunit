250399929e711823859520fa0304a2682011ebcb
Andreas Möller <am@localheinz.com>
2020-12-18 19:02:11 +0100

    Enhancement: Add test for Dispatcher
    Co-authored-by: Andreas Möller <am@localheinz.com>
    Co-authored-by: Arne Blankerts <Arne@Blankerts.de>

---
+++ tests/unit/Event/DispatcherTest.php
@@ -0,0 +1,50 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of PHPUnit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace PHPUnit\Event;
+
+use DummyEvent;
+use NamedType;
+use PHPUnit\Framework\TestCase;
+use SpyingSubscriber;
+
+/**
+ * @covers \PHPUnit\Event\Dispatcher
+ */
+final class DispatcherTest extends TestCase
+{
+    public function testDispatchDispatchesToRegisteredSubscribersForEventType(): void
+    {
+        $firstSubscriber = new SpyingSubscriber(new Types(
+            new NamedType('foo'),
+            new NamedType('bar')
+        ));
+
+        $secondSubscriber = new SpyingSubscriber(new Types(
+            new NamedType('bar'),
+            new NamedType('baz')
+        ));
+
+        $thirdSubscriber = new SpyingSubscriber(new Types(new NamedType('qux')));
+
+        $event = new DummyEvent(new NamedType('bar'));
+
+        $dispatcher = new Dispatcher();
+
+        $dispatcher->register($firstSubscriber);
+        $dispatcher->register($secondSubscriber);
+        $dispatcher->register($thirdSubscriber);
+
+        $dispatcher->dispatch($event);
+
+        $this->assertContains($event, $firstSubscriber->events());
+        $this->assertContains($event, $secondSubscriber->events());
+        $this->assertNotContains($event, $thirdSubscriber->events());
+    }
+}
