9ce9a9374d8f1d45f12f00ed5fd10e63c9e9c85e
Sebastian Bergmann <sb@sebastian-bergmann.de>
2020-12-19 08:19:59 +0100

    Fix CS/WS issue

--- tests/_files/phar-extension/tests/ExtensionTest.php
+++ tests/_files/phar-extension/tests/ExtensionTest.php
@@ -7,8 +7,8 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-use PHPUnit\TestFixture\TestExtension\Test;
 use PHPUnit\Framework\TestCase;
+use PHPUnit\TestFixture\TestExtension\Test;
 
 final class ExtensionTest extends TestCase
 {
