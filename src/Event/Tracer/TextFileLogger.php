1f95d22007a8065c2df143c23248aef91a292071
Andreas Möller <am@localheinz.com>
2020-12-19 17:23:45 +0100

    Implement example text file logging tracer (WIP)
    Co-authored-by: Andreas Möller <am@localheinz.com>
    Co-authored-by: Arne Blankerts <Arne@Blankerts.de>

---
+++ src/Event/Tracer/TextFileLogger.php
@@ -0,0 +1,52 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of PHPUnit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace PHPUnit\Event\Tracer;
+
+use PHPUnit\Event\Event;
+use PHPUnit\Event\Telemetry\Info;
+
+final class TextFileLogger implements Tracer
+{
+    private string $filename;
+
+    public function __construct(string $filename)
+    {
+        $this->filename = $filename;
+    }
+
+    public function trace(Event $event): void
+    {
+        file_put_contents(
+            $this->filename,
+            $this->renderEvent($event),
+            FILE_APPEND
+        );
+    }
+
+    private static function renderEvent(Event $event): string
+    {
+        return sprintf(
+            "%s - %s\n",
+            self::renderTelemetry($event->telemetryInfo()),
+            get_class($event)
+        );
+    }
+
+    private static function renderTelemetry(Info $telemetryInfo): string
+    {
+        return sprintf(
+            '[%s / %s] [%d Bytes / %d Bytes]',
+            $telemetryInfo->durationSinceStart()->asString(),
+            $telemetryInfo->durationSincePrevious()->asString(),
+            $telemetryInfo->memoryUsage()->bytes(),
+            $telemetryInfo->peakMemoryUsage()->bytes(),
+        );
+    }
+}
