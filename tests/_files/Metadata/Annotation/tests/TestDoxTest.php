156b2fb1bc93df2ad0db303f5f19340790f1821f
Sebastian Bergmann <sb@sebastian-bergmann.de>
2020-12-29 14:02:34 +0100

    Initial work on AnnotationParser test fixture

---
+++ tests/_files/Metadata/Annotation/tests/TestDoxTest.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of PHPUnit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace PHPUnit\TestFixture\Metadata\Annotation;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @testdox text
+ */
+final class TestDoxTest extends TestCase
+{
+    /**
+     * @testdox text
+     */
+    public function testOne(): void
+    {
+    }
+}
