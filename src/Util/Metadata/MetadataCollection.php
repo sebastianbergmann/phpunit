4de922430f24d850e5634929572d829ae57614df
Sebastian Bergmann <sb@sebastian-bergmann.de>
2020-12-30 08:01:19 +0100

    Implement MetadataCollection filtering

--- src/Util/Metadata/MetadataCollection.php
+++ src/Util/Metadata/MetadataCollection.php
@@ -9,6 +9,7 @@
  */
 namespace PHPUnit\Util\Metadata;
 
+use function array_filter;
 use function count;
 use Countable;
 use IteratorAggregate;
@@ -58,5 +59,389 @@ final class MetadataCollection implements Countable, IteratorAggregate
     public function getIterator(): MetadataCollectionIterator
     {
         return new MetadataCollectionIterator($this);
+    }
+
+    public function isAfter(): self
+    {
+        return self::fromArray(
+            array_filter(
+                $this->metadata,
+                static function (Metadata $metadata): bool {
+                    return $metadata->isAfter();
+                }
+            )
+        );
+    }
+
+    public function isAfterClass(): self
+    {
+        return self::fromArray(
+            array_filter(
+                $this->metadata,
+                static function (Metadata $metadata): bool {
+                    return $metadata->isAfterClass();
+                }
+            )
+        );
+    }
+
+    public function isBackupGlobals(): self
+    {
+        return self::fromArray(
+            array_filter(
+                $this->metadata,
+                static function (Metadata $metadata): bool {
+                    return $metadata->isBackupGlobals();
+                }
+            )
+        );
+    }
+
+    public function isBackupStaticProperties(): self
+    {
+        return self::fromArray(
+            array_filter(
+                $this->metadata,
+                static function (Metadata $metadata): bool {
+                    return $metadata->isBackupStaticProperties();
+                }
+            )
+        );
+    }
+
+    public function isBeforeClass(): self
+    {
+        return self::fromArray(
+            array_filter(
+                $this->metadata,
+                static function (Metadata $metadata): bool {
+                    return $metadata->isBeforeClass();
+                }
+            )
+        );
+    }
+
+    public function isBefore(): self
+    {
+        return self::fromArray(
+            array_filter(
+                $this->metadata,
+                static function (Metadata $metadata): bool {
+                    return $metadata->isBefore();
+                }
+            )
+        );
+    }
+
+    public function isCodeCoverageIgnore(): self
+    {
+        return self::fromArray(
+            array_filter(
+                $this->metadata,
+                static function (Metadata $metadata): bool {
+                    return $metadata->isCodeCoverageIgnore();
+                }
+            )
+        );
+    }
+
+    public function isCoversClass(): self
+    {
+        return self::fromArray(
+            array_filter(
+                $this->metadata,
+                static function (Metadata $metadata): bool {
+                    return $metadata->isCoversClass();
+                }
+            )
+        );
+    }
+
+    public function isCoversMethod(): self
+    {
+        return self::fromArray(
+            array_filter(
+                $this->metadata,
+                static function (Metadata $metadata): bool {
+                    return $metadata->isCoversMethod();
+                }
+            )
+        );
+    }
+
+    public function isCoversFunction(): self
+    {
+        return self::fromArray(
+            array_filter(
+                $this->metadata,
+                static function (Metadata $metadata): bool {
+                    return $metadata->isCoversFunction();
+                }
+            )
+        );
+    }
+
+    public function isCoversNothing(): self
+    {
+        return self::fromArray(
+            array_filter(
+                $this->metadata,
+                static function (Metadata $metadata): bool {
+                    return $metadata->isCoversNothing();
+                }
+            )
+        );
+    }
+
+    public function isDataProvider(): self
+    {
+        return self::fromArray(
+            array_filter(
+                $this->metadata,
+                static function (Metadata $metadata): bool {
+                    return $metadata->isDataProvider();
+                }
+            )
+        );
+    }
+
+    public function isDepends(): self
+    {
+        return self::fromArray(
+            array_filter(
+                $this->metadata,
+                static function (Metadata $metadata): bool {
+                    return $metadata->isDepends();
+                }
+            )
+        );
+    }
+
+    public function isDoesNotPerformAssertions(): self
+    {
+        return self::fromArray(
+            array_filter(
+                $this->metadata,
+                static function (Metadata $metadata): bool {
+                    return $metadata->isDoesNotPerformAssertions();
+                }
+            )
+        );
+    }
+
+    public function isGroup(): self
+    {
+        return self::fromArray(
+            array_filter(
+                $this->metadata,
+                static function (Metadata $metadata): bool {
+                    return $metadata->isGroup();
+                }
+            )
+        );
+    }
+
+    public function isRunTestsInSeparateProcesses(): self
+    {
+        return self::fromArray(
+            array_filter(
+                $this->metadata,
+                static function (Metadata $metadata): bool {
+                    return $metadata->isRunTestsInSeparateProcesses();
+                }
+            )
+        );
+    }
+
+    public function isRunInSeparateProcess(): self
+    {
+        return self::fromArray(
+            array_filter(
+                $this->metadata,
+                static function (Metadata $metadata): bool {
+                    return $metadata->isRunInSeparateProcess();
+                }
+            )
+        );
+    }
+
+    public function isTest(): self
+    {
+        return self::fromArray(
+            array_filter(
+                $this->metadata,
+                static function (Metadata $metadata): bool {
+                    return $metadata->isTest();
+                }
+            )
+        );
+    }
+
+    public function isPreCondition(): self
+    {
+        return self::fromArray(
+            array_filter(
+                $this->metadata,
+                static function (Metadata $metadata): bool {
+                    return $metadata->isPreCondition();
+                }
+            )
+        );
+    }
+
+    public function isPostCondition(): self
+    {
+        return self::fromArray(
+            array_filter(
+                $this->metadata,
+                static function (Metadata $metadata): bool {
+                    return $metadata->isPostCondition();
+                }
+            )
+        );
+    }
+
+    public function isPreserveGlobalState(): self
+    {
+        return self::fromArray(
+            array_filter(
+                $this->metadata,
+                static function (Metadata $metadata): bool {
+                    return $metadata->isPreserveGlobalState();
+                }
+            )
+        );
+    }
+
+    public function isRequiresFunction(): self
+    {
+        return self::fromArray(
+            array_filter(
+                $this->metadata,
+                static function (Metadata $metadata): bool {
+                    return $metadata->isRequiresFunction();
+                }
+            )
+        );
+    }
+
+    public function isRequiresOperatingSystem(): self
+    {
+        return self::fromArray(
+            array_filter(
+                $this->metadata,
+                static function (Metadata $metadata): bool {
+                    return $metadata->isRequiresOperatingSystem();
+                }
+            )
+        );
+    }
+
+    public function isRequiresOperatingSystemFamily(): self
+    {
+        return self::fromArray(
+            array_filter(
+                $this->metadata,
+                static function (Metadata $metadata): bool {
+                    return $metadata->isRequiresOperatingSystemFamily();
+                }
+            )
+        );
+    }
+
+    public function isRequiresPhp(): self
+    {
+        return self::fromArray(
+            array_filter(
+                $this->metadata,
+                static function (Metadata $metadata): bool {
+                    return $metadata->isRequiresPhp();
+                }
+            )
+        );
+    }
+
+    public function isRequiresPhpExtension(): self
+    {
+        return self::fromArray(
+            array_filter(
+                $this->metadata,
+                static function (Metadata $metadata): bool {
+                    return $metadata->isRequiresPhpExtension();
+                }
+            )
+        );
+    }
+
+    public function isRequiresPhpunit(): self
+    {
+        return self::fromArray(
+            array_filter(
+                $this->metadata,
+                static function (Metadata $metadata): bool {
+                    return $metadata->isRequiresPhpunit();
+                }
+            )
+        );
+    }
+
+    public function isTestDox(): self
+    {
+        return self::fromArray(
+            array_filter(
+                $this->metadata,
+                static function (Metadata $metadata): bool {
+                    return $metadata->isTestDox();
+                }
+            )
+        );
+    }
+
+    public function isTestWith(): self
+    {
+        return self::fromArray(
+            array_filter(
+                $this->metadata,
+                static function (Metadata $metadata): bool {
+                    return $metadata->isTestWith();
+                }
+            )
+        );
+    }
+
+    public function isUsesClass(): self
+    {
+        return self::fromArray(
+            array_filter(
+                $this->metadata,
+                static function (Metadata $metadata): bool {
+                    return $metadata->isUsesClass();
+                }
+            )
+        );
+    }
+
+    public function isUsesMethod(): self
+    {
+        return self::fromArray(
+            array_filter(
+                $this->metadata,
+                static function (Metadata $metadata): bool {
+                    return $metadata->isUsesMethod();
+                }
+            )
+        );
+    }
+
+    public function isUsesFunction(): self
+    {
+        return self::fromArray(
+            array_filter(
+                $this->metadata,
+                static function (Metadata $metadata): bool {
+                    return $metadata->isUsesFunction();
+                }
+            )
+        );
     }
 }
--- tests/unit/Util/Metadata/MetadataCollectionTest.php
+++ tests/unit/Util/Metadata/MetadataCollectionTest.php
@@ -12,12 +12,42 @@ namespace PHPUnit\Util\Metadata;
 use PHPUnit\Framework\TestCase;
 
 /**
- * @covers \PHPUnit\Util\Metadata\Metadata
  * @covers \PHPUnit\Util\Metadata\MetadataCollection
  * @covers \PHPUnit\Util\Metadata\MetadataCollectionIterator
  *
+ * @uses \PHPUnit\Util\Metadata\After
+ * @uses \PHPUnit\Util\Metadata\AfterClass
+ * @uses \PHPUnit\Util\Metadata\BackupGlobals
+ * @uses \PHPUnit\Util\Metadata\BackupStaticProperties
+ * @uses \PHPUnit\Util\Metadata\Before
+ * @uses \PHPUnit\Util\Metadata\BeforeClass
+ * @uses \PHPUnit\Util\Metadata\CodeCoverageIgnore
+ * @uses \PHPUnit\Util\Metadata\CoversClass
+ * @uses \PHPUnit\Util\Metadata\CoversFunction
+ * @uses \PHPUnit\Util\Metadata\CoversMethod
+ * @uses \PHPUnit\Util\Metadata\CoversNothing
+ * @uses \PHPUnit\Util\Metadata\DataProvider
+ * @uses \PHPUnit\Util\Metadata\Depends
+ * @uses \PHPUnit\Util\Metadata\DoesNotPerformAssertions
+ * @uses \PHPUnit\Util\Metadata\Group
  * @uses \PHPUnit\Util\Metadata\Metadata
+ * @uses \PHPUnit\Util\Metadata\PostCondition
+ * @uses \PHPUnit\Util\Metadata\PreCondition
+ * @uses \PHPUnit\Util\Metadata\PreserveGlobalState
+ * @uses \PHPUnit\Util\Metadata\RequiresFunction
+ * @uses \PHPUnit\Util\Metadata\RequiresOperatingSystem
+ * @uses \PHPUnit\Util\Metadata\RequiresOperatingSystemFamily
+ * @uses \PHPUnit\Util\Metadata\RequiresPhp
+ * @uses \PHPUnit\Util\Metadata\RequiresPhpExtension
+ * @uses \PHPUnit\Util\Metadata\RequiresPhpunit
+ * @uses \PHPUnit\Util\Metadata\RunInSeparateProcess
+ * @uses \PHPUnit\Util\Metadata\RunTestsInSeparateProcesses
  * @uses \PHPUnit\Util\Metadata\Test
+ * @uses \PHPUnit\Util\Metadata\TestDox
+ * @uses \PHPUnit\Util\Metadata\TestWith
+ * @uses \PHPUnit\Util\Metadata\UsesClass
+ * @uses \PHPUnit\Util\Metadata\UsesFunction
+ * @uses \PHPUnit\Util\Metadata\UsesMethod
  */
 final class MetadataCollectionTest extends TestCase
 {
@@ -56,5 +86,301 @@ final class MetadataCollectionTest extends TestCase
             $this->assertSame(0, $key);
             $this->assertSame($metadata, $value);
         }
+    }
+
+    public function test_Can_be_filtered_for_AfterClass(): void
+    {
+        $collection = $this->collectionWithOneOfEach()->isAfterClass();
+
+        $this->assertCount(1, $collection);
+        $this->assertTrue($collection->asArray()[0]->isAfterClass());
+    }
+
+    public function test_Can_be_filtered_for_After(): void
+    {
+        $collection = $this->collectionWithOneOfEach()->isAfter();
+
+        $this->assertCount(1, $collection);
+        $this->assertTrue($collection->asArray()[0]->isAfter());
+    }
+
+    public function test_Can_be_filtered_for_BackupGlobals(): void
+    {
+        $collection = $this->collectionWithOneOfEach()->isBackupGlobals();
+
+        $this->assertCount(1, $collection);
+        $this->assertTrue($collection->asArray()[0]->isBackupGlobals());
+    }
+
+    public function test_Can_be_filtered_for_BackupStaticProperties(): void
+    {
+        $collection = $this->collectionWithOneOfEach()->isBackupStaticProperties();
+
+        $this->assertCount(1, $collection);
+        $this->assertTrue($collection->asArray()[0]->isBackupStaticProperties());
+    }
+
+    public function test_Can_be_filtered_for_BeforeClass(): void
+    {
+        $collection = $this->collectionWithOneOfEach()->isBeforeClass();
+
+        $this->assertCount(1, $collection);
+        $this->assertTrue($collection->asArray()[0]->isBeforeClass());
+    }
+
+    public function test_Can_be_filtered_for_Before(): void
+    {
+        $collection = $this->collectionWithOneOfEach()->isBefore();
+
+        $this->assertCount(1, $collection);
+        $this->assertTrue($collection->asArray()[0]->isBefore());
+    }
+
+    public function test_Can_be_filtered_for_CodeCoverageIgnore(): void
+    {
+        $collection = $this->collectionWithOneOfEach()->isCodeCoverageIgnore();
+
+        $this->assertCount(1, $collection);
+        $this->assertTrue($collection->asArray()[0]->isCodeCoverageIgnore());
+    }
+
+    public function test_Can_be_filtered_for_CoversClass(): void
+    {
+        $collection = $this->collectionWithOneOfEach()->isCoversClass();
+
+        $this->assertCount(1, $collection);
+        $this->assertTrue($collection->asArray()[0]->isCoversClass());
+    }
+
+    public function test_Can_be_filtered_for_CoversFunction(): void
+    {
+        $collection = $this->collectionWithOneOfEach()->isCoversFunction();
+
+        $this->assertCount(1, $collection);
+        $this->assertTrue($collection->asArray()[0]->isCoversFunction());
+    }
+
+    public function test_Can_be_filtered_for_CoversMethod(): void
+    {
+        $collection = $this->collectionWithOneOfEach()->isCoversMethod();
+
+        $this->assertCount(1, $collection);
+        $this->assertTrue($collection->asArray()[0]->isCoversMethod());
+    }
+
+    public function test_Can_be_filtered_for_CoversNothing(): void
+    {
+        $collection = $this->collectionWithOneOfEach()->isCoversNothing();
+
+        $this->assertCount(1, $collection);
+        $this->assertTrue($collection->asArray()[0]->isCoversNothing());
+    }
+
+    public function test_Can_be_filtered_for_DataProvider(): void
+    {
+        $collection = $this->collectionWithOneOfEach()->isDataProvider();
+
+        $this->assertCount(1, $collection);
+        $this->assertTrue($collection->asArray()[0]->isDataProvider());
+    }
+
+    public function test_Can_be_filtered_for_Depends(): void
+    {
+        $collection = $this->collectionWithOneOfEach()->isDepends();
+
+        $this->assertCount(1, $collection);
+        $this->assertTrue($collection->asArray()[0]->isDepends());
+    }
+
+    public function test_Can_be_filtered_for_DoesNotPerformAssertions(): void
+    {
+        $collection = $this->collectionWithOneOfEach()->isDoesNotPerformAssertions();
+
+        $this->assertCount(1, $collection);
+        $this->assertTrue($collection->asArray()[0]->isDoesNotPerformAssertions());
+    }
+
+    public function test_Can_be_filtered_for_Group(): void
+    {
+        $collection = $this->collectionWithOneOfEach()->isGroup();
+
+        $this->assertCount(1, $collection);
+        $this->assertTrue($collection->asArray()[0]->isGroup());
+    }
+
+    public function test_Can_be_filtered_for_PostCondition(): void
+    {
+        $collection = $this->collectionWithOneOfEach()->isPostCondition();
+
+        $this->assertCount(1, $collection);
+        $this->assertTrue($collection->asArray()[0]->isPostCondition());
+    }
+
+    public function test_Can_be_filtered_for_PreCondition(): void
+    {
+        $collection = $this->collectionWithOneOfEach()->isPreCondition();
+
+        $this->assertCount(1, $collection);
+        $this->assertTrue($collection->asArray()[0]->isPreCondition());
+    }
+
+    public function test_Can_be_filtered_for_PreserveGlobalState(): void
+    {
+        $collection = $this->collectionWithOneOfEach()->isPreserveGlobalState();
+
+        $this->assertCount(1, $collection);
+        $this->assertTrue($collection->asArray()[0]->isPreserveGlobalState());
+    }
+
+    public function test_Can_be_filtered_for_RequiresFunction(): void
+    {
+        $collection = $this->collectionWithOneOfEach()->isRequiresFunction();
+
+        $this->assertCount(1, $collection);
+        $this->assertTrue($collection->asArray()[0]->isRequiresFunction());
+    }
+
+    public function test_Can_be_filtered_for_RequiresOperatingSystemFamily(): void
+    {
+        $collection = $this->collectionWithOneOfEach()->isRequiresOperatingSystemFamily();
+
+        $this->assertCount(1, $collection);
+        $this->assertTrue($collection->asArray()[0]->isRequiresOperatingSystemFamily());
+    }
+
+    public function test_Can_be_filtered_for_RequiresOperatingSystem(): void
+    {
+        $collection = $this->collectionWithOneOfEach()->isRequiresOperatingSystem();
+
+        $this->assertCount(1, $collection);
+        $this->assertTrue($collection->asArray()[0]->isRequiresOperatingSystem());
+    }
+
+    public function test_Can_be_filtered_for_RequiresPhpExtension(): void
+    {
+        $collection = $this->collectionWithOneOfEach()->isRequiresPhpExtension();
+
+        $this->assertCount(1, $collection);
+        $this->assertTrue($collection->asArray()[0]->isRequiresPhpExtension());
+    }
+
+    public function test_Can_be_filtered_for_RequiresPhp(): void
+    {
+        $collection = $this->collectionWithOneOfEach()->isRequiresPhp();
+
+        $this->assertCount(1, $collection);
+        $this->assertTrue($collection->asArray()[0]->isRequiresPhp());
+    }
+
+    public function test_Can_be_filtered_for_RequiresPhpunit(): void
+    {
+        $collection = $this->collectionWithOneOfEach()->isRequiresPhpunit();
+
+        $this->assertCount(1, $collection);
+        $this->assertTrue($collection->asArray()[0]->isRequiresPhpunit());
+    }
+
+    public function test_Can_be_filtered_for_RunInSeparateProcess(): void
+    {
+        $collection = $this->collectionWithOneOfEach()->isRunInSeparateProcess();
+
+        $this->assertCount(1, $collection);
+        $this->assertTrue($collection->asArray()[0]->isRunInSeparateProcess());
+    }
+
+    public function test_Can_be_filtered_for_RunTestsInSeparateProcesses(): void
+    {
+        $collection = $this->collectionWithOneOfEach()->isRunTestsInSeparateProcesses();
+
+        $this->assertCount(1, $collection);
+        $this->assertTrue($collection->asArray()[0]->isRunTestsInSeparateProcesses());
+    }
+
+    public function test_Can_be_filtered_for_TestDox(): void
+    {
+        $collection = $this->collectionWithOneOfEach()->isTestDox();
+
+        $this->assertCount(1, $collection);
+        $this->assertTrue($collection->asArray()[0]->isTestDox());
+    }
+
+    public function test_Can_be_filtered_for_Test(): void
+    {
+        $collection = $this->collectionWithOneOfEach()->isTest();
+
+        $this->assertCount(1, $collection);
+        $this->assertTrue($collection->asArray()[0]->isTest());
+    }
+
+    public function test_Can_be_filtered_for_TestWith(): void
+    {
+        $collection = $this->collectionWithOneOfEach()->isTestWith();
+
+        $this->assertCount(1, $collection);
+        $this->assertTrue($collection->asArray()[0]->isTestWith());
+    }
+
+    public function test_Can_be_filtered_for_UsesClass(): void
+    {
+        $collection = $this->collectionWithOneOfEach()->isUsesClass();
+
+        $this->assertCount(1, $collection);
+        $this->assertTrue($collection->asArray()[0]->isUsesClass());
+    }
+
+    public function test_Can_be_filtered_for_UsesFunction(): void
+    {
+        $collection = $this->collectionWithOneOfEach()->isUsesFunction();
+
+        $this->assertCount(1, $collection);
+        $this->assertTrue($collection->asArray()[0]->isUsesFunction());
+    }
+
+    public function test_Can_be_filtered_for_UsesMethod(): void
+    {
+        $collection = $this->collectionWithOneOfEach()->isUsesMethod();
+
+        $this->assertCount(1, $collection);
+        $this->assertTrue($collection->asArray()[0]->isUsesMethod());
+    }
+
+    private function collectionWithOneOfEach(): MetadataCollection
+    {
+        return MetadataCollection::fromArray(
+            [
+                new AfterClass,
+                new After,
+                new BackupGlobals(true),
+                new BackupStaticProperties(true),
+                new BeforeClass,
+                new Before,
+                new CodeCoverageIgnore,
+                new CoversClass(''),
+                new CoversFunction(''),
+                new CoversMethod('', ''),
+                new CoversNothing,
+                new DataProvider('', ''),
+                new Depends('', ''),
+                new DoesNotPerformAssertions,
+                new Group(''),
+                new PostCondition,
+                new PreCondition,
+                new PreserveGlobalState(true),
+                new RequiresFunction(''),
+                new RequiresOperatingSystemFamily(''),
+                new RequiresOperatingSystem(''),
+                new RequiresPhpExtension('', '', ''),
+                new RequiresPhp('', ''),
+                new RequiresPhpunit('', ''),
+                new RunInSeparateProcess,
+                new RunTestsInSeparateProcesses,
+                new TestDox(''),
+                new Test,
+                new TestWith([]),
+                new UsesClass(''),
+                new UsesFunction(''),
+                new UsesMethod('', ''),
+            ]
+        );
     }
 }
