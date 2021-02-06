ee4781b317731119cd8ec21a6bdcef4e2bf86f2d
Sebastian Bergmann <sb@sebastian-bergmann.de>
2021-02-06 09:05:33 +0100

    Initial work on test size value object

---
+++ src/Framework/TestSize/Known.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of PHPUnit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace PHPUnit\Framework\TestSize;
+
+/**
+ * @internal This class is not covered by the backward compatibility promise for PHPUnit
+ * @psalm-immutable
+ */
+abstract class Known extends TestSize
+{
+    /**
+     * @psalm-assert-if-true Known $this
+     */
+    public function isKnown(): bool
+    {
+        return true;
+    }
+
+    abstract public function isGreaterThan(self $other): bool;
+}
