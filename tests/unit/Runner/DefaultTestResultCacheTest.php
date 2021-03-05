64d3ce4b4bf7b38e03918ae8cdb16dd11ee40252
Ewout Pieter den Ouden <ewoutdenouden@gmail.com>
2021-03-05 14:01:49 +0100

    Add test coverage for ResultCache format protection

--- tests/unit/Runner/DefaultTestResultCacheTest.php
+++ tests/unit/Runner/DefaultTestResultCacheTest.php
@@ -10,6 +10,8 @@
 namespace PHPUnit\Runner;
 
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\TestStatus\TestStatus;
+use ReflectionClass;
 
 /**
  * @covers \PHPUnit\Runner\DefaultTestResultCache
@@ -30,5 +32,27 @@ final class DefaultTestResultCacheTest extends TestCase
     public function testGetTimeForNonExistentTestNameReturnsFloatZero(): void
     {
         $this->assertSame(0.0, $this->subject->time('doesNotExist'));
+    }
+
+    public function testOldResultCacheFormatDoesNotTriggerError(): void
+    {
+        // PHPUnit before version 10 used integer constants instead of TestStatus.
+        // Note: this is a quick and cheap test for this uncommon edge case, without having
+        // to set up a full end-to-end test with an old cache file
+        $cache          = new DefaultTestResultCache;
+        $reflectedCache = new ReflectionClass($cache);
+        $defects        = $reflectedCache->getProperty('defects');
+        $defects->setAccessible(true);
+        $defects->setValue($cache, [
+            'testOne' => TestStatus::skipped(),
+            'testTwo' => 1,
+        ]);
+
+        $targetCache = new DefaultTestResultCache;
+        $cache->copyStateToCache($targetCache);
+
+        // Validate expected behaviour: the test with old success value is silently ignored
+        $this->assertEquals(TestStatus::skipped(), $targetCache->status('testOne'));
+        $this->assertEquals(TestStatus::unknown(), $targetCache->status('testTwo'));
     }
 }
