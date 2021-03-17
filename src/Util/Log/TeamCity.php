d68718414ac63d0b83a20072158447428593ff5c
Thomas Lamy <thomas.lamy@iventuregroup.com>
2021-03-17 07:59:33 +0100

    #4591 do not treat warnings as error in teamcity

--- src/Util/Log/TeamCity.php
+++ src/Util/Log/TeamCity.php
@@ -89,15 +89,7 @@ final class TeamCity extends ResultPrinter
      */
     public function addWarning(Test $test, Warning $e, float $time): void
     {
-        $this->printEvent(
-            'testFailed',
-            [
-                'name'     => $test->getName(),
-                'message'  => self::getMessage($e),
-                'details'  => self::getDetails($e),
-                'duration' => self::toMilliseconds($time),
-            ]
-        );
+        $this->write(self::getMessage($e) . PHP_EOL);
     }
 
     /**
