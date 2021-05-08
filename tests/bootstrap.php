7e7a744d50314b33ddf97b94cfe7391ae4841adc
Sebastian Bergmann <sb@sebastian-bergmann.de>
2021-05-08 17:02:39 +0200

    This file does not use namespaces, there is no need to prefix built-in function names

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,11 +7,11 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-if (!\defined('TEST_FILES_PATH')) {
-    \define('TEST_FILES_PATH', __DIR__ . \DIRECTORY_SEPARATOR . '_files' . \DIRECTORY_SEPARATOR);
+if (!defined('TEST_FILES_PATH')) {
+    define('TEST_FILES_PATH', __DIR__ . DIRECTORY_SEPARATOR . '_files' . DIRECTORY_SEPARATOR);
 }
 
-\ini_set('precision', '14');
-\ini_set('serialize_precision', '14');
+ini_set('precision', '14');
+ini_set('serialize_precision', '14');
 
 require_once __DIR__ . '/../vendor/autoload.php';
