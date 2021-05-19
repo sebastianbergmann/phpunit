e8479b82b43f78577c9ee8a18263343178017bb4
Eric Stern <eric@lustre.ai>
2021-05-19 14:56:32 -0700

    Fix JSON object order comparision with numeric keys

--- tests/unit/Framework/Constraint/JsonMatchesTest.php
+++ tests/unit/Framework/Constraint/JsonMatchesTest.php
@@ -24,105 +24,43 @@ final class JsonMatchesTest extends TestCase
     public static function provider(): array
     {
         return [
-            'valid JSON' => [
-                true,
-                '',
-                '',
-                json_encode(['Mascott' => 'Tux']),
-                json_encode(['Mascott' => 'Tux']),
-            ],
+            'valid JSON'                                    => [true, json_encode(['Mascott' => 'Tux']), json_encode(['Mascott' => 'Tux'])],
+            'error syntax'                                  => [false, '{"Mascott"::}', json_encode(['Mascott' => 'Tux'])],
+            'error UTF-8'                                   => [false, json_encode('\xB1\x31'), json_encode(['Mascott' => 'Tux'])],
+            'invalid JSON in class instantiation'           => [false, json_encode(['Mascott' => 'Tux']), '{"Mascott"::}'],
+            'string type not equals number'                 => [false, '{"age": "5"}', '{"age": 5}'],
+            'string type not equals boolean'                => [false, '{"age": "true"}', '{"age": true}'],
+            'string type not equals null'                   => [false, '{"age": "null"}', '{"age": null}'],
+            'object fields are unordered'                   => [true, '{"first":1, "second":"2"}', '{"second":"2", "first":1}'],
+            'object fields with numeric keys are unordered' => [true, '{"0":null,"a":{},"b":[],"c":"1","d":1,"e":-1,"f":[1,2],"g":[2,1],"h":{"0":"0","1":"1","2":"2"}}', '{"a":{},"d":1,"b":[],"e":-1,"0":null,"c":"1","f":[1,2],"h":{"2":"2","1":"1","0":"0"},"g":[2,1]}'],
+            'child object fields are unordered'             => [true, '{"Mascott": {"name":"Tux", "age":5}}', '{"Mascott": {"age":5, "name":"Tux"}}'],
+            'null field different from missing field'       => [false, '{"present": true, "missing": null}', '{"present": true}'],
+            'array elements are ordered'                    => [false, '["first", "second"]', '["second", "first"]'],
+            'single boolean valid json'                     => [true, 'true', 'true'],
+            'single number valid json'                      => [true, '5.3', '5.3'],
+            'single null valid json'                        => [true, 'null', 'null'],
+            'objects are not arrays'                        => [false, '{}', '[]'],
+            'arrays are not objects'                        => [false, '[]', '{}'],
+            'objects in arrays are unordered'               => [true, '[{"0":"0","1":"1"},{"2":"2","3":"3"}]', '[{"1":"1","0":"0"},{"2":"2","3":"3"}]'],
+        ];
+    }
 
-            'object fields are unordered' => [
-                true,
-                '',
-                '',
-                '{"second":"2", "first":1}',
-                '{"first":1, "second":"2"}',
-            ],
-
-            'child object fields are unordered' => [
-                true,
-                '',
-                '',
-                '{"Mascott": {"age":5, "name":"Tux"}}',
-                '{"Mascott": {"name":"Tux", "age":5}}',
-            ],
-
-            'single boolean valid json' => [
-                true,
-                '',
-                '',
-                'true',
-                'true',
-            ],
-
-            'single number valid json' => [
-                true,
-                '',
-                '',
-                '5.3',
-                '5.3',
-            ],
-
-            'single null valid json' => [
-                true,
-                '',
-                '',
-                'null',
-                'null',
-            ],
-
-            'invalid JSON in class instantiation' => [
-                false,
-                'Failed asserting that \'{"Mascott":"Tux"}\' matches JSON string "{"Mascott"::}".',
-                '',
-                '{"Mascott"::}',
-                json_encode(['Mascott' => 'Tux']),
-            ],
-
-            'error syntax' => [
-                false,
-                'Failed asserting that \'{"Mascott"::}\' matches JSON string "{"Mascott":"Tux"}".',
-                '',
-                json_encode(['Mascott' => 'Tux']),
-                '{"Mascott"::}',
-            ],
-
-            'error UTF-8' => [
-                false,
-                'Failed asserting that \'' . json_encode('\xB1\x31') . '\' matches JSON string "{"Mascott":"Tux"}".',
-                <<<'EOT'
-Failed asserting that two json values are equal.
---- Expected
-+++ Actual
-@@ @@
--{
--    "Mascott": "Tux"
--}
-+"\\xB1\\x31"
-
-EOT,
-                json_encode(['Mascott' => 'Tux']),
-                json_encode('\xB1\x31'),
-            ],
-
-            'string type not equals number' => [
-                false,
-                'Failed asserting that \'{"age": "5"}\' matches JSON string "{"age": 5}".',
-                <<<'EOT'
-Failed asserting that two json values are equal.
---- Expected
-+++ Actual
-@@ @@
- {
--    "age": 5
-+    "age": "5"
- }
-
-EOT,
-                '{"age": 5}',
-                '{"age": "5"}',
-            ],
+    public static function evaluateThrowsExpectationFailedExceptionWhenJsonIsValidButDoesNotMatchDataprovider(): array
+    {
+        return [
+            'error UTF-8'                                     => [json_encode('\xB1\x31'), json_encode(['Mascott' => 'Tux'])],
+            'string type not equals number'                   => ['{"age": "5"}', '{"age": 5}'],
+            'string type not equals boolean'                  => ['{"age": "true"}', '{"age": true}'],
+            'string type not equals null'                     => ['{"age": "null"}', '{"age": null}'],
+            'null field different from missing field'         => ['{"missing": null, "present": true}', '{"present": true}'],
+            'array elements are ordered'                      => ['["first", "second"]', '["second", "first"]'],
+            'objects with numeric keys are not arrays'        => ['{"0":{}}', '[{}]'],
+            'child array elements are ordered'                => ['{"0":null,"a":{},"b":[],"c":"1","d":1,"e":-1,"f":[1,2],"g":[2,1],"h":{"0":"0","1":"1","2":"2"}}',                 '{"a":{},"d":1,"b":[],"e":-1,"0":null,"c":"1","f":[2,1],"h":{"2":"2","1":"1","0":"0"},"g":[2,1]}'],
+            'child object with numeric fields stay as object' => ['{"0":null,"a":{},"b":[],"c":"1","d":1,"e":-1,"f":[1,2],"g":[2,1],"h":{"0":"0","1":"1","2":"2"}}', '{"a":{},"d":1,"b":[],"e":-1,"0":null,"c":"1","f":[1,2],"h":["0","1","2"],"g":[2,1]}'],
+            'nested arrays are ordered'                       => ['[[1,0],[2,3]]', '[{"1":"1","0":"0"},{"2":"2","3":"3"}]'],
+            'child objects in arrays stay in order'           => ['[{"0":"0","1":"1"},{"2":"2","3":"3"}]', '[{"2":"2","3":"3"},{"1":"1","0":"0"}]'],
+        ];
+    }
 
             'string type not equals boolean' => [
                 false,
