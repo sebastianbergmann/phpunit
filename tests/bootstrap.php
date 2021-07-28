58da926527031fc9c07c74bd2ea68f78d806117c
Sebastian Bergmann <sb@sebastian-bergmann.de>
2021-07-28 15:30:47 +0200

    Refactor

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,23 +9,30 @@
  */
 const TEST_FILES_PATH = __DIR__ . DIRECTORY_SEPARATOR . '_files' . DIRECTORY_SEPARATOR;
 
-if (file_exists(__DIR__ . '/../vendor/autoload.php') && file_exists(__DIR__ . '/autoload.php')) {
-    print 'More than one test fixture autoloader is available, exiting.' . \PHP_EOL;
+$composer = file_exists(__DIR__ . '/../vendor/autoload.php');
+$phar     = file_exists(__DIR__ . '/autoload.php');
+
+if ($composer && $phar) {
+    print 'More than one test fixture autoloader is available, exiting.' . PHP_EOL;
 
     exit(1);
 }
 
-if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+if (!$composer && !$phar) {
+    print 'No test fixture autoloader was registered, exiting.' . PHP_EOL;
+
+    exit(1);
+}
+
+if ($composer) {
     if (!defined('PHPUNIT_COMPOSER_INSTALL')) {
         define('PHPUNIT_COMPOSER_INSTALL', dirname(__DIR__) . '/vendor/autoload.php');
     }
 
     require_once __DIR__ . '/../vendor/autoload.php';
-
-    return;
 }
 
-if (file_exists(__DIR__ . '/autoload.php')) {
+if ($phar) {
     if (!defined('__PHPUNIT_PHAR__')) {
         require_once __DIR__ . '/../build/artifacts/phpunit-snapshot.phar';
     }
@@ -38,12 +45,6 @@ if (file_exists(__DIR__ . '/autoload.php')) {
     foreach (json_decode(file_get_contents($jsonFile), true)['autoload-dev']['files'] as $file) {
         require_once $base . DIRECTORY_SEPARATOR . $file;
     }
-
-    unset($jsonFile, $base, $file);
-
-    return;
 }
 
-print 'No test fixture autoloader was registered, exiting.' . \PHP_EOL;
-
-exit(1);
+unset($composer, $phar, $jsonFile, $base, $file);
