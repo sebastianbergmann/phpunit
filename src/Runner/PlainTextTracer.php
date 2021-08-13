ee5870f8aaa7f9469e43fc07791db0874fe3cca9
Sebastian Bergmann <sb@sebastian-bergmann.de>
2021-08-13 10:17:37 +0200

    Tweak
    Co-authored-by: Andreas Möller <am@localheinz.com>
    Co-authored-by: Arne Blankerts <Arne@Blankerts.de>
    Co-authored-by: Sebastian Bergmann <sb@sebastian-bergmann.de>

--- src/Runner/PlainTextTracer.php
+++ src/Runner/PlainTextTracer.php
@@ -12,7 +12,10 @@ namespace PHPUnit\Runner;
 use const FILE_APPEND;
 use const LOCK_EX;
 use const PHP_EOL;
+use function explode;
 use function file_put_contents;
+use function implode;
+use function str_repeat;
 use PHPUnit\Event\Event;
 use PHPUnit\Event\Tracer\Tracer;
 
@@ -33,9 +36,13 @@ final class PlainTextTracer implements Tracer
 
     public function trace(Event $event): void
     {
+        $telemetryInfo = $this->telemetryInfo($event);
+        $indentation   = PHP_EOL . str_repeat(' ', strlen($telemetryInfo));
+        $lines         = explode(PHP_EOL, $event->asString());
+
         file_put_contents(
             $this->path,
-            $this->telemetryInfo($event) . $event->asString() . PHP_EOL,
+            $telemetryInfo . implode($indentation, $lines) . PHP_EOL,
             FILE_APPEND|LOCK_EX
         );
     }
