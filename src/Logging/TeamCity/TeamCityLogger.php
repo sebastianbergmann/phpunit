e2544d6fab2f1ee719cff1b26760bc36303091dc
Sebastian Bergmann <sb@sebastian-bergmann.de>
2021-08-30 14:51:33 +0200

    Initial work on testSuiteStarted() and testSuiteFinished() event handlers

--- src/Logging/TeamCity/TeamCityLogger.php
+++ src/Logging/TeamCity/TeamCityLogger.php
@@ -9,6 +9,12 @@
  */
 namespace PHPUnit\Logging\TeamCity;
 
+use function class_exists;
+use function explode;
+use function getmypid;
+use function ini_get;
+use function method_exists;
+use function stripos;
 use PHPUnit\Event\EventFacadeIsSealedException;
 use PHPUnit\Event\Facade;
 use PHPUnit\Event\Test\Aborted;
@@ -19,16 +25,25 @@ use PHPUnit\Event\Test\Finished;
 use PHPUnit\Event\Test\PassedWithWarning;
 use PHPUnit\Event\Test\Prepared;
 use PHPUnit\Event\Test\Skipped;
-use PHPUnit\Event\TestSuite\Started;
+use PHPUnit\Event\TestSuite\Finished as TestSuiteFinished;
+use PHPUnit\Event\TestSuite\Started as TestSuiteStarted;
 use PHPUnit\Event\UnknownSubscriberTypeException;
 use PHPUnit\Util\Exception;
 use PHPUnit\Util\Printer;
+use ReflectionClass;
+use ReflectionException;
 
 /**
  * @internal This class is not covered by the backward compatibility promise for PHPUnit
  */
 final class TeamCityLogger extends Printer
 {
+    private bool $isSummaryTestCountPrinted = false;
+
+    private string $startedTestName;
+
+    private false|int $flowId;
+
     /**
      * @throws EventFacadeIsSealedException
      * @throws Exception
@@ -39,14 +54,68 @@ final class TeamCityLogger extends Printer
         parent::__construct($out);
 
         $this->registerSubscribers();
+
+        if (stripos(ini_get('disable_functions'), 'getmypid') === false) {
+            $this->flowId = getmypid();
+        } else {
+            $this->flowId = false;
+        }
     }
 
     public function testSuiteStarted(TestSuiteStarted $event): void
     {
+        if (!$this->isSummaryTestCountPrinted) {
+            $this->isSummaryTestCountPrinted = true;
+
+            $this->printEvent(
+                'testCount',
+                ['count' => $event->testSuiteInfo()->count()]
+            );
+        }
+
+        $suiteName = $event->testSuiteInfo()->name();
+
+        if (empty($suiteName)) {
+            return;
+        }
+
+        $parameters = ['name' => $suiteName];
+
+        if (class_exists($suiteName, false)) {
+            $fileName                   = self::getFileName($suiteName);
+            $parameters['locationHint'] = "php_qn://{$fileName}::\\{$suiteName}";
+        } else {
+            $split = explode('::', $suiteName);
+
+            if (count($split) === 2 && class_exists($split[0]) && method_exists($split[0], $split[1])) {
+                $fileName                   = self::getFileName($split[0]);
+                $parameters['locationHint'] = "php_qn://{$fileName}::\\{$suiteName}";
+                $parameters['name']         = $split[1];
+            }
+        }
+
+        $this->printEvent('testSuiteStarted', $parameters);
     }
 
     public function testSuiteFinished(TestSuiteFinished $event): void
     {
+        $suiteName = $event->testSuiteInfo()->name();
+
+        if (empty($suiteName)) {
+            return;
+        }
+
+        $parameters = ['name' => $suiteName];
+
+        if (!class_exists($suiteName, false)) {
+            $split = explode('::', $suiteName);
+
+            if (\count($split) === 2 && class_exists($split[0]) && method_exists($split[0], $split[1])) {
+                $parameters['name'] = $split[1];
+            }
+        }
+
+        $this->printEvent('testSuiteFinished', $parameters);
     }
 
     public function testPrepared(Prepared $event): void
@@ -81,6 +150,23 @@ final class TeamCityLogger extends Printer
     {
     }
 
+    private function printEvent(string $eventName, array $params = []): void
+    {
+        $this->write("\n##teamcity[{$eventName}");
+
+        if ($this->flowId) {
+            $params['flowId'] = $this->flowId;
+        }
+
+        foreach ($params as $key => $value) {
+            $escapedValue = self::escape((string) $value);
+
+            $this->write(" {$key}='{$escapedValue}'");
+        }
+
+        $this->write("]\n");
+    }
+
     /**
      * @throws EventFacadeIsSealedException
      * @throws UnknownSubscriberTypeException
@@ -97,5 +183,32 @@ final class TeamCityLogger extends Printer
         Facade::registerSubscriber(new TestAbortedSubscriber($this));
         Facade::registerSubscriber(new TestSkippedSubscriber($this));
         Facade::registerSubscriber(new TestConsideredRiskySubscriber($this));
+    }
+
+    private static function escape(string $string): string
+    {
+        return str_replace(
+            ['|', "'", "\n", "\r", ']', '['],
+            ['||', "|'", '|n', '|r', '|]', '|['],
+            $string
+        );
+    }
+
+    /**
+     * @psalm-param class-string $className
+     */
+    private static function getFileName(string $className): string
+    {
+        try {
+            return (new ReflectionClass($className))->getFileName();
+            // @codeCoverageIgnoreStart
+        } catch (ReflectionException $e) {
+            throw new Exception(
+                $e->getMessage(),
+                (int) $e->getCode(),
+                $e
+            );
+        }
+        // @codeCoverageIgnoreEnd
     }
 }
