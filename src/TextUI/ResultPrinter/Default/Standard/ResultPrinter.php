f763c6d385cdf76e19c13d061e4055282e2c1499
Sebastian Bergmann <sb@sebastian-bergmann.de>
2021-11-04 16:13:17 +0100

    Initial work on subscribers for event-based standard result printer

--- src/TextUI/ResultPrinter/Default/Standard/ResultPrinter.php
+++ src/TextUI/ResultPrinter/Default/Standard/ResultPrinter.php
@@ -9,6 +9,16 @@
  */
 namespace PHPUnit\TextUI\ResultPrinter\Standard;
 
+use PHPUnit\Event\EventFacadeIsSealedException;
+use PHPUnit\Event\Facade;
+use PHPUnit\Event\Test\Aborted;
+use PHPUnit\Event\Test\ConsideredRisky;
+use PHPUnit\Event\Test\Errored;
+use PHPUnit\Event\Test\Failed;
+use PHPUnit\Event\Test\Finished;
+use PHPUnit\Event\Test\PassedWithWarning;
+use PHPUnit\Event\Test\Skipped;
+use PHPUnit\Event\UnknownSubscriberTypeException;
 use PHPUnit\Framework\TestResult;
 use PHPUnit\TextUI\ResultPrinter\ResultPrinter as ResultPrinterInterface;
 use PHPUnit\Util\Printer;
@@ -18,11 +28,61 @@ use PHPUnit\Util\Printer;
  */
 final class ResultPrinter extends Printer implements ResultPrinterInterface
 {
+    public function __construct(string $out)
+    {
+        parent::__construct($out);
+
+        $this->registerSubscribers();
+    }
+
     public function printResult(TestResult $result): void
     {
     }
 
     public function write(string $buffer): void
     {
+    }
+
+    public function testAborted(Aborted $event): void
+    {
+    }
+
+    public function testConsideredRisky(ConsideredRisky $event): void
+    {
+    }
+
+    public function testErrored(Errored $event): void
+    {
+    }
+
+    public function testFailed(Failed $event): void
+    {
+    }
+
+    public function testFinished(Finished $event): void
+    {
+    }
+
+    public function testPassedWithWarning(PassedWithWarning $event): void
+    {
+    }
+
+    public function testSkipped(Skipped $event): void
+    {
+    }
+
+    /**
+     * @throws EventFacadeIsSealedException
+     * @throws UnknownSubscriberTypeException
+     */
+    private function registerSubscribers(): void
+    {
+        Facade::registerSubscriber(new TestFinishedSubscriber($this));
+        Facade::registerSubscriber(new TestConsideredRiskySubscriber($this));
+        Facade::registerSubscriber(new TestPassedWithWarningSubscriber($this));
+        Facade::registerSubscriber(new TestErroredSubscriber($this));
+        Facade::registerSubscriber(new TestFailedSubscriber($this));
+        Facade::registerSubscriber(new TestAbortedSubscriber($this));
+        Facade::registerSubscriber(new TestSkippedSubscriber($this));
     }
 }
