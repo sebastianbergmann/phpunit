d511c833065ee88873cfbff69c75a6aaeac1a006
Stefano Arlandini <sarlandini@alice.it>
2022-01-08 18:51:26 +0100

    Fix access to optional array key when using `debug_backtrace()` function

--- src/Framework/Exception/InvalidArgumentException.php
+++ src/Framework/Exception/InvalidArgumentException.php
@@ -21,14 +21,18 @@ final class InvalidArgumentException extends Exception
 {
     public static function create(int $argument, string $type): self
     {
-        $stack = debug_backtrace();
+        $stack    = debug_backtrace();
+        $function = $stack[1]['function'];
+
+        if (isset($stack[1]['class'])) {
+            $function = sprintf('%s::%s', $stack[1]['class'], $stack[1]['function']);
+        }
 
         return new self(
             sprintf(
-                'Argument #%d of %s::%s() must be %s %s',
+                'Argument #%d of %s() must be %s %s',
                 $argument,
-                $stack[1]['class'],
-                $stack[1]['function'],
+                $function,
                 in_array(lcfirst($type)[0], ['a', 'e', 'i', 'o', 'u'], true) ? 'an' : 'a',
                 $type
             )
