58b7d87f8a45a46f724e2fd5959853610d8327d0
Andreas Möller <am@localheinz.com>
2022-02-19 18:28:55 +0100

    Fix: Do not declare test classes in global namespace

--- tests/unit/Framework/ExecutionOrderDependencyTest.php
+++ tests/unit/Framework/ExecutionOrderDependencyTest.php
@@ -7,8 +7,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-use PHPUnit\Framework\ExecutionOrderDependency;
-use PHPUnit\Framework\TestCase;
+namespace PHPUnit\Framework;
 
 /**
  * @covers \PHPUnit\Framework\ExecutionOrderDependency
