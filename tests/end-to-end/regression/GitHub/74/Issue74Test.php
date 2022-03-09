50afb86886c90b9f7f26baded42bce627a1a7e73
Andreas Möller <am@localheinz.com>
2022-03-09 07:36:11 +0100

    Fix: Remove unnecessary require_once

--- tests/end-to-end/regression/GitHub/74/Issue74Test.php
+++ tests/end-to-end/regression/GitHub/74/Issue74Test.php
@@ -13,8 +13,6 @@ class Issue74Test extends TestCase
 {
     public function testCreateAndThrowNewExceptionInProcessIsolation(): void
     {
-        require_once __DIR__ . '/NewException.php';
-
         throw new NewException('Testing GH-74');
     }
 }
