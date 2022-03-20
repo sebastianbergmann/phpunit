eea187101cfd9a3d01844a7e511d2bae465aef1f
Sebastian Bergmann <sb@sebastian-bergmann.de>
2022-03-20 18:25:53 +0100

    Revert "Sort test suites by name"
    This reverts commit ddec44b781b20b50fd80e5a0f47eac363ebedd54.

--- src/Util/Configuration.php
+++ src/Util/Configuration.php
@@ -33,7 +33,6 @@ use function libxml_use_internal_errors;
 use function preg_match;
 use function putenv;
 use function realpath;
-use function sort;
 use function sprintf;
 use function stream_resolve_include_path;
 use function strlen;
@@ -923,8 +922,6 @@ final class Configuration
             /* @var DOMElement $node */
             $names[] = $node->getAttribute('name');
         }
-
-        sort($names);
 
         return $names;
     }
