0b489e3e4357c976657d859c6107d8ce61ac333e
Sebastian Bergmann <sb@sebastian-bergmann.de>
2022-04-06 08:29:34 +0200

    Unfortunately, methods from traits used by the class are at the very end of the array returned by ReflectionClass::getMethods()

--- src/Util/Reflection.php
+++ src/Util/Reflection.php
@@ -9,6 +9,7 @@
  */
 namespace PHPUnit\Util;
 
+use PHPUnit\Framework\Assert;
 use PHPUnit\Framework\TestCase;
 use ReflectionClass;
 use ReflectionMethod;
@@ -26,19 +27,12 @@ final class Reflection
         $methods = [];
 
         foreach ($class->getMethods() as $method) {
-            /*
-             * ReflectionClass::getMethods() returns an array of ReflectionMethod
-             * objects in the following order:
-             *
-             *   - Methods in the class for which ReflectionClass::getMethods() was called
-             *   - Methods in the parent class (if any) of the class for which ReflectionClass::getMethods() was called
-             *   - ...
-             *
-             * We can stop processing ReflectionMethod objects when we reach the first
-             * object for a method in PHPUnit\Framework\TestCase.
-             */
             if ($method->getDeclaringClass()->getName() === TestCase::class) {
-                break;
+                continue;
+            }
+
+            if ($method->getDeclaringClass()->getName() === Assert::class) {
+                continue;
             }
 
             $methods[] = $method;
