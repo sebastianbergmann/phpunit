92a3284fa31662134eec08b576fc875ce7cf24c4
Alexandru Patranescu <alexandru.patranescu@payu.ro>
2022-04-10 07:39:30 +0300

    extract building the exclude list outside of building the directory list

--- src/TextUI/TestSuiteMapper.php
+++ src/TextUI/TestSuiteMapper.php
@@ -45,15 +45,15 @@ final class TestSuiteMapper
                 $testSuite      = new TestSuiteObject($testSuiteConfiguration->name());
                 $testSuiteEmpty = true;
 
+                $exclude = [];
+
+                foreach ($testSuiteConfiguration->exclude()->asArray() as $file) {
+                    $exclude[] = $file->path();
+                }
+
                 foreach ($testSuiteConfiguration->directories() as $directory) {
                     if (!version_compare(PHP_VERSION, $directory->phpVersion(), $directory->phpVersionOperator()->asString())) {
                         continue;
-                    }
-
-                    $exclude = [];
-
-                    foreach ($testSuiteConfiguration->exclude()->asArray() as $file) {
-                        $exclude[] = $file->path();
                     }
 
                     $files = (new Facade)->getFilesAsArray(
