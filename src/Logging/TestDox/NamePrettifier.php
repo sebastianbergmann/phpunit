990a9b4936bfbdd821b4ec3aaa67ddef6bcf8a35
Sebastian Bergmann <sb@sebastian-bergmann.de>
2022-05-03 15:45:20 +0200

    Remove superfluous cast

--- src/Logging/TestDox/NamePrettifier.php
+++ src/Logging/TestDox/NamePrettifier.php
@@ -160,7 +160,7 @@ final class NamePrettifier
         if (is_int($test->dataName())) {
             $data = Color::dim(' with data set ') . Color::colorize('fg-cyan', (string) $test->dataName());
         } else {
-            $data = Color::dim(' with ') . Color::colorize('fg-cyan', Color::visualizeWhitespace((string) $test->dataName()));
+            $data = Color::dim(' with ') . Color::colorize('fg-cyan', Color::visualizeWhitespace($test->dataName()));
         }
 
         return $data;
