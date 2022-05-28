abd4a50ff3c568553b73597312ee825b16db3126
Sebastian Bergmann <sb@sebastian-bergmann.de>
2022-05-28 09:45:19 +0200

    This interface is not covered by the backward compatibility promise for PHPUnit

--- src/TextUI/ResultPrinter/ResultPrinter.php
+++ src/TextUI/ResultPrinter/ResultPrinter.php
@@ -12,7 +12,7 @@ namespace PHPUnit\TextUI\ResultPrinter;
 use PHPUnit\Framework\TestResult;
 
 /**
- * @no-named-arguments Parameter names are not covered by the backward compatibility promise for PHPUnit
+ * @internal This interface is not covered by the backward compatibility promise for PHPUnit
  */
 interface ResultPrinter
 {
