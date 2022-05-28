405268c1bbdd91eac8912809ce5115acbb778919
Sebastian Bergmann <sb@sebastian-bergmann.de>
2022-05-28 08:51:27 +0200

    This class is not covered by the backward compatibility promise for PHPUnit

--- src/TextUI/ResultPrinter/Standard/ResultPrinter.php
+++ src/TextUI/ResultPrinter/Standard/ResultPrinter.php
@@ -40,7 +40,7 @@ use SebastianBergmann\Timer\ResourceUsageFormatter;
 use SebastianBergmann\Timer\Timer;
 
 /**
- * @no-named-arguments Parameter names are not covered by the backward compatibility promise for PHPUnit
+ * @internal This class is not covered by the backward compatibility promise for PHPUnit
  */
 final class ResultPrinter extends Printer implements ResultPrinterInterface
 {
