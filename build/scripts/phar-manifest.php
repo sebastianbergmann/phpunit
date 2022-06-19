1b1bc6b503472481dd29d4244b677921f68d840e
Sebastian Bergmann <sb@sebastian-bergmann.de>
2022-06-19 11:10:15 +0200

    Initial work on SBOM support for PHAR

--- build/scripts/phar-manifest.php
+++ build/scripts/phar-manifest.php
@@ -1,10 +1,10 @@
 #!/usr/bin/env php
 <?php declare(strict_types=1);
-if ($argc !== 2) {
+if ($argc !== 3) {
     fwrite(
         STDERR,
         sprintf(
-            '%s /path/to/manifest.txt' . PHP_EOL,
+            '%s /path/to/manifest.txt /path/to/sbom.xml' . PHP_EOL,
             $argv[0]
         )
     );
@@ -16,6 +16,7 @@ $dependencies = dependencies();
 $version      = version();
 
 manifest($argv[1], $version, $dependencies);
+sbom($argv[2], $version, $dependencies);
 
 function manifest(string $outputFilename, string $version, array $dependencies): void
 {
@@ -32,6 +33,53 @@ function manifest(string $outputFilename, string $version, array $dependencies):
     }
 
     file_put_contents($outputFilename, $buffer);
+}
+
+function sbom(string $outputFilename, string $version, array $dependencies): void
+{
+    $writer = new XMLWriter;
+
+    $writer->openMemory();
+    $writer->setIndent(true);
+    $writer->startDocument();
+
+    $writer->startElement('bom');
+    $writer->writeAttribute('xmlns', 'https://cyclonedx.org/schema/bom/1.4');
+
+    $writer->startElement('components');
+
+    writeComponent(
+        $writer,
+        'phpunit',
+        'phpunit',
+        $version,
+        'The PHP Unit Testing framework',
+        ['BSD-3-Clause']
+    );
+
+    foreach ($dependencies as $dependency) {
+        [$group, $name]    = explode('/', $dependency['name']);
+        $dependencyVersion = $dependency['version'];
+
+        if (!preg_match('/^[v= ]*(([0-9]+)(\\.([0-9]+)(\\.([0-9]+)(-([0-9]+))?(-?([a-zA-Z-+][a-zA-Z0-9.\\-:]*)?)?)?)?)$/', $dependencyVersion)) {
+            $dependencyVersion .=  '@' . $dependency['source']['reference'];
+        }
+
+        writeComponent(
+            $writer,
+            $group,
+            $name,
+            $dependencyVersion,
+            $dependency['description'],
+            $dependency['license']
+        );
+    }
+
+    $writer->endElement();
+    $writer->endElement();
+    $writer->endDocument();
+
+    file_put_contents($outputFilename, $writer->outputMemory());
 }
 
 function dependencies(): array
@@ -56,4 +104,37 @@ function version(): string
     $hash   = @exec('git log -1 --format="%H"');
 
     return $branch . '@' . $hash;
+}
+
+function writeComponent(XMLWriter $writer, string $group, string $name, string $version, string $description, array $licenses): void
+{
+    $writer->startElement('component');
+    $writer->writeAttribute('type', 'library');
+
+    $writer->writeElement('group', $group);
+    $writer->writeElement('name', $name);
+    $writer->writeElement('version', $version);
+    $writer->writeElement('description', $description);
+
+    $writer->startElement('licenses');
+
+    foreach ($licenses as $license) {
+        $writer->startElement('license');
+        $writer->writeElement('id', $license);
+        $writer->endElement();
+    }
+
+    $writer->endElement();
+
+    $writer->writeElement(
+        'purl',
+        sprintf(
+            'pkg:composer/%s/%s@%s',
+            $group,
+            $name,
+            $version
+        )
+    );
+
+    $writer->endElement();
 }
