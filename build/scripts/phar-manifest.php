f40f184c19be0787dc24a8989df685577c01f45c
Sebastian Bergmann <sb@sebastian-bergmann.de>
2022-06-19 10:27:21 +0200

    Refactor

--- build/scripts/phar-manifest.php
+++ build/scripts/phar-manifest.php
@@ -1,27 +1,59 @@
 #!/usr/bin/env php
 <?php declare(strict_types=1);
-print 'phpunit/phpunit: ';
+if ($argc !== 2) {
+    fwrite(
+        STDERR,
+        sprintf(
+            '%s /path/to/manifest.txt' . PHP_EOL,
+            $argv[0]
+        )
+    );
 
-$tag = @\exec('git describe --tags 2>&1');
-
-if (\strpos($tag, '-') === false && \strpos($tag, 'No names found') === false) {
-    print $tag;
-} else {
-    $branch = @\exec('git rev-parse --abbrev-ref HEAD');
-    $hash   = @\exec('git log -1 --format="%H"');
-    print $branch . '@' . $hash;
+    exit(1);
 }
 
-print "\n";
+$dependencies = dependencies();
+$version      = version();
 
-$lock = \json_decode(\file_get_contents(__DIR__ . '/../../composer.lock'));
+manifest($argv[1], $version, $dependencies);
 
-foreach ($lock->packages as $package) {
-    print $package->name . ': ' . $package->version;
+function manifest(string $outputFilename, string $version, array $dependencies): void
+{
+    $buffer = 'phpunit/phpunit: ' . $version . "\n";
 
-    if (!\preg_match('/^[v= ]*(([0-9]+)(\\.([0-9]+)(\\.([0-9]+)(-([0-9]+))?(-?([a-zA-Z-+][a-zA-Z0-9\\.\\-:]*)?)?)?)?)$/', $package->version)) {
-        print '@' . $package->source->reference;
+    foreach ($dependencies as $dependency) {
+        $buffer .= $dependency['name'] . ': ' . $dependency['version'];
+
+        if (!preg_match('/^[v= ]*(([0-9]+)(\\.([0-9]+)(\\.([0-9]+)(-([0-9]+))?(-?([a-zA-Z-+][a-zA-Z0-9.\\-:]*)?)?)?)?)$/', $dependency['version'])) {
+            $buffer .=  '@' . $dependency['source']['reference'];
+        }
+
+        $buffer .=  "\n";
     }
 
-    print "\n";
+    file_put_contents($outputFilename, $buffer);
+}
+
+function dependencies(): array
+{
+    return json_decode(
+        file_get_contents(
+            __DIR__ . '/../../composer.lock'
+        ),
+        true
+    )['packages'];
+}
+
+function version(): string
+{
+    $tag = @exec('git describe --tags 2>&1');
+
+    if (strpos($tag, '-') === false && strpos($tag, 'No names found') === false) {
+        return $tag;
+    }
+
+    $branch = @exec('git rev-parse --abbrev-ref HEAD');
+    $hash   = @exec('git log -1 --format="%H"');
+
+    return $branch . '@' . $hash;
 }
