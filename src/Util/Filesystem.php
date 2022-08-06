455d21dd7cb950a8a00caa17a36c0cc323a55bb2
Sebastian Bergmann <sb@sebastian-bergmann.de>
2022-08-06 11:46:52 +0200

    Delete unused code

--- src/Util/Filesystem.php
+++ src/Util/Filesystem.php
@@ -9,31 +9,14 @@
  */
 namespace PHPUnit\Util;
 
-use const DIRECTORY_SEPARATOR;
 use function is_dir;
 use function mkdir;
-use function str_replace;
 
 /**
  * @internal This class is not covered by the backward compatibility promise for PHPUnit
  */
 final class Filesystem
 {
-    /**
-     * Maps class names to source file names.
-     *
-     *   - PEAR CS:   Foo_Bar_Baz -> Foo/Bar/Baz.php
-     *   - Namespace: Foo\Bar\Baz -> Foo/Bar/Baz.php
-     */
-    public static function classNameToFilename(string $className): string
-    {
-        return str_replace(
-            ['_', '\\'],
-            DIRECTORY_SEPARATOR,
-            $className
-        ) . '.php';
-    }
-
     public static function createDirectory(string $directory): bool
     {
         return !(!is_dir($directory) && !@mkdir($directory, 0777, true) && !is_dir($directory));
