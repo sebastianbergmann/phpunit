31e2b1942f277ac5e653f12afdcd6c92aebee2b4
Sebastian Bergmann <sb@sebastian-bergmann.de>
2022-08-09 08:47:38 +0200

    Initial work on (not end-to-end) tests for ResultPrinter

---
+++ tests/unit/TextUI/ResultPrinterTest.php
@@ -0,0 +1,206 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of PHPUnit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace PHPUnit\TextUI;
+
+use function hrtime;
+use Exception;
+use PHPUnit\Event\Code\TestMethod;
+use PHPUnit\Event\Code\Throwable;
+use PHPUnit\Event\Telemetry\Duration;
+use PHPUnit\Event\Telemetry\HRTime;
+use PHPUnit\Event\Telemetry\Info;
+use PHPUnit\Event\Telemetry\MemoryUsage;
+use PHPUnit\Event\Telemetry\Snapshot;
+use PHPUnit\Event\Test\BeforeFirstTestMethodErrored;
+use PHPUnit\Event\Test\ConsideredRisky;
+use PHPUnit\Event\Test\DeprecationTriggered;
+use PHPUnit\Event\Test\Errored;
+use PHPUnit\Event\Test\ErrorTriggered;
+use PHPUnit\Event\Test\Failed;
+use PHPUnit\Event\Test\MarkedIncomplete;
+use PHPUnit\Event\Test\NoticeTriggered;
+use PHPUnit\Event\Test\PassedWithWarning;
+use PHPUnit\Event\Test\PhpDeprecationTriggered;
+use PHPUnit\Event\Test\PhpErrorTriggered;
+use PHPUnit\Event\Test\PhpNoticeTriggered;
+use PHPUnit\Event\Test\PhpunitDeprecationTriggered;
+use PHPUnit\Event\Test\PhpunitWarningTriggered;
+use PHPUnit\Event\Test\PhpWarningTriggered;
+use PHPUnit\Event\Test\Skipped;
+use PHPUnit\Event\Test\WarningTriggered;
+use PHPUnit\Event\TestDataCollection;
+use PHPUnit\Event\TestRunner\WarningTriggered as TestRunnerWarningTriggered;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Medium;
+use PHPUnit\Framework\ExpectationFailedException;
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Metadata\MetadataCollection;
+use PHPUnit\TestRunner\TestResult\TestResult;
+use PHPUnit\Util\Printer;
+
+#[CoversClass(ResultPrinter::class)]
+#[Medium]
+final class ResultPrinterTest extends TestCase
+{
+    #[DataProvider('provider')]
+    public function testPrintsExpectedOutputForTestResultObject(string $expectationFile, TestResult $result): void
+    {
+        $printer = new class implements Printer
+        {
+            private string $buffer = '';
+
+            public function print(string $buffer): void
+            {
+                $this->buffer .= $buffer;
+            }
+
+            public function flush(): void
+            {
+            }
+
+            public function buffer(): string
+            {
+                return $this->buffer;
+            }
+        };
+
+        $resultPrinter = new ResultPrinter(
+            $printer,
+            true,
+            true,
+            false,
+            false
+        );
+
+        $resultPrinter->printResult($result);
+        $resultPrinter->flush();
+
+        $this->assertStringMatchesFormatFile(
+            $expectationFile,
+            $printer->buffer()
+        );
+    }
+
+    /**
+     * @psalm-return array<string,array{0: string, 1: TestResult}>
+     */
+    public function provider(): array
+    {
+        return [
+            'successful test' => [
+                __DIR__ . '/expectations/successful_test.txt',
+                $this->createTestResult(),
+            ],
+
+            'errored test' => [
+                __DIR__ . '/expectations/errored_test.txt',
+                $this->createTestResult(
+                    testErroredEvents: [
+                        new Errored(
+                            $this->telemetryInfo(),
+                            $this->testMethod(),
+                            Throwable::from(new Exception('message'))
+                        ),
+                    ]
+                ),
+            ],
+
+            'failed test' => [
+                __DIR__ . '/expectations/failed_test.txt',
+                $this->createTestResult(
+                    testFailedEvents: [
+                        new Failed(
+                            $this->telemetryInfo(),
+                            $this->testMethod(),
+                            Throwable::from(
+                                new ExpectationFailedException(
+                                    'Failed asserting that false is true.'
+                                )
+                            )
+                        ),
+                    ]
+                ),
+            ],
+        ];
+    }
+
+    /**
+     * @psalm-param list<BeforeFirstTestMethodErrored|Errored> $testErroredEvents
+     * @psalm-param list<Failed> $testFailedEvents
+     * @psalm-param list<PassedWithWarning> $testPassedWithWarningEvents
+     * @psalm-param array<string,list<ConsideredRisky>> $testConsideredRiskyEvents
+     * @psalm-param list<Skipped> $testSkippedEvents
+     * @psalm-param list<MarkedIncomplete> $testMarkedIncompleteEvents
+     * @psalm-param array<string,list<DeprecationTriggered>> $testTriggeredDeprecationEvents
+     * @psalm-param array<string,list<PhpDeprecationTriggered>> $testTriggeredPhpDeprecationEvents
+     * @psalm-param array<string,list<PhpunitDeprecationTriggered>> $testTriggeredPhpunitDeprecationEvents
+     * @psalm-param array<string,list<ErrorTriggered>> $testTriggeredErrorEvents
+     * @psalm-param array<string,list<PhpErrorTriggered>> $testTriggeredPhpErrorEvents
+     * @psalm-param array<string,list<NoticeTriggered>> $testTriggeredNoticeEvents
+     * @psalm-param array<string,list<PhpNoticeTriggered>> $testTriggeredPhpNoticeEvents
+     * @psalm-param array<string,list<WarningTriggered>> $testTriggeredWarningEvents
+     * @psalm-param array<string,list<PhpWarningTriggered>> $testTriggeredPhpWarningEvents
+     * @psalm-param array<string,list<PhpunitWarningTriggered>> $testTriggeredPhpunitWarningEvents
+     * @psalm-param list<TestRunnerWarningTriggered> $testRunnerTriggeredWarningEvents
+     */
+    private function createTestResult(int $numberOfTests = 1, int $numberOfTestsRun = 1, int $numberOfAssertions = 1, array $testErroredEvents = [], array $testFailedEvents = [], array $testPassedWithWarningEvents = [], array $testConsideredRiskyEvents = [], array $testSkippedEvents = [], array $testMarkedIncompleteEvents = [], array $testTriggeredDeprecationEvents = [], array $testTriggeredPhpDeprecationEvents = [], array $testTriggeredPhpunitDeprecationEvents = [], array $testTriggeredErrorEvents = [], array $testTriggeredPhpErrorEvents = [], array $testTriggeredNoticeEvents = [], array $testTriggeredPhpNoticeEvents = [], array $testTriggeredWarningEvents = [], array $testTriggeredPhpWarningEvents = [], array $testTriggeredPhpunitWarningEvents = [], array $testRunnerTriggeredWarningEvents = []): TestResult
+    {
+        return new TestResult(
+            $numberOfTests,
+            $numberOfTestsRun,
+            $numberOfAssertions,
+            $testErroredEvents,
+            $testFailedEvents,
+            $testPassedWithWarningEvents,
+            $testConsideredRiskyEvents,
+            $testSkippedEvents,
+            $testMarkedIncompleteEvents,
+            $testTriggeredDeprecationEvents,
+            $testTriggeredPhpDeprecationEvents,
+            $testTriggeredPhpunitDeprecationEvents,
+            $testTriggeredErrorEvents,
+            $testTriggeredPhpErrorEvents,
+            $testTriggeredNoticeEvents,
+            $testTriggeredPhpNoticeEvents,
+            $testTriggeredWarningEvents,
+            $testTriggeredPhpWarningEvents,
+            $testTriggeredPhpunitWarningEvents,
+            $testRunnerTriggeredWarningEvents
+        );
+    }
+
+    private function testMethod(): TestMethod
+    {
+        return new TestMethod(
+            'FooTest',
+            'testBar',
+            'FooTest.php',
+            1,
+            MetadataCollection::fromArray([]),
+            TestDataCollection::fromArray([])
+        );
+    }
+
+    private function telemetryInfo(): Info
+    {
+        return new Info(
+            new Snapshot(
+                HRTime::fromSecondsAndNanoseconds(...hrtime(false)),
+                MemoryUsage::fromBytes(1000),
+                MemoryUsage::fromBytes(2000)
+            ),
+            Duration::fromSecondsAndNanoseconds(123, 456),
+            MemoryUsage::fromBytes(2000),
+            Duration::fromSecondsAndNanoseconds(234, 567),
+            MemoryUsage::fromBytes(3000)
+        );
+    }
+}
