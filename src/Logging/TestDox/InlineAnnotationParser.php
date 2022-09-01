ec3ffe4ab3a7573affca514a1c4848d5f204520c
Sebastian Bergmann <sb@sebastian-bergmann.de>
2022-09-01 15:46:18 +0200

    Move class to where it will be used

--- src/Logging/TestDox/InlineAnnotationParser.php
+++ src/Logging/TestDox/InlineAnnotationParser.php
@@ -7,7 +7,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-namespace PHPUnit\Metadata\Parser;
+namespace PHPUnit\Logging\TestDox;
 
 use function array_slice;
 use function file;
