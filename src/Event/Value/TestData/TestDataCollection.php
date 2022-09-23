c2e0271036684c756763faa2e55155c0578fc160
Sebastian Bergmann <sb@sebastian-bergmann.de>
2022-09-23 16:31:38 +0200

    This is not immutable
    Co-authored-by: Sebastian Bergmann <sb@sebastian-bergmann.de>
    Co-authored-by: Arne Blankerts <arne@blankerts.de>
    Co-authored-by: Andreas Möller <am@localheinz.com>

--- src/Event/Value/TestData/TestDataCollection.php
+++ src/Event/Value/TestData/TestDataCollection.php
@@ -13,8 +13,6 @@ use Countable;
 use IteratorAggregate;
 
 /**
- * @psalm-immutable
- *
  * @no-named-arguments Parameter names are not covered by the backward compatibility promise for PHPUnit
  */
 final class TestDataCollection implements Countable, IteratorAggregate
