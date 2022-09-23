b498fc79efddf3e8f1b531c7a3796e247c2be0d3
Sebastian Bergmann <sb@sebastian-bergmann.de>
2022-09-23 16:25:46 +0200

    Mark classes as internal
    Co-authored-by: Sebastian Bergmann <sb@sebastian-bergmann.de>
    Co-authored-by: Arne Blankerts <arne@blankerts.de>
    Co-authored-by: Andreas Möller <am@localheinz.com>

--- src/Event/Value/Telemetry/SystemStopWatch.php
+++ src/Event/Value/Telemetry/SystemStopWatch.php
@@ -12,7 +12,7 @@ namespace PHPUnit\Event\Telemetry;
 use function hrtime;
 
 /**
- * @no-named-arguments Parameter names are not covered by the backward compatibility promise for PHPUnit
+ * @internal This class is not covered by the backward compatibility promise for PHPUnit
  */
 final class SystemStopWatch implements StopWatch
 {
