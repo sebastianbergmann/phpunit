da900e81ed83b7a61a8db22432aa8c6090fdfaaa
Sebastian Bergmann <sb@sebastian-bergmann.de>
2022-09-24 06:59:28 +0200

    The Configuration value object is immutable and part of the public API (because it is exposed in the TestRunner\Configured event)

--- src/TextUI/Configuration/Configuration.php
+++ src/TextUI/Configuration/Configuration.php
@@ -10,7 +10,9 @@
 namespace PHPUnit\TextUI\Configuration;
 
 /**
- * @internal This class is not covered by the backward compatibility promise for PHPUnit
+ * @psalm-immutable
+ *
+ * @no-named-arguments Parameter names are not covered by the backward compatibility promise for PHPUnit
  */
 final class Configuration
 {
