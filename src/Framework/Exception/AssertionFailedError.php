6809d7c8fc6a2ed6cc8448a667b19f1952192b23
mondrake <mondrake.org@gmail.com>
2022-10-02 20:58:40 +0200

    Fix AssertionFailedError

--- src/Framework/Exception/AssertionFailedError.php
+++ src/Framework/Exception/AssertionFailedError.php
@@ -9,11 +9,64 @@
  */
 namespace PHPUnit\Framework;
 
+use function get_class;
+use PHPUnit\Util\Filter;
+use Throwable;
+
 /**
  * @internal This class is not covered by the backward compatibility promise for PHPUnit
  */
 class AssertionFailedError extends Exception implements SelfDescribing
 {
+    /**
+     * @var string|null
+     */
+    protected $previousThrowableClass;
+
+    /**
+     * @var string|null
+     */
+    protected $previousThrowableMessage;
+
+    /**
+     * @var int|null
+     */
+    protected $previousThrowableCode;
+
+    /**
+     * @var string|null
+     */
+    protected $previousThrowableTrace;
+
+    public function __construct($message = '', $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+
+        if ($previous) {
+            $this->previousThrowableClass = get_class($previous);
+            $this->previousThrowableMessage = $previous->getMessage();
+            $this->previousThrowableCode = $previous->getCode();
+            $this->previousThrowableTrace = Filter::getFilteredStacktrace($previous);
+        }
+    }
+
+    public function __toString(): string
+    {
+        $string = parent::__toString();
+
+        if ($this->previousThrowableClass) {
+            $string .= "\nCaused by " . $this->previousThrowableClass;
+
+            if ($this->previousThrowableMessage !== '') {
+                $string .= ": " . $this->previousThrowableMessage;
+            }
+
+            $string .= "\n" . $this->previousThrowableTrace;
+        }
+
+        return $string;
+    }
+
     /**
      * Wrapper for getMessage() which is declared as final.
      */
