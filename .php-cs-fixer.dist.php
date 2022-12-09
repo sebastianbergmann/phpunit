1b09a9a41219b727102da8c384d882e00da23635
Sebastian Bergmann <sb@sebastian-bergmann.de>
2022-12-09 08:09:26 +0100

    Exclude source file with PHP 8.2 syntax

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -15,6 +15,7 @@ $finder = PhpCsFixer\Finder::create()
     ->in(__DIR__ . '/tests/end-to-end')
     ->in(__DIR__ . '/tests/unit')
     ->notName('InterfaceWithMethodReturningDisjunctiveNormalFormType.php')
+    ->notName('ReadonlyClass.php')
     ->notName('*.phpt');
 
 $config = new PhpCsFixer\Config;
