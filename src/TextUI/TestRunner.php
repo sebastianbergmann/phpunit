30b409f9eba407df2a7622aec9c9a42d89244f35
Sebastian Bergmann <sb@sebastian-bergmann.de>
2023-01-24 10:05:40 +0100

    Do not let exceptions from implementation details bubble up
    Co-authored-by: Arne Blankerts <Arne@Blankerts.de>
    Co-authored-by: Sebastian Bergmann <sb@sebastian-bergmann.de>

--- src/TextUI/TestRunner.php
+++ src/TextUI/TestRunner.php
@@ -11,28 +11,11 @@ namespace PHPUnit\TextUI;
 
 use function mt_srand;
 use PHPUnit\Event;
-use PHPUnit\Event\NoPreviousThrowableException;
-use PHPUnit\Framework\Exception;
 use PHPUnit\Framework\TestSuite;
 use PHPUnit\Runner\ResultCache\ResultCache;
 use PHPUnit\Runner\TestSuiteSorter;
-use PHPUnit\TextUI\Configuration\CodeCoverageReportNotConfiguredException;
 use PHPUnit\TextUI\Configuration\Configuration;
-use PHPUnit\TextUI\Configuration\FilterNotConfiguredException;
-use PHPUnit\TextUI\Configuration\LoggingNotConfiguredException;
-use PHPUnit\TextUI\Configuration\NoBootstrapException;
-use PHPUnit\TextUI\Configuration\NoConfigurationFileException;
-use PHPUnit\TextUI\Configuration\NoCoverageCacheDirectoryException;
-use PHPUnit\TextUI\Configuration\NoCustomCssFileException;
-use PHPUnit\TextUI\Configuration\NoPharExtensionDirectoryException;
-use SebastianBergmann\CodeCoverage\Driver\PcovNotAvailableException;
-use SebastianBergmann\CodeCoverage\Driver\XdebugNotAvailableException;
-use SebastianBergmann\CodeCoverage\Driver\XdebugNotEnabledException;
-use SebastianBergmann\CodeCoverage\InvalidArgumentException;
-use SebastianBergmann\CodeCoverage\NoCodeCoverageDriverAvailableException;
-use SebastianBergmann\CodeCoverage\NoCodeCoverageDriverWithPathCoverageSupportAvailableException;
-use SebastianBergmann\CodeCoverage\UnintentionallyCoveredCodeException;
-use SebastianBergmann\Timer\TimeSinceStartOfRequestNotAvailableException;
+use Throwable;
 
 /**
  * @internal This class is not covered by the backward compatibility promise for PHPUnit
@@ -40,66 +23,52 @@ use SebastianBergmann\Timer\TimeSinceStartOfRequestNotAvailableException;
 final class TestRunner
 {
     /**
-     * @throws \PHPUnit\Runner\Exception
-     * @throws CodeCoverageReportNotConfiguredException
-     * @throws Event\EventFacadeIsSealedException
-     * @throws Event\RuntimeException
-     * @throws Event\UnknownSubscriberTypeException
-     * @throws Exception
-     * @throws FilterNotConfiguredException
-     * @throws InvalidArgumentException
-     * @throws LoggingNotConfiguredException
-     * @throws NoBootstrapException
-     * @throws NoCodeCoverageDriverAvailableException
-     * @throws NoCodeCoverageDriverWithPathCoverageSupportAvailableException
-     * @throws NoConfigurationFileException
-     * @throws NoCoverageCacheDirectoryException
-     * @throws NoCustomCssFileException
-     * @throws NoPharExtensionDirectoryException
-     * @throws NoPreviousThrowableException
-     * @throws PcovNotAvailableException
-     * @throws TimeSinceStartOfRequestNotAvailableException
-     * @throws UnintentionallyCoveredCodeException
-     * @throws XdebugNotAvailableException
-     * @throws XdebugNotEnabledException
-     * @throws XmlConfiguration\Exception
+     * @throws RuntimeException
      */
     public function run(Configuration $configuration, ResultCache $resultCache, TestSuite $suite): void
     {
-        Event\Facade::emitter()->testRunnerStarted();
+        try {
+            Event\Facade::emitter()->testRunnerStarted();
 
-        if ($configuration->executionOrder() === TestSuiteSorter::ORDER_RANDOMIZED) {
-            mt_srand($configuration->randomOrderSeed());
-        }
+            if ($configuration->executionOrder() === TestSuiteSorter::ORDER_RANDOMIZED) {
+                mt_srand($configuration->randomOrderSeed());
+            }
 
-        if ($configuration->executionOrder() !== TestSuiteSorter::ORDER_DEFAULT ||
-            $configuration->executionOrderDefects() !== TestSuiteSorter::ORDER_DEFAULT ||
-            $configuration->resolveDependencies()) {
-            $resultCache->load();
+            if ($configuration->executionOrder() !== TestSuiteSorter::ORDER_DEFAULT ||
+                $configuration->executionOrderDefects() !== TestSuiteSorter::ORDER_DEFAULT ||
+                $configuration->resolveDependencies()) {
+                $resultCache->load();
 
-            (new TestSuiteSorter($resultCache))->reorderTestsInSuite(
-                $suite,
-                $configuration->executionOrder(),
-                $configuration->resolveDependencies(),
-                $configuration->executionOrderDefects()
+                (new TestSuiteSorter($resultCache))->reorderTestsInSuite(
+                    $suite,
+                    $configuration->executionOrder(),
+                    $configuration->resolveDependencies(),
+                    $configuration->executionOrderDefects()
+                );
+
+                Event\Facade::emitter()->testSuiteSorted(
+                    $configuration->executionOrder(),
+                    $configuration->executionOrderDefects(),
+                    $configuration->resolveDependencies()
+                );
+            }
+
+            (new TestSuiteFilterProcessor)->process($configuration, $suite);
+
+            Event\Facade::emitter()->testRunnerExecutionStarted(
+                Event\TestSuite\TestSuite::fromTestSuite($suite)
             );
 
-            Event\Facade::emitter()->testSuiteSorted(
-                $configuration->executionOrder(),
-                $configuration->executionOrderDefects(),
-                $configuration->resolveDependencies()
+            $suite->run();
+
+            Event\Facade::emitter()->testRunnerExecutionFinished();
+            Event\Facade::emitter()->testRunnerFinished();
+        } catch (Throwable $t) {
+            throw new RuntimeException(
+                $t->getMessage(),
+                (int) $t->getCode(),
+                $t
             );
         }
-
-        (new TestSuiteFilterProcessor)->process($configuration, $suite);
-
-        Event\Facade::emitter()->testRunnerExecutionStarted(
-            Event\TestSuite\TestSuite::fromTestSuite($suite)
-        );
-
-        $suite->run();
-
-        Event\Facade::emitter()->testRunnerExecutionFinished();
-        Event\Facade::emitter()->testRunnerFinished();
     }
 }
