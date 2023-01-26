4bef6b08ee91b8cdf82693122c594646a9b846ae
Sebastian Bergmann <sb@sebastian-bergmann.de>
2023-01-26 08:29:03 +0100

    Automate packaging of test runner extension that is used in end-to-end tests

---
+++ build/test-extension/src/MyExtensionBootstrap.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of PHPUnit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace PHPUnit\TestFixture\MyExtension;
+
+use PHPUnit\Runner\Extension\Extension;
+use PHPUnit\Runner\Extension\Facade;
+use PHPUnit\Runner\Extension\ParameterCollection;
+use PHPUnit\TextUI\Configuration\Configuration;
+
+final class MyExtensionBootstrap implements Extension
+{
+    public function bootstrap(Configuration $configuration, Facade $facade, ParameterCollection $parameters): void
+    {
+    }
+}
