80b4b940faf4da21d354b126b808da71cb52846d
Alessandro Lai <alessandro.lai85@gmail.com>
2023-02-04 00:07:52 +0100

    Write regression test

--- tests/unit/Event/DirectDispatcherTest.php
+++ tests/unit/Event/DirectDispatcherTest.php
@@ -12,7 +12,10 @@ namespace PHPUnit\Event;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 use PHPUnit\TestFixture\DummyEvent;
+use PHPUnit\TestFixture\DummySecondEvent;
+use PHPUnit\TestFixture\DummySecondSubscriber;
 use PHPUnit\TestFixture\DummySubscriber;
+use PHPUnit\TestFixture\SpyingDummyDoubleSubscriber;
 use PHPUnit\TestFixture\SpyingDummySubscriber;
 use RuntimeException;
 
@@ -58,5 +61,29 @@ final class DirectDispatcherTest extends TestCase
         $dispatcher->dispatch($event);
 
         $this->assertContains($event, $subscriber->events());
+    }
+
+    public function testRegisterDispatcherThatImplementsMultipleInterfaces(): void
+    {
+        $event1 = new DummyEvent;
+        $event2 = new DummySecondEvent;
+
+        $typeMap = new TypeMap;
+
+        $typeMap->addMapping(DummySubscriber::class, DummyEvent::class);
+        $typeMap->addMapping(DummySecondSubscriber::class, DummySecondEvent::class);
+
+        $subscriber = new SpyingDummyDoubleSubscriber;
+
+        $dispatcher = new DirectDispatcher($typeMap);
+
+        $dispatcher->registerSubscriber($subscriber);
+
+        $dispatcher->dispatch($event1);
+        $dispatcher->dispatch($event2);
+
+        $this->assertCount(2, $subscriber->events());
+        $this->assertContains($event1, $subscriber->events());
+        $this->assertContains($event2, $subscriber->events());
     }
 }
