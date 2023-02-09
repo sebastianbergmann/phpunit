5a80b4f831026a2b16b0c802b92eb10777fd3783
Filippo Tessarotto <zoeslam@gmail.com>
2023-02-09 14:44:12 +0100

    Add test for bug: JUnit doesn't log error, and it doesn't appear in DefaultPrinter

---
+++ tests/end-to-end/logging/_files/TypeErrorTest.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of PHPUnit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace PHPUnit\TestFixture;
+
+use PHPUnit\Framework\TestCase;
+
+final class TypeErrorTest extends TestCase
+{
+    private \DateTimeImmutable $dateTime;
+
+    protected function setUp(): void
+    {
+        $this->dateTime = new \DateTime();
+    }
+
+    public function testMe(): void
+    {
+        self::assertTrue(true);
+    }
+}
