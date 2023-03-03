303a453918b64e59793bd7b24cc0f135e7954ffc
Sebastian Bergmann <sb@sebastian-bergmann.de>
2023-03-03 07:29:25 +0100

    Simplify

--- src/Framework/Constraint/String/IsJson.php
+++ src/Framework/Constraint/String/IsJson.php
@@ -59,14 +59,23 @@ final class IsJson extends Constraint
 
         json_decode($other);
 
-        $error = (string) JsonMatchesErrorMessageProvider::determineJsonError(
-            json_last_error()
-        );
-
         return sprintf(
             '%s is valid JSON (%s)',
             $this->exporter()->shortenedExport($other),
-            $error
+            $this->determineJsonError(json_last_error())
         );
+    }
+
+    private function determineJsonError(int $error): string
+    {
+        return match ($error) {
+            JSON_ERROR_NONE           => '',
+            JSON_ERROR_DEPTH          => 'Maximum stack depth exceeded',
+            JSON_ERROR_STATE_MISMATCH => 'Underflow or the modes mismatch',
+            JSON_ERROR_CTRL_CHAR      => 'Unexpected control character found',
+            JSON_ERROR_SYNTAX         => 'Syntax error, malformed JSON',
+            JSON_ERROR_UTF8           => 'Malformed UTF-8 characters, possibly incorrectly encoded',
+            default                   => 'Unknown error',
+        };
     }
 }
