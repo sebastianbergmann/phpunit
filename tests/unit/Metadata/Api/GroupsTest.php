513783ccb6eea094be514c4e3b0ebe23bd2bee27
Sebastian Bergmann <sb@sebastian-bergmann.de>
2023-03-08 08:53:47 +0100

    Add tests

--- tests/unit/Metadata/Api/GroupsTest.php
+++ tests/unit/Metadata/Api/GroupsTest.php
@@ -13,9 +13,11 @@ use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Small;
 use PHPUnit\Framework\TestCase;
-use PHPUnit\TestFixture\AssertionExampleTest;
-use PHPUnit\TestFixture\BankAccountTest;
-use PHPUnit\TestFixture\NumericGroupAnnotationTest;
+use PHPUnit\TestFixture\LargeGroupAttributesTest;
+use PHPUnit\TestFixture\MediumGroupAttributesTest;
+use PHPUnit\TestFixture\NoGroupsMetadataTest;
+use PHPUnit\TestFixture\SmallGroupAnnotationsTest;
+use PHPUnit\TestFixture\SmallGroupAttributesTest;
 
 #[CoversClass(Groups::class)]
 #[Small]
@@ -28,44 +30,101 @@ final class GroupsTest extends TestCase
                 [
                     'default',
                 ],
-                AssertionExampleTest::class,
+                NoGroupsMetadataTest::class,
                 'testOne',
+                false,
             ],
 
             [
                 [
-                    'balanceIsInitiallyZero',
-                    'specification',
-                    '1234',
-                    '__phpunit_covers_bankaccount::getbalance',
+                    'the-group',
+                    'the-ticket',
+                    'small',
+                    'another-group',
+                    'another-ticket',
                 ],
-                BankAccountTest::class,
-                'testBalanceIsInitiallyZero',
+                SmallGroupAttributesTest::class,
+                'testOne',
+                false,
             ],
 
             [
                 [
-                    't123456',
-                    '3502',
+                    'the-group',
+                    'the-ticket',
+                    'small',
+                    'another-group',
+                    'another-ticket',
                 ],
-                NumericGroupAnnotationTest::class,
-                'testTicketAnnotationSupportsNumericValue',
+                SmallGroupAnnotationsTest::class,
+                'testOne',
+                false,
             ],
 
             [
                 [
-                    't123456',
-                    '3502',
+                    'the-group',
+                    'the-ticket',
+                    'small',
+                    'another-group',
+                    'another-ticket',
+                    '__phpunit_covers_phpunit\testfixture\coveredclass',
+                    '__phpunit_uses_phpunit\testfixture\coveredclass',
                 ],
-                NumericGroupAnnotationTest::class,
-                'testGroupAnnotationSupportsNumericValue',
+                SmallGroupAttributesTest::class,
+                'testOne',
+                true,
+            ],
+
+            [
+                [
+                    'the-group',
+                    'the-ticket',
+                    'small',
+                    'another-group',
+                    'another-ticket',
+                    '__phpunit_covers_phpunit\testfixture\coveredclass',
+                    '__phpunit_uses_phpunit\testfixture\coveredclass',
+                ],
+                SmallGroupAnnotationsTest::class,
+                'testOne',
+                true,
+            ],
+
+            [
+                [
+                    'medium',
+                ],
+                MediumGroupAttributesTest::class,
+                'testOne',
+                false,
+            ],
+
+            [
+                [
+                    'large',
+                ],
+                LargeGroupAttributesTest::class,
+                'testOne',
+                false,
             ],
         ];
     }
 
     #[DataProvider('provider')]
-    public function testGroupsAreAssigned(array $groups, string $className, string $methodName): void
+    public function testAssignsGroups(array $expected, string $className, string $methodName, bool $includeVirtual): void
     {
-        $this->assertSame($groups, (new Groups)->groups($className, $methodName));
+        $this->assertSame(
+            $expected,
+            (new Groups)->groups($className, $methodName, $includeVirtual)
+        );
+    }
+
+    public function testAssignsSize(): void
+    {
+        $this->assertTrue((new Groups)->size(SmallGroupAttributesTest::class, 'testOne')->isSmall());
+        $this->assertTrue((new Groups)->size(MediumGroupAttributesTest::class, 'testOne')->isMedium());
+        $this->assertTrue((new Groups)->size(LargeGroupAttributesTest::class, 'testOne')->isLarge());
+        $this->assertTrue((new Groups)->size(NoGroupsMetadataTest::class, 'testOne')->isUnknown());
     }
 }
