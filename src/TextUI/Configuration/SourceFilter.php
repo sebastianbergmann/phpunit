7e0217c754c64794e37325128e36e527cc5e4443
Sebastian Bergmann <sb@sebastian-bergmann.de>
2023-04-04 10:54:09 +0200

    Decouple the mapping of a Source object to a list of files from filtering

--- src/TextUI/Configuration/SourceFilter.php
+++ src/TextUI/Configuration/SourceFilter.php
@@ -9,91 +9,15 @@
  */
 namespace PHPUnit\TextUI\Configuration;
 
-use function realpath;
-use SebastianBergmann\FileIterator\Facade as FileIteratorFacade;
-use SplObjectStorage;
-
 /**
  * @internal This class is not covered by the backward compatibility promise for PHPUnit
  */
 final class SourceFilter
 {
-    /**
-     * @psalm-var SplObjectStorage<Source, array<non-empty-string, true>>
-     */
-    private static ?SplObjectStorage $files = null;
-
     public function includes(Source $source, string $path): bool
     {
-        $this->process($source);
+        $files = (new SourceMapper)->map($source);
 
-        return isset(self::$files[$source][$path]);
-    }
-
-    private function process(Source $source): void
-    {
-        if (self::$files === null) {
-            self::$files = new SplObjectStorage;
-        }
-
-        if (isset(self::$files[$source])) {
-            return;
-        }
-
-        $files = [];
-
-        foreach ($source->includeDirectories() as $directory) {
-            foreach ((new FileIteratorFacade)->getFilesAsArray($directory->path(), $directory->suffix(), $directory->prefix()) as $file) {
-                $file = realpath($file);
-
-                if (!$file) {
-                    continue;
-                }
-
-                $files[$file] = true;
-            }
-        }
-
-        foreach ($source->includeFiles() as $file) {
-            $file = realpath($file->path());
-
-            if (!$file) {
-                continue;
-            }
-
-            $files[$file] = true;
-        }
-
-        foreach ($source->excludeDirectories() as $directory) {
-            foreach ((new FileIteratorFacade)->getFilesAsArray($directory->path(), $directory->suffix(), $directory->prefix()) as $file) {
-                $file = realpath($file);
-
-                if (!$file) {
-                    continue;
-                }
-
-                if (!isset($files[$file])) {
-                    continue;
-                }
-
-                unset($files[$file]);
-            }
-        }
-
-        foreach ($source->excludeFiles() as $file) {
-            $file = realpath($file->path());
-
-            if (!$file) {
-                continue;
-            }
-
-            if (!isset($files[$file])) {
-                continue;
-            }
-
-            unset($files[$file]);
-        }
-
-        self::$files[$source] = $files;
+        return isset($files[$path]);
     }
 }
