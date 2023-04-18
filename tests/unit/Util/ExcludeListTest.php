1ca868eda593fc9b95be642448c1bc58cbc40842
Sebastian Bergmann <sb@sebastian-bergmann.de>
2023-04-18 16:16:30 +0200

    Add tests

---
+++ tests/unit/Util/ExcludeListTest.php
@@ -0,0 +1,66 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of PHPUnit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace PHPUnit\Util;
+
+use function realpath;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Small;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(ExcludeList::class)]
+#[CoversClass(InvalidDirectoryException::class)]
+#[Small]
+final class ExcludeListTest extends TestCase
+{
+    public function testIsInitialized(): void
+    {
+        $excludeList = new ExcludeList;
+
+        $this->assertContains(
+            realpath(__DIR__ . '/../../../src'),
+            $excludeList->getExcludedDirectories()
+        );
+    }
+
+    public function testExclusionOfFileCanBeQueried(): void
+    {
+        $excludeList = new ExcludeList;
+
+        $this->assertTrue($excludeList->isExcluded(realpath(__DIR__ . '/../../../src/Framework/TestCase.php')));
+        $this->assertFalse($excludeList->isExcluded(__FILE__));
+    }
+
+    public function testCanBeDisabled(): void
+    {
+        $excludeList = new ExcludeList;
+
+        $excludeList->disable();
+
+        $this->assertFalse($excludeList->isExcluded(realpath(__DIR__ . '/../../../src/Framework/TestCase.php')));
+    }
+
+    public function testAdditionalDirectoryCanBeExcluded(): void
+    {
+        $directory = realpath(__DIR__);
+
+        ExcludeList::addDirectory($directory);
+
+        $excludeList = new ExcludeList;
+
+        $this->assertContains($directory, $excludeList->getExcludedDirectories());
+    }
+
+    public function testAdditionalDirectoryThatDoesNotExistCannotBeExcluded(): void
+    {
+        $this->expectException(InvalidDirectoryException::class);
+
+        ExcludeList::addDirectory('/does/not/exist');
+    }
+}
