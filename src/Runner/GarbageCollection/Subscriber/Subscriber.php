89b8a6c0bef909ee476b5356368e04996cb9fdf5
Sebastian Bergmann <sb@sebastian-bergmann.de>
2023-05-23 07:04:05 +0200

    Initial work on triggering PHP's garbage collector after each n-th test

---
+++ src/Runner/GarbageCollection/Subscriber/Subscriber.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of PHPUnit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace PHPUnit\Runner\GarbageCollection;
+
+/**
+ * @internal This class is not covered by the backward compatibility promise for PHPUnit
+ */
+abstract class Subscriber
+{
+    private readonly GarbageCollectionHandler $handler;
+
+    public function __construct(GarbageCollectionHandler $handler)
+    {
+        $this->handler = $handler;
+    }
+
+    protected function handler(): GarbageCollectionHandler
+    {
+        return $this->handler;
+    }
+}
