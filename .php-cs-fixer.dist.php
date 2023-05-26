0380b0c4bcb97298c8cc3d173ee1f82f3f3a76d6
Andreas Möller <am@localheinz.com>
2023-05-26 14:39:00 +0200

    Enhancement: Enable method_chaining_indentation fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -128,6 +128,7 @@ $config->setFinder($finder)
         'method_argument_space' => [
             'on_multiline' => 'ensure_fully_multiline',
         ],
+        'method_chaining_indentation' => true,
         'modernize_types_casting' => true,
         'multiline_comment_opening_closing' => true,
         'multiline_whitespace_before_semicolons' => true,
