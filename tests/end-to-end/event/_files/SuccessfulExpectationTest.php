6c120215cc1c5a4ffd0ec5f7acfd585eb5bc1536
Sebastian Bergmann <sb@sebastian-bergmann.de>
2023-06-16 17:57:26 +0200

    Add tests

---
+++ tests/end-to-end/event/_files/SuccessfulExpectationTest.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of PHPUnit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace PHPUnit\TestFixture\Event;
+
+use PHPUnit\Framework\TestCase;
+use PHPUnit\TestFixture\MockObject\AnInterface;
+
+final class SuccessfulExpectationTest extends TestCase
+{
+    public function testOne(): void
+    {
+        $mock = $this->createMock(AnInterface::class);
+
+        $mock->expects($this->once())->method('doSomething');
+
+        $mock->doSomething();
+    }
+}
