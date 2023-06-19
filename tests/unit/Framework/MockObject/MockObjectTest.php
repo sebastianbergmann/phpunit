ee29831489380d8c40f0fb28a56f0d7f12e8901a
Sebastian Bergmann <sb@sebastian-bergmann.de>
2023-06-19 13:56:35 +0200

    Revert "Revert "Initial work on new tests for stubs and mocks""
    This reverts commit 192e89b9f7bc7df707bff2a2a00715c4daf72ec2.

--- tests/unit/Framework/MockObject/MockObjectTest.php
+++ tests/unit/Framework/MockObject/MockObjectTest.php
@@ -9,10 +9,63 @@
  */
 namespace PHPUnit\Framework\MockObject;
 
+use PHPUnit\Framework\Attributes\RequiresPhp;
 use PHPUnit\Framework\Attributes\Small;
 use PHPUnit\Framework\TestCase;
+use PHPUnit\TestFixture\MockObject\AClass;
+use PHPUnit\TestFixture\MockObject\AnInterface;
+use PHPUnit\TestFixture\MockObject\AnotherInterface;
+use PHPUnit\TestFixture\MockObject\Enumeration;
+use PHPUnit\TestFixture\MockObject\FinalClass;
+use PHPUnit\TestFixture\MockObject\ReadonlyClass;
 
 #[Small]
 final class MockObjectTest extends TestCase
 {
+    public function testCanBeCreatedForInterface(): void
+    {
+        $mock = $this->createMock(AnInterface::class);
+
+        $this->assertInstanceOf(AnInterface::class, $mock);
+        $this->assertInstanceOf(MockObject::class, $mock);
+    }
+
+    public function testCanBeCreatedForIntersectionOfInterfaces(): void
+    {
+        $mock = $this->createMockForIntersectionOfInterfaces([AnInterface::class, AnotherInterface::class]);
+
+        $this->assertInstanceOf(AnInterface::class, $mock);
+        $this->assertInstanceOf(AnotherInterface::class, $mock);
+        $this->assertInstanceOf(MockObject::class, $mock);
+    }
+
+    public function testCanBeCreatedForClassThatCanBeExtended(): void
+    {
+        $mock = $this->createMock(AClass::class);
+
+        $this->assertInstanceOf(AClass::class, $mock);
+        $this->assertInstanceOf(MockObject::class, $mock);
+    }
+
+    public function testCannotBeCreatedForFinalClass(): void
+    {
+        $this->expectException(ClassIsFinalException::class);
+
+        $this->createMock(FinalClass::class);
+    }
+
+    #[RequiresPhp('8.2')]
+    public function testCannotBeCreatedForReadonlyClass(): void
+    {
+        $this->expectException(ClassIsReadonlyException::class);
+
+        $this->createMock(ReadonlyClass::class);
+    }
+
+    public function testCannotBeCreatedForEnumeration(): void
+    {
+        $this->expectException(ClassIsEnumerationException::class);
+
+        $this->createMock(Enumeration::class);
+    }
 }
