192e89b9f7bc7df707bff2a2a00715c4daf72ec2
Sebastian Bergmann <sb@sebastian-bergmann.de>
2023-06-19 13:49:03 +0200

    Revert "Initial work on new tests for stubs and mocks"
    This reverts commit 764c638f53a3972a6d1ce55f7915a8505d5fcd8f.

--- tests/unit/Framework/MockObject/StubTest.php
+++ tests/unit/Framework/MockObject/StubTest.php
@@ -9,63 +9,10 @@
  */
 namespace PHPUnit\Framework\MockObject;
 
-use PHPUnit\Framework\Attributes\RequiresPhp;
 use PHPUnit\Framework\Attributes\Small;
 use PHPUnit\Framework\TestCase;
-use PHPUnit\TestFixture\MockObject\AClass;
-use PHPUnit\TestFixture\MockObject\AnInterface;
-use PHPUnit\TestFixture\MockObject\AnotherInterface;
-use PHPUnit\TestFixture\MockObject\Enumeration;
-use PHPUnit\TestFixture\MockObject\FinalClass;
-use PHPUnit\TestFixture\MockObject\ReadonlyClass;
 
 #[Small]
 final class StubTest extends TestCase
 {
-    public function testCanBeCreatedForInterface(): void
-    {
-        $stub = $this->createStub(AnInterface::class);
-
-        $this->assertInstanceOf(AnInterface::class, $stub);
-        $this->assertInstanceOf(Stub::class, $stub);
-    }
-
-    public function testCanBeCreatedForIntersectionOfInterfaces(): void
-    {
-        $stub = $this->createStubForIntersectionOfInterfaces([AnInterface::class, AnotherInterface::class]);
-
-        $this->assertInstanceOf(AnInterface::class, $stub);
-        $this->assertInstanceOf(AnotherInterface::class, $stub);
-        $this->assertInstanceOf(Stub::class, $stub);
-    }
-
-    public function testCanBeCreatedForClassThatCanBeExtended(): void
-    {
-        $stub = $this->createStub(AClass::class);
-
-        $this->assertInstanceOf(AClass::class, $stub);
-        $this->assertInstanceOf(Stub::class, $stub);
-    }
-
-    public function testCannotBeCreatedForFinalClass(): void
-    {
-        $this->expectException(ClassIsFinalException::class);
-
-        $this->createStub(FinalClass::class);
-    }
-
-    #[RequiresPhp('8.2')]
-    public function testCannotBeCreatedForReadonlyClass(): void
-    {
-        $this->expectException(ClassIsReadonlyException::class);
-
-        $this->createStub(ReadonlyClass::class);
-    }
-
-    public function testCannotBeCreatedForEnumeration(): void
-    {
-        $this->expectException(ClassIsEnumerationException::class);
-
-        $this->createStub(Enumeration::class);
-    }
 }
