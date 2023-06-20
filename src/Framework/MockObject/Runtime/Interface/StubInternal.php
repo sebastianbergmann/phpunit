23d8b19713279112a0754f51a1a0a03d9403d1a1
Sebastian Bergmann <sb@sebastian-bergmann.de>
2023-06-20 11:54:32 +0200

    Add __phpunit_initConfigurableMethods() to StubInternal interface

--- src/Framework/MockObject/Runtime/Interface/StubInternal.php
+++ src/Framework/MockObject/Runtime/Interface/StubInternal.php
@@ -14,6 +14,8 @@ namespace PHPUnit\Framework\MockObject;
  */
 interface StubInternal extends Stub
 {
+    public static function __phpunit_initConfigurableMethods(ConfigurableMethod ...$configurableMethods): void;
+
     public function __phpunit_getInvocationHandler(): InvocationHandler;
 
     public function __phpunit_setReturnValueGeneration(bool $returnValueGeneration): void;
