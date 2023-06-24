a3aaf968bdeb7ed1d61c35d18e59c4a14514cf07
Sebastian Bergmann <sb@sebastian-bergmann.de>
2023-06-24 07:34:42 +0200

    Improve message

--- src/Framework/MockObject/Exception/ReturnValueNotConfiguredException.php
+++ src/Framework/MockObject/Exception/ReturnValueNotConfiguredException.php
@@ -20,7 +20,7 @@ final class ReturnValueNotConfiguredException extends \PHPUnit\Framework\Excepti
     {
         parent::__construct(
             sprintf(
-                'Return value inference disabled and no expectation set up for %s::%s()',
+                'No return value is configured for %s::%s() and return value generation is disabled',
                 $invocation->className(),
                 $invocation->methodName(),
             ),
