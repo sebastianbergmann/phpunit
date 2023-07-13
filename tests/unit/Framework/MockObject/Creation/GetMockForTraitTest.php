3533bc92210e86184909dd2caecfff432e94fffa
Sebastian Bergmann <sb@sebastian-bergmann.de>
2023-07-13 09:08:54 +0200

    Add test for TestCase::getMockForTrait()

---
+++ tests/unit/Framework/MockObject/Creation/GetMockForTraitTest.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of PHPUnit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace PHPUnit\Framework\MockObject;
+
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\Medium;
+use PHPUnit\Framework\Attributes\TestDox;
+use PHPUnit\Framework\TestCase;
+use PHPUnit\TestFixture\MockObject\TraitWithConcreteAndAbstractMethod;
+
+#[Group('test-doubles')]
+#[Group('test-doubles/creation')]
+#[Group('test-doubles/mock-object')]
+#[Medium]
+#[TestDox('getMockForTrait()')]
+final class GetMockForTraitTest extends TestCase
+{
+    public function testCreatesMockObjectForTraitAndAllowsConfigurationOfAbstractMethods(): void
+    {
+        $mock = $this->getMockForTrait(TraitWithConcreteAndAbstractMethod::class);
+
+        $mock->method('abstractMethod')->willReturn(true);
+
+        $this->assertTrue($mock->concreteMethod());
+    }
+}
