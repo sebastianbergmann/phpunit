bc79eb97c2ce170a52bd27e2be495f1bfe8c490f
Sebastian Bergmann <sb@sebastian-bergmann.de>
2023-07-14 07:03:28 +0200

    Delete unused code

--- src/Framework/Exception/Exception.php
+++ src/Framework/Exception/Exception.php
@@ -11,10 +11,7 @@ namespace PHPUnit\Framework;
 
 use function array_keys;
 use function get_object_vars;
-use PHPUnit\Util\Filter;
-use PHPUnit\Util\ThrowableToStringMapper;
 use RuntimeException;
-use Stringable;
 use Throwable;
 
 /**
@@ -39,7 +36,7 @@ use Throwable;
  *
  * @internal This class is not covered by the backward compatibility promise for PHPUnit
  */
-class Exception extends RuntimeException implements \PHPUnit\Exception, Stringable
+class Exception extends RuntimeException implements \PHPUnit\Exception
 {
     protected array $serializableTrace;
 
@@ -52,20 +49,6 @@ class Exception extends RuntimeException implements \PHPUnit\Exception, Stringab
         foreach (array_keys($this->serializableTrace) as $key) {
             unset($this->serializableTrace[$key]['args']);
         }
-    }
-
-    /**
-     * @throws Exception
-     */
-    public function __toString(): string
-    {
-        $string = ThrowableToStringMapper::map($this);
-
-        if ($trace = Filter::getFilteredStacktrace($this)) {
-            $string .= "\n" . $trace;
-        }
-
-        return $string;
     }
 
     public function __sleep(): array
