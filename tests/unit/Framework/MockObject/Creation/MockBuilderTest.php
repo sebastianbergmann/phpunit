5c8bd32e640d6fd19bbccd742b7c20563d2932fe
Sebastian Bergmann <sb@sebastian-bergmann.de>
2023-08-14 10:02:33 +0200

    Add test

---
+++ tests/unit/Framework/MockObject/Creation/MockBuilderTest.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of PHPUnit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace PHPUnit\Framework\MockObject;
+
+use function md5;
+use function mt_rand;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\Medium;
+use PHPUnit\Framework\TestCase;
+use PHPUnit\TestFixture\MockObject\InterfaceWithReturnTypeDeclaration;
+
+#[CoversClass(MockBuilder::class)]
+#[Group('test-doubles')]
+#[Group('test-doubles/creation')]
+#[Group('test-doubles/mock-object')]
+#[Medium]
+final class MockBuilderTest extends TestCase
+{
+    public function testCanCreateMockObjectWithSpecifiedClassName(): void
+    {
+        $className = 'random_' . md5((string) mt_rand());
+
+        $o = $this->getMockBuilder(InterfaceWithReturnTypeDeclaration::class)
+            ->setMockClassName($className)
+            ->getMock();
+
+        $this->assertSame($className, $o::class);
+    }
+}
