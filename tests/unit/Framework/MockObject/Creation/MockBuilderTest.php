d9c43cd5d9371330af66d581ebc8c636b2d5e7e7
Sebastian Bergmann <sb@sebastian-bergmann.de>
2023-08-14 16:33:18 +0200

    Add tests

--- tests/unit/Framework/MockObject/Creation/MockBuilderTest.php
+++ tests/unit/Framework/MockObject/Creation/MockBuilderTest.php
@@ -15,9 +15,11 @@ use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\Group;
 use PHPUnit\Framework\Attributes\Medium;
 use PHPUnit\Framework\TestCase;
+use PHPUnit\TestFixture\MockObject\ExtendableClass;
 use PHPUnit\TestFixture\MockObject\InterfaceWithReturnTypeDeclaration;
 
 #[CoversClass(MockBuilder::class)]
+#[CoversClass(CannotUseAddMethodsException::class)]
 #[Group('test-doubles')]
 #[Group('test-doubles/creation')]
 #[Group('test-doubles/mock-object')]
@@ -33,5 +35,27 @@ final class MockBuilderTest extends TestCase
             ->getMock();
 
         $this->assertSame($className, $double::class);
+    }
+
+    public function testCanCreateMockObjectForExtendableClassWhileAddingMethodsToIt(): void
+    {
+        $double = $this->getMockBuilder(ExtendableClass::class)
+            ->addMethods(['additionalMethod'])
+            ->getMock();
+
+        $value = 'value';
+
+        $double->method('additionalMethod')->willReturn($value);
+
+        $this->assertSame($value, $double->additionalMethod());
+    }
+
+    public function testCannotCreateMockObjectForExtendableClassAddingMethodsToItThatItAlreadyHas(): void
+    {
+        $this->expectException(CannotUseAddMethodsException::class);
+
+        $this->getMockBuilder(ExtendableClass::class)
+            ->addMethods(['doSomething'])
+            ->getMock();
     }
 }
