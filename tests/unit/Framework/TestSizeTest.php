0ae6fa7619502fd3bf337d53cfc02a4691b8c5db
Sebastian Bergmann <sb@sebastian-bergmann.de>
2023-08-19 13:43:06 +0200

    Fix code coverage target

--- tests/unit/Framework/TestSizeTest.php
+++ tests/unit/Framework/TestSizeTest.php
@@ -17,7 +17,7 @@ use PHPUnit\Framework\TestCase;
 #[CoversClass(Known::class)]
 #[CoversClass(Large::class)]
 #[CoversClass(Medium::class)]
-#[CoversClass(Small::class)]
+#[CoversClass(\PHPUnit\Framework\TestSize\Small::class)]
 #[CoversClass(TestSize::class)]
 #[CoversClass(Unknown::class)]
 #[Small]
