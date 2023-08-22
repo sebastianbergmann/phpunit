e0719f86ad65833b4f06eaf819d5a63a12137841
Sebastian Bergmann <sb@sebastian-bergmann.de>
2023-08-22 05:35:57 +0200

    Reorganize

--- tests/unit/Logging/TestDox/NamePrettifierTest.php
+++ tests/unit/Logging/TestDox/NamePrettifierTest.php
@@ -7,13 +7,12 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-namespace PHPUnit\Util\TestDox;
+namespace PHPUnit\Logging\TestDox;
 
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\Group;
 use PHPUnit\Framework\Attributes\Small;
 use PHPUnit\Framework\TestCase;
-use PHPUnit\Logging\TestDox\NamePrettifier;
 
 #[CoversClass(NamePrettifier::class)]
 #[Group('testdox')]
