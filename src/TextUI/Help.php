c84e2f8b936c7be3e0eaf40dce5aa6a12f68586e
Sebastian Bergmann <sb@sebastian-bergmann.de>
2023-08-25 11:55:15 +0200

    Update help output

--- src/TextUI/Help.php
+++ src/TextUI/Help.php
@@ -29,8 +29,7 @@ final class Help
     private const LEFT_MARGIN = '  ';
     private const HELP_TEXT   = [
         'Usage' => [
-            ['text' => 'phpunit [options] UnitTest.php'],
-            ['text' => 'phpunit [options] <directory>'],
+            ['text' => 'phpunit [options] <directory|file> ...'],
         ],
 
         'Configuration' => [
