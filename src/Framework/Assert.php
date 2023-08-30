aae0016e5c0d0a6a005e985a528aabfcab61b35f
Sebastian Bergmann <sb@sebastian-bergmann.de>
2023-08-30 14:02:46 +0200

    Fix CS/WS issues

--- src/Framework/Assert.php
+++ src/Framework/Assert.php
@@ -430,7 +430,6 @@ abstract class Assert
      * or an attribute of an object contains only values of a given type.
      *
      * @param object|string $haystackClassOrObject
-     * @param bool          $isNativeType
      *
      * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
      * @throws Exception
@@ -482,7 +481,6 @@ abstract class Assert
      * type.
      *
      * @param object|string $haystackClassOrObject
-     * @param bool          $isNativeType
      *
      * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
      * @throws Exception
