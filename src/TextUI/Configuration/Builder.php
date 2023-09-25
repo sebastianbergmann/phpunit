c6eb87b40b4e294d028c60be8ba274ce514abd31
Sebastian Bergmann <sb@sebastian-bergmann.de>
2023-09-25 14:24:21 +0200

    Ignore convenience wrapper around Configuration loading/parsing to be used by test runner wrappers from code coverage

--- src/TextUI/Configuration/Builder.php
+++ src/TextUI/Configuration/Builder.php
@@ -18,6 +18,8 @@ use PHPUnit\TextUI\XmlConfiguration\Loader;
 
 /**
  * @no-named-arguments Parameter names are not covered by the backward compatibility promise for PHPUnit
+ *
+ * @codeCoverageIgnore
  */
 final class Builder
 {
