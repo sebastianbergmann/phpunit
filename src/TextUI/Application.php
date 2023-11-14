ab302b9617316f79567eb56b16b51bf34e305728
Sebastian Bergmann <sb@sebastian-bergmann.de>
2023-11-14 09:55:13 +0100

    Fix location information (first exception of exception chain should just be a wrapper)

--- src/TextUI/Application.php
+++ src/TextUI/Application.php
@@ -638,6 +638,10 @@ final class Application
 
         $first = true;
 
+        if ($t->getPrevious()) {
+            $t = $t->getPrevious();
+        }
+
         do {
             printf(
                 '%s%s: %s:%d%s%s%s%s',
