6ad7e606a4b14b641cf5586fcd06ce5d2f2f18af
Michael Voříšek <mvorisek@mvorisek.cz>
2023-11-30 13:30:03 +0100

    replace supressed detection of `@` (3739 issue)

--- src/Runner/ErrorHandler.php
+++ src/Runner/ErrorHandler.php
@@ -17,8 +17,10 @@ use const E_DEPRECATED;
 use const E_ERROR;
 use const E_NOTICE;
 use const E_PARSE;
+use const E_RECOVERABLE_ERROR;
 use const E_STRICT;
 use const E_USER_DEPRECATED;
+use const E_USER_ERROR;
 use const E_USER_NOTICE;
 use const E_USER_WARNING;
 use const E_WARNING;
@@ -37,6 +39,7 @@ use PHPUnit\Util\ExcludeList;
 final class ErrorHandler
 {
     private const UNHANDLEABLE_LEVELS         = E_ERROR | E_PARSE | E_CORE_ERROR | E_CORE_WARNING | E_COMPILE_ERROR | E_COMPILE_WARNING;
+    private const UNSUPPRESSEABLE_LEVELS      = E_ERROR | E_PARSE | E_CORE_ERROR | E_COMPILE_ERROR | E_USER_ERROR | E_RECOVERABLE_ERROR;
     private static ?self $instance            = null;
     private ?Baseline $baseline               = null;
     private bool $enabled                     = false;
@@ -52,7 +55,7 @@ final class ErrorHandler
      */
     public function __invoke(int $errorNumber, string $errorString, string $errorFile, int $errorLine): bool
     {
-        $suppressed = !($errorNumber & error_reporting());
+        $suppressed = (error_reporting() & ~self::UNSUPPRESSEABLE_LEVELS) === 0;
 
         if ($suppressed && (new ExcludeList)->isExcluded($errorFile)) {
             return false;
