f17d908e68c0bb04db7cb938da70b37187089686
Andreas Möller <am@localheinz.com>
2023-12-11 12:12:08 +0100

    Fix: Reuse reference to emitter

--- src/Framework/TestCase.php
+++ src/Framework/TestCase.php
@@ -720,7 +720,7 @@ abstract class TestCase extends Assert implements Reorderable, SelfDescribing, T
         }
 
         if ($this->status->isSuccess()) {
-            Event\Facade::emitter()->testPassed(
+            $emitter->testPassed(
                 $this->valueObjectForEvents(),
             );
 
