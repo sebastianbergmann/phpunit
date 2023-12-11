43a79f90481cb86fd0ac5c1f27b68f3bdbcaaba2
Paweł Tomulik <pawel@tomulik.pl>
2023-12-11 18:32:26 +0100

    created regression test for #5614

---
+++ tests/end-to-end/regression/5614/Issue5614Test.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of PHPUnit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace PHPUnit\TestFixture\Issue5614;
+
+use PHPUnit\Framework\TestCase;
+
+final class Issue5614Test extends TestCase
+{
+    public static function provideRecursiveArray(): iterable {
+        $array = [];
+        $array[0] = &$array;
+        yield [$array];
+    }
+
+    /**
+     * @dataProvider provideRecursiveArray
+     */
+    public function testRecursiveArray(array $array): void {
+        $this->assertTrue(true);
+    }
+}
