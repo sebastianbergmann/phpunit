da9e44c00575d12d92bf2c0b93f9a32979982d9c
Ewout Pieter den Ouden <e.den.ouden@amsterdam.nl>
2023-12-12 22:47:57 +0100

    Improve tests

--- tests/unit/Util/ColorTest.php
+++ tests/unit/Util/ColorTest.php
@@ -78,18 +78,22 @@ final class ColorTest extends TestCase
             'fitting text' => [
                 40,
                 "this is fine\n" .
-                "all lines fit nicely even the long\n" .
-                'ones',
+                "\n" .
+                "all lines fit nicely even the extra long ones with inlined exceptions and var_dumps\n" .
+                'bottom text',
                 Color::colorize('red', 'this is fine                      ') . "\n" .
+                Color::colorize('red', '                                  ') . "\n" .
                 Color::colorize('red', 'all lines fit nicely even the long') . "\n" .
                 Color::colorize('red', 'ones                              '),
             ],
             'oversized text' => [
                 20,
                 "this is also fine\n" .
+                "\n" .
                 "the very long lines do not stretch the whole textbox\n" .
                 'anymore',
                 Color::colorize('red', 'this is also fine   ') . "\n" .
+                Color::colorize('red', '                    ') . "\n" .
                 Color::colorize('red', 'the very long lines do not stretch the whole textbox') . "\n" .
                 Color::colorize('red', 'anymore             '),
             ],
