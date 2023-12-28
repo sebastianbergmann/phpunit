b06de8ca735ee990c9dc06f4f8db88a81fbd74df
Sam L <samlitowitz@users.noreply.github.com>
2023-12-28 08:21:42 -0500

    Address linter issues

--- tests/end-to-end/regression/5574/Issue5574Test.php
+++ tests/end-to-end/regression/5574/Issue5574Test.php
@@ -1,17 +1,25 @@
 <?php declare(strict_types=1);
 
-// REFURL: https://github.com/sebastianbergmann/phpunit/issues/5574#issuecomment-1827845849
-
+/*
+ * This file is part of PHPUnit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace PHPUnit\TestFixture\Issue5574;
 
+use Error;
+use Exception;
 use PHPUnit\Framework\TestCase;
 
 final class Issue5574Test extends TestCase
 {
     public function testThrownWrappedThrowablesOutputsCorrectStackTraceForEach(): void
     {
-        $innerException = new \Error('Inner Exception');
-        $outerException = new \Exception('My exception', 0, $innerException);
+        $innerException = new Error('Inner Exception');
+        $outerException = new Exception('My exception', 0, $innerException);
 
         throw $outerException;
     }
