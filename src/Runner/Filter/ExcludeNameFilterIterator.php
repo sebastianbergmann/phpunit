050cb380c4158f00cbcd4e5c2c3db8a12f47146a
Marko Petrovic <maracs1990@gmail.com>
2023-12-30 13:26:19 +0100

    remove unnecessary filter methods

--- src/Runner/Filter/ExcludeNameFilterIterator.php
+++ src/Runner/Filter/ExcludeNameFilterIterator.php
@@ -21,49 +21,14 @@ class ExcludeNameFilterIterator extends NameFilterIterator
      */
     protected function setFilter(string $filter): void
     {
-        if (@preg_match($filter, '') === false) {
-            // Handles:
-            //  * testAssertEqualsSucceeds#4
-            //  * testAssertEqualsSucceeds#4-8
-            if (preg_match('/^(.*?)#(\d+)(?:-(\d+))?$/', $filter, $matches)) {
-                if (isset($matches[3]) && $matches[2] < $matches[3]) {
-                    $filter = sprintf(
-                        '^(?:(?!%s).)*with data set #(\d+)$',
-                        $matches[1],
-                    );
-
-                    $this->filterMin = (int) $matches[2];
-                    $this->filterMax = (int) $matches[3];
-                } else {
-                    // if the exclude-filter is name it will match anything that doesn't contain name
-                    $filter = sprintf(
-                        '^(?:(?!%s).)*with data set #%s$',
-                        $matches[1],
-                        $matches[2],
-                    );
-                }
-            } // Handles:
-            //  * testDetermineJsonError@JSON_ERROR_NONE
-            //  * testDetermineJsonError@JSON.*
-            elseif (preg_match('/^(.*?)@(.+)$/', $filter, $matches)) {
-                $filter = sprintf(
-                    '^(?:(?!%s).)*with data set "%s"$',
-                    $matches[1],
-                    $matches[2],
-                );
-            }
-
-            // Escape delimiters in regular expression. Do NOT use preg_quote,
-            // to keep magic characters.
-            $filter = sprintf(
-                '/^(?:(?!%s).)*/i',
-                str_replace(
-                    '/',
-                    '\\/',
-                    $filter,
-                ),
-            );
-        }
+        $filter = sprintf(
+            '/^(?:(?!%s).)*/i',
+            str_replace(
+                '/',
+                '\\/',
+                $filter,
+            ),
+        );
 
         $this->filter = $filter;
     }
