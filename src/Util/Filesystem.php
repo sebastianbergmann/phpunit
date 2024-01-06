59754bfa06cd14549181f5d6110a5733f26a77c1
Sebastian Bergmann <sb@sebastian-bergmann.de>
2024-01-06 16:04:21 +0100

    Extract method

--- src/Util/Filesystem.php
+++ src/Util/Filesystem.php
@@ -10,7 +10,10 @@
 namespace PHPUnit\Util;
 
 use function is_dir;
+use function is_string;
 use function mkdir;
+use function realpath;
+use function str_starts_with;
 
 /**
  * @internal This class is not covered by the backward compatibility promise for PHPUnit
@@ -20,5 +23,25 @@ final class Filesystem
     public static function createDirectory(string $directory): bool
     {
         return !(!is_dir($directory) && !@mkdir($directory, 0o777, true) && !is_dir($directory));
+    }
+
+    /**
+     * @psalm-param non-empty-string $path
+     *
+     * @return false|non-empty-string
+     */
+    public static function resolvePathOrStream(string $path): false|string
+    {
+        if (str_starts_with($path, 'php://') || str_starts_with($path, 'socket://')) {
+            return $path;
+        }
+
+        $path = realpath($path);
+
+        if (is_string($path) && !empty($path)) {
+            return $path;
+        }
+
+        return false;
     }
 }
