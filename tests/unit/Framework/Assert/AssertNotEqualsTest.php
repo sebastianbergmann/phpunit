f8dd0abed86524e4fa25b22ae86ac259e02f5382
Marko Petrovic <maracs1990@gmail.com>
2024-01-12 18:22:27 +0100

    better test

--- tests/unit/Framework/Assert/AssertNotEqualsTest.php
+++ tests/unit/Framework/Assert/AssertNotEqualsTest.php
@@ -2,13 +2,22 @@
 
 namespace PHPUnit\Framework;
 
+use PHPUnit\Framework\Constraint\IsEqual;
+use PHPUnit\Framework\Constraint\LogicalNot;
+
 class AssertNotEqualsTest extends TestCase
 {
     public function testConfusingMessagesForLogicalNot()
     {
+        $expectedMessage = "Failed asserting that 'test contains something' is not equal to 'test contains something'.";
         $a = 'test contains something';
         $b = 'test contains something';
-
-        $this->assertNotEquals($b, $a);
+        $constraint = new LogicalNot(new IsEqual($a));
+        try {
+            Assert::assertThat($b, $constraint);
+        } catch (\Exception $e) {
+            $actualMessage = $e->getMessage();
+            $this->assertSame($expectedMessage, $actualMessage);
+        }
     }
 }
