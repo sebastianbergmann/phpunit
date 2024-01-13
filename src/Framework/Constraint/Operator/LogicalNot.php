d981ececdd63426e6f0ae4843f759002d21fd45b
Sebastian Bergmann <sb@sebastian-bergmann.de>
2024-01-13 10:11:09 +0100

    Fix CS/WS issues

--- src/Framework/Constraint/Operator/LogicalNot.php
+++ src/Framework/Constraint/Operator/LogicalNot.php
@@ -54,10 +54,13 @@ final class LogicalNot extends UnaryOperator
             preg_match('/(\'[\w\W]*\')([\w\W]*)(\'[\w\W]*\')/i', $string, $matches);
         }
 
-        $positives = array_map(static function (string $s)
-        {
-            return '/\\b' . preg_quote($s, '/') . '/';
-        }, $positives);
+        $positives = array_map(
+            static function (string $s)
+            {
+                return '/\\b' . preg_quote($s, '/') . '/';
+            },
+            $positives,
+        );
 
         if (count($matches) > 0) {
             $nonInput = $matches[2];
