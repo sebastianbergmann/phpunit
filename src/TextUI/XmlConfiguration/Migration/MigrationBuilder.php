76791baf27ec2f247bc574f0bec835f987f670b0
Sebastian Bergmann <sb@sebastian-bergmann.de>
2024-01-18 17:19:35 +0100

    Remove superfluous code

--- src/TextUI/XmlConfiguration/Migration/MigrationBuilder.php
+++ src/TextUI/XmlConfiguration/Migration/MigrationBuilder.php
@@ -9,8 +9,6 @@
  */
 namespace PHPUnit\TextUI\XmlConfiguration;
 
-use function array_key_exists;
-use function sprintf;
 use function version_compare;
 
 /**
@@ -47,15 +45,6 @@ final class MigrationBuilder
      */
     public function build(string $fromVersion): array
     {
-        if (!array_key_exists($fromVersion, self::AVAILABLE_MIGRATIONS)) {
-            throw new MigrationBuilderException(
-                sprintf(
-                    'Migration from schema version %s is not supported',
-                    $fromVersion,
-                ),
-            );
-        }
-
         $stack = [];
 
         foreach (self::AVAILABLE_MIGRATIONS as $version => $migrations) {
