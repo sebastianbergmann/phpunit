5e2d16e6c5a63d4f9ba845e0b2a552aff7761186
Sebastian Bergmann <sb@sebastian-bergmann.de>
2024-02-04 07:42:40 +0100

    Revert "Extract method"
    This reverts commit 59754bfa06cd14549181f5d6110a5733f26a77c1.

--- src/TextUI/Configuration/Cli/Builder.php
+++ src/TextUI/Configuration/Cli/Builder.php
@@ -15,9 +15,10 @@ use function explode;
 use function getcwd;
 use function is_file;
 use function is_numeric;
+use function realpath;
 use function sprintf;
+use function str_starts_with;
 use PHPUnit\Runner\TestSuiteSorter;
-use PHPUnit\Util\Filesystem;
 use SebastianBergmann\CliParser\Exception as CliParserException;
 use SebastianBergmann\CliParser\Parser as CliParser;
 
@@ -806,29 +807,37 @@ final class Builder
                     break;
 
                 case '--log-events-text':
-                    $logEventsText = Filesystem::resolvePathOrStream($option[1]);
+                    if (str_starts_with($option[1], 'php://') || str_starts_with($option[1], 'socket://')) {
+                        $logEventsText = $option[1];
+                    } else {
+                        $logEventsText = realpath($option[1]);
 
-                    if (!$logEventsText) {
-                        throw new Exception(
-                            sprintf(
-                                'The path "%s" specified for the --log-events-text option could not be resolved',
-                                $option[1],
-                            ),
-                        );
+                        if (!$logEventsText) {
+                            throw new Exception(
+                                sprintf(
+                                    'The path "%s" specified for the --log-events-text option could not be resolved',
+                                    $option[1],
+                                ),
+                            );
+                        }
                     }
 
                     break;
 
                 case '--log-events-verbose-text':
-                    $logEventsVerboseText = Filesystem::resolvePathOrStream($option[1]);
+                    if (str_starts_with($option[1], 'php://') || str_starts_with($option[1], 'socket://')) {
+                        $logEventsVerboseText = $option[1];
+                    } else {
+                        $logEventsVerboseText = realpath($option[1]);
 
-                    if (!$logEventsVerboseText) {
-                        throw new Exception(
-                            sprintf(
-                                'The path "%s" specified for the --log-events-verbose-text option could not be resolved',
-                                $option[1],
-                            ),
-                        );
+                        if (!$logEventsVerboseText) {
+                            throw new Exception(
+                                sprintf(
+                                    'The path "%s" specified for the --log-events-verbose-text option could not be resolved',
+                                    $option[1],
+                                ),
+                            );
+                        }
                     }
 
                     break;
