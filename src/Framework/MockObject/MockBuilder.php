69c25ce32499c03fdd7077b5f728ff5eb44b9e00
Sebastian Bergmann <sb@sebastian-bergmann.de>
2024-02-07 15:48:25 +0100

    Closes #5316

--- src/Framework/MockObject/MockBuilder.php
+++ src/Framework/MockObject/MockBuilder.php
@@ -56,7 +56,6 @@ final class MockBuilder
     private bool $originalConstructor   = true;
     private bool $originalClone         = true;
     private bool $autoload              = true;
-    private bool $cloneArguments        = false;
     private bool $callOriginalMethods   = false;
     private ?object $proxyTarget        = null;
     private bool $returnValueGeneration = true;
@@ -100,7 +99,7 @@ final class MockBuilder
             $this->originalConstructor,
             $this->originalClone,
             $this->autoload,
-            $this->cloneArguments,
+            false,
             $this->callOriginalMethods,
             $this->proxyTarget,
             false,
@@ -270,46 +269,6 @@ final class MockBuilder
         );
 
         $this->autoload = true;
-
-        return $this;
-    }
-
-    /**
-     * Disables the cloning of arguments passed to mocked methods.
-     *
-     * @return $this
-     *
-     * @deprecated https://github.com/sebastianbergmann/phpunit/issues/5315
-     */
-    public function disableArgumentCloning(): self
-    {
-        if (!$this->calledFromTestCase()) {
-            EventFacade::emitter()->testTriggeredPhpunitDeprecation(
-                $this->testCase->valueObjectForEvents(),
-                'MockBuilder::disableArgumentCloning() is deprecated and will be removed in PHPUnit 12 without replacement.',
-            );
-        }
-
-        $this->cloneArguments = false;
-
-        return $this;
-    }
-
-    /**
-     * Enables the cloning of arguments passed to mocked methods.
-     *
-     * @return $this
-     *
-     * @deprecated https://github.com/sebastianbergmann/phpunit/issues/5315
-     */
-    public function enableArgumentCloning(): self
-    {
-        EventFacade::emitter()->testTriggeredPhpunitDeprecation(
-            $this->testCase->valueObjectForEvents(),
-            'MockBuilder::enableArgumentCloning() is deprecated and will be removed in PHPUnit 12 without replacement.',
-        );
-
-        $this->cloneArguments = true;
 
         return $this;
     }
--- src/Framework/TestCase.php
+++ src/Framework/TestCase.php
@@ -1419,7 +1419,6 @@ abstract class TestCase extends Assert implements Reorderable, SelfDescribing, T
         $mockBuilder = $this->getMockBuilder($originalClassName)
             ->disableOriginalConstructor()
             ->disableOriginalClone()
-            ->disableArgumentCloning()
             ->onlyMethods($methods);
 
         if (!self::generateReturnValuesForTestDoubles()) {
