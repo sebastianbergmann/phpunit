697b8677a551f1ecbc2c95625fd78e3671d0f724
Sebastian Bergmann <sb@sebastian-bergmann.de>
2024-03-08 13:06:05 +0100

    Fix --exclude-filter CLI option
    Co-authored-by: Sebastian Bergmann <sb@sebastian-bergmann.de>
    Co-authored-by: Arne Blankerts <Arne@Blankerts.de>
    Co-authored-by: Andreas Möller <am@localheinz.com>
    Co-authored-by: Sebastian Heuer <sebastian@phpeople.de>
    Co-authored-by: Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
    Co-authored-by: Frank Sons <frank.sons@code-quality.de>

--- src/Runner/Filter/NameFilterIterator.php
+++ src/Runner/Filter/NameFilterIterator.php
@@ -11,9 +11,11 @@ namespace PHPUnit\Runner\Filter;
 
 use function end;
 use function preg_match;
+use function sprintf;
+use function str_replace;
 use PHPUnit\Framework\Test;
-use PHPUnit\Framework\TestCase;
 use PHPUnit\Framework\TestSuite;
+use PHPUnit\Runner\PhptTestCase;
 use RecursiveFilterIterator;
 use RecursiveIterator;
 
@@ -60,7 +62,7 @@ abstract class NameFilterIterator extends RecursiveFilterIterator
             return true;
         }
 
-        if (!$test instanceof TestCase) {
+        if ($test instanceof PhptTestCase) {
             return false;
         }
 
@@ -73,13 +75,68 @@ abstract class NameFilterIterator extends RecursiveFilterIterator
             $accepted = $set >= $this->dataSetMinimum && $set <= $this->dataSetMaximum;
         }
 
-        return $accepted;
+        return $this->doAccept($accepted);
     }
+
+    abstract protected function doAccept(bool $result): bool;
 
     /**
      * @psalm-param non-empty-string $filter
      *
      * @psalm-return array{regularExpression: non-empty-string, dataSetMinimum: ?int, dataSetMaximum: ?int}
      */
-    abstract protected function prepareFilter(string $filter): array;
+    private function prepareFilter(string $filter): array
+    {
+        $dataSetMinimum = null;
+        $dataSetMaximum = null;
+
+        if (@preg_match($filter, '') === false) {
+            // Handles:
+            //  * testAssertEqualsSucceeds#4
+            //  * testAssertEqualsSucceeds#4-8
+            if (preg_match('/^(.*?)#(\d+)(?:-(\d+))?$/', $filter, $matches)) {
+                if (isset($matches[3]) && $matches[2] < $matches[3]) {
+                    $filter = sprintf(
+                        '%s.*with data set #(\d+)$',
+                        $matches[1],
+                    );
+
+                    $dataSetMinimum = (int) $matches[2];
+                    $dataSetMaximum = (int) $matches[3];
+                } else {
+                    $filter = sprintf(
+                        '%s.*with data set #%s$',
+                        $matches[1],
+                        $matches[2],
+                    );
+                }
+            } // Handles:
+            //  * testDetermineJsonError@JSON_ERROR_NONE
+            //  * testDetermineJsonError@JSON.*
+            elseif (preg_match('/^(.*?)@(.+)$/', $filter, $matches)) {
+                $filter = sprintf(
+                    '%s.*with data set "%s"$',
+                    $matches[1],
+                    $matches[2],
+                );
+            }
+
+            // Escape delimiters in regular expression. Do NOT use preg_quote,
+            // to keep magic characters.
+            $filter = sprintf(
+                '/%s/i',
+                str_replace(
+                    '/',
+                    '\\/',
+                    $filter,
+                ),
+            );
+        }
+
+        return [
+            'regularExpression' => $filter,
+            'dataSetMinimum'    => $dataSetMinimum,
+            'dataSetMaximum'    => $dataSetMaximum,
+        ];
+    }
 }
--- tests/end-to-end/cli/exclude-filter/match.phpt
+++ tests/end-to-end/cli/exclude-filter/match.phpt
@@ -1,5 +1,5 @@
 --TEST--
-phpunit --exclude-filter one tests/FooTest.php
+phpunit --exclude-filter testThree ../../_files/groups/tests/FooTest.php
 --FILE--
 <?php declare(strict_types=1);
 $traceFile = tempnam(sys_get_temp_dir(), __FILE__);
@@ -10,7 +10,7 @@ $_SERVER['argv'][] = '--no-output';
 $_SERVER['argv'][] = '--log-events-text';
 $_SERVER['argv'][] = $traceFile;
 $_SERVER['argv'][] = '--exclude-filter';
-$_SERVER['argv'][] = 'testone';
+$_SERVER['argv'][] = 'testThree';
 $_SERVER['argv'][] = __DIR__ . '/../../_files/groups/tests/FooTest.php';
 
 require_once __DIR__ . '/../../../bootstrap.php';
@@ -27,9 +27,9 @@ Test Suite Loaded (3 tests)
 Event Facade Sealed
 Test Runner Started
 Test Suite Sorted
-Test Suite Filtered (3 tests)
-Test Runner Execution Started (3 tests)
-Test Suite Started (PHPUnit\TestFixture\Groups\FooTest, 3 tests)
+Test Suite Filtered (2 tests)
+Test Runner Execution Started (2 tests)
+Test Suite Started (PHPUnit\TestFixture\Groups\FooTest, 2 tests)
 Test Preparation Started (PHPUnit\TestFixture\Groups\FooTest::testOne)
 Test Prepared (PHPUnit\TestFixture\Groups\FooTest::testOne)
 Test Passed (PHPUnit\TestFixture\Groups\FooTest::testOne)
@@ -38,11 +38,7 @@ Test Preparation Started (PHPUnit\TestFixture\Groups\FooTest::testTwo)
 Test Prepared (PHPUnit\TestFixture\Groups\FooTest::testTwo)
 Test Passed (PHPUnit\TestFixture\Groups\FooTest::testTwo)
 Test Finished (PHPUnit\TestFixture\Groups\FooTest::testTwo)
-Test Preparation Started (PHPUnit\TestFixture\Groups\FooTest::testThree)
-Test Prepared (PHPUnit\TestFixture\Groups\FooTest::testThree)
-Test Passed (PHPUnit\TestFixture\Groups\FooTest::testThree)
-Test Finished (PHPUnit\TestFixture\Groups\FooTest::testThree)
-Test Suite Finished (PHPUnit\TestFixture\Groups\FooTest, 3 tests)
+Test Suite Finished (PHPUnit\TestFixture\Groups\FooTest, 2 tests)
 Test Runner Execution Finished
 Test Runner Finished
 PHPUnit Finished (Shell Exit Code: 0)
