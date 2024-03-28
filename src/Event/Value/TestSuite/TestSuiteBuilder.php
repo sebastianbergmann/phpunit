5ad4bcfbaa214627d90b3ebe321be4cbdbbda02f
Sebastian Bergmann <sb@sebastian-bergmann.de>
2024-03-28 10:52:14 +0100

    Delete dead code

--- src/Event/Value/TestSuite/TestSuiteBuilder.php
+++ src/Event/Value/TestSuite/TestSuiteBuilder.php
@@ -31,18 +31,6 @@ final class TestSuiteBuilder
      */
     public static function from(FrameworkTestSuite $testSuite): TestSuite
     {
-        $groups = [];
-
-        foreach ($testSuite->groupDetails() as $groupName => $tests) {
-            if (!isset($groups[$groupName])) {
-                $groups[$groupName] = [];
-            }
-
-            foreach ($tests as $test) {
-                $groups[$groupName][] = $test::class;
-            }
-        }
-
         $tests = [];
 
         self::process($testSuite, $tests);
