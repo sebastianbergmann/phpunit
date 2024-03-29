47748c1f154806e084da6304ec349707becd8a86
Markus Staab <maggus.staab@googlemail.com>
2024-03-29 18:37:08 +0100

    Fix PHPStan reported issues

--- src/Runner/Baseline/Reader.php
+++ src/Runner/Baseline/Reader.php
@@ -85,7 +85,6 @@ final class Reader
 
                     $description = $issueElement->textContent;
 
-                    assert(!empty($file));
                     assert($line > 0);
                     assert(!empty($hash));
                     assert(!empty($description));
