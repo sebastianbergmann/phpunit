b24c32d0bca9e16979dd486d16ca64261bddbdf9
Markus Staab <markus.staab@redaxo.de>
2024-04-02 11:10:54 +0200

    cs

--- src/Framework/TestSuite.php
+++ src/Framework/TestSuite.php
@@ -63,7 +63,8 @@ class TestSuite implements IteratorAggregate, Reorderable, SelfDescribing, Test
     /**
      * @psalm-var array<string,list<Test>>
      */
-    private array $groups         = [];
+    private array $groups = [];
+
     /**
      * @psalm-var ?list<ExecutionOrderDependency>
      */
@@ -72,7 +73,8 @@ class TestSuite implements IteratorAggregate, Reorderable, SelfDescribing, Test
     /**
      * @psalm-var list<Test>
      */
-    private array $tests             = [];
+    private array $tests = [];
+
     /**
      * @psalm-var ?list<ExecutionOrderDependency>
      */
