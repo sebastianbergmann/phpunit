793693f63d0144403b11d978a61aacc265ae5c53
Sebastian Bergmann <sb@sebastian-bergmann.de>
2024-04-07 11:02:17 +0200

    Remove check that is no longer needed

--- src/Framework/MockObject/Generator/Generator.php
+++ src/Framework/MockObject/Generator/Generator.php
@@ -26,7 +26,6 @@ use function interface_exists;
 use function is_array;
 use function is_object;
 use function md5;
-use function method_exists;
 use function mt_rand;
 use function preg_match;
 use function preg_match_all;
@@ -646,7 +645,7 @@ final class Generator
                 throw new ClassIsFinalException($_mockClassName['fullClassName']);
             }
 
-            if (method_exists($class, 'isReadOnly') && $class->isReadOnly()) {
+            if ($class->isReadOnly()) {
                 throw new ClassIsReadonlyException($_mockClassName['fullClassName']);
             }
 
