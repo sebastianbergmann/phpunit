9f951cace701903a96eb47685b2f278f6510013f
Markus Staab <maggus.staab@googlemail.com>
2024-04-14 08:36:19 +0200

    Fix "PHP Warning: Uninitialized string offset 0"

--- src/Metadata/Api/DataProvider.php
+++ src/Metadata/Api/DataProvider.php
@@ -256,7 +256,7 @@ final class DataProvider
         foreach (explode("\n", $annotationContent) as $candidateRow) {
             $candidateRow = trim($candidateRow);
 
-            if ($candidateRow[0] !== '[') {
+            if ($candidateRow === '' || $candidateRow[0] !== '[') {
                 break;
             }
 
