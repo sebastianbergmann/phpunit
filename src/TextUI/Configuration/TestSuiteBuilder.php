4edd2c9d8cfa2e29bb20611e1ee6017f2097e977
Sebastian Bergmann <sb@sebastian-bergmann.de>
2024-08-02 08:00:19 +0200

    Remove superfluous type annotation

--- src/TextUI/Configuration/TestSuiteBuilder.php
+++ src/TextUI/Configuration/TestSuiteBuilder.php
@@ -88,7 +88,6 @@ final readonly class TestSuiteBuilder
     /**
      * @param non-empty-string       $path
      * @param list<non-empty-string> $suffixes
-     * @param ?TestSuite             $suite
      *
      * @throws \PHPUnit\Framework\Exception
      */
