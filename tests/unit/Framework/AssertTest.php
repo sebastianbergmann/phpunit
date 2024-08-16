23c7646b4ccd88a4dc383ceef9a8a11c57c8b972
Sebastian Bergmann <sb@sebastian-bergmann.de>
2024-08-16 15:12:51 +0200

    Add tests

--- tests/unit/Framework/AssertTest.php
+++ tests/unit/Framework/AssertTest.php
@@ -1026,6 +1026,21 @@ XML;
         $this->assertThat([], $this->isEmpty());
     }
 
+    public function testAssertThatFileIsReadable(): void
+    {
+        $this->assertThat(__FILE__, $this->isReadable());
+    }
+
+    public function testAssertThatFileIsWritable(): void
+    {
+        $this->assertThat(__FILE__, $this->isWritable());
+    }
+
+    public function testAssertThatDirectoryExists(): void
+    {
+        $this->assertThat(__DIR__, $this->directoryExists());
+    }
+
     public function testAssertThatFileExists(): void
     {
         $this->assertThat(__FILE__, $this->fileExists());
@@ -1094,6 +1109,21 @@ XML;
         $this->expectException(AssertionFailedError::class);
 
         $this->assertFileNotEquals(
+            TEST_FILES_PATH . 'foo.xml',
+            TEST_FILES_PATH . 'foo.xml',
+        );
+    }
+
+    public function testAssertFileNotEqualsCanonicalizing(): void
+    {
+        $this->assertFileNotEqualsCanonicalizing(
+            TEST_FILES_PATH . 'foo.xml',
+            TEST_FILES_PATH . 'bar.xml',
+        );
+
+        $this->expectException(AssertionFailedError::class);
+
+        $this->assertFileNotEqualsCanonicalizing(
             TEST_FILES_PATH . 'foo.xml',
             TEST_FILES_PATH . 'foo.xml',
         );
