ce29d42d447ace1f1314079e9bb9d6f8afc4f013
Kuba Werłos <werlos@gmail.com>
2024-09-07 20:49:36 +0200

    Do not fail with "An error occurred inside PHPUnit" when data provider key is not int or string

--- src/Metadata/Api/DataProvider.php
+++ src/Metadata/Api/DataProvider.php
@@ -14,8 +14,10 @@ use const PREG_OFFSET_CAPTURE;
 use function array_key_exists;
 use function assert;
 use function explode;
+use function get_debug_type;
 use function is_array;
 use function is_int;
+use function is_string;
 use function json_decode;
 use function json_last_error;
 use function json_last_error_msg;
@@ -181,20 +183,28 @@ final class DataProvider
             foreach ($data as $key => $value) {
                 if (is_int($key)) {
                     $result[] = $value;
-                } elseif (array_key_exists($key, $result)) {
-                    Event\Facade::emitter()->dataProviderMethodFinished(
-                        $testMethod,
-                        ...$methodsCalled,
-                    );
+                } elseif (is_string($key)) {
+                    if (array_key_exists($key, $result)) {
+                        Event\Facade::emitter()->dataProviderMethodFinished(
+                            $testMethod,
+                            ...$methodsCalled,
+                        );
 
+                        throw new InvalidDataProviderException(
+                            sprintf(
+                                'The key "%s" has already been defined by a previous data provider',
+                                $key,
+                            ),
+                        );
+                    }
+                    $result[$key] = $value;
+                } else {
                     throw new InvalidDataProviderException(
                         sprintf(
-                            'The key "%s" has already been defined by a previous data provider',
-                            $key,
+                            'The key must be int or string, %s given',
+                            get_debug_type($key),
                         ),
                     );
-                } else {
-                    $result[$key] = $value;
                 }
             }
         }
