aaea82757bb738a39ca21542a4415211bd099e0d
Sebastian Bergmann <sb@sebastian-bergmann.de>
2024-10-05 10:37:18 +0200

    Improve name (and work around https://github.com/theseer/Autoload/issues/115)

--- src/TextUI/Configuration/Xml/Migration/Migrations/UpdateSchemaLocation.php
+++ src/TextUI/Configuration/Xml/Migration/Migrations/UpdateSchemaLocation.php
@@ -22,7 +22,7 @@ use PHPUnit\Runner\Version;
  */
 final readonly class UpdateSchemaLocation implements Migration
 {
-    private const NAMESPACE                  = 'http://www.w3.org/2001/XMLSchema-instance';
+    private const NAMESPACE_URI              = 'http://www.w3.org/2001/XMLSchema-instance';
     private const LOCAL_NAME_SCHEMA_LOCATION = 'noNamespaceSchemaLocation';
 
     public function migrate(DOMDocument $document): void
@@ -31,14 +31,14 @@ final readonly class UpdateSchemaLocation implements Migration
 
         assert($root instanceof DOMElement);
 
-        $existingSchemaLocation = $root->getAttributeNodeNS(self::NAMESPACE, self::LOCAL_NAME_SCHEMA_LOCATION)->value;
+        $existingSchemaLocation = $root->getAttributeNodeNS(self::NAMESPACE_URI, self::LOCAL_NAME_SCHEMA_LOCATION)->value;
 
         if (str_contains($existingSchemaLocation, '://') === false) { // If the current schema location is a relative path, don't update it
             return;
         }
 
         $root->setAttributeNS(
-            self::NAMESPACE,
+            self::NAMESPACE_URI,
             'xsi:' . self::LOCAL_NAME_SCHEMA_LOCATION,
             'https://schema.phpunit.de/' . Version::series() . '/phpunit.xsd',
         );
