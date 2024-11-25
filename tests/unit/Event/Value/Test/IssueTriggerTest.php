683009b2908bb5bff0d183925ceb5fe964576eac
Sebastian Bergmann <sb@sebastian-bergmann.de>
2024-11-25 17:52:48 +0100

    Separate TestTrigger from SelfTrigger

--- tests/unit/Event/Value/Test/IssueTriggerTest.php
+++ tests/unit/Event/Value/Test/IssueTriggerTest.php
@@ -26,7 +26,7 @@ final class IssueTriggerTest extends TestCase
         $trigger = IssueTrigger::test();
 
         $this->assertTrue($trigger->isTest());
-        $this->assertTrue($trigger->isSelf());
+        $this->assertFalse($trigger->isSelf());
         $this->assertFalse($trigger->isDirect());
         $this->assertFalse($trigger->isIndirect());
         $this->assertFalse($trigger->isUnknown());
