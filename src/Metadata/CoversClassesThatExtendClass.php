d196deed62b9eaeeea71c38c245be3f32de65d3b
Markus Staab <maggus.staab@googlemail.com>
2024-12-23 20:14:56 +0100

    Fix cs

--- src/Metadata/CoversClassesThatExtendClass.php
+++ src/Metadata/CoversClassesThatExtendClass.php
@@ -22,7 +22,7 @@ final readonly class CoversClassesThatExtendClass extends Metadata
     private string $className;
 
     /**
-     * @param int<0, 1> $level
+     * @param int<0, 1>    $level
      * @param class-string $className
      */
     protected function __construct(int $level, string $className)
