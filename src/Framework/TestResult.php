31da4d3a9f8f4dd4c6c1fd7a7aafbc8656079a27
Sebastian Bergmann <sb@sebastian-bergmann.de>
2025-03-04 13:14:46 +0100

    Tweak

--- src/Framework/TestResult.php
+++ src/Framework/TestResult.php
@@ -930,7 +930,7 @@ final class TestResult implements Countable
                 $test,
                 new OutputError(
                     sprintf(
-                        'This test printed output: %s',
+                        'Test code or tested code printed unexpected output: %s',
                         $test->getActualOutput()
                     )
                 ),
