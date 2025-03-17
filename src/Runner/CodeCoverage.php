6565fa71330164d2e9eeac6a44a27f3c566909eb
Sebastian Bergmann <sb@sebastian-bergmann.de>
2025-03-17 08:09:38 +0100

    Trigger warning when code coverage analysis is performed and no cache directory is configured

--- src/Runner/CodeCoverage.php
+++ src/Runner/CodeCoverage.php
@@ -122,6 +122,12 @@ final class CodeCoverage
 
             $this->deactivate();
         }
+
+        if (!$configuration->hasCoverageCacheDirectory()) {
+            EventFacade::emitter()->testRunnerTriggeredPhpunitWarning(
+                'No cache directory configured, result of static analysis for code coverage will not be cached',
+            );
+        }
     }
 
     /**
