db75fdd891cb16adb0f00fdf0bff11e02b7567dd
Sebastian Bergmann <sb@sebastian-bergmann.de>
2025-03-23 09:38:07 +0100

    Use stricter comparison

--- src/TextUI/Configuration/Xml/Loader.php
+++ src/TextUI/Configuration/Xml/Loader.php
@@ -147,7 +147,7 @@ final readonly class Loader
         $otr     = null;
         $element = $this->element($xpath, 'logging/otr');
 
-        if ($element) {
+        if ($element !== null) {
             $otr = new Otr(
                 new File(
                     $this->toAbsolutePath(
