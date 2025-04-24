9f2c9d76623bd69f61ed8e84776345e47f583b8c
Sebastian Bergmann <sb@sebastian-bergmann.de>
2025-04-24 13:13:21 +0200

    Validate generated logfiles against Open Test Reporting schemas

---
+++ tests/end-to-end/logging/open-test-reporting/validate_and_print.php
@@ -0,0 +1,40 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of PHPUnit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+function validate_and_print(string $logfile): void
+{
+    \libxml_use_internal_errors(true);
+
+    $document = new DOMDocument;
+    $document->load($logfile);
+
+    if (!$document->schemaValidate(__DIR__ . '/schema/otr.xml')) {
+        print 'Generated XML document does not validate against Open Test Reporting schemas:' . \PHP_EOL . \PHP_EOL;
+
+        foreach (\libxml_get_errors() as $error) {
+            \printf(
+                '- Line %d: %s' . \PHP_EOL,
+                $error->line,
+                \trim($error->message),
+            );
+        }
+
+        unset($error);
+
+        print \PHP_EOL;
+    }
+
+    \libxml_clear_errors();
+
+    unset($document);
+
+    print \file_get_contents($logfile);
+
+    \unlink($logfile);
+}
