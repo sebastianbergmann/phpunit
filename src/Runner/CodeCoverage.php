598ce03efbfa2e25d2b7010af2ba803e806d0bc9
leon.schaub <leon.schaub@orca.ch>
2025-05-19 15:17:34 +0200

    Set default Clover coverage project name
    Some Clover XML report processing tools require the project name to be set.
    
    e.g. https://issues.jenkins.io/browse/JENKINS-75648

--- src/Runner/CodeCoverage.php
+++ src/Runner/CodeCoverage.php
@@ -229,7 +229,7 @@ final class CodeCoverage
 
             try {
                 $writer = new CloverReport;
-                $writer->process($this->codeCoverage(), $configuration->coverageClover());
+                $writer->process($this->codeCoverage(), $configuration->coverageClover(), 'Clover Coverage');
 
                 $this->codeCoverageGenerationSucceeded($printer);
 
