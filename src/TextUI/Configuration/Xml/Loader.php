d3d8b7862c9579cc224ec05c2381ee0734f32327
Sebastian Bergmann <sb@sebastian-bergmann.de>
2025-06-27 06:18:27 +0200

    Closes #6249

--- src/TextUI/Configuration/Xml/Loader.php
+++ src/TextUI/Configuration/Xml/Loader.php
@@ -10,6 +10,7 @@
 namespace PHPUnit\TextUI\XmlConfiguration;
 
 use const DIRECTORY_SEPARATOR;
+use const PHP_EOL;
 use const PHP_VERSION;
 use function assert;
 use function defined;
@@ -18,6 +19,7 @@ use function explode;
 use function is_numeric;
 use function preg_match;
 use function realpath;
+use function sprintf;
 use function str_contains;
 use function str_starts_with;
 use function strlen;
@@ -74,6 +76,7 @@ use PHPUnit\Util\Xml\Loader as XmlLoader;
 use PHPUnit\Util\Xml\XmlException;
 use SebastianBergmann\CodeCoverage\Report\Html\Colors;
 use SebastianBergmann\CodeCoverage\Report\Thresholds;
+use Throwable;
 
 /**
  * @no-named-arguments Parameter names are not covered by the backward compatibility promise for PHPUnit
@@ -113,18 +116,33 @@ final readonly class Loader
 
         assert($configurationFileRealpath !== false && $configurationFileRealpath !== '');
 
-        return new LoadedFromFileConfiguration(
-            $configurationFileRealpath,
-            (new Validator)->validate($document, $xsdFilename),
-            $this->extensions($xpath),
-            $this->source($configurationFileRealpath, $xpath),
-            $this->codeCoverage($configurationFileRealpath, $xpath),
-            $this->groups($xpath),
-            $this->logging($configurationFileRealpath, $xpath),
-            $this->php($configurationFileRealpath, $xpath),
-            $this->phpunit($configurationFileRealpath, $document),
-            $this->testSuite($configurationFileRealpath, $xpath),
-        );
+        $validationResult = (new Validator)->validate($document, $xsdFilename);
+
+        try {
+            return new LoadedFromFileConfiguration(
+                $configurationFileRealpath,
+                $validationResult,
+                $this->extensions($xpath),
+                $this->source($configurationFileRealpath, $xpath),
+                $this->codeCoverage($configurationFileRealpath, $xpath),
+                $this->groups($xpath),
+                $this->logging($configurationFileRealpath, $xpath),
+                $this->php($configurationFileRealpath, $xpath),
+                $this->phpunit($configurationFileRealpath, $document),
+                $this->testSuite($configurationFileRealpath, $xpath),
+            );
+        } catch (Throwable $t) {
+            $message = sprintf(
+                'Cannot load XML configuration file %s',
+                $configurationFileRealpath,
+            );
+
+            if ($validationResult->hasValidationErrors()) {
+                $message .= ' because it has validation errors:' . PHP_EOL . $validationResult->asString();
+            }
+
+            throw new Exception($message, previous: $t);
+        }
     }
 
     private function logging(string $filename, DOMXPath $xpath): Logging
--- tests/end-to-end/generic/defaulttestsuite-using-testsuite-without-name.phpt
+++ tests/end-to-end/generic/defaulttestsuite-using-testsuite-without-name.phpt
@@ -3,7 +3,6 @@ phpunit --configuration=__DIR__.'/../../_files/configuration.testsuite_no_name.x
 --FILE--
 <?php declare(strict_types=1);
 $_SERVER['argv'][] = '--do-not-cache-result';
-$_SERVER['argv'][] = '--no-progress';
 $_SERVER['argv'][] = '--configuration';
 $_SERVER['argv'][] = __DIR__.'/../../_files/configuration.testsuite_no_name.xml';
 
@@ -12,4 +11,7 @@ require_once __DIR__ . '/../../bootstrap.php';
 --EXPECTF--
 PHPUnit %s by Sebastian Bergmann and contributors.
 
-assert(!empty($name))
+Cannot load XML configuration file %sconfiguration.testsuite_no_name.xml because it has validation errors:
+
+  Line 3:
+  - Element 'testsuite': The attribute 'name' is required but missing.
