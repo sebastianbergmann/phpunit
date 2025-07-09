5688c14f884c1b33e822f4206f6cc028bdb31b48
Sebastian Bergmann <sb@sebastian-bergmann.de>
2025-07-09 16:06:49 +0200

    Add tests

--- tests/unit/Logging/TestDox/NamePrettifierTest.php
+++ tests/unit/Logging/TestDox/NamePrettifierTest.php
@@ -14,7 +14,11 @@ use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Group;
 use PHPUnit\Framework\Attributes\Small;
 use PHPUnit\Framework\TestCase;
+use PHPUnit\TestFixture\BackedEnumeration;
+use PHPUnit\TestFixture\Enumeration;
 use PHPUnit\TestFixture\TestDox\TestDoxAttributeOnTestClassTest;
+use PHPUnit\TestFixture\TestDoxTest;
+use stdClass;
 
 #[CoversClass(NamePrettifier::class)]
 #[Group('testdox')]
@@ -124,6 +128,58 @@ final class NamePrettifierTest extends TestCase
     }
 
     /**
+     * @return non-empty-list<array{0: non-empty-string, 1: TestCase, 2: bool}>
+     */
+    public static function objectProvider(): array
+    {
+        $object = new class {
+            public function __toString(): string
+            {
+                return 'object as string';
+            }
+        };
+
+        $data = [['string'], true, 0.0, 1, 'string', $object, new stdClass, Enumeration::Test, BackedEnumeration::Test, null, ''];
+
+        $testWithDataWithIntegerKey = new TestDoxTest('testTwo');
+        $testWithDataWithIntegerKey->setData(0, $data);
+
+        $testWithDataWithStringKey = new TestDoxTest('testTwo');
+        $testWithDataWithStringKey->setData('a', $data);
+
+        $testWithDataAndTestDoxPlaceholders = new TestDoxTest('testFour');
+        $testWithDataAndTestDoxPlaceholders->setData('a', $data);
+
+        return [
+            [
+                'One',
+                new TestDoxTest('testOne'),
+                false,
+            ],
+            [
+                'Two with data set #0',
+                $testWithDataWithIntegerKey,
+                false,
+            ],
+            [
+                'Two with data set "a"',
+                $testWithDataWithStringKey,
+                false,
+            ],
+            [
+                'This is a custom test description',
+                new TestDoxTest('testThree'),
+                false,
+            ],
+            [
+                'This is a custom test description with placeholders array true 0.0 1 string object as string stdClass Test test null \'\' default',
+                $testWithDataAndTestDoxPlaceholders,
+                false,
+            ],
+        ];
+    }
+
+    /**
      * @param non-empty-string $expected
      * @param non-empty-string $className
      */
@@ -141,5 +197,14 @@ final class NamePrettifierTest extends TestCase
     public function testNameOfTestMethodCanBePrettified(string $expected, string $methodName): void
     {
         $this->assertSame($expected, (new NamePrettifier)->prettifyTestMethodName($methodName));
+    }
+
+    /**
+     * @param non-empty-string $expected
+     */
+    #[DataProvider('objectProvider')]
+    public function test_TestCase_can_be_prettified(string $expected, TestCase $testCase, bool $colorize): void
+    {
+        $this->assertSame($expected, (new NamePrettifier)->prettifyTestCase($testCase, $colorize));
     }
 }
