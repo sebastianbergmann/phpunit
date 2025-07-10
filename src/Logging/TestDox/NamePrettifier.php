016505e6b9138bac624bef71cb0373403e387ea1
Sebastian Bergmann <sb@sebastian-bergmann.de>
2025-07-10 13:45:37 +0200

    Cache result of prettifyTestCase()

--- src/Logging/TestDox/NamePrettifier.php
+++ src/Logging/TestDox/NamePrettifier.php
@@ -60,6 +60,11 @@ final class NamePrettifier
     private array $strings = [];
 
     /**
+     * @var array<non-empty-string, non-empty-string>
+     */
+    private array $prettifiedTestCases = [];
+
+    /**
      * @param class-string $className
      */
     public function prettifyTestClassName(string $className): string
@@ -172,39 +177,38 @@ final class NamePrettifier
 
     public function prettifyTestCase(TestCase $test, bool $colorize): string
     {
-        $annotationWithPlaceholders = false;
-        $methodLevelTestDox         = MetadataRegistry::parser()->forMethod($test::class, $test->name())->isTestDox()->isMethodLevel();
+        $key = $test::class . '#' . $test->name();
 
-        if ($methodLevelTestDox->isNotEmpty()) {
-            $methodLevelTestDox = $methodLevelTestDox->asArray()[0];
+        if ($test->usesDataProvider()) {
+            $key .= '#' . $test->dataName();
+        }
 
-            assert($methodLevelTestDox instanceof TestDox);
+        if ($colorize) {
+            $key .= '#colorize';
+        }
 
-            $result = $methodLevelTestDox->text();
+        if (isset($this->prettifiedTestCases[$key])) {
+            return $this->prettifiedTestCases[$key];
+        }
 
-            if (str_contains($result, '$')) {
-                $annotation   = $result;
-                $providedData = $this->mapTestMethodParameterNamesToProvidedDataValues($test, $colorize);
+        $testDox      = MetadataRegistry::parser()->forMethod($test::class, $test->name())->isTestDox()->isMethodLevel();
+        $isCustomized = false;
 
-                $variables = array_map(
-                    static fn (string $variable): string => sprintf(
-                        '/%s(?=\b)/',
-                        preg_quote($variable, '/'),
-                    ),
-                    array_keys($providedData),
-                );
+        if ($testDox->isNotEmpty()) {
+            $testDox = $testDox->asArray()[0];
 
-                $result = preg_replace($variables, $providedData, $annotation);
+            assert($testDox instanceof TestDox);
 
-                $annotationWithPlaceholders = true;
-            }
+            [$result, $isCustomized] = $this->processTestDox($test, $testDox, $colorize);
         } else {
             $result = $this->prettifyTestMethodName($test->name());
         }
 
-        if (!$annotationWithPlaceholders && $test->usesDataProvider()) {
+        if (!$isCustomized && $test->usesDataProvider()) {
             $result .= $this->prettifyDataSet($test, $colorize);
         }
+
+        $this->prettifiedTestCases[$key] = $result;
 
         return $result;
     }
@@ -304,5 +308,34 @@ final class NamePrettifier
         }
 
         return $value::class;
+    }
+
+    /**
+     * @return array{0: string, 1: bool}
+     */
+    private function processTestDox(TestCase $test, TestDox $testDox, bool $colorize): array
+    {
+        $placeholdersUsed = false;
+
+        $result = $testDox->text();
+
+        if (str_contains($result, '$')) {
+            $annotation   = $result;
+            $providedData = $this->mapTestMethodParameterNamesToProvidedDataValues($test, $colorize);
+
+            $variables = array_map(
+                static fn (string $variable): string => sprintf(
+                    '/%s(?=\b)/',
+                    preg_quote($variable, '/'),
+                ),
+                array_keys($providedData),
+            );
+
+            $result = preg_replace($variables, $providedData, $annotation);
+
+            $placeholdersUsed = true;
+        }
+
+        return [$result, $placeholdersUsed];
     }
 }
