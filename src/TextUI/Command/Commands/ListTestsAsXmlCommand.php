c3c2eee920e5f92cc6fe3430e235ec26cdb2c809
Sebastian Bergmann <sb@sebastian-bergmann.de>
2025-07-30 08:37:30 +0200

    Closes #6097 (using proposed changes from #6278)

--- src/TextUI/Command/Commands/ListTestsAsXmlCommand.php
+++ src/TextUI/Command/Commands/ListTestsAsXmlCommand.php
@@ -10,11 +10,13 @@
 namespace PHPUnit\TextUI\Command;
 
 use const PHP_EOL;
+use function assert;
 use function file_put_contents;
 use function ksort;
 use function sprintf;
 use PHPUnit\Framework\TestCase;
 use PHPUnit\Runner\PhptTestCase;
+use ReflectionClass;
 use XMLWriter;
 
 /**
@@ -70,9 +72,13 @@ final readonly class ListTestsAsXmlCommand implements Command
                         $writer->endElement();
                     }
 
+                    $file = (new ReflectionClass($test))->getFileName();
+
+                    assert($file !== false);
+
                     $writer->startElement('testClass');
                     $writer->writeAttribute('name', $test::class);
-                    $writer->writeAttribute('file', $test->valueObjectForEvents()->file());
+                    $writer->writeAttribute('file', $file);
 
                     $currentTestClass = $test::class;
                 }
