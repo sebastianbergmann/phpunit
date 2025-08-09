5bc0a0245236523a0a80b4d067b4843004b4c692
Sebastian Bergmann <sb@sebastian-bergmann.de>
2025-08-09 06:56:04 +0200

    Revert "Do not use PHP 8.2 syntax"
    This reverts commit 14da4da4dba20188712ff9426d3a78da61ecf9aa.

--- src/TextUI/Command/Commands/CheckPhpConfigurationCommand.php
+++ src/TextUI/Command/Commands/CheckPhpConfigurationCommand.php
@@ -24,7 +24,7 @@ use SebastianBergmann\Environment\Console;
  *
  * @internal This class is not covered by the backward compatibility promise for PHPUnit
  */
-final class CheckPhpConfigurationCommand implements Command
+final readonly class CheckPhpConfigurationCommand implements Command
 {
     /**
      * @var non-empty-array<non-empty-string, array{expectedValue: non-empty-string, valueForConfiguration: non-empty-string, requiredExtensions: list<non-empty-string>}>
@@ -66,7 +66,7 @@ final class CheckPhpConfigurationCommand implements Command
             'requiredExtensions'    => [],
         ],
     ];
-    private readonly bool $colorize;
+    private bool $colorize;
 
     public function __construct()
     {
