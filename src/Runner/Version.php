5fd1b6e8ab560e0c62600591d438d22a8d978d68
Sebastian Bergmann <sb@sebastian-bergmann.de>
2025-08-09 09:12:41 +0200

    Prepare release

--- src/Runner/Version.php
+++ src/Runner/Version.php
@@ -35,7 +35,7 @@ final class Version
         }
 
         if (self::$version === '') {
-            self::$version = (new VersionId('12.3.0', dirname(__DIR__, 2)))->asString();
+            self::$version = (new VersionId('12.3.1', dirname(__DIR__, 2)))->asString();
         }
 
         return self::$version;
