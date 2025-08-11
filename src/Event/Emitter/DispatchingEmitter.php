2eaa6fe40661eb34db2a38d30f11ab9bab27cbee
Sebastian Bergmann <sb@sebastian-bergmann.de>
2025-08-11 07:21:15 +0200

    Revert "Take #[IgnorePhpunitDeprecations] attribute into account for test runner deprecations"
    This reverts commit b3be029cd69c276031342ad13769b5dc0486a035.

--- src/Event/Emitter/DispatchingEmitter.php
+++ src/Event/Emitter/DispatchingEmitter.php
@@ -1208,13 +1208,6 @@ final class DispatchingEmitter implements Emitter
      */
     public function testRunnerTriggeredPhpunitDeprecation(string $message): void
     {
-        try {
-            if (TestMethodBuilder::fromCallStack()->metadata()->isIgnorePhpunitDeprecations()->isNotEmpty()) {
-                return;
-            }
-        } catch (NoTestCaseObjectOnCallStackException) {
-        }
-
         $this->dispatcher->dispatch(
             new TestRunner\DeprecationTriggered(
                 $this->telemetryInfo(),
