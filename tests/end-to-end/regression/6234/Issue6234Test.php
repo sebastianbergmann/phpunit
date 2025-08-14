bfd162260976e55338e32885d70fe8149fe96dcf
Markus Staab <maggus.staab@googlemail.com>
2025-08-14 10:27:21 +0200

    Detect premature end of phpunit main process

---
+++ tests/end-to-end/regression/6234/Issue6234Test.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of PHPUnit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace PHPUnit\TestFixture;
+
+use PHPUnit\Framework\TestCase;
+
+final class Issue6234Test extends TestCase
+{
+    public function testExitWithoutProcessIsolation(): void
+    {
+        $this->assertTrue(true);
+        $this->assertTrue(true);
+
+        exit(1);
+    }
+}
