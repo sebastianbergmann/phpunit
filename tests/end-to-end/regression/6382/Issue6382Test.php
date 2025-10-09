f177dc77f09c0c5809fa227f73ce646fc4c865de
Markus Staab <markus.staab@redaxo.de>
2025-10-09 10:58:50 +0200

    fix namespace

--- tests/end-to-end/regression/6382/Issue6382Test.php
+++ tests/end-to-end/regression/6382/Issue6382Test.php
@@ -1,4 +1,13 @@
-<?php
+<?php declare(strict_types=1);
+/*
+ * This file is part of PHPUnit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace PHPUnit\TestFixture\Issue6382;
 
 use PHPUnit\Framework\TestCase;
 use Greg0ire\PhpunitReproducer\Child6382;
