5107851313ba9824679dd8bb3ab98208a0c61863
Daniel Leech <daniel@dantleech.com>
2025-10-25 14:07:22 +0100

    Remve commented test

--- tests/unit/TextUI/SourceFilterTest.php
+++ tests/unit/TextUI/SourceFilterTest.php
@@ -321,25 +321,6 @@ final class SourceFilterTest extends AbstractSouceFilterTestCase
                     ),
                 ),
             ],
-            // this seems rather more like a bug
-            // 'globstar with any single char prefix includes sibling files' => [
-            //    [
-            //        self::fixturePath('a/PrefixSuffix.php')     => false,
-            //        self::fixturePath('a/c/PrefixSuffix.php')   => true,
-            //        self::fixturePath('a/c/d/PrefixSuffix.php') => true,
-            //    ],
-            //    self::createSource(
-            //        includeDirectories: FilterDirectoryCollection::fromArray(
-            //            [
-            //                new FilterDirectory(
-            //                    self::fixturePath('a/c/Z**'),
-            //                    '',
-            //                    '.php',
-            //                ),
-            //            ],
-            //        ),
-            //    ),
-            // ],
             'globstar with any more than a single char prefix does not include sibling files' => [
                 [
                     self::fixturePath('a/PrefixSuffix.php')     => false,
