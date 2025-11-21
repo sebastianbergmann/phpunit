052faca21a79fc39a08e849b45f13d274d4c49bd
Christina Koenig <christina.koenig@looriva.de>
2025-11-21 09:07:24 +0100

    fix codestyle issues

--- src/Framework/MockObject/Runtime/Rule/ConsecutiveParameterSets.php
+++ src/Framework/MockObject/Runtime/Rule/ConsecutiveParameterSets.php
@@ -13,7 +13,6 @@ use function array_shift;
 use function count;
 use function is_array;
 use function sprintf;
-use PHPUnit\Framework\Exception;
 use PHPUnit\Framework\ExpectationFailedException;
 use PHPUnit\Framework\MockObject\Invocation as BaseInvocation;
 use PHPUnit\Framework\MockObject\NoMoreParameterSetsConfiguredException;
@@ -37,9 +36,7 @@ final class ConsecutiveParameterSets implements ParametersRule
     private int $numberOfConfiguredParameterSets = 0;
 
     /**
-     * @param array<mixed> $parameters
-     *
-     * @throws Exception
+     * @param list<Parameters> $stack
      */
     public function __construct(array $stack)
     {
--- src/Framework/MockObject/Runtime/Rule/UnorderedParameterSets.php
+++ src/Framework/MockObject/Runtime/Rule/UnorderedParameterSets.php
@@ -15,7 +15,6 @@ use function count;
 use function implode;
 use function is_array;
 use function sprintf;
-use PHPUnit\Framework\Exception;
 use PHPUnit\Framework\ExpectationFailedException;
 use PHPUnit\Framework\MockObject\Invocation as BaseInvocation;
 use PHPUnit\Framework\MockObject\NoMoreParameterSetsConfiguredException;
@@ -39,9 +38,7 @@ final class UnorderedParameterSets implements ParametersRule
     private int $numberOfConfiguredParameterSets = 0;
 
     /**
-     * @param array<mixed> $parameters
-     *
-     * @throws Exception
+     * @param list<Parameters> $stack
      */
     public function __construct(array $stack)
     {
