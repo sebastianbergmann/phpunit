86806a59d10617fb751f92fc1d93f5e67187a6af
Sebastian Bergmann <sb@sebastian-bergmann.de>
2025-12-02 08:45:14 +0100

    Remove option that is no longer needed now that PHP 8.5 is supported

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -27,7 +27,6 @@ $finder = PhpCsFixer\Finder::create()
 
 $config = new PhpCsFixer\Config;
 $config->setFinder($finder)
-    ->setUnsupportedPhpVersionAllowed(true)
     ->setRiskyAllowed(true)
     ->setRules([
         'align_multiline_comment' => true,
