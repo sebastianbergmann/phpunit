eaa3ae487b1b43cf56b1d18f55b7071b3f15da48
Basilius Sauter <basilius.sauter@unibas.ch>
2026-01-14 19:01:54 +0100

    Fixes #6470: Mocking a class with a covariant property hook setter leads to an unexpected fatal error
    Set hooks allow covariant setters to expand the scope of a parameter (eg, converting strings into Datetimes). PHPUnit uses wrongly the type for the property for the setter instead of the setters type. @see Issue #6470.

--- tests/unit/Framework/MockObject/Generator/PropertyTest.php
+++ tests/unit/Framework/MockObject/Generator/PropertyTest.php
@@ -24,7 +24,7 @@ final class PropertyTest extends TestCase
     {
         $name = 'property-name';
 
-        $property = new HookedProperty($name, Type::fromName('string', false), false, false);
+        $property = new HookedProperty($name, Type::fromName('string', false), false, false, Type::fromName('string', false));
 
         $this->assertSame($name, $property->name());
     }
@@ -33,36 +33,46 @@ final class PropertyTest extends TestCase
     {
         $type = Type::fromName('string', false);
 
-        $property = new HookedProperty('property-name', $type, false, false);
+        $property = new HookedProperty('property-name', $type, false, false, $type);
 
         $this->assertSame($type, $property->type());
     }
 
     public function testMayHaveGetHook(): void
     {
-        $property = new HookedProperty('property-name', Type::fromName('string', false), true, false);
+        $property = new HookedProperty('property-name', Type::fromName('string', false), true, false, Type::fromName('string', false));
 
         $this->assertTrue($property->hasGetHook());
     }
 
     public function testMayNotHaveGetHook(): void
     {
-        $property = new HookedProperty('property-name', Type::fromName('string', false), false, false);
+        $property = new HookedProperty('property-name', Type::fromName('string', false), false, false, Type::fromName('string', false));
 
         $this->assertFalse($property->hasGetHook());
     }
 
     public function testMayHaveSetHook(): void
     {
-        $property = new HookedProperty('property-name', Type::fromName('string', false), false, true);
+        $property = new HookedProperty('property-name', Type::fromName('string', false), false, true, Type::fromName('string', false));
 
         $this->assertTrue($property->hasSetHook());
     }
 
     public function testMayNotHaveSetHook(): void
     {
-        $property = new HookedProperty('property-name', Type::fromName('string', false), false, false);
+        $property = new HookedProperty('property-name', Type::fromName('string', false), false, false, Type::fromName('string', false));
 
         $this->assertFalse($property->hasSetHook());
+    }
+
+    public function testHasSetterType(): void
+    {
+        $type       = Type::fromName('string', false);
+        $setterType = Type::fromName('string', true);
+
+        $property = new HookedProperty('property-name', $type, false, false, $setterType);
+
+        $this->assertSame($setterType, $property->setterType());
     }
 }
