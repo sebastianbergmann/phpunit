ce652a59c0462e424a908af30f41570144e840d6
Sebastian Bergmann <sb@sebastian-bergmann.de>
2026-02-06 07:12:56 +0100

    Start development of PHPUnit 13.1

---
+++ ChangeLog-13.1.md
@@ -0,0 +1,7 @@
+# Changes in PHPUnit 13.1
+
+All notable changes of the PHPUnit 13.1 release series are documented in this file using the [Keep a CHANGELOG](https://keepachangelog.com/) principles.
+
+## [13.1.0] - 2026-04-03
+
+[13.1.0]: https://github.com/sebastianbergmann/phpunit/compare/13.0...main
--- src/Runner/Version.php
+++ src/Runner/Version.php
@@ -35,7 +35,7 @@ final class Version
         }
 
         if (self::$version === '') {
-            self::$version = new VersionId('13.0.0', dirname(__DIR__, 2))->asString();
+            self::$version = new VersionId('13.1', dirname(__DIR__, 2))->asString();
         }
 
         return self::$version;
