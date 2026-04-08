60ec2d81c85f2cd576ca4904e3afc5633eeb42cc
Sebastian Bergmann <sb@sebastian-bergmann.de>
2026-04-08 06:49:12 +0200

    Update dependencies

---
+++ tests/end-to-end/regression/4923/Issue4923Test.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of PHPUnit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace PHPUnit\TestFixture\Issue4923;
+
+use PHPUnit\Framework\TestCase;
+use stdClass;
+
+final class Issue4923Test extends TestCase
+{
+    public function testOne(): void
+    {
+        $obj = new stdClass;
+
+        $a = [123, $obj, 'hello'];
+        $b = [$obj, 'hello', 123];
+
+        $this->assertEqualsCanonicalizing($a, $b);
+    }
+}
