d9083d3f7f0d505985814a70d7b6b8402ce0be62
Sebastian Bergmann <sb@sebastian-bergmann.de>
2026-04-22 13:15:18 +0200

    Improve tests

--- tests/unit/Framework/ExecutionOrderDependencyTest.php
+++ tests/unit/Framework/ExecutionOrderDependencyTest.php
@@ -10,111 +10,246 @@
 namespace PHPUnit\Framework;
 
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Small;
+use PHPUnit\Metadata\Metadata;
 
 #[CoversClass(ExecutionOrderDependency::class)]
+#[Small]
 class ExecutionOrderDependencyTest extends TestCase
 {
-    public static function createFromParametersProvider(): array
+    public function testCanBeInvalid(): void
     {
-        return [
-            // Dependency on specific class::method target
-            ['Class1', 'test1', 'Class1::test1', false],
-            ['Class2', 'test2', 'Class2::test2', false],
-            ['Class3::method3', null, 'Class3::method3', false],
+        $dependency = ExecutionOrderDependency::invalid();
 
-            // Dependency on whole class
-            ['Class4', null, 'Class4::class', true],
-            ['Class5', '', 'Class5::class', true],
-            ['Class6', 'class', 'Class6::class', true],
-            ['Class7::class', null, 'Class7::class', true],
-        ];
+        $this->assertFalse($dependency->isValid());
+        $this->assertSame('', $dependency->getTarget());
+        $this->assertSame('', (string) $dependency);
+        $this->assertSame('', $dependency->getTargetClassName());
+        $this->assertFalse($dependency->shallowClone());
+        $this->assertFalse($dependency->deepClone());
     }
 
-    public static function createWithCloneOptionProvider(): array
+    public function testCanBeDependencyOnClass(): void
     {
-        return [
-            'no clone'      => [false, false, false, false],
-            'deep clone'    => [true, false, false, true],
-            'shallow clone' => [false, true, true, false],
-        ];
-    }
+        $metadata   = Metadata::dependsOnClass('SomeClass', false, false);
+        $dependency = ExecutionOrderDependency::forClass($metadata);
 
-    public function testCreateDependencyOnClassFromClassNameOnly(): void
-    {
-        $dependency = new ExecutionOrderDependency('ClassDependency');
-
+        $this->assertTrue($dependency->isValid());
         $this->assertTrue($dependency->targetIsClass());
-        $this->assertSame('ClassDependency::class', $dependency->getTarget());
-        $this->assertSame('ClassDependency', $dependency->getTargetClassName());
+        $this->assertSame('SomeClass::class', $dependency->getTarget());
+        $this->assertSame('SomeClass::class', (string) $dependency);
+        $this->assertSame('SomeClass', $dependency->getTargetClassName());
+        $this->assertFalse($dependency->deepClone());
+        $this->assertFalse($dependency->shallowClone());
     }
 
-    #[DataProvider('createFromParametersProvider')]
-    public function testCreateDependencyFromParameters(string $className, ?string $methodName, string $expectedTarget, bool $expectedTargetIsClass): void
+    public function testCanBeDependencyOnClassWithDeepClone(): void
     {
-        $dependency = new ExecutionOrderDependency($className, $methodName);
+        $metadata   = Metadata::dependsOnClass('SomeClass', true, false);
+        $dependency = ExecutionOrderDependency::forClass($metadata);
 
-        $this->assertSame(
-            $expectedTarget,
-            $dependency->getTarget(),
-            'Incorrect dependency class::method target',
-        );
-
-        $this->assertSame(
-            $expectedTargetIsClass,
-            $dependency->targetIsClass(),
-            'Incorrect targetIsClass',
-        );
+        $this->assertTrue($dependency->deepClone());
+        $this->assertFalse($dependency->shallowClone());
     }
 
-    #[DataProvider('createWithCloneOptionProvider')]
-    public function testCreateDependencyWithCloneOption(bool $deepClone, bool $shallowClone, bool $expectedShallowClone, bool $expectedDeepClone): void
+    public function testCanBeDependencyOnClassWithShallowClone(): void
     {
-        $dependency = new ExecutionOrderDependency('ClassName', 'methodName', $deepClone, $shallowClone);
+        $metadata   = Metadata::dependsOnClass('SomeClass', false, true);
+        $dependency = ExecutionOrderDependency::forClass($metadata);
 
-        $this->assertSame(
-            $expectedShallowClone,
-            $dependency->shallowClone(),
-            'Incorrect shallowClone option',
-        );
-
-        $this->assertSame(
-            $expectedDeepClone,
-            $dependency->deepClone(),
-            'Incorrect clone option',
-        );
+        $this->assertFalse($dependency->deepClone());
+        $this->assertTrue($dependency->shallowClone());
     }
 
-    public function testMergeHandlesEmptyDependencyLists(): void
+    public function testCanBeDependencyOnMethod(): void
     {
-        $depOne = new ExecutionOrderDependency('classOne');
-        $depTwo = new ExecutionOrderDependency('classTwo::methodTwo');
+        $metadata   = Metadata::dependsOnMethod('SomeClass', 'someMethod', false, false);
+        $dependency = ExecutionOrderDependency::forMethod($metadata);
+
+        $this->assertTrue($dependency->isValid());
+        $this->assertFalse($dependency->targetIsClass());
+        $this->assertSame('SomeClass::someMethod', $dependency->getTarget());
+        $this->assertSame('SomeClass::someMethod', (string) $dependency);
+        $this->assertFalse($dependency->deepClone());
+        $this->assertFalse($dependency->shallowClone());
+    }
+
+    public function testCanBeDependencyOnMethodWithDeepClone(): void
+    {
+        $metadata   = Metadata::dependsOnMethod('SomeClass', 'someMethod', true, false);
+        $dependency = ExecutionOrderDependency::forMethod($metadata);
+
+        $this->assertTrue($dependency->deepClone());
+        $this->assertFalse($dependency->shallowClone());
+    }
+
+    public function testCanBeDependencyOnMethodShallowClone(): void
+    {
+        $metadata   = Metadata::dependsOnMethod('SomeClass', 'someMethod', false, true);
+        $dependency = ExecutionOrderDependency::forMethod($metadata);
+
+        $this->assertFalse($dependency->deepClone());
+        $this->assertTrue($dependency->shallowClone());
+    }
+
+    public function testConstructorParsesDoubleColonNotationForClass(): void
+    {
+        $dependency = new ExecutionOrderDependency('Class::class');
+
+        $this->assertSame('Class::class', $dependency->getTarget());
+        $this->assertSame('Class::class', (string) $dependency);
+        $this->assertTrue($dependency->targetIsClass());
+    }
+
+    public function testConstructorParsesDoubleColonNotationForMethod(): void
+    {
+        $dependency = new ExecutionOrderDependency('Class::method');
+
+        $this->assertSame('Class::method', $dependency->getTarget());
+        $this->assertSame('Class::method', (string) $dependency);
+        $this->assertFalse($dependency->targetIsClass());
+    }
+
+    public function testInvalidDependenciesAreFiltered(): void
+    {
+        $valid = ExecutionOrderDependency::forMethod(
+            Metadata::dependsOnMethod('ClassA', 'methodA', false, false),
+        );
+
+        $invalid = ExecutionOrderDependency::invalid();
+
+        $result = ExecutionOrderDependency::filterInvalid([$valid, $invalid]);
+
+        $this->assertCount(1, $result);
+        $this->assertSame($valid, $result[0]);
+    }
+
+    public function testInvalidDependenciesAreFiltered2(): void
+    {
+        $result = ExecutionOrderDependency::filterInvalid([
+            ExecutionOrderDependency::invalid(),
+            ExecutionOrderDependency::invalid(),
+        ]);
+
+        $this->assertSame([], $result);
+    }
+
+    public function testValidDependenciesAreNotFiltered(): void
+    {
+        $a = ExecutionOrderDependency::forMethod(
+            Metadata::dependsOnMethod('ClassA', 'methodA', false, false),
+        );
+
+        $b = ExecutionOrderDependency::forMethod(
+            Metadata::dependsOnMethod('ClassB', 'methodB', false, false),
+        );
+
+        $result = ExecutionOrderDependency::filterInvalid([$a, $b]);
+
+        $this->assertCount(2, $result);
+        $this->assertSame($a, $result[0]);
+        $this->assertSame($b, $result[1]);
+    }
+
+    public function testEmptyListsCanBeMerged(): void
+    {
+        $a = ExecutionOrderDependency::forClass(
+            Metadata::dependsOnClass('classOne', false, false),
+        );
+
+        $b = ExecutionOrderDependency::forMethod(
+            Metadata::dependsOnMethod('classTwo', 'methodTwo', false, false),
+        );
 
         $this->assertSame(
-            [$depOne, $depTwo],
+            [$a, $b],
             ExecutionOrderDependency::mergeUnique(
                 [],
-                [$depOne, $depTwo],
+                [$a, $b],
             ),
             'Left side of merge could be empty',
         );
 
         $this->assertSame(
-            [$depOne, $depTwo],
+            [$a, $b],
             ExecutionOrderDependency::mergeUnique(
-                [$depOne, $depTwo],
+                [$a, $b],
                 [],
             ),
             'Right side of merge could be empty',
         );
     }
 
-    public function testEmptyClassOrCallable(): void
+    public function testMergingDoesNotAddDuplicates(): void
     {
-        $empty = new ExecutionOrderDependency('');
-        $this->assertFalse($empty->shallowClone());
-        $this->assertFalse($empty->deepClone());
-        $this->assertFalse($empty->targetIsClass());
-        $this->assertSame('', $empty->getTargetClassName());
+        $a = ExecutionOrderDependency::forMethod(
+            Metadata::dependsOnMethod('ClassA', 'methodA', false, false),
+        );
+
+        $b = ExecutionOrderDependency::forMethod(
+            Metadata::dependsOnMethod('ClassB', 'methodB', false, false),
+        );
+
+        $c = ExecutionOrderDependency::forMethod(
+            Metadata::dependsOnMethod('ClassC', 'methodC', false, false),
+        );
+
+        $duplicate = ExecutionOrderDependency::forMethod(
+            Metadata::dependsOnMethod('ClassA', 'methodA', false, false),
+        );
+
+        $result = ExecutionOrderDependency::mergeUnique([$a, $b], [$duplicate, $c]);
+
+        $this->assertCount(3, $result);
+        $this->assertSame('ClassA::methodA', $result[0]->getTarget());
+        $this->assertSame('ClassB::methodB', $result[1]->getTarget());
+        $this->assertSame('ClassC::methodC', $result[2]->getTarget());
+    }
+
+    public function testDiffReturnsLeftWhenRightIsEmpty(): void
+    {
+        $a = ExecutionOrderDependency::forMethod(
+            Metadata::dependsOnMethod('ClassA', 'methodA', false, false),
+        );
+
+        $b = ExecutionOrderDependency::forMethod(
+            Metadata::dependsOnMethod('ClassB', 'methodB', false, false),
+        );
+
+        $result = ExecutionOrderDependency::diff([$a, $b], []);
+
+        $this->assertSame([$a, $b], $result);
+    }
+
+    public function testDiffReturnsEmptyWhenLeftIsEmpty(): void
+    {
+        $dependency = ExecutionOrderDependency::forMethod(
+            Metadata::dependsOnMethod('ClassA', 'methodA', false, false),
+        );
+
+        $result = ExecutionOrderDependency::diff([], [$dependency]);
+
+        $this->assertSame([], $result);
+    }
+
+    public function testDiffRemovesMatchingDependencies(): void
+    {
+        $a = ExecutionOrderDependency::forMethod(
+            Metadata::dependsOnMethod('ClassA', 'methodA', false, false),
+        );
+
+        $b = ExecutionOrderDependency::forMethod(
+            Metadata::dependsOnMethod('ClassB', 'methodB', false, false),
+        );
+
+        $c = ExecutionOrderDependency::forMethod(
+            Metadata::dependsOnMethod('ClassC', 'methodC', false, false),
+        );
+
+        $result = ExecutionOrderDependency::diff([$a, $b, $c], [$b]);
+
+        $this->assertCount(2, $result);
+        $this->assertSame($a, $result[0]);
+        $this->assertSame($c, $result[1]);
     }
 }
