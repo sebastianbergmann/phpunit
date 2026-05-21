06304c21929d919bdb969db46008ece78a39c29e
Sebastian Bergmann <sb@sebastian-bergmann.de>
2026-05-21 06:50:17 +0200

    Update expectations

--- tests/unit/Framework/Constraint/Equality/IsEqualCanonicalizingTest.php
+++ tests/unit/Framework/Constraint/Equality/IsEqualCanonicalizingTest.php
@@ -169,8 +169,8 @@ Failed asserting that two arrays are equal.
 +++ Actual
 @@ @@
  Array (
--    0 => 'value'
-+    0 => 'another-value'
+-    'value'
++    'another-value'
  )
 
 EOT,
@@ -192,8 +192,8 @@ Failed asserting that two objects are equal.
 +++ Actual
 @@ @@
  stdClass Object (
--    0 => 'bar'
-+    0 => 'foo'
+-    'foo' => 'bar'
++    'bar' => 'foo'
  )
 
 EOT,
