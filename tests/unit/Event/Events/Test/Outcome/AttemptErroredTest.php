621dc7bff22302fef12913bb0d4a0a2234aedf90
Sebastian Bergmann <sb@sebastian-bergmann.de>
2026-06-12 17:59:09 +0200

    Add tests

---
+++ tests/unit/Event/Events/Test/Outcome/AttemptErroredTest.php
@@ -0,0 +1,63 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of PHPUnit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace PHPUnit\Event\Test;
+
+use Exception;
+use PHPUnit\Event\AbstractEventTestCase;
+use PHPUnit\Event\Code;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\Small;
+
+#[CoversClass(AttemptErrored::class)]
+#[Small]
+#[Group('event-system')]
+#[Group('event-system/events')]
+final class AttemptErroredTest extends AbstractEventTestCase
+{
+    public function testConstructorSetsValues(): void
+    {
+        $telemetryInfo = $this->telemetryInfo();
+        $test          = $this->testValueObject();
+        $throwable     = $this->throwable();
+
+        $event = new AttemptErrored(
+            $telemetryInfo,
+            $test,
+            $throwable,
+        );
+
+        $this->assertSame($telemetryInfo, $event->telemetryInfo());
+        $this->assertSame($test, $event->test());
+        $this->assertSame($throwable, $event->throwable());
+    }
+
+    public function testCanBeRepresentedAsString(): void
+    {
+        $event = new AttemptErrored(
+            $this->telemetryInfo(),
+            $this->testValueObject(),
+            $this->throwable(),
+        );
+
+        $this->assertStringEqualsStringIgnoringLineEndings(
+            <<<'EOT'
+Test Attempt Errored (FooTest::testBar)
+error
+EOT,
+            $event->asString(),
+        );
+    }
+
+    private function throwable(): Code\Throwable
+    {
+        return Code\ThrowableBuilder::from(new Exception('error'));
+    }
+}
---
+++ tests/unit/Event/Events/Test/Outcome/AttemptFailedTest.php
@@ -0,0 +1,94 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of PHPUnit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace PHPUnit\Event\Test;
+
+use Exception;
+use PHPUnit\Event\AbstractEventTestCase;
+use PHPUnit\Event\Code;
+use PHPUnit\Event\Code\ComparisonFailure;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\Small;
+
+#[CoversClass(AttemptFailed::class)]
+#[Small]
+#[Group('event-system')]
+#[Group('event-system/events')]
+final class AttemptFailedTest extends AbstractEventTestCase
+{
+    public function testConstructorSetsValues(): void
+    {
+        $telemetryInfo     = $this->telemetryInfo();
+        $test              = $this->testValueObject();
+        $throwable         = $this->throwable();
+        $comparisonFailure = $this->comparisonFailure();
+
+        $event = new AttemptFailed(
+            $telemetryInfo,
+            $test,
+            $throwable,
+            $comparisonFailure,
+        );
+
+        $this->assertSame($telemetryInfo, $event->telemetryInfo());
+        $this->assertSame($test, $event->test());
+        $this->assertSame($throwable, $event->throwable());
+        $this->assertSame($comparisonFailure, $event->comparisonFailure());
+        $this->assertTrue($event->hasComparisonFailure());
+    }
+
+    public function testThrowsExceptionOnAccessToUnspecifiedComparisonFailure(): void
+    {
+        $telemetryInfo = $this->telemetryInfo();
+        $test          = $this->testValueObject();
+        $throwable     = $this->throwable();
+
+        $event = new AttemptFailed(
+            $telemetryInfo,
+            $test,
+            $throwable,
+            null,
+        );
+
+        $this->assertFalse($event->hasComparisonFailure());
+
+        $this->expectException(NoComparisonFailureException::class);
+
+        $event->comparisonFailure();
+    }
+
+    public function testCanBeRepresentedAsString(): void
+    {
+        $event = new AttemptFailed(
+            $this->telemetryInfo(),
+            $this->testValueObject(),
+            $this->throwable(),
+            null,
+        );
+
+        $this->assertStringEqualsStringIgnoringLineEndings(
+            <<<'EOT'
+Test Attempt Failed (FooTest::testBar)
+failure
+EOT,
+            $event->asString(),
+        );
+    }
+
+    private function throwable(): Code\Throwable
+    {
+        return Code\ThrowableBuilder::from(new Exception('failure'));
+    }
+
+    private function comparisonFailure(): ComparisonFailure
+    {
+        return new ComparisonFailure('expected', 'actual', 'diff');
+    }
+}
