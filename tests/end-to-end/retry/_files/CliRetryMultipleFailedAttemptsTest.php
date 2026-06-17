9363a8df48b4b10532f2b65a813f5a03305e8c1d
Sebastian Bergmann <sb@sebastian-bergmann.de>
2026-06-17 07:31:04 +0200

    Add test

---
+++ tests/end-to-end/retry/_files/CliRetryMultipleFailedAttemptsTest.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of PHPUnit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace PHPUnit\TestFixture\Retry;
+
+use PHPUnit\Framework\TestCase;
+
+final class CliRetryMultipleFailedAttemptsTest extends TestCase
+{
+    private static int $count = 0;
+
+    public function testOne(): void
+    {
+        self::$count++;
+
+        if (self::$count < 3) {
+            $this->fail('Failure on attempt ' . self::$count);
+        }
+
+        $this->assertTrue(true);
+    }
+}
