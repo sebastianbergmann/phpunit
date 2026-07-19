b0e82872df7371f03e5feb2acb20a8b3b3f04cc8
Sebastian Bergmann <sb@sebastian-bergmann.de>
2026-07-19 09:54:47 +0200

    Register a chunk's in-process units with the aggregator only when the chunk runs, so that a unit that leads a later chunk is not run inside the test-suite envelope of the chunk before it

---
+++ tests/end-to-end/parallel/chunk-boundaries/_files/tests/two/MainProcessTest.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of PHPUnit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace PHPUnit\TestFixture\ParallelChunkBoundaries;
+
+use PHPUnit\Framework\Attributes\DoNotRunInParallel;
+use PHPUnit\Framework\TestCase;
+
+#[DoNotRunInParallel]
+final class MainProcessTest extends TestCase
+{
+    public function testThatPasses(): void
+    {
+        $this->assertTrue(true);
+    }
+}
