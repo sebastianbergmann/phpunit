sebastianbergmann/phpunit

commit 612d48fdebdc05bb92c55a4c54be4efb9105b519

Remove unnecessary array_keys()

src/Logging/TestDox/TestResult/TestResultCollector.php
@@ -9,7 +9,6 @@
  */
 namespace PHPUnit\Logging\TestDox;
 
-use function array_keys;
 use function array_merge;
 use function assert;
 use function is_subclass_of;
@@ -86,9 +85,9 @@ final class TestResultCollector
                 $testsByDeclaringClass[$declaringClassName][] = $test;
             }
 
-            foreach (array_keys($testsByDeclaringClass) as $declaringClassName) {
+            foreach ($testsByDeclaringClass as $declaringClassName) {
                 usort(
-                    $testsByDeclaringClass[$declaringClassName],
+                    $declaringClassName,
                     static function (TestDoxTestMethod $a, TestDoxTestMethod $b): int
                     {
                         return $a->test()->line() <=> $b->test()->line();

commit 80764d58a9af2f34b55cc9cff6c3c005717a1570

Fix regression introduced in 612d48fde that stopped test methods from being sorted by source code location

src/Logging/TestDox/TestResult/TestResultCollector.php
@@ -9,6 +9,7 @@
  */
 namespace PHPUnit\Logging\TestDox;
 
+use function array_keys;
 use function array_merge;
 use function assert;
 use function is_subclass_of;
@@ -86,9 +87,9 @@ final class TestResultCollector
                 $testsByDeclaringClass[$declaringClassName][] = $test;
             }
 
-            foreach ($testsByDeclaringClass as $declaringClassName) {
+            foreach (array_keys($testsByDeclaringClass) as $declaringClassName) {
                 usort(
-                    $declaringClassName,
+                    $testsByDeclaringClass[$declaringClassName],
                     static function (TestDoxTestMethod $a, TestDoxTestMethod $b): int
                     {
                         return $a->test()->line() <=> $b->test()->line();

tests/end-to-end/testdox/_files/method-order-inheritance/InheritanceTest.php
@@ -1,0 +1,38 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of PHPUnit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace PHPUnit\TestFixture\TestDox\MethodOrderInheritance;
+
+use PHPUnit\Framework\TestCase;
+
+final class InheritanceTest extends ParentTestCase
+{
+    public function testDeclaredFirstInChild(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testDeclaredSecondInChild(): void
+    {
+        $this->assertTrue(true);
+    }
+}
+
+abstract class ParentTestCase extends TestCase
+{
+    public function testDeclaredFirstInParent(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testDeclaredSecondInParent(): void
+    {
+        $this->assertTrue(true);
+    }
+}

tests/end-to-end/testdox/_files/method-order-source-location/SourceLocationTest.php
@@ -1,0 +1,30 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of PHPUnit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace PHPUnit\TestFixture\TestDox\MethodOrderSourceLocation;
+
+use PHPUnit\Framework\TestCase;
+
+final class SourceLocationTest extends TestCase
+{
+    public function testDeclaredFirst(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testDeclaredSecond(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testDeclaredThird(): void
+    {
+        $this->assertTrue(true);
+    }
+}

tests/end-to-end/testdox/method-order-inheritance.phpt
@@ -1,0 +1,29 @@
+--TEST--
+TestDox: Test methods are grouped by their declaring class, with inherited test methods listed before the ones declared in the test class itself
+--FILE--
+<?php declare(strict_types=1);
+$_SERVER['argv'][] = '--do-not-cache-result';
+$_SERVER['argv'][] = '--no-configuration';
+$_SERVER['argv'][] = '--no-progress';
+$_SERVER['argv'][] = '--testdox';
+$_SERVER['argv'][] = '--colors=never';
+$_SERVER['argv'][] = '--order-by=reverse';
+$_SERVER['argv'][] = __DIR__ . '/_files/method-order-inheritance';
+
+require_once __DIR__ . '/../../bootstrap.php';
+
+(new PHPUnit\TextUI\Application)->run($_SERVER['argv']);
+--EXPECTF--
+PHPUnit %s by Sebastian Bergmann and contributors.
+
+Runtime:       %s
+
+Time: %s, Memory: %s
+
+Inheritance (PHPUnit\TestFixture\TestDox\MethodOrderInheritance\Inheritance)
+ ✔ Declared first in parent
+ ✔ Declared second in parent
+ ✔ Declared first in child
+ ✔ Declared second in child
+
+OK (4 tests, 4 assertions)

tests/end-to-end/testdox/method-order-source-location.phpt
@@ -1,0 +1,28 @@
+--TEST--
+TestDox: Test methods are sorted by their location in the source code, not by the order in which they were run
+--FILE--
+<?php declare(strict_types=1);
+$_SERVER['argv'][] = '--do-not-cache-result';
+$_SERVER['argv'][] = '--no-configuration';
+$_SERVER['argv'][] = '--no-progress';
+$_SERVER['argv'][] = '--testdox';
+$_SERVER['argv'][] = '--colors=never';
+$_SERVER['argv'][] = '--order-by=reverse';
+$_SERVER['argv'][] = __DIR__ . '/_files/method-order-source-location';
+
+require_once __DIR__ . '/../../bootstrap.php';
+
+(new PHPUnit\TextUI\Application)->run($_SERVER['argv']);
+--EXPECTF--
+PHPUnit %s by Sebastian Bergmann and contributors.
+
+Runtime:       %s
+
+Time: %s, Memory: %s
+
+Source Location (PHPUnit\TestFixture\TestDox\MethodOrderSourceLocation\SourceLocation)
+ ✔ Declared first
+ ✔ Declared second
+ ✔ Declared third
+
+OK (3 tests, 3 assertions)