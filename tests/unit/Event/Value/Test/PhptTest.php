ffb794cf4a26c6268d35ce8e23c64f4d2cfd4828
Sebastian Bergmann <sb@sebastian-bergmann.de>
2026-08-06 07:25:53 +0200

    Add tests

--- tests/unit/Event/Value/Test/PhptTest.php
+++ tests/unit/Event/Value/Test/PhptTest.php
@@ -30,7 +30,72 @@ final class PhptTest extends TestCase
         $this->assertSame($file, $test->file());
         $this->assertSame($file, $test->id());
         $this->assertSame($file, $test->name());
+        $this->assertSame($file, $test->sortId());
         $this->assertTrue($test->isPhpt());
         $this->assertFalse($test->isTestMethod());
+        $this->assertSame(1, $test->repetition());
+        $this->assertSame(1, $test->totalRepetitions());
+        $this->assertFalse($test->isRepeated());
+        $this->assertSame(1, $test->attempt());
+        $this->assertSame(1, $test->maxAttempts());
+        $this->assertFalse($test->isRetried());
+    }
+
+    public function testRepetitionValues(): void
+    {
+        $test = new Phpt('test.phpt', 2, 3);
+
+        $this->assertSame(2, $test->repetition());
+        $this->assertSame(3, $test->totalRepetitions());
+        $this->assertTrue($test->isRepeated());
+    }
+
+    public function testNameIncludesRepetitionWhenRepeated(): void
+    {
+        $test = new Phpt('test.phpt', 2, 3);
+
+        $this->assertSame('test.phpt (repetition 2 of 3)', $test->name());
+        $this->assertSame('test.phpt (repetition 2 of 3)', $test->id());
+    }
+
+    public function testAttemptValues(): void
+    {
+        $test = new Phpt('test.phpt', 1, 1, 2, 3);
+
+        $this->assertSame(2, $test->attempt());
+        $this->assertSame(3, $test->maxAttempts());
+        $this->assertTrue($test->isRetried());
+    }
+
+    public function testNameIncludesAttemptForAdditionalAttempts(): void
+    {
+        $test = new Phpt('test.phpt', 1, 1, 2, 3);
+
+        $this->assertSame('test.phpt (attempt 2 of 3)', $test->name());
+        $this->assertSame('test.phpt (attempt 2 of 3)', $test->id());
+    }
+
+    public function testNameDoesNotIncludeAttemptForFirstAttempt(): void
+    {
+        $test = new Phpt('test.phpt', 1, 1, 1, 3);
+
+        $this->assertTrue($test->isRetried());
+        $this->assertSame('test.phpt', $test->name());
+        $this->assertSame('test.phpt', $test->id());
+    }
+
+    public function testNameIncludesRepetitionAndAttempt(): void
+    {
+        $test = new Phpt('test.phpt', 2, 3, 2, 3);
+
+        $this->assertSame('test.phpt (repetition 2 of 3) (attempt 2 of 3)', $test->name());
+        $this->assertSame('test.phpt (repetition 2 of 3) (attempt 2 of 3)', $test->id());
+    }
+
+    public function testSortIdIsStableAcrossRepetitionsAndAttempts(): void
+    {
+        $test = new Phpt('test.phpt', 2, 3, 2, 3);
+
+        $this->assertSame('test.phpt', $test->sortId());
     }
 }
