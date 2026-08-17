d3368ff36e6d5ea65551836795695f60393a3d9a
Sebastian Bergmann <sb@sebastian-bergmann.de>
2026-08-17 06:23:02 +0200

    Closes #6904

---
+++ tests/end-to-end/generic/_files/process-isolation-without-issue-trigger-identification/src/Deprecator.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of PHPUnit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace PHPUnit\TestFixture\ProcessIsolationWithoutIssueTriggerIdentification;
+
+use const E_USER_DEPRECATED;
+use function trigger_error;
+
+final class Deprecator
+{
+    public function deprecate(): bool
+    {
+        trigger_error('deprecation triggered by first-party code', E_USER_DEPRECATED);
+
+        return true;
+    }
+}
