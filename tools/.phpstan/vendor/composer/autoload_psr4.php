5b0c0255240a49951c058fa6e24e5e792fc71f1e
Sebastian Bergmann <sb@sebastian-bergmann.de>
2026-08-24 09:56:29 +0200

    Do not depend on tomasvotruba/type-coverage

--- tools/.phpstan/vendor/composer/autoload_psr4.php
+++ tools/.phpstan/vendor/composer/autoload_psr4.php
@@ -6,9 +6,6 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Webmozart\\Assert\\' => array($vendorDir . '/webmozart/assert/src'),
-    'TomasVotruba\\TypeCoverage\\' => array($vendorDir . '/tomasvotruba/type-coverage/src'),
-    'Rector\\TypePerfect\\' => array($vendorDir . '/tomasvotruba/type-coverage/packages/type-perfect/src'),
     'PHPStan\\ExtensionInstaller\\' => array($vendorDir . '/phpstan/extension-installer/src'),
     'PHPStan\\' => array($vendorDir . '/phpstan/phpstan-strict-rules/src'),
     'Ergebnis\\PHPStan\\Rules\\' => array($vendorDir . '/ergebnis/phpstan-rules/src'),
--- tools/.phpstan/vendor/tomasvotruba/type-coverage/packages/type-perfect/tests/Rules/NarrowPublicClassMethodParamTypeRule/Source/SomeInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Rector\TypePerfect\Tests\Rules\NarrowPublicClassMethodParamTypeRule\Source;
-
-interface SomeInterface
-{}
