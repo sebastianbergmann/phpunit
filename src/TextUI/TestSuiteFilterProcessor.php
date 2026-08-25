2f199adce59425b4045c1692aa275562a755f9f2
Sebastian Bergmann <sb@sebastian-bergmann.de>
2026-08-25 12:23:12 +0200

    Fix annotations

--- src/TextUI/TestSuiteFilterProcessor.php
+++ src/TextUI/TestSuiteFilterProcessor.php
@@ -35,12 +35,11 @@ final readonly class TestSuiteFilterProcessor
     }
 
     /**
+     * @param ?list<non-empty-string> $selectedTests the tests that test impact analysis selected, when it selected any
+     *
      * @throws Event\RuntimeException
      * @throws FilterNotConfiguredException
      * @throws RuntimeException
-     */
-    /**
-     * @param ?list<non-empty-string> $selectedTests the tests that test impact analysis selected, when it selected any
      */
     public function process(Configuration $configuration, TestSuite $suite, ?array $selectedTests = null): void
     {
