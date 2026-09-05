1feebbabb8565783966a86c8e5363d92ced90709
Sebastian Bergmann <sb@sebastian-bergmann.de>
2026-09-05 12:13:15 +0200

    Keep what a test that covers nothing executed out of the code coverage report

--- src/Framework/TestRunner/TestRunner.php
+++ src/Framework/TestRunner/TestRunner.php
@@ -167,7 +167,7 @@ final class TestRunner
         }
 
         if ($collectCodeCoverage) {
-            CodeCoverage::instance()->start($test);
+            CodeCoverage::instance()->start($test, $collectCodeCoverageForTestImpactDataOnly);
         }
 
         try {
